d4d468821a4e38af6701cdec1f1c392eacd9c6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:54 +0000

    Add package dLDS-discrete/0.0.2/dLDS_discrete-0.0.2.tar.gz

---
+++ packages/dLDS-discrete/dLDS_discrete-0.0.2.tar.gz/dLDS_discrete-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    author="noga mudrik",
+
+    name="dLDS_discrete",
+    version="0.0.2",
+    
+    author_email="<nmudrik1@jhmi.edu>",
+    description="dLDS discrete model package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.6",
+)
+
+#    package_dir={"": "src"},
+#    packages=setuptools.find_packages(where="src"),
+#author="nmudrik1",
+#    url="https://github.com/NogaMudrik/Discrete-Python-Model---dLDS-paper",
+#    project_urls={
+#        "mmain code": "https://github.com/NogaMudrik/Discrete-Python-Model---dLDS-paper",
+#    }
