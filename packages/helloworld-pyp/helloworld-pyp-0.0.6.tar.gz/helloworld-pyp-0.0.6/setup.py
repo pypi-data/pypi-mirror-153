4702892b74318868f1f62da5c4b5a19c54fe3aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package helloworld-pyp/0.0.6/helloworld-pyp-0.0.6.tar.gz

---
+++ packages/helloworld-pyp/helloworld-pyp-0.0.6.tar.gz/helloworld-pyp-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="helloworld-pyp",
+    version="0.0.6",
+    author="Mahesh Maximus",
+    author_email="",
+    description="helloworld pyp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahesh-maximus/helloworld-pyp",
+    project_urls={
+        "Bug Tracker": "https://github.com/mahesh-maximus/helloworld-pyp/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'':"src"},
+    packages=find_packages("src"),
+    python_requires=">=3.6",
+    entry_points={
+                        'console_scripts': [
+                                'hwpypcmd=hwpyp.mypy:sayHello',
+                        ]
+                }
+)
