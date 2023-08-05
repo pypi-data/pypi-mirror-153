2453bc1e8d00eb62090cb216c8a63559473a9013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:19 +0000

    Add package ansys-sphinx-theme/0.4.dev0/ansys-sphinx-theme-0.4.dev0.tar.gz

---
+++ packages/ansys-sphinx-theme/ansys-sphinx-theme-0.4.dev0.tar.gz/ansys-sphinx-theme-0.4.dev0/doc/source/conf.py
@@ -0,0 +1,101 @@
+"""Sphinx documentation configuration file."""
+from datetime import datetime
+
+from sphinx.builders.latex import LaTeXBuilder
+
+LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
+
+from ansys_sphinx_theme import (
+    __version__,
+    ansys_favicon,
+    ansys_logo_black,
+    ansys_logo_white,
+    ansys_logo_white_cropped,
+    latex,
+    watermark,
+)
+
+# Project information
+project = "ansys_sphinx_theme"
+copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
+author = "Ansys Inc."
+release = version = __version__
+
+# use the default ansys logo
+html_logo = ansys_logo_black
+html_theme = "ansys_sphinx_theme"
+
+# specify the location of your github repo
+html_theme_options = {
+    "github_url": "https://github.com/pyansys/pyansys-sphinx-theme",
+    "additional_breadcrumbs": [
+        ("PyAnsys", "https://docs.pyansys.com/"),
+    ],
+}
+
+html_short_title = html_title = "Ansys Sphinx Theme"
+
+# Sphinx extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "numpydoc",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "ansys_sphinx_theme",
+]
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/dev", None),
+    # kept here as an example
+    # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    # "numpy": ("https://numpy.org/devdocs", None),
+    # "matplotlib": ("https://matplotlib.org/stable", None),
+    # "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
+    # "pyvista": ("https://docs.pyvista.org/", None),
+}
+
+# numpydoc configuration
+numpydoc_show_class_members = False
+numpydoc_xref_param_type = True
+
+# Consider enabling numpydoc validation. See:
+# https://numpydoc.readthedocs.io/en/latest/validation.html#
+numpydoc_validate = True
+numpydoc_validation_checks = {
+    "GL06",  # Found unknown section
+    "GL07",  # Sections are in the wrong order.
+    "GL08",  # The object does not have a docstring
+    "GL09",  # Deprecation warning should precede extended summary
+    "GL10",  # reST directives {directives} must be followed by two colons
+    "SS01",  # No summary found
+    "SS02",  # Summary does not start with a capital letter
+    # "SS03", # Summary does not end with a period
+    "SS04",  # Summary contains heading whitespaces
+    # "SS05", # Summary must start with infinitive verb, not third person
+    "RT02",  # The first line of the Returns section should contain only the
+    # type, unless multiple values are being returned"
+}
+
+# Favicon
+html_favicon = ansys_favicon
+
+# static path
+html_static_path = ["_static"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# The suffix(es) of source filenames.
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+# additional logos for the latex coverpage
+latex_additional_files = [watermark, ansys_logo_white, ansys_logo_white_cropped]
+
+# change the preamble of latex with customized title page
+# variables are the title of pdf, watermark
+latex_elements = {"preamble": latex.generate_preamble(html_title)}
