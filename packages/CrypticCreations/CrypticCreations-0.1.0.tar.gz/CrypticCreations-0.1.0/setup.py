d305ec7ca17d0bbdb5281c11baf8fae7337fe96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:45 +0000

    Add package CrypticCreations/0.1.0/CrypticCreations-0.1.0.tar.gz

---
+++ packages/CrypticCreations/CrypticCreations-0.1.0.tar.gz/CrypticCreations-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='CrypticCreations',
+      version='0.1.0',
+      description='Create ciphers or random strings for DnD players to try and read aloud for the group.',
+      url='https://github.com/ClutchTech/CrypticCreations',
+      keywords='dnd ciphers random',
+      author='Clutch_Reboot',
+      author_email='clutchshadow26@gmail.com',
+      license='GNU General Public License v2.0',
+      packages=['CrypticCreations'],
+      zip_safe=False)
