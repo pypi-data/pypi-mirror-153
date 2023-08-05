32bf3eff50f5f165f8d106d2d65834c3215bf714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:56 +0000

    Add package workspace-clients/2.0.1/workspace_clients-2.0.1-py3-none-any.whl

---
+++ packages/workspace-clients/workspace_clients-2.0.1-py3-none-any.whl/workspace_clients/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "2.0.1"
+__all__ = [
+    "WorkspaceServiceAssetsClient",
+    "WorkspaceServiceContainersClient",
+    "AssetModel",
+    "ContainerModel",
+]
+
+try:
+    # Attempts to import the client class
+    # Allowed to fail importing so the package metadata can be read for building
+    from .assets_client import WorkspaceServiceAssetsClient
+    from .containers_client import WorkspaceServiceContainersClient
+    from .models import AssetModel, ContainerModel
+except (ImportError, ModuleNotFoundError):  # pragma: no cover
+    pass  # pragma: no cover
