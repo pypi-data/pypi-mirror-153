c65fc3fcdfa96164e234623f525b5fe7827bcd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:32 +0000

    Add package glcm-cupy/0.1.9/glcm_cupy-0.1.9.tar.gz

---
+++ packages/glcm-cupy/glcm_cupy-0.1.9.tar.gz/glcm_cupy-0.1.9/glcm_cupy/__init__.py
@@ -0,0 +1,17 @@
+from glcm_cupy.conf import HOMOGENEITY, CONTRAST, ASM, MEAN, VAR, CORRELATION
+from glcm_cupy.glcm import glcm, GLCM, Direction
+from glcm_cupy.cross import glcm_cross, GLCMCross
+
+__all__ = [
+    "glcm",
+    "glcm_cross",
+    "GLCM",
+    "GLCMCross",
+    "HOMOGENEITY",
+    "CONTRAST",
+    "ASM",
+    "MEAN",
+    "VAR",
+    "CORRELATION",
+    "Direction",
+]
---
+++ packages/glcm-cupy/glcm_cupy-0.1.9.tar.gz/glcm_cupy-0.1.9/glcm_cupy/conf.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent.absolute()
+
+MAX_VALUE_SUPPORTED = 256
+NO_OF_VALUES_SUPPORTED = 256 ** 2
+
+MAX_THREADS = 512  # Min. Maximum supported threads.
+
+# For a 1000 x 256 x 256 GLCM, you need ~ 262MB of memory.
+# Each cell is 4 bytes (float32)
+MAX_PARTITION_SIZE = 1000
+
+NO_OF_FEATURES = 6
+
+HOMOGENEITY = 0
+CONTRAST = 1
+ASM = 2
+MEAN = 3
+VAR = 4
+CORRELATION = 5
