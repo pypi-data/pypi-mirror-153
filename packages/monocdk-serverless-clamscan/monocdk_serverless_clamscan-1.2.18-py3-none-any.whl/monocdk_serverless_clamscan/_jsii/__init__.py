d4c8ab6fd431987e22c3a6e2d1bd68ad6328710f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:45 +0000

    Add package monocdk-serverless-clamscan/1.2.18/monocdk_serverless_clamscan-1.2.18-py3-none-any.whl

---
+++ packages/monocdk-serverless-clamscan/monocdk_serverless_clamscan-1.2.18-py3-none-any.whl/monocdk_serverless_clamscan/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.core._jsii
+import constructs._jsii
+import monocdk._jsii
+import monocdk_nag._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "monocdk-serverless-clamscan",
+    "1.2.18",
+    __name__[0:-6],
+    "monocdk-serverless-clamscan@1.2.18.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
