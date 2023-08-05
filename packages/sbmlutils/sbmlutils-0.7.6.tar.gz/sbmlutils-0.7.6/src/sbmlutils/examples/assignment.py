6738de55740c313c2bce6565c901e22aa73a81d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:57 +0000

    Add package sbmlutils/0.7.6/sbmlutils-0.7.6.tar.gz

---
+++ packages/sbmlutils/sbmlutils-0.7.6.tar.gz/sbmlutils-0.7.6/src/sbmlutils/examples/assignment.py
@@ -0,0 +1,80 @@
+"""AssignmentRule and InitialAssignment example."""
+from sbmlutils.examples import templates
+from sbmlutils.factory import *
+from sbmlutils.resources import EXAMPLES_DIR
+
+
+class U(Units):
+    """UnitsDefinition."""
+
+    hr = UnitDefinition("hr")
+    mg = UnitDefinition("mg")
+    m2 = UnitDefinition("m2", "meter^2")
+    kg = UnitDefinition("kg")
+    l_per_hr = UnitDefinition("l_per_hr", "liter/hr")
+    l_per_kg = UnitDefinition("l_per_kg", "liter/kg")
+    mg_per_l = UnitDefinition("mg_per_l", "mg/liter")
+    mg_per_hr = UnitDefinition("mg_per_hr", "mg/hr")
+
+
+_m = Model(
+    "assignment",
+    name="model with assignments",
+    creators=templates.creators,
+    notes="""
+    # Example model demonstrating `InitialAssignment` and `AssignmentRule`.
+    """
+    + templates.terms_of_use,
+    units=U,
+    model_units=ModelUnits(
+        time=U.hr,
+        extent=U.mg,
+        substance=U.mg,
+        length=U.meter,
+        area=U.m2,
+        volume=U.liter,
+    ),
+)
+
+_m.parameters = [
+    # dosing
+    Parameter("Ave", 0, U.mg, constant=False),
+    Parameter("D", 0, U.mg, constant=False),
+    Parameter("IVDOSE", 0, U.mg, constant=True),
+    Parameter("PODOSE", 100, U.mg, constant=True),
+    Parameter("k1", 0.1, U.l_per_hr, constant=True),
+    # whole body data
+    Parameter("BW", 70, U.kg, True),
+    Parameter("FVve", 0.0514, U.l_per_kg, True),
+]
+
+_m.assignments = [
+    InitialAssignment("Ave", "IVDOSE", U.mg),
+    InitialAssignment("D", "PODOSE", U.mg),
+]
+
+_m.rules = [
+    # concentrations
+    AssignmentRule("Cve", "Ave/Vve", U.mg_per_l),
+    # volumes
+    AssignmentRule("Vve", "BW*FVve", U.liter),
+]
+
+_m.rate_rules = [
+    RateRule("Ave", "- k1*Cve", U.mg_per_hr),
+]
+
+model = _m
+
+
+def create(tmp: bool = False) -> None:
+    """Create model."""
+    create_model(
+        models=model,
+        output_dir=EXAMPLES_DIR,
+        tmp=tmp,
+    )
+
+
+if __name__ == "__main__":
+    create()
---
+++ packages/sbmlutils/sbmlutils-0.7.6.tar.gz/sbmlutils-0.7.6/src/sbmlutils/examples/reaction.py
@@ -0,0 +1,57 @@
+"""Create reaction example."""
+import numpy as np
+
+from sbmlutils.examples import templates
+from sbmlutils.factory import *
+from sbmlutils.metadata import *
+from sbmlutils.resources import EXAMPLES_DIR
+
+
+_m = Model(
+    "reaction",
+    name="model with reaction",
+    notes="""
+    # Reaction definition
+    This example demonstrates the creation of a reaction.
+    """
+    + templates.terms_of_use,
+    creators=templates.creators,
+    compartments=[
+        Compartment(sid="c", name="cytosol", value=np.NaN),
+    ],
+    species=[
+        Species(
+            sid="x",
+            compartment="c",
+            sboTerm=SBO.SIMPLE_CHEMICAL,
+            initialConcentration=np.NaN,
+        ),
+        Species(
+            sid="y",
+            compartment="c",
+            sboTerm=SBO.SIMPLE_CHEMICAL,
+            initialConcentration=np.NaN,
+        ),
+    ],
+    reactions=[
+        Reaction(
+            sid="v1",
+            equation="x -> y",
+            compartment="c",
+        )
+    ],
+)
+
+
+def create(tmp: bool = False) -> None:
+    """Create model."""
+    create_model(
+        models=_m,
+        output_dir=EXAMPLES_DIR,
+        tmp=tmp,
+        units_consistency=False,
+    )
+
+
+if __name__ == "__main__":
+    create()
