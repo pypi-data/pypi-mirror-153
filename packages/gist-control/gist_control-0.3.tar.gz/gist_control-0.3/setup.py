f17c4864489a03937e221ea91f90b5b2daa92056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:40 +0000

    Add package gist-control/0.3/gist_control-0.3.tar.gz

---
+++ packages/gist-control/gist_control-0.3.tar.gz/gist_control-0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="gist_control",
+    version="0.3",
+    author="Gaëtan Hrlt",
+    author_email="gaetan.hrlt+dev@gmail.com",
+    description="Control your Github Gists!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ghrlt/gist-control",
+    project_urls={
+    	"Developer website": "https://ghr.lt?f=gist-control",
+        "Bug Tracker": "https://github.com/ghrlt/gist-control/issues",
+    },
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["gist_control"],
+    python_requires=">=3.6",
+)
