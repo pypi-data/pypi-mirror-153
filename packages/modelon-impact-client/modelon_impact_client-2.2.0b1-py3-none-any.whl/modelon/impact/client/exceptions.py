a656be2a6f8e443b69f002e9a34418b52c35434b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:04 +0000

    Add package modelon-impact-client/2.2.0b1/modelon_impact_client-2.2.0b1-py3-none-any.whl

---
+++ packages/modelon-impact-client/modelon_impact_client-2.2.0b1-py3-none-any.whl/modelon/impact/client/exceptions.py
@@ -0,0 +1,26 @@
+class Error(Exception):
+    pass
+
+
+class UnsupportedSemanticVersionError(Error):
+    pass
+
+
+class NoAssignedLicenseError(Error):
+    pass
+
+
+class OperationTimeOutError(Error):
+    pass
+
+
+class OperationFailureError(Error):
+    pass
+
+
+class OperationNotCompleteError(Error):
+    pass
+
+
+class OperationCompleteError(Error):
+    pass
