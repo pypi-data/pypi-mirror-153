4410fb4e767609d06441bb857e4cb7b282a9a8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package Library-API/0.0.2/Library_API-0.0.2-py3-none-any.whl

---
+++ packages/Library-API/Library_API-0.0.2-py3-none-any.whl/GeoAPI/countries.py
@@ -0,0 +1,32 @@
+import json
+
+import requests
+
+
+class Countries:
+
+    def __init__(self):
+        self.url = "https://wft-geo-db.p.rapidapi.com/v1/geo/countries/"
+        self.headers = {
+            "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+            "X-RapidAPI-Key": "9956beec72msh8f5b5e09b33da88p1d8b8cjsneac0d0fcdcea"
+        }
+
+    def get_country_details(self, country_id):
+        new_url = self.url + country_id
+        response = requests.request("GET", new_url, headers=self.headers)
+        json_data = json.loads(response.text)
+        formatted_string = json.dumps(json_data, indent=2)
+        return formatted_string
+
+    def get_region_details(self, country_id, region_id):
+        new_url = self.url + country_id + "/regions/" + region_id
+        response = requests.request("GET", new_url, headers=self.headers)
+        json_data = json.loads(response.text)
+        formatted_string = json.dumps(json_data, indent=2)
+        return formatted_string
+
+
+if __name__ == '__main__':
+    api = Countries()
+    print(api.get_region_details("US", "CA"))
