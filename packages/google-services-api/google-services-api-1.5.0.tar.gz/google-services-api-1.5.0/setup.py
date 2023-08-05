2e2bb5a3f26d17530fcbf40599fce9f591705021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package google-services-api/1.5.0/google-services-api-1.5.0.tar.gz

---
+++ packages/google-services-api/google-services-api-1.5.0.tar.gz/google-services-api-1.5.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='google-services-api',
+    version='1.5.0',
+    description='Google Maps and Google Maps reviews scraper by Outscraper API',
+    long_description=readme(),
+    classifiers = ['Programming Language :: Python',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                    'Intended Audience :: Developers',
+                    'Topic :: Utilities',
+    ],
+    keywords='extractor google api maps search json scrape parser reviews google play',
+    url='https://github.com/outscraper/google-maps-scraper-pyhton',
+    author='Outscraper',
+    author_email='team@outscraper.com',
+    license='MIT',
+    packages=['outscraper'],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+    long_description_content_type='text/x-rst',
+)
