3ba41f4fd2a3466424d5de96aa6ef28c28728513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:07 +0000

    Add package alfort/0.1.8/alfort-0.1.8.tar.gz

---
+++ packages/alfort/alfort-0.1.8.tar.gz/alfort-0.1.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alfort']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'alfort',
+    'version': '0.1.8',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Masahiro Wada',
+    'author_email': 'argon.argon.argon@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
