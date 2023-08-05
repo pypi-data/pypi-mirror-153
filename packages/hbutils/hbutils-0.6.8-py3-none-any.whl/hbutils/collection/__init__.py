a57cbd406a19b6fa510ddfd854c997f2b4e32d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:06 +0000

    Add package hbutils/0.6.8/hbutils-0.6.8-py3-none-any.whl

---
+++ packages/hbutils/hbutils-0.6.8-py3-none-any.whl/hbutils/collection/__init__.py
@@ -0,0 +1,8 @@
+"""
+Overview:
+    Collection module, include the basic utilities with collections like dict, list and tuple.
+"""
+from .dimension import *
+from .functional import *
+from .sequence import *
+from .structural import *
