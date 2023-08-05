c69c7b0f13a912f4b24b3e921578cf506be47adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:53 +0000

    Add package python-ramda/0.1.4/python_ramda-0.1.4.tar.gz

---
+++ packages/python-ramda/python_ramda-0.1.4.tar.gz/python_ramda-0.1.4/ramda/private/_equals.py
@@ -0,0 +1,31 @@
+from math import isnan
+
+from ._helper import getAttribute
+from ._isArrayLike import _isArrayLike
+from ._isFunction import _isFunction
+from ._isNumber import _isNumber
+
+
+def _equals(a, b):
+  if _isNumber(a) and isnan(a) and _isNumber(b) and isnan(b):
+    return True
+  # pylint: disable=unidiomatic-typecheck
+  if type(a) != type(b):
+    return False
+  if isinstance(a, BaseException):
+    # Exception
+    return str(a) == str(b)
+  if _isArrayLike(a) and _isArrayLike(b):
+    # Array-like
+    if len(a) != len(b):
+      return False
+    # pylint: disable=consider-using-enumerate
+    for i in range(len(a)):
+      if not _equals(a[i], b[i]):
+        return False
+    return True
+  if _isFunction(getAttribute(a, 'equals')) and _isFunction(getAttribute(b, 'equals')):
+    # dispatch to objects' own equals method
+    return a.equals(b) and b.equals(a)
+  # default equals
+  return a == b
