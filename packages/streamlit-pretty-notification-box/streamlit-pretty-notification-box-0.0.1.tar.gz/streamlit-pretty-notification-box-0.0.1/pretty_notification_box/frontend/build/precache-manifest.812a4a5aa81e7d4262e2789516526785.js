f17d9a0b92abb947c37a780d641945410098e86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package streamlit-pretty-notification-box/0.0.1/streamlit-pretty-notification-box-0.0.1.tar.gz

---
+++ packages/streamlit-pretty-notification-box/streamlit-pretty-notification-box-0.0.1.tar.gz/streamlit-pretty-notification-box-0.0.1/pretty_notification_box/frontend/build/precache-manifest.812a4a5aa81e7d4262e2789516526785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c8f791e55914ee47dcf9835aff000c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53cd2b24eda52667bea2",
+    "url": "./static/css/main.63c75312.chunk.css"
+  },
+  {
+    "revision": "b4a8661fbf9eec619e2a",
+    "url": "./static/js/2.57a6278a.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.57a6278a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53cd2b24eda52667bea2",
+    "url": "./static/js/main.190f9354.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
