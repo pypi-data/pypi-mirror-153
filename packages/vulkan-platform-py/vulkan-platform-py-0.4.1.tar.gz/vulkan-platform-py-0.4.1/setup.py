c93162eb17d32c59474ef3991f6bf845c8f614a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:18 +0000

    Add package vulkan-platform-py/0.4.1/vulkan-platform-py-0.4.1.tar.gz

---
+++ packages/vulkan-platform-py/vulkan-platform-py-0.4.1.tar.gz/vulkan-platform-py-0.4.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vulkan_platform_py']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['GPUtil>=1.4.0,<2.0.0',
+ 'dill>=0.3.5,<0.4.0',
+ 'py-cpuinfo>=8.0.0,<9.0.0',
+ 'pydantic>=1.9.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'vulkan-platform-py',
+    'version': '0.4.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Rayan Hatout',
+    'author_email': 'rayan.hatout@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10',
+}
+
+
+setup(**setup_kwargs)
