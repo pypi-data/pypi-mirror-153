59448bb63425304ee1e5d858d0b7681bc2ca5a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:59 +0000

    Add package mlogpp/1.4/mlogpp-1.4.tar.gz

---
+++ packages/mlogpp/mlogpp-1.4.tar.gz/mlogpp-1.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="mlogpp",
+    version="1.4",
+    author="albi-c",
+    description="mlog++ to mindustry logic compiler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albi-c/mlog++",
+    project_urls={
+        "Bug Tracker": "https://github.com/albi-c/mlog++/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
