0565926e36621e774182e1401502cd25d08a8b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:20 +0000

    Add package defern/1.0.0/defern-1.0.0-py3-none-any.whl

---
+++ packages/defern/defern-1.0.0-py3-none-any.whl/defern/__init__.py
@@ -0,0 +1,12 @@
+from .__about__ import __version__
+from .defern import (defern, deferner, defern_this, here, defer)
+
+
+__all__ = [
+    __version__,
+    "defern",
+    "deferner",
+    "defern_this",
+    "here",
+    "defer",
+]
