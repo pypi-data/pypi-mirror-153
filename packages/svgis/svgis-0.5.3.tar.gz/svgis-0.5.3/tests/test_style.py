d4c498f88c768f1b9d4693a607c9db7b18c5a65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package svgis/0.5.3/svgis-0.5.3.tar.gz

---
+++ packages/svgis/svgis-0.5.3.tar.gz/svgis-0.5.3/tests/test_style.py
@@ -0,0 +1,190 @@
+"""Tests on the map styling tools, most of which are CSS manipulation."""
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+# pylint: disable=unused-import
+import os
+import re
+import unittest
+from io import BytesIO, StringIO
+from xml.dom import minidom
+
+try:
+    from lxml import etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+
+from svgis import dom, style
+
+from . import TEST_CSS, TEST_SVG
+
+
+class CssTestCase(unittest.TestCase):
+    svg = TEST_SVG
+    css = TEST_CSS
+
+    css1 = '''.class-name { fill: orange;}'''
+
+    file = 'tests/fixtures/test.svg'
+
+    classes = ('apple', 'potato')
+
+    properties = {
+        'apple': 'fruit',
+        'pear': 1,
+        'kale': 'leafy green',
+    }
+
+    def testInlineCSS(self):
+        svg = style.add_style(self.svg, self.css)
+        inlined = style.inline(svg)
+        self.assertNotEqual(inlined, self.svg)
+
+        doc = minidom.parseString(inlined)
+        self.assertIn('fill:purple', inlined)
+
+        polygon_style = doc.getElementsByTagName('polygon').item(0).getAttribute('style')
+        self.assertIn('stroke:green', polygon_style)
+        self.assertIn('fill:orange', polygon_style)
+
+        cat_style = doc.getElementsByTagName('polyline').item(1).getAttribute('style')
+        self.assertIn('fill:red', cat_style)
+
+        polyline_style = doc.getElementsByTagName('polyline').item(0).getAttribute('style')
+        self.assertIn('stroke:blue', polyline_style)
+
+    def test_add_style(self):
+        new = style.add_style(self.file, self.css)
+        result = minidom.parseString(new).getElementsByTagName('defs').item(0).getElementsByTagName('style').item(0)
+        assert self.css in result.toxml()
+
+    def test_replace_style(self):
+        new = style.add_style(self.file, self.css, replace=True)
+        result = minidom.parseString(new).getElementsByTagName('defs').item(0).getElementsByTagName('style').item(0)
+        assert self.css in result.toxml()
+
+    def test_add_style_missing_def(self):
+        with open(self.file) as f:
+            replaced_svg = re.sub(r'<defs></defs>', '', f.read())
+
+        try:
+            io_svg = BytesIO(replaced_svg)
+        except TypeError:
+            io_svg = StringIO(replaced_svg)
+
+        new = style.add_style(io_svg, self.css)
+        result = minidom.parseString(new).getElementsByTagName('defs').item(0).getElementsByTagName('style').item(0)
+        self.assertIn(self.css, result.toxml())
+
+    def testReScale(self):
+        result = style.rescale('tests/fixtures/test.svg', 1.37)
+        self.assertIn('scale(1.37)', result[0:2000])
+
+    def testPickStyle(self):
+        stylefile = 'tmp.css'
+
+        with open(stylefile, 'w') as w:
+            w.write(self.css)
+
+        try:
+            result = style.pick(stylefile)
+            self.assertEqual(self.css, result)
+
+        finally:
+            os.remove('tmp.css')
+
+        result = style.pick(self.css)
+        self.assertEqual(self.css, result)
+
+        self.assertIsNone(style.pick(None))
+
+    def testAddCli(self):
+        result = style.add_style(self.file, self.css)
+        self.assertIn(self.css, result[0:2000])
+
+        cssfile = 'tmp.css'
+        with open(cssfile, 'w') as w:
+            w.write(self.css)
+
+        try:
+            result = style.add_style(self.file, cssfile)
+            self.assertIn(self.css, result[0:2000])
+
+        finally:
+            os.remove('tmp.css')
+
+    def testAddStyleNoDefs(self):
+        svg = self.svg.replace('<defs></defs>', '')
+        new = style.add_style(svg, self.css)
+        result = minidom.parseString(new).getElementsByTagName('defs').item(0).getElementsByTagName('style').item(0)
+        assert self.css in result.toxml()
+
+    def testSanitize(self):
+        assert style.sanitize(None) == 'None'
+        assert style.sanitize('') == ''
+        self.assertEqual(style.sanitize('ü'), '_ü')
+        self.assertEqual(style.sanitize('!foo'), '_!foo')
+        assert style.sanitize('müller') == 'müller'
+
+        self.assertEqual(style.sanitize(1), '_1')
+
+        self.assertEqual(style.sanitize('foo.bar'), 'foobar')
+        self.assertEqual(style.sanitize('fooba.r'), 'foobar')
+
+        self.assertEqual(style.sanitize('.foo'), 'foo')
+
+        self.assertEqual(style.sanitize('foo#bar'), 'foobar')
+        self.assertEqual(style.sanitize('foobar#'), 'foobar')
+
+        self.assertEqual(style.sanitize('x \t'), 'x_')
+
+        self.assertEqual(style.sanitize('"huh"'), 'huh')
+
+    def testConstructClasses(self):
+        self.assertEqual(style.construct_classes(('foo',), {'foo': 'bar'}), ['foo_bar'])
+        self.assertEqual(style.construct_classes(['foo'], {'foo': 'bar'}), ['foo_bar'])
+
+        self.assertEqual(style.construct_classes(['foo'], {'foo': None}), ['foo_None'])
+
+    def testCreateClasses(self):
+        classes = style.construct_classes(self.classes, self.properties)
+        self.assertEqual(classes, ['apple_fruit'])
+
+        classes = style.construct_classes(self.classes, {'apple': 'fruit'})
+        self.assertEqual(classes, ['apple_fruit'])
+
+        classes = style.construct_classes(self.classes, {'apple': 'früit'})
+        self.assertEqual(classes, ['apple_früit'])
+
+        classes = style.construct_classes(self.classes, {'apple': 1})
+        self.assertEqual(classes, ['apple_1'])
+
+    def testCreateClassesMissing(self):
+        classes = style.construct_classes(self.classes, {'apple': ''})
+        self.assertEqual(classes, ['apple_'])
+
+        classes = style.construct_classes(self.classes, {'apple': None})
+        self.assertEqual(classes, ['apple_None'])
+
+    def testPartialStyleName(self):
+        inlined = style.inline(self.svg)
+        self.assertNotIn('orange', inlined)
+
+    def testReplaceComments(self):
+        css = """
+        // foo
+        """
+        result = style.replace_comments(css)
+        self.assertIn('/* foo */', result)
+
+    def testConstructDataFields(self):
+        fields = style.construct_datas(['a', 'b'], {'a': '1', 'b': '_ _'})
+        self.assertEqual(fields['data-a'], '1')
+        self.assertEqual(fields['data-b'], '_ _')
+        self.assertNotIn('data-c', fields)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/svgis/svgis-0.5.3.tar.gz/svgis-0.5.3/tests/test_svg.py
@@ -0,0 +1,110 @@
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+import unittest
+from xml.dom import minidom
+
+import six
+
+from svgis import style, svg
+
+
+class SvgTestCase(unittest.TestCase):
+    def setUp(self):
+        self.file = 'tests/fixtures/test.svg'
+        self.newstyle = 'stroke {color:red;}'
+
+    def test_rescale(self):
+        new = style.rescale(self.file, 100)
+
+        g = minidom.parseString(new).getElementsByTagName('g')[0]
+        assert 'scale(100)' in g.attributes.get('transform').value
+
+    def test_create(self):
+        s = svg.drawing((100, 100), [])
+        self.assertIsInstance(s, six.string_types)
+
+        s = svg.drawing((100, 100), [])
+        self.assertIsInstance(s, six.string_types)
+
+        s = svg.drawing((100, 100), [], style=self.newstyle)
+        self.assertIn(self.newstyle, s)
+
+    def testGroup(self):
+        g = svg.group()
+        self.assertIsInstance(g, six.string_types)
+
+        g = svg.group(transform="translate(10,10)")
+        self.assertIn('transform="translate(10,10)"', g)
+
+        g = svg.group(transform="scale(10)")
+        self.assertIn('transform="scale(10)"', g)
+
+    def testAttribs(self):
+        args = {'transform': 'translate(10, 10)', 'fill': 'black'}
+        self.assertIn('transform="translate(10, 10)"', svg.toattribs(**args))
+        self.assertIn('fill="black"', svg.toattribs(**args))
+
+    def testDrawCircle(self):
+        point = svg.circle((0.0, 0.0), r=2)
+        self.assertIsInstance(point, six.string_types)
+        assert 'r="2"' in point
+        self.assertIn('cy="0.0"', point)
+        assert 'cx="0.0"' in point
+
+    def testDrawPath(self):
+        coordinates = [
+            (0.0, 0.0),
+            (10.0, 0.0),
+            (10.0, 10.0),
+            (0.0, 10.0),
+            (0.0, 0.0),
+            'M',
+            (4.0, 4.0),
+            (4.0, 5.0),
+            (5.0, 5.0),
+            (5.0, 4.0),
+            (4.0, 4.0),
+            'Z',
+        ]
+
+        path = svg.path(coordinates)
+        self.assertIsInstance(path, six.string_types)
+        self.assertIn(' M ', path)
+        self.assertIn('Z"', path)
+        self.assertIn('10.0,0.0', path)
+
+    def assertPartsIn(self, fixture, test):
+        for f in fixture:
+            self.assertIn(f, test)
+
+    def testRect(self):
+        rect = svg.rect((0, 0), 10, 10)
+        self.assertIsInstance(rect, type(''))
+        fix = '<rect', 'x="0"', 'y="0"', 'width="10"', 'height="10"', '/>'
+        self.assertPartsIn(fix, rect)
+
+    def testLine(self):
+        line = svg.line((0, 0), (10, 10))
+        assert isinstance(line, type(''))
+        fix = '<line', 'x1="0"', 'y1="0"', 'x2="10"', 'y2="10"', '/>'
+        self.assertPartsIn(fix, line)
+
+    def testText(self):
+        text = svg.text('hi', (2, 2))
+        assert isinstance(text, type(''))
+        fix = '<text', 'x="2"', 'y="2"', 'hi', '</text>'
+
+        self.assertPartsIn(fix, text)
+
+        text = svg.text('hi', (2.22222, 2.222222), precision=2)
+        assert isinstance(text, type(''))
+        fix = '<text', 'x="2.22"', 'y="2.22"', 'hi', '</text>'
+
+        self.assertPartsIn(fix, text)
+
+
+if __name__ == '__main__':
+    unittest.main()
