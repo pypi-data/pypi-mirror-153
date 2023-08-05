8dc6d6a9204c7d4f00785a3178326c5e850b8276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:46 +0000

    Add package joseki/1.2.0/joseki-1.2.0-py3-none-any.whl

---
+++ packages/joseki/joseki-1.2.0-py3-none-any.whl/joseki/__init__.py
@@ -0,0 +1,11 @@
+"""Joseki."""
+from . import accessor
+from .core import Identifier  # pyflakes.ignore
+from .core import make  # pyflakes.ignore
+
+
+__all__ = [
+    "accessor",
+    "Identifier",
+    "make",
+]
