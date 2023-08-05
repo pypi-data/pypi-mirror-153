e1fcb7e39d83ed5f3223d22421d859d8a36dd5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package electromagnetopy/0.0.1/electromagnetopy-0.0.1.tar.gz

---
+++ packages/electromagnetopy/electromagnetopy-0.0.1.tar.gz/electromagnetopy-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['electromagnetopy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'electromagnetopy',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Darren Chaddock',
+    'author_email': 'dchaddoc@ucalgary.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
