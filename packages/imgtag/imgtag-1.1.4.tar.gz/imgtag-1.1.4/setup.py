ff8e616806c804524021bbfb37a7c71f3d678e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package imgtag/1.1.4/imgtag-1.1.4.tar.gz

---
+++ packages/imgtag/imgtag-1.1.4.tar.gz/imgtag-1.1.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imgtag",
+    version="1.1.4",
+    author="Ella Jameson",
+    author_email="ellagjamson@gmail.com",
+    description="Simple XMP Image Tag & Metadata Editing Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nimaid/python-imgtag",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["python-xmp-toolkit>=2.0.1", "psutil>=5.9.0"]
+)
