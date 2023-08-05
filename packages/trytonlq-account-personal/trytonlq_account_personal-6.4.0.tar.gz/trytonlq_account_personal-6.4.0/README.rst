7ae7f3726ab5275ae79856acccecaf658e94de29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:59 +0000

    Add package trytonlq-account-personal/6.4.0/trytonlq_account_personal-6.4.0.tar.gz

---
+++ packages/trytonlq-account-personal/trytonlq_account_personal-6.4.0.tar.gz/trytonlq_account_personal-6.4.0/README.rst
@@ -0,0 +1,15 @@
+#######################
+Account Personal Module
+#######################
+
+The *Account Personal Module* provides a chart of accounts for personal
+accounting.
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   usage
+   design
+   support
+   license
---
+++ packages/trytonlq-account-personal/trytonlq_account_personal-6.4.0.tar.gz/trytonlq_account_personal-6.4.0/tests/test_module.py
@@ -0,0 +1,12 @@
+# This file is part of the account_personal Tryton module.
+# Please see the COPYRIGHT and README.rst files at the top level of this
+# package for full copyright notices, license terms and support information.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class AccountPersonalTestCase(ModuleTestCase):
+    "Test Account Personal module"
+    module = 'account_personal'
+
+
+del ModuleTestCase
