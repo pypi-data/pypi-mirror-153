95723b030192f39b6e7db899ed6bf261b2bda1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:40 +0000

    Add package approvaltests/5.2.0/approvaltests-5.2.0-py3-none-any.whl

---
+++ packages/approvaltests/approvaltests-5.2.0-py3-none-any.whl/approvaltests/namer/default_namer_factory.py
@@ -0,0 +1,18 @@
+from contextlib import contextmanager
+from typing import Optional, Any
+
+from approvaltests.core.options import Options
+from approvaltests.core.namer import Namer
+from approvaltests.namer.stack_frame_namer import StackFrameNamer
+
+
+def get_default_namer(extension: Optional[str] = None) -> Namer:
+    return StackFrameNamer(extension)
+
+
+class NamerFactory:
+    @staticmethod
+    def with_parameters(*args: Any):
+        from approvaltests.core.scenario_namer import ScenarioNamer
+        namer = ScenarioNamer(get_default_namer(), *args)
+        return Options().with_namer(namer)
