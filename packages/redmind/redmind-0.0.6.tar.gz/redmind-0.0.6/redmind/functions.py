917255bef3b56410a71b622e7060651e12d83454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package redmind/0.0.6/redmind-0.0.6.tar.gz

---
+++ packages/redmind/redmind-0.0.6.tar.gz/redmind-0.0.6/redmind/functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def mse(y, y_pred) -> np.float64:
+    return np.mean(np.power(y_pred - y, 2))
+    
+def mse_prime(y, y_pred) -> np.ndarray:
+    return 2 * (y_pred - y) / np.size(y)
+
+def binary_cross_entropy(y ,y_pred) -> np.float64:
+    return np.mean(-y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred))
+
+def binary_cross_entropy_prime(y, y_pred) -> np.ndarray:
+    return ((1 - y) / (1 - y_pred) - y / y_pred) / np.size(y)
+
+#################################
+# Learning rate decay functions #
+#################################
+def lr_decay(learning_rate, epoch, decay_rate):
+    """Standard learning rate decay algorithm"""
+    return (1 / (1 + decay_rate * epoch)) * learning_rate
