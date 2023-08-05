5499ceb1d7e0ef0806c248c418b0bb1857884fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:14 +0000

    Add package blrec/1.8.0a0/blrec-1.8.0a0.tar.gz

---
+++ packages/blrec/blrec-1.8.0a0.tar.gz/blrec-1.8.0a0/src/blrec/bili/exceptions.py
@@ -0,0 +1,40 @@
+
+import attr
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
+class ApiRequestError(Exception):
+    code: int
+    message: str
+
+
+class LiveRoomHidden(Exception):
+    pass
+
+
+class LiveRoomLocked(Exception):
+    pass
+
+
+class LiveRoomEncrypted(Exception):
+    pass
+
+
+class NoStreamAvailable(Exception):
+    pass
+
+
+class NoStreamFormatAvailable(Exception):
+    pass
+
+
+class NoStreamCodecAvailable(Exception):
+    pass
+
+
+class NoStreamQualityAvailable(Exception):
+    pass
+
+
+class NoAlternativeStreamAvailable(Exception):
+    pass
