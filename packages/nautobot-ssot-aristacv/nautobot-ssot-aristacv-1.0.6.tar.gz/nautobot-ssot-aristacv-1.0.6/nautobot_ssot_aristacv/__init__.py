2e937c698f603cc91899f1237866aed2f2f51192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:56 +0000

    Add package nautobot-ssot-aristacv/1.0.6/nautobot-ssot-aristacv-1.0.6.tar.gz

---
+++ packages/nautobot-ssot-aristacv/nautobot-ssot-aristacv-1.0.6.tar.gz/nautobot-ssot-aristacv-1.0.6/nautobot_ssot_aristacv/__init__.py
@@ -0,0 +1,43 @@
+"""Plugin declaration for aristacv_sync."""
+
+__version__ = "1.0.6"
+
+import os
+
+from nautobot.extras.plugins import PluginConfig
+from django.db.models.signals import post_migrate
+
+
+class NautobotSSOTAristaCVConfig(PluginConfig):
+    """Plugin configuration for the nautobot_ssot_aristacv plugin."""
+
+    name = "nautobot_ssot_aristacv"
+    verbose_name = "Nautobot SSoT Arista CloudVision"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "Nautobot SSoT Arista CloudVision."
+    base_url = "nautobot-sot_aristacv"
+    required_settings = []
+    min_version = "1.0.0"
+    max_version = "1.9999"
+    default_settings = {
+        "cvp_host": os.getenv("CVP_HOST"),
+        "cvp_user": os.getenv("CVP_USER"),
+        "cvp_password": os.getenv("CVP_PASSWORD"),
+        "insecure": os.getenv("CVP_INSECURE") or False,
+        "cvp_token": os.getenv("CVP_TOKEN"),
+    }
+    caching_config = {}
+
+    def ready(self):
+        """Callback invoked after the plugin is loaded."""
+        super().ready()
+
+        from .signals import (  # pylint: disable=import-outside-toplevel
+            post_migrate_create_custom_fields,
+        )
+
+        post_migrate.connect(post_migrate_create_custom_fields)
+
+
+config = NautobotSSOTAristaCVConfig  # pylint:disable=invalid-name
