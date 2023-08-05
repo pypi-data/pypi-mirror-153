996a7f06ddd226f7742d452154649fe20f6b25c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:22 +0000

    Add package ezstools/1/ezstools-1.tar.gz

---
+++ packages/ezstools/ezstools-1.tar.gz/ezstools-1/README.md
@@ -0,0 +1,10 @@
+### ezstools
+Libreria con funciones y utilidades variadas
+
+#### Modulos:
+- console
+- datastructures
+- funcs
+- json_tools
+- random_generator
+- templates
---
+++ packages/ezstools/ezstools-1.tar.gz/ezstools-1/ezstools/__init__.py
@@ -0,0 +1,8 @@
+from .random_generator import *
+from .errorshelp import *
+from .funcs import *
+from .console import *
+from .datastructures import *
+from .templates import *
+from .json_tools import *
+
