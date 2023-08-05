127f488d2a7b57d7e01e8ec08cd860d8f5c0d6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package docarray/0.13.17/docarray-0.13.17.tar.gz

---
+++ packages/docarray/docarray-0.13.17.tar.gz/docarray-0.13.17/docarray/array/storage/base/backend.py
@@ -0,0 +1,34 @@
+from abc import ABC
+from dataclasses import is_dataclass, asdict
+from typing import Dict, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ....typing import DocumentArraySourceType, ArrayType
+
+
+class BaseBackendMixin(ABC):
+    TYPE_MAP: Dict
+
+    def _init_storage(
+        self,
+        _docs: Optional['DocumentArraySourceType'] = None,
+        copy: bool = False,
+        *args,
+        **kwargs
+    ):
+        self._load_offset2ids()
+
+    def _get_storage_infos(self) -> Optional[Dict]:
+        if hasattr(self, '_config') and is_dataclass(self._config):
+            return {k: str(v) for k, v in asdict(self._config).items()}
+
+    def _map_id(self, _id: str) -> str:
+        return _id
+
+    def _map_embedding(self, embedding: 'ArrayType') -> 'ArrayType':
+        from ....math.ndarray import to_numpy_array
+
+        return to_numpy_array(embedding)
+
+    def _map_type(self, col_type: str) -> str:
+        return self.TYPE_MAP[col_type]
