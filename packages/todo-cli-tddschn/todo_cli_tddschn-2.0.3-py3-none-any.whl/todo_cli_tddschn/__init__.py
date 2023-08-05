3226b821fc9eb42e3dd32dc159acaa0ee00896fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:28 +0000

    Add package todo-cli-tddschn/2.0.3/todo_cli_tddschn-2.0.3-py3-none-any.whl

---
+++ packages/todo-cli-tddschn/todo_cli_tddschn-2.0.3-py3-none-any.whl/todo_cli_tddschn/__init__.py
@@ -0,0 +1,43 @@
+__version__ = '2.0.3'
+__app_name__ = 'todo'
+__app_name_full__ = 'todo-cli-tddschn'
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+
+try:
+    from logging_utils_tddschn import get_logger
+    logger, _DEBUG = get_logger(__app_name__)
+except:
+    import logging
+    from logging import NullHandler
+    logger = logging.getLogger(__app_name__)
+    logger.addHandler(NullHandler())
+
+
+class Priority(str, Enum):
+    """Priority levels for to-dos."""
+
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+
+class Status(str, Enum):
+    """Status of a to-do."""
+    TODO = 'todo'
+    DONE = 'done'
+    DELETED = 'deleted'
+    CANCELLED = 'cancelled'
+    WIP = 'wip'
+
+
+# create a custom exception class TodoInitError
+class TodoInitError(Exception):
+    """Custom exception class for todo-cli-tddschn."""
+
+    def __init__(self, message: str, error_code: int = 1) -> None:
+        """Initialize the TodoInitError class."""
+        super().__init__(message)
+        self.error_code = error_code
