4adb7f366f5d6feb81844b0cc1ce4f3936bcbd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package rstar/0.0.1/rstar-0.0.1.tar.gz

---
+++ packages/rstar/rstar-0.0.1.tar.gz/rstar-0.0.1/README.md
@@ -0,0 +1,7 @@
+# rstar
+
+Python bindings to the Rust [`rstar`](https://github.com/georust/rstar) library for creating R* Trees.
+
+At the moment, this is functional but very few Rust methods are wrapped. This is mainly used to test different methods for [spatially partitioning GeoParquet files](https://github.com/kylebarron/spatially-partitioned-geoparquet).
+
+Raise an issue to discuss exposing other methods.
---
+++ packages/rstar/rstar-0.0.1.tar.gz/rstar-0.0.1/src/lib.rs
@@ -0,0 +1,15 @@
+use crate::rstar::{Leaf, ParentNode, RTree, AABB};
+use pyo3::prelude::*;
+mod rstar;
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn rstar(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<AABB>()?;
+    m.add_class::<Leaf>()?;
+    m.add_class::<ParentNode>()?;
+    m.add_class::<RTree>()?;
+    Ok(())
+}
