84bb7fe2575586ff4153b31ba10a87958e4ce638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:18 +0000

    Add package dl621/2.1.6/dl621-2.1.6.tar.gz

---
+++ packages/dl621/dl621-2.1.6.tar.gz/dl621-2.1.6/dl621/__init__.py
@@ -0,0 +1,10 @@
+from .core import (__default_user_agent__,
+                   __default_name_pattern__,
+                   __default_download_timeout__,
+                   __default_memory_limit_ratio__,
+                   __e621_base_url__,
+                   __e621_endpoint_posts__,
+                   __e621_posts_per_request_limit__,
+                   get_info_json,
+                   get_info_json_multiple,
+                   download_image)
