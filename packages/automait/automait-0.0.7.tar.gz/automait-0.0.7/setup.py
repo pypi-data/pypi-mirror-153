1f7c4222997d3ab2b21dce77b7e4963e5143ea39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package automait/0.0.7/automait-0.0.7.tar.gz

---
+++ packages/automait/automait-0.0.7.tar.gz/automait-0.0.7/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import urllib
+import json
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def current_pypi_version(package: str) -> str:
+    req = urllib.request.Request(f"https://pypi.python.org/pypi/{package}/json")
+    r = urllib.request.urlopen(req)
+    if r.code == 200:
+        t = json.loads(r.read())
+        releases = t.get("releases", [])
+        if releases:
+            return sorted(releases)[-1]
+
+
+setuptools.setup(
+    name="automait",
+    version="0.0.7",
+    author="Lukas Leuschen",
+    author_email="lukas.leuschen@automait.ai",
+    description="automait package for interfacing our modeling services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/automait_public/automait_client",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/automait_public/automait_client/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
