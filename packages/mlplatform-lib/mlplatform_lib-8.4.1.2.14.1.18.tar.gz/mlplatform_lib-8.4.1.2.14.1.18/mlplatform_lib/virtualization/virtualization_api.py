7c66de7b4b33deca7c83d3305e47cc7f271558a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:04 +0000

    Add package mlplatform-lib/8.4.1.2.14.1.18/mlplatform_lib-8.4.1.2.14.1.18.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.1.2.14.1.18.tar.gz/mlplatform_lib-8.4.1.2.14.1.18/mlplatform_lib/virtualization/virtualization_api.py
@@ -0,0 +1,16 @@
+from mlplatform_lib.api_client import ApiClient
+from mlplatform_lib.virtualization.virtualization_http_client import VirtualizationHttpClient
+
+class VirtualizationApi:
+    def __init__(self, api_client: ApiClient = None):
+        if api_client is None:
+            api_client = ApiClient()
+        self.api_client = api_client
+
+        self.virtualization_client = VirtualizationHttpClient(
+            virtualization_addr=self.api_client.virtualization_addr, api_client=api_client
+        )
+
+    def download_unstructured(self, do_id: str) :
+        print("Start download_unstructured in Virtualization Api")
+        self.virtualization_client.download_unstructured(do_id=do_id)
