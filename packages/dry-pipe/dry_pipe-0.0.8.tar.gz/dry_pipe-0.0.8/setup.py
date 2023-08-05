12458eb78224f191be3aa49aba6988d2740ac26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:49 +0000

    Add package dry-pipe/0.0.8/dry_pipe-0.0.8.tar.gz

---
+++ packages/dry-pipe/dry_pipe-0.0.8.tar.gz/dry_pipe-0.0.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+minimal_requirements = [
+    'click==8.1.3',
+    'uvicorn==0.17.6',
+    'python-socketio==5.6.0',
+    'psutil==5.9.1',
+    'rich==12.4.4',
+    'PyYAML==5.4.1',
+    'requests==2.27.1'
+]
+
+extra_requirements = [
+    'paramiko==2.11.0'
+]
+
+setup(
+    name='dry_pipe',
+    version='0.0.8',
+    packages=['dry_pipe'],
+    install_requires=minimal_requirements,
+    extras_require={"full": extra_requirements},
+    #package_data={'': ['ui/dist/main.js']},
+    entry_points='''
+        [console_scripts]
+        drypipe=dry_pipe.cli:run_cli
+    ''',
+)
