ad4cdde887ccce9daa3126bbc18e10acbe8c0d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:13 +0000

    Add package scale-nucleus/0.12b2/scale-nucleus-0.12b2.tar.gz

---
+++ packages/scale-nucleus/scale-nucleus-0.12b2.tar.gz/scale-nucleus-0.12b2/nucleus/metrics/__init__.py
@@ -0,0 +1,34 @@
+from .base import Metric, ScalarResult
+from .categorization_metrics import CategorizationF1
+from .cuboid_metrics import CuboidIOU, CuboidPrecision, CuboidRecall
+from .filtering import (
+    FieldFilter,
+    ListOfOrAndFilters,
+    MetadataFilter,
+    apply_filters,
+)
+from .polygon_metrics import (
+    PolygonAveragePrecision,
+    PolygonIOU,
+    PolygonMAP,
+    PolygonMetric,
+    PolygonPrecision,
+    PolygonRecall,
+)
+from .segmentation_metrics import (
+    SegmentationAveragePrecision,
+    SegmentationFWAVACC,
+    SegmentationIOU,
+    SegmentationMAP,
+    SegmentationMaskMetric,
+    SegmentationPrecision,
+    SegmentationRecall,
+)
+from .segmentation_to_poly_metrics import (
+    SegmentationMaskToPolyMetric,
+    SegmentationToPolyAveragePrecision,
+    SegmentationToPolyIOU,
+    SegmentationToPolyMAP,
+    SegmentationToPolyPrecision,
+    SegmentationToPolyRecall,
+)
