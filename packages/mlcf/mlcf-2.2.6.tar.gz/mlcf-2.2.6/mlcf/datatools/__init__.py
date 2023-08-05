a3846fca74542dad0fcb16f8146ce052f79e6fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:44 +0000

    Add package mlcf/2.2.6/mlcf-2.2.6.tar.gz

---
+++ packages/mlcf/mlcf-2.2.6.tar.gz/mlcf-2.2.6/mlcf/datatools/__init__.py
@@ -0,0 +1,8 @@
+"""Data Tools package.
+
+This package provide tools for data processing.
+"""
+
+from mlcf.datatools.data_reader import *  # noqa
+from mlcf.datatools.utils import *  # noqa
+from mlcf.datatools.standardisation import *  # noqa
---
+++ packages/mlcf/mlcf-2.2.6.tar.gz/mlcf-2.2.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+install_requirements = [
+    "pandas-ta",
+    "tsfresh",
+    "sklearn",
+    "tables",
+    "TA-Lib"
+]
+
+setup(
+    install_requires=install_requirements,
+)
