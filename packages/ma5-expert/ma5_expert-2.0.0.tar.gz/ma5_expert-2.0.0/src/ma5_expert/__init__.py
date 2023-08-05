5b24c769ef2a1bb7bec5e3837d611586fe471fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:30 +0000

    Add package ma5-expert/2.0.0/ma5_expert-2.0.0.tar.gz

---
+++ packages/ma5-expert/ma5_expert-2.0.0.tar.gz/ma5_expert-2.0.0/src/ma5_expert/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '1.0.3'
+
+import logging
+import sys
+
+from ma5_expert.system import logger
+
+logger.init(LoggerStream = sys.stdout)
+log = logging.getLogger("ma5_expert")
+log.setLevel(logging.INFO)
+
+from ma5_expert import cutflow
+from ma5_expert import histogram
+
+__all__ = cutflow.__all__
