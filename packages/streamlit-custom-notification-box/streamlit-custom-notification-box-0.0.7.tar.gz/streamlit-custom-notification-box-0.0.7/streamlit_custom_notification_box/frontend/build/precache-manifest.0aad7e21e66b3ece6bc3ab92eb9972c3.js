448dacf099b3d5208150c44073cab7a738934599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:35 +0000

    Add package streamlit-custom-notification-box/0.0.7/streamlit-custom-notification-box-0.0.7.tar.gz

---
+++ packages/streamlit-custom-notification-box/streamlit-custom-notification-box-0.0.7.tar.gz/streamlit-custom-notification-box-0.0.7/streamlit_custom_notification_box/frontend/build/precache-manifest.0aad7e21e66b3ece6bc3ab92eb9972c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7eff5be9bcba0b869d773dcf59d0b85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42cda99aad8ca09e5e41",
+    "url": "./static/css/main.0563a884.chunk.css"
+  },
+  {
+    "revision": "b6a19adb9585ec53bd31",
+    "url": "./static/js/2.f9b5f23c.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.f9b5f23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cda99aad8ca09e5e41",
+    "url": "./static/js/main.325eef74.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
