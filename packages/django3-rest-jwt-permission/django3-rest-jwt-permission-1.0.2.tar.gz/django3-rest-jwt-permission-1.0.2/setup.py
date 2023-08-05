23d571748684bd67c186a4d60bbcd7c30b9d2bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:19 +0000

    Add package django3-rest-jwt-permission/1.0.2/django3-rest-jwt-permission-1.0.2.tar.gz

---
+++ packages/django3-rest-jwt-permission/django3-rest-jwt-permission-1.0.2.tar.gz/django3-rest-jwt-permission-1.0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="django3-rest-jwt-permission",
+    version="1.0.2",
+    description="Django3 Rest Framework JWT Permissions",
+    license="MIT",
+    author="Rati Matchavariani",
+    author_email="rati.matchavariani@hellotend.com",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tend/rest-jwt-permission",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Environment :: Web Environment",
+        "Topic :: Internet :: WWW/HTTP",
+        "Intended Audience :: Developers"
+    ]
+)
