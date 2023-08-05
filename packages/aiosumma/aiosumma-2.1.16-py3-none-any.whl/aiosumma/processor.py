b98d10fa9d7a8ceedcd586f84120023a47f6ffdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:30 +0000

    Add package aiosumma/2.1.16/aiosumma-2.1.16-py3-none-any.whl

---
+++ packages/aiosumma/aiosumma-2.1.16-py3-none-any.whl/aiosumma/processor.py
@@ -0,0 +1,36 @@
+from izihawa_nlptools.language_detect import detect_language
+from izihawa_nlptools.utils import despace_full
+
+from aiosumma.context import QueryContext
+from aiosumma.errors import ParserError
+from aiosumma.parser import default_parser
+from aiosumma.parser.errors import ParseError
+
+
+class ProcessedQuery:
+    def __init__(self, structured_query, context):
+        self.structured_query = structured_query
+        self.context = context
+
+    def is_empty(self):
+        return not bool(self.structured_query)
+
+    def to_summa_query(self):
+        return self.structured_query.to_summa_query() if self.structured_query else {'all': {}}
+
+
+class QueryProcessor:
+    def __init__(self, transformers=None):
+        self.transformers = transformers or []
+
+    def process(self, query, language):
+        query = despace_full(query)
+        query = query.lower() if query else None
+        try:
+            structured_query = default_parser.parse(query) if query else None
+        except ParseError as error:
+            raise ParserError(query=query, nested_error=error)
+        context = QueryContext(language=detect_language(query) or language)
+        for transformer in self.transformers:
+            structured_query = transformer.visit(structured_query, context=context)
+        return ProcessedQuery(structured_query=structured_query, context=context)
