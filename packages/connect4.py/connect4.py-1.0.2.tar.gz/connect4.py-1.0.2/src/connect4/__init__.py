69c84301f0519ec7e1e5f34473af68ccd3eee40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package connect4.py/1.0.2/connect4.py-1.0.2.tar.gz

---
+++ packages/connect4.py/connect4.py-1.0.2.tar.gz/connect4.py-1.0.2/src/connect4/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'connect4.py'
+__author__ = 'airo.pi_'
+__license__ = 'MIT'
+__version__ = '1.0.2'
+
+from .core import Connect4 as Connect4
+from .core import Players as Players
+from .errors import ColumnFull as ColumnFull
+from .errors import GameOver as GameOver
+from .errors import InvalidColumn as InvalidColumn
+
+__all__ = ['Connect4', 'Players', 'ColumnFull', 'GameOver', 'InvalidColumn']
