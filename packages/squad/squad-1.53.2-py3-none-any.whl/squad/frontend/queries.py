ee98a9129753a240f8b398eb0ffda9bc7bce8ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:47 +0000

    Add package squad/1.53.2/squad-1.53.2-py3-none-any.whl

---
+++ packages/squad/squad-1.53.2-py3-none-any.whl/squad/frontend/queries.py
@@ -0,0 +1,19 @@
+from django.utils.translation import gettext as _
+from squad.core.models import Metric
+from squad.core.utils import join_name
+
+
+def get_metrics_list(project):
+    unique_names = set()
+
+    metric_set = Metric.objects.filter(environment__project=project).values('suite__slug', 'metadata__name')
+    for m in metric_set:
+        unique_names.add(join_name(m['suite__slug'], m['metadata__name']))
+
+    metric_names = [{"name": name} for name in sorted(unique_names)]
+
+    metrics = [{"name": ":summary:", "label": _("Summary of all metrics per build")}]
+    metrics += [{"name": ":dynamic_summary:", "label": _("Summary of selected metrics"), "dynamic": "yes"}]
+    metrics += [{"name": ":tests:", "label": _("Test pass %"), "max": 100, "min": 0}]
+    metrics += metric_names
+    return metrics
