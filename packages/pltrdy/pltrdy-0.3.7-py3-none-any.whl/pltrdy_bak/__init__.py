0a4466130fad370fcaf83da7587876284e9b652c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:11 +0000

    Add package pltrdy/0.3.7/pltrdy-0.3.7-py3-none-any.whl

---
+++ packages/pltrdy/pltrdy-0.3.7-py3-none-any.whl/pltrdy_bak/__init__.py
@@ -0,0 +1,18 @@
+import pltrdy.percentiles
+import pltrdy.assertion
+import pltrdy.explore_results
+import pltrdy.gen_script
+import pltrdy.pickle
+import pltrdy.rouge
+import pltrdy.stats
+import pltrdy.time
+import pltrdy.tee
+import pltrdy.wc  # noqa F401
+import pltrdy.quiet
+import pltrdy.tmpdir
+import pltrdy.types
+
+from pltrdy.describe import describe_str, describe_lines, describe, tab_print
+from pltrdy.reload import reload_class
+
+__version__ = "0.3.3.1"
---
+++ packages/pltrdy/pltrdy-0.3.7-py3-none-any.whl/pltrdy_bak/wc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+    Python wrapper around `wc` command
+"""
+import subprocess
+
+
+def linecount(path):
+    return wordcount(path, args="-l")
+
+
+def wordcount(path, args=["-w"]):
+    o = subprocess.run(["wc"] + args + [path], stdout=subprocess.PIPE)
+    wc = int(o.stdout.decode('utf-8').split()[0])
+    return wc
