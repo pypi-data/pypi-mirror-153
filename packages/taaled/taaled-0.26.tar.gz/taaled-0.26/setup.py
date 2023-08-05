15abd6538ceec2aa230f577c30629b0ccffb7ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package taaled/0.26/taaled-0.26.tar.gz

---
+++ packages/taaled/taaled-0.26.tar.gz/taaled-0.26/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="taaled",
+    version="0.26",
+    author="Kristopher Kyle",
+    author_email="kristopherkyle1@gmail.com",
+    description="Advanced analysis of lexical diversity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LCR-ADS-Lab/TAALED",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
