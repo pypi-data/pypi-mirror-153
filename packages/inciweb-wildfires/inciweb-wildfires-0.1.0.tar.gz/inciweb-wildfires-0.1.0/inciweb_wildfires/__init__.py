902370f57d559fc2429f511017c73216a5f809a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:16 +0000

    Add package inciweb-wildfires/0.1.0/inciweb-wildfires-0.1.0.tar.gz

---
+++ packages/inciweb-wildfires/inciweb-wildfires-0.1.0.tar.gz/inciweb-wildfires-0.1.0/inciweb_wildfires/__init__.py
@@ -0,0 +1,24 @@
+import requests
+from geojson import Feature, FeatureCollection, Point
+
+
+def get_incidents():
+    """
+    Get active incidents data from InciWeb.
+
+    Returns GeoJson FeatureCollection.
+    """
+    # Get the data
+    url = "https://inciweb.nwcg.gov/feeds/json/esri/"
+    r = requests.get(url)
+    content = r.json()["markers"]
+
+    # Convert to GeoJSON features
+    feature_list = []
+    for d in content:
+        p = Point(map(float, (d["lng"], d["lat"])))
+        f = Feature(geometry=p, properties=d)
+        feature_list.append(f)
+
+    # Return the GeoJSON feature collection
+    return FeatureCollection(feature_list)
