a4edd61d3e727abe6137baab288f3c05e2a2a9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:46 +0000

    Add package homeassistant-stubs/2022.6.0b5/homeassistant_stubs-2022.6.0b5-py3-none-any.whl

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2022.6.0b5-py3-none-any.whl/homeassistant-stubs/components/rainmachine/model.pyi
@@ -0,0 +1,7 @@
+class RainMachineDescriptionMixinApiCategory:
+    api_category: str
+    def __init__(self, api_category) -> None: ...
+
+class RainMachineDescriptionMixinUid:
+    uid: int
+    def __init__(self, uid) -> None: ...
