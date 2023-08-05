58e11c4bcd870a490dd86654455101f502af3fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:20 +0000

    Add package pyytdl/1.3.2/pyytdl-1.3.2-py3-none-any.whl

---
+++ packages/pyytdl/pyytdl-1.3.2-py3-none-any.whl/pyyt/config.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+
+def return_config(file_type="mp3", destination_folder=None):
+
+    if destination_folder != None:
+        DEFAULT_PATH = destination_folder
+    else:
+        if os.path.exists('data.json'):
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+                DEFAULT_PATH = data['path']
+        else:
+            DEFAULT_PATH = ''
+
+    if file_type == "mp3":
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl': f'{DEFAULT_PATH}/%(title)s.%(ext)s',
+            'DEFAULT_PATH': DEFAULT_PATH,
+            'quiet': True,
+        }
+
+    elif file_type == "mp4":
+        ydl_opts = {
+            'postprocessors': [{
+                'key': 'FFmpegMetadata',
+            }],
+            'outtmpl': f'{DEFAULT_PATH}/%(title)s.%(ext)s',
+            'DEFAULT_PATH': DEFAULT_PATH,
+            'quiet': True,
+        }
+
+    return ydl_opts
