d8ddcde5b492dc1968cab3b1d0b0f4f6a4fc676f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:07 +0000

    Add package pynnacle/1.0.1/pynnacle-1.0.1-py3-none-any.whl

---
+++ packages/pynnacle/pynnacle-1.0.1-py3-none-any.whl/pynnacle/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pizazz."""
+
+__title__ = "pynnacle"
+__author__ = "Stephen R A King"
+__description__ = "A utility class to simplify the sending of emails"
+__version__ = "1.0.1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Stephen R A King"
