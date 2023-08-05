c13ce538aeabde8a49aefad517392781966e49e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:56 +0000

    Add package watchmen-ml-python-sdk/0.1.8/watchmen-ml-python-sdk-0.1.8.tar.gz

---
+++ packages/watchmen-ml-python-sdk/watchmen-ml-python-sdk-0.1.8.tar.gz/watchmen-ml-python-sdk-0.1.8/ml_sdk/ml/model/notebook.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class WatchmenNotebook(BaseModel):
+    name:str = None
+    storageType:str = None
+    storageLocation:str = None
+    environment:dict = {}
+    dependencies:dict = {}
+
+
+
+
