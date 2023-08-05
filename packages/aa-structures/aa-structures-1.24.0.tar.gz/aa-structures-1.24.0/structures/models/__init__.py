3ef2e132a3151ca9d2486dc6549e9fa4d88b4051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package aa-structures/1.24.0/aa-structures-1.24.0.tar.gz

---
+++ packages/aa-structures/aa-structures-1.24.0.tar.gz/aa-structures-1.24.0/structures/models/__init__.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+
+from .eveuniverse import (
+    EsiNameLocalization,
+    EveCategory,
+    EveConstellation,
+    EveGroup,
+    EveMoon,
+    EvePlanet,
+    EveRegion,
+    EveSolarSystem,
+    EveSovereigntyMap,
+    EveSpaceType,
+    EveType,
+)
+from .notifications import (
+    EveEntity,
+    FuelAlert,
+    FuelAlertConfig,
+    JumpFuelAlert,
+    JumpFuelAlertConfig,
+    Notification,
+    NotificationType,
+    Webhook,
+    get_default_notification_types,
+)
+from .owners import Owner, OwnerCharacter
+from .structures import (
+    PocoDetails,
+    Structure,
+    StructureItem,
+    StructureService,
+    StructureTag,
+)
