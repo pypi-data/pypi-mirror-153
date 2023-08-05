59f54f61036e3f50cb96d3f18ae93112bb81ea0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:19:27 +0000

    Add package reefledge/1.4.0a2/reefledge-1.4.0a2.tar.gz

---
+++ packages/reefledge/reefledge-1.4.0a2.tar.gz/reefledge-1.4.0a2/reefledge/version.py
@@ -0,0 +1,10 @@
+"""Holds the package version number."""
+
+from typing import Final, Dict
+
+import pdoc
+
+__pdoc__: Final[Dict[str, bool]] = {'__version__': True}
+
+__version__: Final[str] = '1.4.0' + '-alpha.2'
+"""Package version number"""
