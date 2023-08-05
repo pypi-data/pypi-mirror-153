a6af908c16a235fd3cb5218a660895b2ea82cbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:07 +0000

    Add package garpix-notify/5.6.2/garpix_notify-5.6.2.tar.gz

---
+++ packages/garpix-notify/garpix_notify-5.6.2.tar.gz/garpix_notify-5.6.2/garpix_notify/models/choices.py
@@ -0,0 +1,30 @@
+class TYPE:
+    EMAIL = 0
+    SMS = 1
+    PUSH = 2
+    TELEGRAM = 3
+    VIBER = 4
+    SYSTEM = 5
+
+    CHOICES = (
+        (EMAIL, 'E-mail'),
+        (SMS, 'SMS'),
+        (PUSH, 'Push'),
+        (TELEGRAM, 'Telegram'),
+        (VIBER, 'Viber'),
+        (SYSTEM, 'System'),
+    )
+
+
+class STATE:
+    DISABLED = -2
+    REJECTED = -1
+    WAIT = 0
+    DELIVERED = 1
+
+    CHOICES = (
+        (DELIVERED, 'Доставлено'),
+        (REJECTED, 'Отклонено'),
+        (WAIT, 'В ожидании'),
+        (DISABLED, 'Не отправлено (отправка запрещена настройками)'),
+    )
