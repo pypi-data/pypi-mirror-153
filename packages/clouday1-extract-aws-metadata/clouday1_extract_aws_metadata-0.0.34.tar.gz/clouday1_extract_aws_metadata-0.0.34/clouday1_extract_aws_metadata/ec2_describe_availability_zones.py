17a8fb0bb47329474c8c4450dee15dd8280439bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:31 +0000

    Add package clouday1-extract-aws-metadata/0.0.34/clouday1_extract_aws_metadata-0.0.34.tar.gz

---
+++ packages/clouday1-extract-aws-metadata/clouday1_extract_aws_metadata-0.0.34.tar.gz/clouday1_extract_aws_metadata-0.0.34/clouday1_extract_aws_metadata/ec2_describe_availability_zones.py
@@ -0,0 +1,26 @@
+import boto3
+from json import JSONEncoder
+import datetime
+import json
+import os
+
+
+class DateTimeEncoder(JSONEncoder):
+    # Override the default method
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+
+
+def ec2_describe_availability_zone(region,AWS_ACCESS_KEY,AWS_SECRET_KEY):
+
+    client = boto3.client('ec2', region_name=region,
+    aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+    response = client.describe_availability_zones()
+    json_list = json.dumps(response, indent=4, cls=DateTimeEncoder)
+    script_dir = os.path.dirname('.')
+    file_path_write = os.path.join(
+        script_dir, 'data/ec2-describe-availability-zones-'+region+'.json')
+    with open(file_path_write, 'w')as outfile:
+        outfile.write(json_list)
+        outfile.close()
