e3040dd059009a6dd60b8a4206d27f9784447770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package imagequant/1.0.3/imagequant-1.0.3.tar.gz

---
+++ packages/imagequant/imagequant-1.0.3.tar.gz/imagequant-1.0.3/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+long_description = ""
+if os.path.isfile("README.rst"):
+    long_description = open("README.rst", "r", encoding="utf-8").read()
+
+
+setup(
+    name="imagequant",
+    version="1.0.3",
+    description="Image Quantization Library",
+    url="https://github.com/wanadev/imagequant-python",
+    license="BSD-3-Clause",
+    long_description=long_description,
+    keywords="image libimagequant pngquant cffi",
+    author="Wanadev",
+    author_email="contact@wanadev.fr",
+    maintainer="Fabien LOISON",
+    packages=find_packages(),
+    setup_requires=[
+        "cffi>=1.0.0",
+    ],
+    install_requires=[
+        "cffi>=1.0.0",
+    ],
+    extras_require={
+        "dev": [
+            "nox",
+            "flake8",
+            "black",
+            "pytest",
+        ],
+        "pil": [
+            "pillow",
+        ],
+    },
+    cffi_modules=[
+        "imagequant/libimagequant_build.py:ffibuilder",
+    ],
+)
