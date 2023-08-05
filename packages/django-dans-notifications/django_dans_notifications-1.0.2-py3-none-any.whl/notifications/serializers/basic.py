d4d40e5396d20691d4d73471e44851714b230ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package django-dans-notifications/1.0.2/django_dans_notifications-1.0.2-py3-none-any.whl

---
+++ packages/django-dans-notifications/django_dans_notifications-1.0.2-py3-none-any.whl/notifications/serializers/basic.py
@@ -0,0 +1,24 @@
+from ..models.basic import NotificationBasic
+
+"""
+============================================================================================ #
+BASIC SERIALIZERS ========================================================================== #
+============================================================================================ #
+"""
+
+
+#
+# NOTIFICATION BASIC ========================== #
+#
+class NotificationBasicSerializer(BaseSerializer):
+    class Meta:
+        model = NotificationBasic
+        fields = (
+            "id",
+            "message",
+            "read",
+            "datetime_sent",
+            "sent_successfully",
+            "sender",
+            "recipients",
+        )
