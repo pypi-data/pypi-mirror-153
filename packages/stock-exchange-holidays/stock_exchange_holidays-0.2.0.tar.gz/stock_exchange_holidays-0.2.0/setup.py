1106afa89e6e338eb33602c0cdae601002adc0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:02 +0000

    Add package stock-exchange-holidays/0.2.0/stock_exchange_holidays-0.2.0.tar.gz

---
+++ packages/stock-exchange-holidays/stock_exchange_holidays-0.2.0.tar.gz/stock_exchange_holidays-0.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+__description__ = "Worldwide Stock Exchange holidays (NYSE, CME, B3 and others)"
+with open("README.md", "r") as fh:
+    __long_description__ = fh.read()
+
+
+setup(
+    name='stock_exchange_holidays',
+    version='0.2.0',
+    author='Leonardo Gregianin',
+    author_email='leogregianin@gmail.com',
+    description=__description__,
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    url='https://github.com/leogregianin/stock-exchange-holidays',
+    zip_safe=False,
+    python_requires='>=3.6',
+    license='MIT',
+    keywords="stock, holidays",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
