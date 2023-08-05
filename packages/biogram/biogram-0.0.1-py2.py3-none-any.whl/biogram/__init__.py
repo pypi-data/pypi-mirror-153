af001f3068ecacc5dfba770e97b599765ded951e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package biogram/0.0.1/biogram-0.0.1-py2.py3-none-any.whl

---
+++ packages/biogram/biogram-0.0.1-py2.py3-none-any.whl/biogram/__init__.py
@@ -0,0 +1,19 @@
+"""Make diagrams for biological entities.
+
+Import the package::
+
+   import biogram
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   create_schema_graph
+   view
+"""
+
+__version__ = "0.0.1"
+
+from ._sqlalchemy import create_schema_graph, create_uml_graph  # noqa
+from ._utils import view
