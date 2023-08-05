44ac364ebc69c85090ed9a045a9a26971371e16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:02 +0000

    Add package Classeviva.py/0.1.0b4/Classeviva.py-0.1.0b4.tar.gz

---
+++ packages/Classeviva.py/Classeviva.py-0.1.0b4.tar.gz/Classeviva.py-0.1.0b4/src/classeviva/__main__.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+
+if __name__ == "__main__":
+    webbrowser.open("https://pypi.org/project/classeviva.py/")
---
+++ packages/Classeviva.py/Classeviva.py-0.1.0b4.tar.gz/Classeviva.py-0.1.0b4/src/classeviva/collegamenti/collegamenti.py
@@ -0,0 +1,9 @@
+class Collegamenti:
+    base: str = "https://web.spaggiari.eu/rest"
+    accesso: str = f"{base}/v1/auth/login"
+    stato: str = f"{base}/v1/auth/status"
+    biglietto: str = f"{base}/v1/auth/ticket"
+    documenti: str = f"{base}/v1/students/{{}}/documents"
+    controllo_documento: str = f"{base}/v1/students/{{}}/documents/check/{{}}"
+    leggi_documento: str = f"{base}/v1/students/{{}}/documents/read/{{}}"
+    assenze: str = f"{base}/v1/students/{{}}/absences/details"
