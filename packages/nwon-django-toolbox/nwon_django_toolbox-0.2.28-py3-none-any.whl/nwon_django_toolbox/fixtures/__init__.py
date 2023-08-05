b723b50a6e5fe82cc0841070ddd4b59d0f5abf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:07 +0000

    Add package nwon-django-toolbox/0.2.28/nwon_django_toolbox-0.2.28-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.2.28-py3-none-any.whl/nwon_django_toolbox/fixtures/__init__.py
@@ -0,0 +1,11 @@
+from nwon_django_toolbox.fixtures.test_fixtures_handling import (
+    dump_test_fixtures,
+    load_test_fixtures,
+    read_test_fixture_file,
+)
+
+__all__ = [
+    "dump_test_fixtures",
+    "load_test_fixtures",
+    "read_test_fixture_file",
+]
