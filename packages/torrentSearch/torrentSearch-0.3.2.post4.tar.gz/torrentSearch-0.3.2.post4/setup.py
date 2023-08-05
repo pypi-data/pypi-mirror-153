e26ee6638c66fe6a2c6e81fba24781c85690bdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package torrentSearch/0.3.2.post4/torrentSearch-0.3.2.post4.tar.gz

---
+++ packages/torrentSearch/torrentSearch-0.3.2.post4.tar.gz/torrentSearch-0.3.2.post4/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import os
+import re
+from setuptools import setup, find_packages
+from sys import path
+from os.path import dirname
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+exec(open('torrentSearch/__version__.py').read())
+
+setup(
+    name='torrentSearch',
+    version=__version__,
+    packages=find_packages(),
+    package_data={
+        'torrentSearch': ['default_config.ini'],
+    },
+    author='KevinMidboe',
+    description='Search For Torrents',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'bs4',
+        'docopt',
+	    'colored'
+    ],
+    url='https://github.com/KevinMidboe/torrent_search',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'torrentsearch = torrentSearch.__main__:main',
+        ],
+    },
+)
