f719f13a01326b3907a36e3f150135c54101dff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package captur-ml-sdk/0.1.73/captur-ml-sdk-0.1.73.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.73.tar.gz/captur-ml-sdk-0.1.73/captur_ml_sdk/dtypes/interfaces/MLGateway/MLRegisterModel.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, root_validator
+
+from captur_ml_sdk.dtypes.generics import Meta
+from captur_ml_sdk.dtypes.interfaces.validators import check_model_exists_in_gcp
+from typing import Optional
+
+class ModelRegisterRequest(BaseModel):
+    meta: Optional[Meta]
+    name: str
+    version: str
+    dataset_id: str
+    model_type: Optional[str]
+
+    _model_exists = root_validator(check_model_exists_in_gcp, allow_reuse=True)
