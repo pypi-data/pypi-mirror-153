f584cc858ec383e362fdb2c341b4a8a7077a4c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package kotonohagetter/0.0.2/kotonohagetter-0.0.2.tar.gz

---
+++ packages/kotonohagetter/kotonohagetter-0.0.2.tar.gz/kotonohagetter-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kotonohagetter",
+    version="0.0.2",
+    author="rikito ohnishi",
+    author_email="ohnishi.rikito@gmail.com",
+    description='A package for visualization of aggregate data of players in "Kotoha Tango"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/plumchloride/kotonohatango-getter",
+    project_urls={
+        "Bug Tracker": "https://github.com/plumchloride/kotonohatango-getter",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['kotonohagetter'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'kotonohagetter = kotonohagetter:main'
+        ]
+    },
+)
