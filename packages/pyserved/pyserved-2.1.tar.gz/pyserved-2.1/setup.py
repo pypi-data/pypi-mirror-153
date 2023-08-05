cf6d8a3fdd5b3892d3e4461b31716471a02474d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package pyserved/2.1/pyserved-2.1.tar.gz

---
+++ packages/pyserved/pyserved-2.1.tar.gz/pyserved-2.1/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pyserved",
+    version="2.1",
+    description="Network transfer/hosting utilities by SblipDev.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/SblipDev/pyserved/",
+    author="Shaurya Pratap Singh",
+    author_email="shaurya.p.singh21@gmail.com",
+    license="MIT",
+    packages=["pyserved"],
+    install_requires=["rich", "choam"],
+    include_package_data=True,
+    scripts=["pyserved/bin/pdlisten",
+            "pyserved/bin/pdsnd", "pyserved/bin/pdhost"],
+)
