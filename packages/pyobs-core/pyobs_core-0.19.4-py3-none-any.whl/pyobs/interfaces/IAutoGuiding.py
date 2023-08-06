5ea53a9d520239b49a1455497537a4609846d0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:37 +0000

    Add package pyobs-core/0.19.4/pyobs_core-0.19.4-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-0.19.4-py3-none-any.whl/pyobs/interfaces/IAutoGuiding.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any
+
+from .IStartStop import IStartStop
+
+
+class IAutoGuiding(IStartStop, metaclass=ABCMeta):
+    """The module can perform auto-guiding."""
+
+    __module__ = "pyobs.interfaces"
+
+    @abstractmethod
+    async def set_exposure_time(self, exposure_time: float, **kwargs: Any) -> None:
+        """Set the exposure time for the auto-guider.
+
+        Args:
+            exposure_time: Exposure time in secs.
+        """
+        ...
+
+
+__all__ = ["IAutoGuiding"]
