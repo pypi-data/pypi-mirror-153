d60c1f5711c7295cd588980d6a80ce41d6bb23cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:27 +0000

    Add package html-and-py/1.0.10/html_and_py-1.0.10-py3-none-any.whl

---
+++ packages/html-and-py/html_and_py-1.0.10-py3-none-any.whl/html_and_py/__init__.py
@@ -0,0 +1,11 @@
+from src.html_and_py.html_and_py \
+    import HTML_PROTOTYPE, HTML_VOID_ELEMENTS, \
+    create_html, is_self_enclosing, render_html, update_html
+
+__all__ = ["HTML_PROTOTYPE",
+           "HTML_VOID_ELEMENTS",
+           "create_html",
+           "is_self_enclosing",
+           "render_html",
+           "update_html"
+           ]
