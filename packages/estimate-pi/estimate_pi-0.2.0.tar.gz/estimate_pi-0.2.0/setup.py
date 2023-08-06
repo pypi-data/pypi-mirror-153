41a2cfe24396779635f3264c505e6ea3d64fa2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:54 +0000

    Add package estimate-pi/0.2.0/estimate_pi-0.2.0.tar.gz

---
+++ packages/estimate-pi/estimate_pi-0.2.0.tar.gz/estimate_pi-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['estimate_pi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.5.2,<4.0.0', 'numpy>=1.22.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'estimate-pi',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Chang-Goo Kim',
+    'author_email': 'changgoo@princeton.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/estimate-pi/estimate_pi-0.2.0.tar.gz/estimate_pi-0.2.0/src/estimate_pi/plot_tools.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .estimate_pi import *
+
+
+def plot_darts(ndarts, circle=True):
+    """
+    Function that displays a throw of darts and highlights darts in the unit circle with a different colour
+
+    Parameters
+    ----------
+    ndarts : int
+        number of dart throws
+    circle : bool, optional
+        if True, draw the boundary
+
+    Returns
+    -------
+    None
+    """
+    darts = throw(ndarts)
+    incircle = is_in_unit_circle(darts)
+
+    plt.figure(figsize=(5, 5))
+    plt.plot(darts[0, ~incircle], darts[1, ~incircle], ".", label="not in unit circle")
+    plt.plot(darts[0, incircle], darts[1, incircle], ".", label="in unit circle")
+    plt.gca().set_aspect("equal")
+    plt.legend()
+
+    if circle:
+        draw_circle(r=1)
+    return
+
+
+def draw_circle(r=1, x0=0, y0=0):
+    """Simple function to draw a circle with radius r centered at x0, y0 on the current axis
+
+    Parameters
+    ----------
+    r : float
+        radius of the circle
+    x0, y0 : float
+        center of the circle
+    """
+    theta = np.linspace(0, 2 * np.pi)
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    plt.plot(x, y, "k:")
