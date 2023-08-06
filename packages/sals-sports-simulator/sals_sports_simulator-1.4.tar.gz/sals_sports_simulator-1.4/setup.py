c9e642f108b2965fb607bc2faba0ebba62744185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package sals-sports-simulator/1.4/sals_sports_simulator-1.4.tar.gz

---
+++ packages/sals-sports-simulator/sals_sports_simulator-1.4.tar.gz/sals_sports_simulator-1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name = 'sals_sports_simulator',
+      version = '1.4',
+      description = 'Simulate the results of a sporting event.',
+      packages = ['sals_sports_simulator'],
+      author = 'Sal Cacciatore',
+      author_email = 'scacciatore1@gmail.com',
+      zip_safe = False)
+
+
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
