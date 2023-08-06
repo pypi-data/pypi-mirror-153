0f72fd370d417cb2ec56aef3c42f10901c126ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:07 +0000

    Add package flask-native-stubs/0.2.5/flask_native_stubs-0.2.5-py3-none-any.whl

---
+++ packages/flask-native-stubs/flask_native_stubs-0.2.5-py3-none-any.whl/flask_native_stubs/global_controls.py
@@ -0,0 +1,13 @@
+COLLECT_RUNTIME_INFO = False
+
+SERIALIZATION = 'json'
+
+SIMULATION_MODE = False
+''' do not edit this value manually. this is an "alias" to
+    `COLLECT_RUNTIME_INFO`. '''
+
+EXCEPTION_HANDLE = 0
+''' 0: raise exception                  (default)
+    1: return str(exception)            (suggest)
+    2: return traceback.format_exc()    (for debug only!)
+'''
