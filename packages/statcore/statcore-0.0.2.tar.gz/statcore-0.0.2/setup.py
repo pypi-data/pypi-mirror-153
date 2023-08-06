1ed2f0a2329238dc9a605e24b0b694a95f79dc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:59 +0000

    Add package statcore/0.0.2/statcore-0.0.2.tar.gz

---
+++ packages/statcore/statcore-0.0.2.tar.gz/statcore-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+setup(
+    name='statcore',
+    version='0.0.2',
+    author='Andrew Barisser',
+    license='MIT',
+    packages=find_packages(),
+    ext_modules=cythonize(
+        [
+#            Extension(name="table", sources=["statcore/table.pyx"]),
+       #     Extension(name="fibz", sources=["statcore/fib.pyx"]),
+        ],
+        compiler_directives={'language_level' : "3"},
+        annotate=True
+        ),# + [Extension(name="smath", sources=["statcore/math.cpp"])],
+    zip_safe=False,
+    install_requires=[
+            'numpy'
+        ],
+    tests_require=['pytest-cov', 'pytest'])
