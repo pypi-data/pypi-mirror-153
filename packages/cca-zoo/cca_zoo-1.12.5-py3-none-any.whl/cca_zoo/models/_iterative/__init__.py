fb300657312aa2a88f780ad14feec8d4389c6e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:34 +0000

    Add package cca-zoo/1.12.5/cca_zoo-1.12.5-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.12.5-py3-none-any.whl/cca_zoo/models/_iterative/__init__.py
@@ -0,0 +1,10 @@
+from ._altmaxvar import AltMaxVar
+from ._elastic import ElasticCCA, SCCA_IPLS
+from ._pddgcca import PDD_GCCA
+from ._pls_als import PLS_ALS
+from ._pmd import SCCA_PMD
+from ._scca_admm import SCCA_ADMM
+from ._scca_parkhomenko import SCCA_Parkhomenko
+from ._spancca import SCCA_Span
+from ._swcca import SWCCA
+from .utils import ProxFrobenius, ProxLasso
