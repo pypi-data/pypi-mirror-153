875652d516f6651314730b42a4a6c3fd4e9115a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:03 +0000

    Add package database-version-control/0.1.15/database_version_control-0.1.15.tar.gz

---
+++ packages/database-version-control/database_version_control-0.1.15.tar.gz/database_version_control-0.1.15/setup.py
@@ -0,0 +1,57 @@
+"""Minimal setup file for tasks project."""
+
+from setuptools import find_packages, setup
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('src/dvc/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='database_version_control',
+    version=main_ns['__version__'],
+    license='proprietary',
+    python_requires='>=3.7.*',
+    description='A library for doing database version control',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Ken Ho',
+    author_email='kenho811@gmail.com',
+    url='https://github.com/kenho811/Python_Database_Version_Control/tree/release',
+    packages=(find_packages(where="src")),
+    package_dir={"": "src"},
+    # Include SQL files (seen as data by setup tools)
+    package_data={'': ['*.sql']},
+    include_package_data=True,
+    install_requires=[
+        'psycopg2',
+        'typer[all]',
+        'pyyaml',
+
+    ],
+    extras_require={
+            'dev': [
+                'pytest',
+                'pytest-cov',
+                'pytest-docker',
+                'pytest-html',
+                'sphinx',
+                'flake8',
+            ]
+    },
+    entry_points='''
+    [console_scripts]
+    dvc=dvc.app.cli:app
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/database-version-control/database_version_control-0.1.15.tar.gz/database_version_control-0.1.15/src/dvc/app/cli/__init__.py
@@ -0,0 +1,24 @@
+"""
+Define the main commands of the CLI
+"""
+import logging
+import typer
+
+from dvc.version import __version__
+from dvc.app.cli import config, database, sql
+
+# Set default logging to INFO
+logging.root.setLevel(logging.INFO)
+
+app = typer.Typer()
+app.add_typer(config.app, name='cfg', help="Config related subcommands")
+app.add_typer(database.app, name='db', help="Database related subcommands")
+app.add_typer(sql.app, name='sql', help="SQL files related subcommands")
+
+
+@app.command()
+def version():
+    """
+    Show CLI version
+    """
+    typer.echo(__version__)
