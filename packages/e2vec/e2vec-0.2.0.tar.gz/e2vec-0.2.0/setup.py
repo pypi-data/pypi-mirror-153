63bb9973935ab3320886f6f55f014e3323d92553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:24 +0000

    Add package e2vec/0.2.0/e2vec-0.2.0.tar.gz

---
+++ packages/e2vec/e2vec-0.2.0.tar.gz/e2vec-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['e2vec']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['timm>=0.5.4,<0.6.0', 'transformers>=4.19.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'e2vec',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'An Pham',
+    'author_email': 'ancs21.ps@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.12,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
