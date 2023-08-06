c738240d6a4cca6a8805ef53ea11fabb0a407634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:37 +0000

    Add package fakedata/0.2.1/fakedata-0.2.1.tar.gz

---
+++ packages/fakedata/fakedata-0.2.1.tar.gz/fakedata-0.2.1/fakedata/__init__.py
@@ -0,0 +1,10 @@
+from fakedata.fakedata import FakeData
+
+RandomOneName = FakeData.RandomOneName
+RandomTwoName = FakeData.RandomTwoName
+RandomUSAName = FakeData.RandomUSAName
+RandomName = FakeData.RandomName
+CheckSex = FakeData.CheckSex
+RandomSex = FakeData.RandomSex
+RandomEmail = FakeData.RandomEmail
+PersonalizedEmail = FakeData.PersonalizedEmail
