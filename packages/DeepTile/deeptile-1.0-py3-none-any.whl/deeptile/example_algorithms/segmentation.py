e613df1c489f50c33a9284e4684f76e8d0cb3d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:24 +0000

    Add package DeepTile/1.0/deeptile-1.0-py3-none-any.whl

---
+++ packages/DeepTile/deeptile-1.0-py3-none-any.whl/deeptile/example_algorithms/segmentation.py
@@ -0,0 +1,91 @@
+import numpy as np
+from deeptile.algorithms import transform
+
+
+def cellori_segmentation(model_parameters, eval_parameters):
+
+    from cellori import Cellori
+
+    def func_segment(tile):
+
+        mask = Cellori(tile, **model_parameters).segment(**eval_parameters)[0]
+
+        return mask
+
+    func_segment = transform(func_segment)
+
+    return func_segment
+
+
+def cellpose_segmentation(model_parameters, eval_parameters):
+
+    from cellpose.models import Cellpose
+    from cellpose.io import logger_setup
+    logger_setup()
+
+    model = Cellpose(**model_parameters)
+
+    def func_segment(tile):
+
+        mask = model.eval(tile, tile=False, **eval_parameters)[0]
+
+        return mask
+
+    func_segment = transform(func_segment)
+
+    return func_segment
+
+
+def deepcell_mesmer_segmentation(model_parameters, eval_parameters):
+
+    from deepcell.applications import Mesmer
+
+    model = Mesmer(**model_parameters)
+
+    def func_segment(tile, batch_size):
+
+        tile = np.moveaxis(tile, 1, -1)
+        mask = model.predict(tile, batch_size=batch_size, **eval_parameters)
+        mask = np.moveaxis(mask, -1, 1)
+
+        return mask
+
+    func_segment = transform(func_segment, default_batch_size=8)
+
+    return func_segment
+
+
+def deepcell_nuclear_segmentation(model_parameters, eval_parameters):
+
+    from deepcell.applications import NuclearSegmentation
+
+    model = NuclearSegmentation(**model_parameters)
+
+    def func_segment(tile, batch_size):
+
+        tile = np.expand_dims(tile, axis=-1)
+        mask = model.predict(tile, batch_size=batch_size, **eval_parameters)[:, :, :, 0]
+
+        return mask
+
+    func_segment = transform(func_segment, default_batch_size=8)
+
+    return func_segment
+
+
+def deepcell_cytoplasm_segmentation(model_parameters, eval_parameters):
+
+    from deepcell.applications import CytoplasmSegmentation
+
+    model = CytoplasmSegmentation(**model_parameters)
+
+    def func_segment(tile, batch_size):
+
+        tile = np.expand_dims(tile, axis=-1)
+        mask = model.predict(tile, batch_size=batch_size, **eval_parameters)[:, :, :, 0]
+
+        return mask
+
+    func_segment = transform(func_segment, default_batch_size=8)
+
+    return func_segment
