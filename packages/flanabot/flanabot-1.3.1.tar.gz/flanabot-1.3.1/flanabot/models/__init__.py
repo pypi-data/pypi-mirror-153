886d5313aa1c21c6789fffefbcbc0a3a4ce7ed05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:40 +0000

    Add package flanabot/1.3.1/flanabot-1.3.1.tar.gz

---
+++ packages/flanabot/flanabot-1.3.1.tar.gz/flanabot-1.3.1/flanabot/models/__init__.py
@@ -0,0 +1,5 @@
+from flanabot.models.chat import *
+from flanabot.models.enums import *
+from flanabot.models.message import *
+from flanabot.models.punishment import *
+from flanabot.models.weather_chart import *
