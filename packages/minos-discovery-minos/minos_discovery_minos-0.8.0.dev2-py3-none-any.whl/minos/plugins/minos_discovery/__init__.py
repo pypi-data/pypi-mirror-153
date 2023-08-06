2a095c62011faedc30fc9641e15a033b31b17102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:59 +0000

    Add package minos-discovery-minos/0.8.0.dev2/minos_discovery_minos-0.8.0.dev2-py3-none-any.whl

---
+++ packages/minos-discovery-minos/minos_discovery_minos-0.8.0.dev2-py3-none-any.whl/minos/plugins/minos_discovery/__init__.py
@@ -0,0 +1,9 @@
+"""The minos-discovery plugin of the Minos Framework."""
+
+__author__ = "Minos Framework Devs"
+__email__ = "hey@minos.run"
+__version__ = "0.8.0.dev2"
+
+from .client import (
+    MinosDiscoveryClient,
+)
