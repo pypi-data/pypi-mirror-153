cfcc804d889e62ed9281f14ef2c5755ba39f939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:29:44 +0000

    Add package hestia-earth-models/0.27.1/hestia_earth_models-0.27.1-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.27.1-py3-none-any.whl/hestia_earth/models/cycle/feedConversionRatio/feedConversionRatioCarbon.py
@@ -0,0 +1,28 @@
+from hestia_earth.models.utils.input import get_feed
+
+from hestia_earth.models.log import logger
+from . import MODEL
+
+REQUIREMENTS = {
+    "Cycle": {
+        "inputs": [{
+            "@type": "Input",
+            "term.units": "kg",
+            "term.termType": ["crop", "animalProduct", "other"],
+            "properties": [{"@type": "Property", "value": "", "term.@id": "energyContentHigherHeatingValue"}]
+        }]
+    }
+}
+RETURNS = {
+    "Practice": [{
+        "value": "",
+        "statsDefinition": "modelled"
+    }]
+}
+TERM_ID = 'feedConversionRatioCarbon'
+
+
+def run(cycle: dict):
+    feed = get_feed(cycle.get('inputs', []))
+    logger.debug('model=%s, term=%s, feed=%s', MODEL, TERM_ID, feed)
+    return feed * 0.021
