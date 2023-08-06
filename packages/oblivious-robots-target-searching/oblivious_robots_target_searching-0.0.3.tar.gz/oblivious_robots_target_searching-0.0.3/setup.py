3ec3ca26424afd739fed900f8d606c0cda6577c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:51 +0000

    Add package oblivious-robots-target-searching/0.0.3/oblivious_robots_target_searching-0.0.3.tar.gz

---
+++ packages/oblivious-robots-target-searching/oblivious_robots_target_searching-0.0.3.tar.gz/oblivious_robots_target_searching-0.0.3/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='oblivious_robots_target_searching',
+    version='0.0.3',
+    license='MIT',
+    author="Sairyö No Developers",
+    author_email='tech@sairyonodevs.in',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Sairyo-No-Developers/oblivious-robots-target-search',
+    keywords='oblivious robots target searching graph',
+    install_requires=[
+          'networkx',
+          'matplotlib',
+          'uuid',
+          'names',
+          'pytest'
+      ],
+
+)
