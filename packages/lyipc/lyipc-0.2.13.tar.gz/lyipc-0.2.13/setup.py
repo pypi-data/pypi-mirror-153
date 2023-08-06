47b1929b69c14124a3877323ed658783c5397cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:19 +0000

    Add package lyipc/0.2.13/lyipc-0.2.13.tar.gz

---
+++ packages/lyipc/lyipc-0.2.13.tar.gz/lyipc-0.2.13/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+
+def readme():
+    with open('README.md') as fx:
+        return fx.read()
+
+
+setup(name='lyipc',
+      version='0.2.13',
+      description='Inter-process communication for Klayout',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/atait/klayout-ipc',
+      author='Alex Tait',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lyipc', 'lyipc/client'],
+      install_requires=['lygadgets>=0.1.19'],
+      package_data={'': ['*.lym']},
+      include_package_data=True,
+      cmdclass={},
+      )
