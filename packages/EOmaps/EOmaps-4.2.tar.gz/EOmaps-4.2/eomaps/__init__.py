e1ac2457acb7b7435684488e82315a2db9351460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:27 +0000

    Add package EOmaps/4.2/EOmaps-4.2.tar.gz

---
+++ packages/EOmaps/EOmaps-4.2.tar.gz/EOmaps-4.2/eomaps/__init__.py
@@ -0,0 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
+
+__author__ = "Raphael Quast"
+
+
+from .eomaps import Maps, MapsGrid
