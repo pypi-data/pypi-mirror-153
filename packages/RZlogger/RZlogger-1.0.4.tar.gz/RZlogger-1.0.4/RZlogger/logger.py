1765efbddaa17a521c515ec9274e4152213c99c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:05 +0000

    Add package RZlogger/1.0.4/RZlogger-1.0.4.tar.gz

---
+++ packages/RZlogger/RZlogger-1.0.4.tar.gz/RZlogger-1.0.4/RZlogger/logger.py
@@ -0,0 +1,21 @@
+
+
+class logger:
+    def __init__(self, type, filename = ""):
+        self.type = type
+        if filename == "":
+            self.filename = "RZlogger.txt"
+        else:
+            self.filename =  filename
+
+
+    def log(self , data):
+        if self.type == "console":
+            print(data)
+        elif self.type == "file":
+            f = open(self.filename, "a")
+            f.write(data)
+            f.close()
+        else:
+            print(data)
+
