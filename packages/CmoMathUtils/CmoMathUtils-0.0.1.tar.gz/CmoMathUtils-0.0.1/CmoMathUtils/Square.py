c8cac29ec9ed4995b73042d86f93df607210fc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package CmoMathUtils/0.0.1/CmoMathUtils-0.0.1.tar.gz

---
+++ packages/CmoMathUtils/CmoMathUtils-0.0.1.tar.gz/CmoMathUtils-0.0.1/CmoMathUtils/Square.py
@@ -0,0 +1,8 @@
+class Square:
+    def __init__(self):
+        """
+        Initializing code
+        """
+
+    def squareOf(self, num:int) -> int:
+        return num * num
---
+++ packages/CmoMathUtils/CmoMathUtils-0.0.1.tar.gz/CmoMathUtils-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CmoMathUtils",
+    version="0.0.1",
+    author="Sridhar Kondoji",
+    author_email="sridhar.kondoji@am.jll.com",
+    packages=find_packages()
+)
