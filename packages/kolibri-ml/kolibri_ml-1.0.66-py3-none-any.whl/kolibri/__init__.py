5d2b392b15b2b99202fdeea3f81f858d2ea40e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:12 +0000

    Add package kolibri-ml/1.0.66/kolibri_ml-1.0.66-py3-none-any.whl

---
+++ packages/kolibri-ml/kolibri_ml-1.0.66-py3-none-any.whl/kolibri/__init__.py
@@ -0,0 +1,17 @@
+from kolibri.config import register_all
+from kolibri.config import ModelConfig
+from kolibri.model_loader import ModelLoader
+from kolibri.model_trainer import ModelTrainer
+from kolibri.version import __version__
+import mlflow
+import kolibri
+
+
+from os import path, mkdir
+from pathlib import Path
+from github import Github
+
+kolibri.mlflow=mlflow
+
+register_all()
+
