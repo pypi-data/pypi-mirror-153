131636828272f032ac9fd242180a5123ee54060e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:19 +0000

    Add package MaskOff/0.2/MaskOff-0.2.tar.gz

---
+++ packages/MaskOff/MaskOff-0.2.tar.gz/MaskOff-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name='MaskOff',
+      version='0.2',
+      url='https://github.com/ALEX-MGN/Maskoff.git',
+      license='MIT',
+      author='crazyShurik',
+      author_email='pereverzev0701@mail.ru',
+      )
