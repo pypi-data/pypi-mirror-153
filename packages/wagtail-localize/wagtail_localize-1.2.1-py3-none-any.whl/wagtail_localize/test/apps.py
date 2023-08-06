44d3c671e0033569eb940ae0d5202f30ba751c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:28 +0000

    Add package wagtail-localize/1.2.1/wagtail_localize-1.2.1-py3-none-any.whl

---
+++ packages/wagtail-localize/wagtail_localize-1.2.1-py3-none-any.whl/wagtail_localize/test/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WagtailLocalizeTestAppConfig(AppConfig):
+    label = "wagtail_localize_test"
+    name = "wagtail_localize.test"
+    verbose_name = "Wagtail Localize Test app"
+    default_auto_field = "django.db.models.AutoField"
