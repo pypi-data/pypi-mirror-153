bbc81d729810f08d21ec0556ba130333eab1a21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:34 +0000

    Add package fastvalidate/0.0.3/fastvalidate-0.0.3.tar.gz

---
+++ packages/fastvalidate/fastvalidate-0.0.3.tar.gz/fastvalidate-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(
+    name="fastvalidate",
+    version="0.0.3",
+    url="https://github.com/mojtabaahn/fast-validate",
+    author="Mojtabaa Habibain",
+    author_email="mojtabaa.hn@gmail.com",
+    description="Easy validation for python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
