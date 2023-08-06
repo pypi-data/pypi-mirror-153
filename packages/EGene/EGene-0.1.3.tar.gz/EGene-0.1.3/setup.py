94a7e5378c886d9b7925d592c0b94e79f02e9d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:00 +0000

    Add package EGene/0.1.3/EGene-0.1.3.tar.gz

---
+++ packages/EGene/EGene-0.1.3.tar.gz/EGene-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="EGene",
+    version="0.1.3",
+    author="Ethan Anderson",
+    author_email="telan4892@gmail.com",
+    url="https://github.com/Elan456/EGene",
+    install_requires=['pygame'],
+    packages=["egene"],
+    license="MIT",
+    description="Tool for applying, visualizing, and training neural networks using a genetic algorithm",
+)
