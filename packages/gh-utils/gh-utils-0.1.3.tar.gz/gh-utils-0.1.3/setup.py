3627df7d67f62b9a751d74ff30a83768a0ba4c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:11 +0000

    Add package gh-utils/0.1.3/gh-utils-0.1.3.tar.gz

---
+++ packages/gh-utils/gh-utils-0.1.3.tar.gz/gh-utils-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gh_utils']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['ghcrar = gh_utils.gh_create_repo_and_add_to_remote:main']}
+
+setup_kwargs = {
+    'name': 'gh-utils',
+    'version': '0.1.3',
+    'description': 'GitHub CLI Utilities',
+    'long_description': '',
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tddschn/gh-utils',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
