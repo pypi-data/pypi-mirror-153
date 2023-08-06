752c4ad31114351aa41f895211eaf9df0392fd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:17 +0000

    Add package cloudops-google-bigquery/0.1.0/cloudops_google_bigquery-0.1.0.tar.gz

---
+++ packages/cloudops-google-bigquery/cloudops_google_bigquery-0.1.0.tar.gz/cloudops_google_bigquery-0.1.0/cloudops_google_bigquery/__init__.py
@@ -0,0 +1,2 @@
+"""The cloudops_google_bigquery package."""
+__version__ = "0.1.0"
---
+++ packages/cloudops-google-bigquery/cloudops_google_bigquery-0.1.0.tar.gz/cloudops_google_bigquery-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cloudops_google_bigquery']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cloudops-google-bigquery',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Manuel Castillo',
+    'author_email': 'manucalop@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
