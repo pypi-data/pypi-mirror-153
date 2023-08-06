b97fcbd00471e251230413c02c744d268e5b6cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:03 +0000

    Add package dict-and-html/1.0.10/dict_and_html-1.0.10-py3-none-any.whl

---
+++ packages/dict-and-html/dict_and_html-1.0.10-py3-none-any.whl/dict_and_html/modules/strategies/strategy_keys.py
@@ -0,0 +1,26 @@
+###########################
+# strategy_keys.py
+###########################
+def attribute_strategy_for_key(key,
+                               key_strategy: dict = None) -> dict:
+    attributes = {}
+
+    if key_strategy is None:
+        # print("default key strategy", len([key]))
+        len([key])  # just to avoid triggering warnings
+    else:
+        ##
+        # Example of strategy
+        ##
+        # if type(key) == float:
+        #     attributes.update({
+        #         'style': 'font-style: italic;'
+        #     })
+        # else:
+        #     print("Not float: %s" % key)
+        pass
+
+    return attributes
+###########################
+# strategy_keys.py
+###########################
