e8879db38233645f3fe9ee634a5477eec9fd9714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:01 +0000

    Add package multiprocess-kafka-consumer/0.1.9/multiprocess_kafka_consumer-0.1.9.tar.gz

---
+++ packages/multiprocess-kafka-consumer/multiprocess_kafka_consumer-0.1.9.tar.gz/multiprocess_kafka_consumer-0.1.9/multiprocess_kafka_consumer/multiprocess_kafka_consumer.py
@@ -0,0 +1,48 @@
+from kafka import KafkaConsumer
+import multiprocessing as mp
+from queue import Queue
+import json
+
+
+def create_worker_queue(n_worker: int):
+    worker_queue = Queue(n_worker)
+    for i in range(n_worker):
+        worker_queue.put(i)
+    return worker_queue
+
+
+def get_next_worker(worker_queue: Queue):
+    next_worker = worker_queue.get()
+    worker_queue.put(next_worker)
+    return next_worker, worker_queue
+
+
+def multiprocess_kafka_consumer(
+    input_topic: list,
+    group_id: str,
+    n_worker: int,
+    bootstrap_servers: str,
+    callable_function,
+    **kwargs
+):
+    consumer = KafkaConsumer(
+        *input_topic,
+        group_id=group_id,
+        bootstrap_servers=bootstrap_servers,
+        value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+        **kwargs,
+    )
+    message_queue = [mp.Manager().Queue() for i in range(n_worker)]
+    worker_queue = create_worker_queue(n_worker)
+
+    for i in range(n_worker):
+        worker = mp.Process(
+            target=callable_function,
+            args=(message_queue[i],),
+        )
+        worker.daemon = True
+        worker.start()
+
+    for message in consumer:
+        next_worker, worker_queue = get_next_worker(worker_queue)
+        message_queue[next_worker].put(message)
