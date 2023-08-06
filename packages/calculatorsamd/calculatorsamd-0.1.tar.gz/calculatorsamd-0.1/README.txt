5b6698e0767cf1fc9c3ef52d2e26d330fa2baf9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:05 +0000

    Add package calculatorsamd/0.1/calculatorsamd-0.1.tar.gz

---
+++ packages/calculatorsamd/calculatorsamd-0.1.tar.gz/calculatorsamd-0.1/README.txt
@@ -0,0 +1,6 @@
+This is a very basic calculator for addition,subtraction,multiplication and division.
+The commands are operation_num(a,b).
+operations== add_num(a,b)
+    sub_num(a,b)
+    mul_num(a,b)
+    div_num(a,b)
---
+++ packages/calculatorsamd/calculatorsamd-0.1.tar.gz/calculatorsamd-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='calculatorsamd', 
+ version='0.1',
+ author="Aadil Nazir",
+ author_email="daraadi4@gmail.com",
+ description="very basic calculator",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
