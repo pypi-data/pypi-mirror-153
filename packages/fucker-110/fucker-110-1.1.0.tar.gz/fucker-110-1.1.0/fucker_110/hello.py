080e97ec78265585da1fc635440e64216ca9dba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:21 +0000

    Add package fucker-110/1.1.0/fucker-110-1.1.0.tar.gz

---
+++ packages/fucker-110/fucker-110-1.1.0.tar.gz/fucker-110-1.1.0/fucker_110/hello.py
@@ -0,0 +1,3 @@
+
+
+msg = "hell world"
---
+++ packages/fucker-110/fucker-110-1.1.0.tar.gz/fucker-110-1.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fucker_110']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'fucker-110',
+    'version': '1.1.0',
+    'description': '',
+    'long_description': None,
+    'author': '蒋经伟',
+    'author_email': 'jingwei.jiang@mihoyo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
