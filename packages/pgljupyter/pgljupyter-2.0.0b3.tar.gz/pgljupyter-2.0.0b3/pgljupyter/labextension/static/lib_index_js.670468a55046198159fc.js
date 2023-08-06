738481cb729bdad49566959fba698cdb7b74cee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:00 +0000

    Add package pgljupyter/2.0.0b3/pgljupyter-2.0.0b3.tar.gz

---
+++ packages/pgljupyter/pgljupyter-2.0.0b3.tar.gz/pgljupyter-2.0.0b3/pgljupyter/labextension/static/lib_index_js.670468a55046198159fc.js
@@ -0,0 +1,58 @@
+"use strict";
+(self["webpackChunkpgljupyter"] = self["webpackChunkpgljupyter"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "BoolEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.BoolEditorModel),
+/* harmony export */   "BoolEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.BoolEditorView),
+/* harmony export */   "CurveEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.CurveEditorModel),
+/* harmony export */   "CurveEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.CurveEditorView),
+/* harmony export */   "FloatEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.FloatEditorModel),
+/* harmony export */   "FloatEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.FloatEditorView),
+/* harmony export */   "GeomWidget": () => (/* reexport safe */ _widgets_geom__WEBPACK_IMPORTED_MODULE_1__.GeomWidget),
+/* harmony export */   "IntEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.IntEditorModel),
+/* harmony export */   "IntEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.IntEditorView),
+/* harmony export */   "LsystemWidgetModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.LsystemWidgetModel),
+/* harmony export */   "LsystemWidgetView": () => (/* reexport safe */ _widgets_widgets__WEBPACK_IMPORTED_MODULE_2__.LsystemWidgetView),
+/* harmony export */   "MODULE_NAME": () => (/* reexport safe */ _version__WEBPACK_IMPORTED_MODULE_0__.MODULE_NAME),
+/* harmony export */   "MODULE_VERSION": () => (/* reexport safe */ _version__WEBPACK_IMPORTED_MODULE_0__.MODULE_VERSION),
+/* harmony export */   "MaterialEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.MaterialEditorModel),
+/* harmony export */   "MaterialEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.MaterialEditorView),
+/* harmony export */   "PGLWidgetModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.PGLWidgetModel),
+/* harmony export */   "PGLWidgetView": () => (/* reexport safe */ _widgets_widgets__WEBPACK_IMPORTED_MODULE_2__.PGLWidgetView),
+/* harmony export */   "ParameterEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.ParameterEditorModel),
+/* harmony export */   "ParameterEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.ParameterEditorView),
+/* harmony export */   "SceneWidgetModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.SceneWidgetModel),
+/* harmony export */   "SceneWidgetView": () => (/* reexport safe */ _widgets_widgets__WEBPACK_IMPORTED_MODULE_2__.SceneWidgetView),
+/* harmony export */   "StringEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__.StringEditorModel),
+/* harmony export */   "StringEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__.StringEditorView),
+/* harmony export */   "_CurveEditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__._CurveEditorModel),
+/* harmony export */   "_CurveEditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__._CurveEditorView),
+/* harmony export */   "_EditorModel": () => (/* reexport safe */ _widgets_models__WEBPACK_IMPORTED_MODULE_4__._EditorModel),
+/* harmony export */   "_EditorView": () => (/* reexport safe */ _widgets_editors__WEBPACK_IMPORTED_MODULE_3__._EditorView),
+/* harmony export */   "rendererFactory": () => (/* reexport safe */ _widgets_geom__WEBPACK_IMPORTED_MODULE_1__.rendererFactory)
+/* harmony export */ });
+/* harmony import */ var _version__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./version */ "./lib/version.js");
+/* harmony import */ var _widgets_geom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./widgets/geom */ "./lib/widgets/geom.js");
+/* harmony import */ var _widgets_widgets__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./widgets/widgets */ "./lib/widgets/widgets.js");
+/* harmony import */ var _widgets_editors__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./widgets/editors */ "./lib/widgets/editors.js");
+/* harmony import */ var _widgets_models__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./widgets/models */ "./lib/widgets/models.js");
+/* harmony import */ var _labplugin__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./labplugin */ "./lib/labplugin.js");
+
+
+
+
+
+
+//# sourceMappingURL=index.js.map
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.670468a55046198159fc.js.map
