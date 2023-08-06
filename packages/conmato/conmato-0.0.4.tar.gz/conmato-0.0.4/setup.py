b450b3bd65a31c940788ec505fe72e61a6dee606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:29 +0000

    Add package conmato/0.0.4/conmato-0.0.4.tar.gz

---
+++ packages/conmato/conmato-0.0.4.tar.gz/conmato-0.0.4/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/ngocjr7/codeforces-management-tools/issues',
+    'Documentation': 'https://github.com/ngocjr7/codeforces-management-tools/blob/master/README.md',
+    'Source Code': 'https://github.com/ngocjr7/codeforces-management-tools'
+}
+
+f = """
+beautifulsoup4
+certifi
+chardet
+cssselect
+idna
+lxml
+mosspy
+numpy
+pandas
+pydot
+pyparsing
+pyquery
+python-dateutil
+pytz
+requests
+six
+soupsieve
+urllib3
+openpyxl
+jinja2>=2.11.3
+xlrd
+click
+pyyaml
+tqdm
+appdirs
+scoss
+"""
+
+# with open('./requirements.txt') as f:
+install_requires = f.strip().split('\n')
+
+setup(name='conmato',
+      description='A Command Line Interface for Codeforces Management Tools',
+      author='Ngoc Bui',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      project_urls=PROJECT_URLS,
+      author_email='ngocjr7@gmail.com',
+      version='0.0.4',
+      entry_points='''
+        [console_scripts]
+        conmato=conmato.cli:cli
+      ''',
+      packages=find_packages(),
+      install_requires=install_requires,
+      python_requires='>=3.6')
+
