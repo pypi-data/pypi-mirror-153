2179b16603018c6a619624b9179dcb11fd292cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:45 +0000

    Add package ast-selector/0.1.0/ast_selector-0.1.0.tar.gz

---
+++ packages/ast-selector/ast_selector-0.1.0.tar.gz/ast_selector-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ast_selector']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ast-selector',
+    'version': '0.1.0',
+    'description': 'Easily find and query AST elements by using CSS Selector-like syntax',
+    'long_description': '# AST Selector\n',
+    'author': 'Guilherme Latrova',
+    'author_email': 'hello@guilatrova.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/guilatrova/ast_selector',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
