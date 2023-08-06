4f4051f3a8d22c36fa8c137110d4d50d9f09e513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:13 +0000

    Add package susc/1.5.4/susc-1.5.4.tar.gz

---
+++ packages/susc/susc-1.5.4.tar.gz/susc-1.5.4/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+setup(
+    name="susc",
+    version="1.5.4",
+    description="SpeedAPI schema compiler",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/speedapi/susc",
+    author="portasynthinca3",
+    author_email="hi@portasynthinca3.me",
+    license="BSD 3-Clause",
+    packages=["susc"],
+    keywords=["api", "protocol", "language"],
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': ['susc=susc.__main__:main'],
+    }
+)
