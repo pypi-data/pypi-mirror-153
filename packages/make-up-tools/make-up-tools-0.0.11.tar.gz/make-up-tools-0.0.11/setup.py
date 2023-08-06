7069c2f1eaa19206fd4b94edc7ce277a352a8e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:50 +0000

    Add package make-up-tools/0.0.11/make-up-tools-0.0.11.tar.gz

---
+++ packages/make-up-tools/make-up-tools-0.0.11.tar.gz/make-up-tools-0.0.11/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='make-up-tools',
+      version='0.0.11',
+      description='just for job',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type="text/markdown",
+      author='NA',
+      author_email='martinlord@foxmail.com',
+      url='https://gitee.com/mahaoyang/make_up',
+      packages=find_packages(
+          include=[
+              'make_up_tools*',
+          ]
+      ),
+      package_data={
+          '': ['*.txt'],
+      },
+      include_package_data=True,
+      license='LICENSE.txt',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
