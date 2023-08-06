556fa47cec6d9aa02cb4dd1d67ba7b88672f5d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:50 +0000

    Add package pymince/1.0.2/pymince-1.0.2.tar.gz

---
+++ packages/pymince/pymince-1.0.2.tar.gz/pymince-1.0.2/pymince/logging.py
@@ -0,0 +1,23 @@
+import contextlib
+import logging
+import time
+
+
+@contextlib.contextmanager
+def timed_block(name):
+    """
+    Example:
+
+    >> logging.basicConfig(level=logging.DEBUG)
+    >> with timed_block("sleeping"):
+        >> time.sleep(1)
+
+    INFO:root:Generating [sleeping]
+    DEBUG:root:Finished [sleeping in 1.002 ms.]
+    """
+    logging.info('Generating [%s]', name)
+    t0 = time.time()
+    try:
+        yield None
+    finally:
+        logging.debug('Finished [%s in %.3f ms.]', name, time.time() - t0)
