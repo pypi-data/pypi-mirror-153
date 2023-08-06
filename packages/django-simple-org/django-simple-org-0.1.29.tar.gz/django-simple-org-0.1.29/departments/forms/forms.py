515c044a3cac078d0127730b9a136c6986ec7fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:01 +0000

    Add package django-simple-org/0.1.29/django-simple-org-0.1.29.tar.gz

---
+++ packages/django-simple-org/django-simple-org-0.1.29.tar.gz/django-simple-org-0.1.29/departments/forms/forms.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@File        : forms.py
+@Author      : yu wen yang
+@Time        : 2022/5/5 5:38 下午
+@Description :
+"""
+from django import forms
+
+
+class DepartmentForms(forms.Form):
+    name = forms.CharField(min_length=2, max_length=32)
+    charger_id = forms.IntegerField(required=False)
+    parent_id = forms.IntegerField(required=False)
+    organization_id = forms.IntegerField()
+
+
+class MoveDepartmentIndexForms(forms.Form):
+    department_id = forms.IntegerField()
+    type = forms.CharField(min_length=2, max_length=4)
+
---
+++ packages/django-simple-org/django-simple-org-0.1.29.tar.gz/django-simple-org-0.1.29/departments/views/roles.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@File        : roles.py
+@Author      : yu wen yang
+@Time        : 2022/5/13 9:49 上午
+@Description :
+"""
