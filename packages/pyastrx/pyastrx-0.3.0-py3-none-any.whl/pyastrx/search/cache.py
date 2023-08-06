e62f54c580a97feb13781124c8cf03f135c4a57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:10 +0000

    Add package pyastrx/0.3.0/pyastrx-0.3.0-py3-none-any.whl

---
+++ packages/pyastrx/pyastrx-0.3.0-py3-none-any.whl/pyastrx/search/cache.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Dict, Tuple
+
+from pyastrx.data_typing import FileInfo
+
+
+class Cache:
+    """
+    A file cache for pyastrx.
+    """
+    def __init__(self) -> None:
+        self._cache: Dict[str, FileInfo] = {}
+
+    def update(self, filename: str) -> Tuple[bool, float]:
+        """ If the cache should be updated or not
+        """
+        file_path = Path(filename)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File '{filename}' not found.")
+
+        last_modified = file_path.stat().st_mtime
+        if filename not in self._cache.keys():
+            return True, last_modified
+        info = self.get(filename)
+        # check cache
+        if info.last_modified == last_modified:
+            return False, last_modified
+        return True, last_modified
+
+    def get(self, filename: str) -> FileInfo:
+        """
+        Get a value from the cache.
+        """
+        return self._cache[filename]
+
+    def set(
+            self, filename: str,
+            file_info: FileInfo) -> None:
+        """
+        Set a value in the cache.
+        """
+        self._cache[filename] = file_info
