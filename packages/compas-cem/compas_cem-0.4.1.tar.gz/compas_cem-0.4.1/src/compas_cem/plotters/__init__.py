c6e4b2d3b12a51e21e7e567080811285291cec3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package compas-cem/0.4.1/compas_cem-0.4.1.tar.gz

---
+++ packages/compas-cem/compas_cem-0.4.1.tar.gz/compas_cem-0.4.1/src/compas_cem/plotters/__init__.py
@@ -0,0 +1,29 @@
+"""
+compas_cem.plotters
+****************************
+
+.. currentmodule:: compas_cem.plotters
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    FormArtist
+    TopologyArtist
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+# from .<module> import *
+from .formartist import *  # noqa F403
+from .topologyartist import *  # noqa F403
+# from .proxy import *  # noqa F403
+
+__all__ = [name for name in dir() if not name.startswith('_')]
