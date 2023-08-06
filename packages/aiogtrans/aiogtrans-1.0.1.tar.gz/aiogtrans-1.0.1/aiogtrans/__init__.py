84e9039805f73cb0789a12d784147e9c18dd5fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:45 +0000

    Add package aiogtrans/1.0.1/aiogtrans-1.0.1.tar.gz

---
+++ packages/aiogtrans/aiogtrans-1.0.1.tar.gz/aiogtrans-1.0.1/aiogtrans/__init__.py
@@ -0,0 +1,8 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '1.0.1'
+
+
+from aiogtrans.client import Translator
+from aiogtrans.constants import LANGCODES, LANGUAGES
+from aiogtrans.models import Translated, Detected
