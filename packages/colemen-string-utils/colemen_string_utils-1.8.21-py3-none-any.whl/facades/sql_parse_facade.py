a27d8f15f073aa87d499adf67ea0a14dce661f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package colemen-string-utils/1.8.21/colemen_string_utils-1.8.21-py3-none-any.whl

---
+++ packages/colemen-string-utils/colemen_string_utils-1.8.21-py3-none-any.whl/facades/sql_parse_facade.py
@@ -0,0 +1,5 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+
+from utils.parse_sql import strip_sql_quotes,strip_comments,escape_quoted_chars,parse
