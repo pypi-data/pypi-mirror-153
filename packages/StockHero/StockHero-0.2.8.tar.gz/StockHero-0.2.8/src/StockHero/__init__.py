1541ec59ff29d46e7e1142d851c3aecd5695e363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:10 +0000

    Add package StockHero/0.2.8/StockHero-0.2.8.tar.gz

---
+++ packages/StockHero/StockHero-0.2.8.tar.gz/StockHero-0.2.8/src/StockHero/__init__.py
@@ -0,0 +1,6 @@
+from .StockHero import Ticker
+from .StockHero import StockExchange
+from .StockHero import StockValuation
+
+from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __author__, __author_email__, __license__, __copyright__, __thanks__
