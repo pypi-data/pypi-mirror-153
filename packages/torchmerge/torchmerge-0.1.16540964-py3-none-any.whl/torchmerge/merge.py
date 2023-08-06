39d3a7b9d0317bd913c9737710ae45f76f619e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:07 +0000

    Add package torchmerge/0.1.16540964/torchmerge-0.1.16540964-py3-none-any.whl

---
+++ packages/torchmerge/torchmerge-0.1.16540964-py3-none-any.whl/torchmerge/merge.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+""" Main entry
+
+
+
+"""
+import os, sys, time, datetime,inspect, json,gc, random
+from box import Box
+from utilmy.deeplearning.ttorch.model_ensemble import *
+
+
+
+
+
+###################################################################################################
+if __name__ == "__main__":
+    import fire ;
+    fire.Fire()
+
+
+
+
