7a3827de27c4cac8002afb5c780e7d583f070f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:43 +0000

    Add package J-Guesser-game/0.0.1/J_Guesser_game-0.0.1.tar.gz

---
+++ packages/J-Guesser-game/J_Guesser_game-0.0.1.tar.gz/J_Guesser_game-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="J_Guesser_game",
+    version="0.0.1",
+    author="Author's name",
+    description="Description's package",
+    packages=["J_Guesser_game"]
+)
