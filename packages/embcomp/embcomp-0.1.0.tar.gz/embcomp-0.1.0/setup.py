c5897ae9990bed556531c1eb93614e095ac0f324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:28 +0000

    Add package embcomp/0.1.0/embcomp-0.1.0.tar.gz

---
+++ packages/embcomp/embcomp-0.1.0.tar.gz/embcomp-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['embcomp']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch']
+
+setup_kwargs = {
+    'name': 'embcomp',
+    'version': '0.1.0',
+    'description': 'Composition of embeddings',
+    'long_description': None,
+    'author': 'Damien Sileo',
+    'author_email': 'damien.sileo@kuleuven.be',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)
