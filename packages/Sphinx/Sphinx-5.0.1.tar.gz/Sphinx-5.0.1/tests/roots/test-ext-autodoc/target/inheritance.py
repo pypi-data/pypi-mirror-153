b04f6df95644e17fa9d6874fab09a03229dc957c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:21 +0000

    Add package Sphinx/5.0.1/Sphinx-5.0.1.tar.gz

---
+++ packages/Sphinx/Sphinx-5.0.1.tar.gz/Sphinx-5.0.1/tests/roots/test-ext-autodoc/target/inheritance.py
@@ -0,0 +1,22 @@
+class Base(object):
+    def inheritedmeth(self):
+        """Inherited function."""
+
+    @classmethod
+    def inheritedclassmeth(cls):
+        """Inherited class method."""
+
+    @staticmethod
+    def inheritedstaticmeth(cls):
+        """Inherited static method."""
+
+
+class Derived(Base):
+    def inheritedmeth(self):
+        # no docstring here
+        pass
+
+
+class MyList(list):
+    def meth(self):
+        """docstring"""
