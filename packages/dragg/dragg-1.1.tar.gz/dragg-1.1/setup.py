302be21b51950d6febcd5aa47e91f0b6bd81a6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:29 +0000

    Add package dragg/1.1/dragg-1.1.tar.gz

---
+++ packages/dragg/dragg-1.1.tar.gz/dragg-1.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='dragg',
+      license='MIT',
+      version='1.1',
+      author='Aisling Pigott and Cory Mosiman',
+      author_email='aisling.pigott@colorado.edu',
+      packages=find_packages(),
+      scripts=["dragg/main.py"],
+      install_requires=[
+        'redis',
+        'pathos',
+        'logging'
+        'cvxpy',
+        'numpy',
+        'collections',
+        'itertools',
+        'pandas',
+        'prettytable',
+        'datetime',
+        'plotly',
+        'sys',
+        'toml',
+        'random',
+        'names'],
+      py_modules=['dragg'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+     )
