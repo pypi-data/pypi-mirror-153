f5fbfa934aad5009bb6d05497c090bae2e376b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:24 +0000

    Add package dash-mp-components/0.4.29/dash_mp_components-0.4.29.tar.gz

---
+++ packages/dash-mp-components/dash_mp_components-0.4.29.tar.gz/dash_mp_components-0.4.29/dash_mp_components/PeriodicElement.py
@@ -0,0 +1,33 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class PeriodicElement(Component):
+    """A PeriodicElement component.
+This component displays a periodic element
+
+Keyword arguments:
+
+- id (string; optional)
+
+- element (string; optional)
+
+- size (number; optional)"""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, size=Component.UNDEFINED, element=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'element', 'size']
+        self._type = 'PeriodicElement'
+        self._namespace = 'dash_mp_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'element', 'size']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(PeriodicElement, self).__init__(**args)
