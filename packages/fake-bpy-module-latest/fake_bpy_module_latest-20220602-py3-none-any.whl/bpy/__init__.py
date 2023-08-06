52ce273c7864cd1652e8ea3eecaa90790da111a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:43 +0000

    Add package fake-bpy-module-latest/20220602/fake_bpy_module_latest-20220602-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220602-py3-none-any.whl/bpy/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import typing
+import bpy.types
+
+from . import ops
+from . import types
+from . import app
+from . import props
+from . import utils
+from . import path
+from . import msgbus
+from . import context
+
+data: 'bpy.types.BlendData' = None
+''' Access to Blender's internal data
+'''
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220602-py3-none-any.whl/gpu/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import select
+from . import types
+from . import texture
+from . import capabilities
+from . import platform
+from . import state
+from . import matrix
+from . import shader
