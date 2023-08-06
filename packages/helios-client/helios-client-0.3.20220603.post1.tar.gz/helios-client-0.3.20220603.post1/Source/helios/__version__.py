7e3b70dbcd09dd5fce32b71ab97021fcab8c11a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:36 +0000

    Add package helios-client/0.3.20220603.post1/helios-client-0.3.20220603.post1.tar.gz

---
+++ packages/helios-client/helios-client-0.3.20220603.post1.tar.gz/helios-client-0.3.20220603.post1/Source/helios/__version__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#
+#   Helios, intelligent music.
+#   Copyright (C) 2015-2022 Cartesian Theatre. All rights reserved.
+#
+
+# Module version...
+#  <https://www.python.org/dev/peps/pep-0440/>
+version = '0.3.20220603.post1'
