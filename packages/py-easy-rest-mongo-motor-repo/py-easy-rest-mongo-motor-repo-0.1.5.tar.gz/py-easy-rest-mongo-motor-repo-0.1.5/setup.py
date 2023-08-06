405b4cc1742edda51cbab511759d0a0f4f42c2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:42 +0000

    Add package py-easy-rest-mongo-motor-repo/0.1.5/py-easy-rest-mongo-motor-repo-0.1.5.tar.gz

---
+++ packages/py-easy-rest-mongo-motor-repo/py-easy-rest-mongo-motor-repo-0.1.5.tar.gz/py-easy-rest-mongo-motor-repo-0.1.5/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-easy-rest-mongo-motor-repo",
+    version="0.1.5",
+    author="Jean Pinzon",
+    author_email="jean.pinzon1@gmail.com",
+    description="Mongo repo to use with Py Easy REST",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(
+        exclude=(
+            'tests',
+        ),
+    ),
+    install_requires=[
+        "motor==3.0.0",
+    ],
+    extras_require={
+        'tests': [
+            "pytest==7.1.2",
+            "pytest-asyncio==0.18.3",
+            "pytest-cov==3.0.0",
+            "flake8==4.0.1",
+            "aiounittest==1.4.1",
+            "twine==4.0.0",
+            "build==0.7.0",
+        ],
+    },
+    python_requires='>=3.8',
+)
