0ba52be79fbdc65804a51bfb6934bbfab068b811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:47 +0000

    Add package oresat-olaf/0.3.0/oresat-olaf-0.3.0.tar.gz

---
+++ packages/oresat-olaf/oresat-olaf-0.3.0.tar.gz/oresat-olaf-0.3.0/olaf/resources/fwrite.py
@@ -0,0 +1,67 @@
+from os import remove, listdir
+from os.path import basename
+from pathlib import Path
+from enum import IntEnum, auto
+
+import canopen
+from loguru import logger
+
+from ..common.resource import Resource
+from ..common.oresat_file_cache import OreSatFileCache
+
+
+class Subindex(IntEnum):
+    FILE_NAME = auto()
+    FILE_DATA = auto()
+
+
+class FwriteResource(Resource):
+    '''Resource for writing files over the CAN bus'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.tmp_dir = '/tmp/oresat/fwrite'
+        Path(self.tmp_dir).mkdir(parents=True, exist_ok=True)
+        logger.debug(f'fread tmp dir is {self.tmp_dir}')
+        for i in listdir(self.tmp_dir):
+            remove(f'{self.tmp_dir}/{i}')
+
+        self.index = 0x3004
+
+        self.file_path = ''
+
+    def on_read(self, index, subindex, od):
+
+        ret = None
+
+        if index == self.index and self.file_path and subindex == Subindex.FILE_NAME:
+            ret = basename(self.file_path)
+
+        return ret
+
+    def on_write(self, index, subindex, od, data):
+
+        if index != self.index:
+            return
+
+        if subindex == Subindex.FILE_NAME:
+            file_name = data.decode()
+            self.file_path = self.tmp_dir + '/' + file_name
+        elif subindex == Subindex.FILE_DATA:
+            if not self.file_path:
+                logger.error('fwrite file path was not set before file data was sent')
+                return
+
+            try:
+                with open(self.file_path, 'wb') as f:
+                    f.write(data)
+                logger.info(f'receive new file: {basename(self.file_path)}')
+                self.fwrite_cache.add(self.file_path, consume=True)
+            except FileNotFoundError as exc:
+                logger.error(exc)
+
+            self.file_path = ''
+
+            # clear buffers to not waste memory
+            self.od[index][subindex].value = ''
---
+++ packages/oresat-olaf/oresat-olaf-0.3.0.tar.gz/oresat-olaf-0.3.0/olaf/resources/logs.py
@@ -0,0 +1,37 @@
+import tarfile
+from os import geteuid, listdir
+from time import time, clock_settime, CLOCK_REALTIME
+
+import canopen
+from loguru import logger
+
+from ..common.resource import Resource
+from ..common.oresat_file import new_oresat_file
+from ..common.oresat_file_cache import OreSatFileCache
+
+
+class LogsResource(Resource):
+    '''Resource for getting logs'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.delay = 1.0
+
+        self.index = 0x3006
+        self.logs_dir_path = '/var/log/journal/'
+        self.obj = self.od[self.index]
+
+    def on_loop(self):
+
+        if self.obj.value:
+            self.obj.value = False
+            logger.info('Making a copy of logs')
+
+            tar_file_path = '/tmp/' + new_oresat_file('logs', ext='.tar.xz')
+
+            with tarfile.open(tar_file_path, 'w:xz') as t:
+                for i in listdir(self.logs_dir_path):
+                    t.add(self.logs_dir_path + '/' + i, arcname=i)
+
+            self.fread_cache.add(tar_file_path, consume=True)
