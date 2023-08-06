ebaf52f935db7504fe64ebfc30b9f17d2065e598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:29 +0000

    Add package datazen/3.0.4/datazen-3.0.4.tar.gz

---
+++ packages/datazen/datazen-3.0.4.tar.gz/datazen-3.0.4/datazen_bootstrap/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=2.1.0
+# hash=588e428a54dabc062a7f6642629bb0c1
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A simple interface to setuptools's setup function."
+PKG_NAME = "setuptools-wrapper"
+VERSION = "0.1.1"
