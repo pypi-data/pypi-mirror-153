782bc863b206f920e77e1c177d5105e3da191fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:06 +0000

    Add package pyyandexlms/0.2.1/PyYandexLMS-0.2.1-py3-none-any.whl

---
+++ packages/pyyandexlms/PyYandexLMS-0.2.1-py3-none-any.whl/PyYandexLMS/models/task.py
@@ -0,0 +1,22 @@
+from typing import List, Union, Optional
+
+from PyYandexLMS.models.base.main import BaseModel
+from PyYandexLMS.models.base.task import BaseTask
+from PyYandexLMS.models.submission import Submission
+from PyYandexLMS.models.variant import Variant
+
+
+class Task(BaseTask):
+    variant: Variant
+    description: str
+    compilers_ids: List[int]
+    solution_id: int
+    latest_submission: Union[Submission, None]
+
+
+class TaskType(BaseModel):
+    id: int
+    type: str
+    color: str
+    order: int
+    tasks: Optional[List[BaseTask]]
