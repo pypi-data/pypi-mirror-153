80d304672905f655663a129542b55368ad955f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:45 +0000

    Add package odoo12-addon-somconnexio/12.0.1.9.2/odoo12-addon-somconnexio-12.0.1.9.2.tar.gz

---
+++ packages/odoo12-addon-somconnexio/odoo12-addon-somconnexio-12.0.1.9.2.tar.gz/odoo12-addon-somconnexio-12.0.1.9.2/odoo/addons/somconnexio/services/contract_iban_change_service.py
@@ -0,0 +1,7 @@
+class ContractIbanChangeService:
+    def __init__(self, env):
+        self.env = env
+
+    def run_from_api(self, **params):
+        self.env['contract.iban.change.wizard'].with_delay().run_from_api(**params)
+        return {"result": "OK"}
