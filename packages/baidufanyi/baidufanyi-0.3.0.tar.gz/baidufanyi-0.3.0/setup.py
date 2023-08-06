11f13365dad49bb04c62b51bdbbe20aafd27eee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:55 +0000

    Add package baidufanyi/0.3.0/baidufanyi-0.3.0.tar.gz

---
+++ packages/baidufanyi/baidufanyi-0.3.0.tar.gz/baidufanyi-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="baidufanyi",
+    version="0.3.0",
+    author="xuanzhi33",
+    author_email="xuanzhi33@qq.com",
+    url="https://github.com/xuanzhi33/baidufanyi",
+    description="A simple SDK for the Baidu Translation API (unofficial)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPL-3.0",
+    install_requires=["requests"],
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
