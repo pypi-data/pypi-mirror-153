a938707dca911755716886f33e6c07942f4613b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:22 +0000

    Add package jsw-bio/1.0.8/jsw-bio-1.0.8.tar.gz

---
+++ packages/jsw-bio/jsw-bio-1.0.8.tar.gz/jsw-bio-1.0.8/jsw_bio/base/keyword.py
@@ -0,0 +1,17 @@
+import nltk
+
+
+def keyword(in_string, **kwargs):
+    sentences = nltk.sent_tokenize(in_string)
+    selector = kwargs.get('selector', lambda item: 'NNP' == item[1])
+    callback = kwargs.get('callback', lambda item: item[0].lower())
+    res = []
+    data = []
+
+    for sent in sentences:
+        data = data + nltk.pos_tag(nltk.word_tokenize(sent))
+    for word in data:
+        if selector(word):
+            res.append(callback(word))
+
+    return res
