1865060557ef35ae1e86b7f9fe625164797d910c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package cloudops-google-secretmanager/0.1.0/cloudops-google-secretmanager-0.1.0.tar.gz

---
+++ packages/cloudops-google-secretmanager/cloudops-google-secretmanager-0.1.0.tar.gz/cloudops-google-secretmanager-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cloudops_google_secretmanager']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-secret-manager>=2.11.0,<3.0.0', 'google-crc32c>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cloudops-google-secretmanager',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Manuel Castillo',
+    'author_email': 'manucalop@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
