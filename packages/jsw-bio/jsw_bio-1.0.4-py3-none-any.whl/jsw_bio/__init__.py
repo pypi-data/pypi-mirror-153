351a98be08b0ed79da506624043310965270e944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:59 +0000

    Add package jsw-bio/1.0.4/jsw_bio-1.0.4-py3-none-any.whl

---
+++ packages/jsw-bio/jsw_bio-1.0.4-py3-none-any.whl/jsw_bio/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+
+version = pkg_resources.get_distribution('jsw-bio').version
+__version__ = version
+
+# next base
+from jsw_bio.base.url import url
+from jsw_bio.base.keyword import keyword
+from jsw_bio.ncbi.ncbi_download_accids import ncbi_download_accids
