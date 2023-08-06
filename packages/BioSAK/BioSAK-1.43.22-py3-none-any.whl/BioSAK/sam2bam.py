2ed06f1321d188edc56f83022427323f2c39eda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:29 +0000

    Add package BioSAK/1.43.22/BioSAK-1.43.22-py3-none-any.whl

---
+++ packages/BioSAK/BioSAK-1.43.22-py3-none-any.whl/BioSAK/sam2bam.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+from BioSAK.global_functions import sep_path_basename_ext
+
+
+sam2bam_usage = '''
+========== sam2bam example command ==========
+
+module load samtools/1.15
+BioSAK sam2bam -sam input.sam -t 12
+
+=============================================
+'''
+
+
+def sam2bam(args):
+
+    sam_in     = args['sam']
+    thread_num = args['t']
+
+    sam_path, sam_basename, sam_ext = sep_path_basename_ext(sam_in)
+
+    pwd_bam         = '%s/%s.bam'           % (sam_path, sam_basename)
+    pwd_bam_sorted  = '%s/%s_sorted.bam'    % (sam_path, sam_basename)
+
+    cmd_samtools_view   = 'samtools view -bS %s -o %s --threads %s'  % (sam_in, pwd_bam, thread_num)
+    cmd_samtools_sort   = 'samtools sort %s -o %s --threads %s'      % (pwd_bam, pwd_bam_sorted, thread_num)
+    cmd_samtools_index  = 'samtools index %s --threads %s'           % (pwd_bam_sorted, thread_num)
+
+    os.system(cmd_samtools_view)
+    os.system(cmd_samtools_sort)
+    os.system(cmd_samtools_index)
+
+    os.system('rm %s' % pwd_bam)
+
+
+if __name__ == '__main__':
+
+    sam2bam_parser = argparse.ArgumentParser(usage=sam2bam_usage)
+    sam2bam_parser.add_argument('-sam', required=True,                       help='sam file')
+    sam2bam_parser.add_argument('-t',   required=False, type=int, default=1, help='number of threads')
+    args = vars(sam2bam_parser.parse_args())
+    sam2bam(args)
