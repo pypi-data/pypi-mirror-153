9b8af51a0e0253892cd0c3742448308940283804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:49 +0000

    Add package django-data-history/0.1.2/django_data_history-0.1.2-py3-none-any.whl

---
+++ packages/django-data-history/django_data_history-0.1.2-py3-none-any.whl/django_data_history/middlewares.py
@@ -0,0 +1,16 @@
+import uuid
+from django.conf import settings
+
+class HttpXRequestIdMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.header_name = getattr(settings, "REQUEST_ID_HEADER", "HTTP_X_REQUEST_ID")
+        self.auto_generate_request_id = getattr(settings, "AUTO_GENERATE_REQUEST_ID", True)
+
+    def __call__(self, request, *args, **kwargs):
+        http_x_request_id = request.META.get(self.header_name, None)
+        if (http_x_request_id is None) and self.auto_generate_request_id:
+            http_x_request_id = uuid.uuid4().hex
+        setattr(request, "request_id", http_x_request_id)
+        return self.get_response(request, *args, **kwargs)
