20340396e9ff8c4c95029d81c537b18b28818c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:32 +0000

    Add package SigProfilerAssignment/0.0.6/SigProfilerAssignment-0.0.6.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.6.tar.gz/SigProfilerAssignment-0.0.6/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.6'
+version = '0.0.6'
+Update = 'New Release. 1.Bug Fixes with differnt context types 2. Updated documentation 3. Included signature subgroups'
+    
+    
