45d2d7e16c4da8b4a257f00d92511fb77ba94517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:02 +0000

    Add package straindesign/1.2/straindesign-1.2.tar.gz

---
+++ packages/straindesign/straindesign-1.2.tar.gz/straindesign-1.2/straindesign/__init__.py
@@ -0,0 +1,26 @@
+from importlib.util import find_spec as module_exists
+
+avail_solvers = []
+if module_exists("swiglpk"):
+    avail_solvers += ["glpk"]
+if module_exists("cplex"):
+    avail_solvers += ["cplex"]
+if module_exists("gurobipy"):
+    avail_solvers += ["gurobi"]
+if module_exists("pyscipopt"):
+    avail_solvers += ["scip"]
+
+from .names import *
+from .solver_interface import *
+from .indicatorConstraints import *
+from .pool import *
+from .efmtool import *
+from .parse_constr import *
+from .lptools import *
+from .networktools import *
+from .strainDesignModule import *
+from .strainDesignSolution import *
+from .strainDesignMILPBuilder import *
+from .strainDesignMILP import *
+from .strainDesigner import *
+from .compute_strain_designs import *
