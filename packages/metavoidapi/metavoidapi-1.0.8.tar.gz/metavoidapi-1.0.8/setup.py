f3bba8c43234948755878722ad279f6b33ad8699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:31 +0000

    Add package metavoidapi/1.0.8/metavoidapi-1.0.8.tar.gz

---
+++ packages/metavoidapi/metavoidapi-1.0.8.tar.gz/metavoidapi-1.0.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as txt:
+    long_description = txt.read()
+
+setuptools.setup(
+    name='metavoidapi',
+    version='1.0.8',
+    description='Metavoid Api Wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='metavoid',
+    author_email='metavoidteam@gmail.com',
+    url='https://github.com/metavoidteam/metavoidapi-py.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    install_requires= ['requests'],
+    python_requires='>=3.6'
+)
