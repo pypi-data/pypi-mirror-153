3e6101453dd858be869da15e352e8a3613f00113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:33 +0000

    Add package datafusion/0.6.0/datafusion-0.6.0.tar.gz

---
+++ packages/datafusion/datafusion-0.6.0.tar.gz/datafusion-0.6.0/src/lib.rs
@@ -0,0 +1,56 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use mimalloc::MiMalloc;
+use pyo3::prelude::*;
+
+pub mod catalog;
+mod context;
+mod dataframe;
+pub mod errors;
+mod expression;
+mod functions;
+mod udaf;
+mod udf;
+pub mod utils;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+/// Low-level DataFusion internal package.
+///
+/// The higher-level public API is defined in pure python files under the
+/// datafusion directory.
+#[pymodule]
+fn _internal(py: Python, m: &PyModule) -> PyResult<()> {
+    // Register the python classes
+    m.add_class::<catalog::PyCatalog>()?;
+    m.add_class::<catalog::PyDatabase>()?;
+    m.add_class::<catalog::PyTable>()?;
+    m.add_class::<context::PySessionContext>()?;
+    m.add_class::<dataframe::PyDataFrame>()?;
+    m.add_class::<expression::PyExpr>()?;
+    m.add_class::<udf::PyScalarUDF>()?;
+    m.add_class::<udaf::PyAggregateUDF>()?;
+
+    // Register the functions as a submodule
+    let funcs = PyModule::new(py, "functions")?;
+    functions::init_module(funcs)?;
+    m.add_submodule(funcs)?;
+
+    Ok(())
+}
