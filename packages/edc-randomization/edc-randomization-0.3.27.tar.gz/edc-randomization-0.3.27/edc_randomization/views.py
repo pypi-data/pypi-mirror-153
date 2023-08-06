a7118dbf3b532a3f9366a6158273971c0a38c0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:36 +0000

    Add package edc-randomization/0.3.27/edc-randomization-0.3.27.tar.gz

---
+++ packages/edc-randomization/edc-randomization-0.3.27.tar.gz/edc-randomization-0.3.27/edc_randomization/views.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.views.generic.base import TemplateView
+from edc_dashboard.view_mixins import EdcViewMixin
+from edc_navbar import NavbarViewMixin
+
+
+class HomeView(EdcViewMixin, NavbarViewMixin, TemplateView):
+
+    template_name = f"edc_randomization/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
+    navbar_name = "edc_randomization"
+    navbar_selected_item = "edc_randomization"
