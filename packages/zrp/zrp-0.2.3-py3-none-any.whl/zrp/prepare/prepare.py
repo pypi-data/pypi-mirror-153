e2428c29fbc95be56c39c48036fa6d0737378453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:37 +0000

    Add package zrp/0.2.3/zrp-0.2.3-py3-none-any.whl

---
+++ packages/zrp/zrp-0.2.3-py3-none-any.whl/zrp/prepare/prepare.py
@@ -0,0 +1,181 @@
+from os.path import dirname, join, expanduser
+from zrp.validate import *
+from .preprocessing import *
+from .geo_geocoder import *
+from .acs_mapper import *
+from .base import BaseZRP
+from .utils import *
+from os import path as op
+import pandas as pd
+import numpy as np
+import warnings
+import glob
+import json
+import sys
+import os
+import re
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+
+class ZRP_Prepare(BaseZRP):
+    """
+    Prepares data to generate race & ethnicity proxies
+
+    Parameters
+    ------------
+    file_path: str, optional
+        Path where to put artifacts and other files generated during intermediate steps.
+    """
+    
+    def __init__(self, file_path=None, *args, **kwargs):
+        super().__init__(file_path=file_path, *args, **kwargs)
+        self.params_dict =  kwargs
+        
+    def fit(self, data):
+        if self.census_tract:
+            tract_lengths =  data[self.census_tract].str.len()
+            tract_len  = most_common(tract_lengths)
+            if not (data[self.census_tract].isalnum).any():
+                raise ValueError("Cannot provide non-numeric Census Tract code, please remove non-numeric census tract records.")
+            if tract_len != 11:
+                raise ValueError("Improper Census Tract format provided. The tool requires the full state fips, county fips, and tract format. (ie '01001020201')")
+        if self.block_group:
+            bg_lengths =  data[self.block_group].str.len()
+            bg_len  = most_common(bg_lengths)
+            if bg_len != 12:  
+                raise ValueError("Improper Census Block Group format provided. The tool requires the full state fips, county fips, tract, and block group format. (ie '010010202001')")
+    
+    def transform(self, input_data):
+        """
+        Parameters
+        ----------
+        input_data: pd.Dataframe
+            Dataframe to be transformed
+        """  
+        curpath = dirname(__file__)
+        
+        # Load Data
+        try:
+            data = input_data.copy()
+            print("Data is loaded")
+        except AttributeError:
+            data = load_file(self.file_path)
+            print("Data file is loaded")
+            
+        data_path = join(curpath, f'../data/processed')
+        lookup_tables_config = load_json(join(data_path, "lookup_tables_config.json"))
+
+        geo_folder = op.join(data_path, "geo", lookup_tables_config['geo_year'])
+        acs_folder = op.join(data_path, 'acs', lookup_tables_config['acs_year'], lookup_tables_config['acs_span'])
+        
+        if not ((os.path.isdir(geo_folder)) &
+                (os.path.isdir(acs_folder ))
+               ):
+            raise AssertionError("Missing required support files please see the README for how to download the support files: https://github.com/zestai/zrp/blob/main/README.rst#install ")
+        if not ((len(os.listdir(geo_folder)) > 0) &
+                (len(os.listdir(acs_folder)) > 0)):
+            raise AssertionError("Missing required support files please see the README for how to download the support files: https://github.com/zestai/zrp/blob/main/README.rst#install ") 
+            
+        gen_process = ProcessStrings(file_path=self.file_path, **self.params_dict)
+        gen_process.fit(data)
+        data = gen_process.transform(data)
+        
+        processed = True
+        replicate = True
+        print("")
+
+        print("[Start] Preparing geo data")
+        
+        inv_state_map = load_json(join(data_path, "inv_state_mapping.json"))
+        data['zest_in_state_fips'] = data[self.state].replace(inv_state_map)
+        print("")
+
+        if self.census_tract is not None:
+            try:
+                data[self.zip_code] = np.where((data[self.zip_code].isna()) |\
+                                        (data[self.zip_code].str.contains("None")),
+                                           None,
+                                           data[self.zip_code].apply(lambda x: x.zfill(5)))
+                data['GEOID_ZIP'] = data[self.zip_code]
+
+            except (ValueError, KeyError) as e:
+                pass
+            geo_coded = data.copy()
+            try:
+                geo_coded['GEOID_BG'] = geo_coded[self.block_group]
+            except (ValueError, KeyError) as e:
+                pass
+            geo_coded['GEOID_CT'] = geo_coded[self.census_tract]
+            
+        elif (self.census_tract is not None) & (self.street_address is not None):
+            geocode = ZGeo(file_path=self.file_path, **self.params_dict)
+            geocode.fit(geo_coded)
+            geocode_out = [] 
+            geo_grps = data.groupby([self.state])
+            geo_dict = {}
+            for s, g in geo_grps:
+                geo_dict[s] = g
+            gdkys = list(geo_dict.keys())
+            print("  The following states are included in the data:", gdkys)
+            
+            if not set(gdkys) <= set(list(inv_state_map.keys())):
+                raise ValueError("Provided non-standard state codes. Please use standard 2-letter abbreviation to indicate states to geocode, ex:'CA' for Californina")
+            
+            geo_out = [] 
+            for s in tqdm(gdkys):                
+                print(" ... on state:", str(s))
+                geo = inv_state_map[s].zfill(2)
+                output = geocode.transform(geo_dict[s], geo, True)
+                geocode_out.append(output)
+            geo_coded = pd.concat(geocode_out)
+            geo_coded = geo_coded.drop_duplicates()
+        else:
+            geocode = ZGeo(file_path=self.file_path, **self.params_dict)
+            geocode_out = [] 
+            geo_grps = data.groupby([self.state])
+            geo_dict = {}
+            for s, g in geo_grps:
+                geo_dict[s] = g
+            gdkys = list(geo_dict.keys())
+            print("  The following states are included in the data:", gdkys)
+                  
+            if not set(gdkys) <= set(list(inv_state_map.keys())):
+                raise ValueError("Provided non-standard state codes. Please use standard 2-letter abbreviation to indicate states to geocode, ex:'CA' for Californina")
+
+            geo_out = [] 
+            for s in tqdm(gdkys):
+                print("   ... on state:", str(s))
+                geo = inv_state_map[s].zfill(2)
+                output = geocode.transform(geo_dict[s], geo, processed, replicate, True)
+                geocode_out.append(output)
+            geo_coded = pd.concat(geocode_out)
+        
+        # append data unable to enter geo mapping
+        geo_coded_keys = list(geo_coded.ZEST_KEY_COL.values)
+        data = data[~data.index.isin(geo_coded_keys)]
+        geo_coded = pd.concat([geo_coded, data])
+        
+
+        print("")
+        
+        print("[Completed] Preparing geo data")
+        print("")
+        print("[Start] Preparing ACS data")
+        
+        print("   [Start] Validating ACS input data")
+        validate = ValidateGeocoded()
+        validate.fit()
+        acs_validator = validate.transform(geo_coded)
+        save_json(acs_validator, self.out_path, "input_acs_validator.json")
+        print("   [Completed] Validating ACS input data")
+        print("")
+
+        amp = ACSModelPrep(**self.params_dict)
+        amp.fit()
+        data_out = amp.transform(geo_coded, False)
+        print("[Complete] Preparing ACS data")
+        print("")
+        
+        return(data_out)
---
+++ packages/zrp/zrp-0.2.3-py3-none-any.whl/zrp/zrp.py
@@ -0,0 +1,139 @@
+from zrp.modeling.predict import BISGWrapper, ZRP_Predict
+from os.path import dirname, join, expanduser
+from zrp.prepare.prepare import ZRP_Prepare
+from zrp.prepare.base import BaseZRP
+from zrp.prepare.utils import *
+import pandas as pd
+import numpy as np
+import warnings
+import surgeo
+import pickle
+import joblib
+import json
+import pycm
+import sys
+import os
+import re
+
+
+class ZRP(BaseZRP):
+    """
+    Zest Race Predictor, predicts race & ethnicity using name & geograhpic data
+    
+    Parameters
+    ----------
+    support_files_path: "data/processed"
+        File path with support data.
+    key: str, default 'ZEST_KEY'
+        Key to set as index. If not provided, a key will be generated.
+    first_name: str, default 'first_name'
+        Name of first name column
+    middle_name: str, default 'middle_name'
+        Name of middle name column
+    last_name: str, default 'last_name'
+        Name of last name/surname column
+    house_number: str, default 'house_number'
+        Name of house number column. Also known as primary address number this is the unique number assigned to a building to delineate it from others on a street. This is usually the first component of a delivery address line.
+    street_address: str, default 'street_address'
+        Name of street address column. The street address is usually comprised of predirectional, street name, and street suffix. 
+    city: str, default 'city'
+        Name of city column.
+    state: str, default 'state'
+        Name of state column.
+    zip_code: str, default 'zip_code'
+        Name of zip or postal code column.
+    race: str, default 'race'
+        Name of the race column
+    proxy: str, default 'probs'
+        Specifies whether to return proxy results as 'probs' (the proxy probabilities) or 'labels'.
+    census_tract: str
+        Name of census tract column
+    street_address_2: str, optional
+        Name of additional address column
+    name_prefix: str, optional
+        Name of column containing full name preix (ie Dr, Sr, and Esq )
+    name_suffix: str, optional
+        Name of column containing full name suffix (ie jr, iii, and phd)
+    na_values: list, optional
+        List of missing values to replace.
+    file_path: str, optional
+        Path where to put artifacts and other files generated during intermediate steps. 
+    geocode: bool, default True
+        Whether to geocode.
+    bisg: bool, default True
+        Whether to return BISG proxies. 
+    readout: bool, default True
+        Whether to return a readout.
+    n_jobs: int, default 49
+        Number of jobs in parallel
+    year: str, default '2019'
+        ACS year to use.
+    span: str, default '5'
+        Year span of ACS data to use.
+    runname: str, default 'test'
+    """
+
+    def __init__(self, file_path=None, pipe_path=None, *args, **kwargs):
+        super().__init__(file_path=file_path, *args, **kwargs)
+        self.pipe_path = pipe_path
+        self.params_dict =  kwargs
+
+    def fit(self):
+        return self
+    
+    def rename_data_columns(self, data):
+        """
+        Renames the user specified columns of the input data to the default column names expected by ZRP.
+        """
+        renamed_columns = {self.first_name: "first_name", self.middle_name: "middle_name", self.last_name: "last_name", self.house_number: "house_number", self.street_address: "street_address", self.city: "city", self.state: "state", self.zip_code: "zip_code"}
+        data = data.rename(columns=renamed_columns)
+        self.params_dict = {}
+        return data
+
+    def transform(self, input_data):
+        """
+        Processes input data and generates ZRP predictions. Generates BISG predictions additionally if specified.
+
+        Parameters
+        -----------
+        input_data: pd.Dataframe
+            Dataframe to be transformed
+        """
+        # Load Data
+        try:
+            data = input_data.copy()
+        except AttributeError:
+            data = load_file(self.file_path)
+            
+        
+        data = self.rename_data_columns(data)
+        self.reset_column_names()
+        
+        make_directory(self.out_path)
+
+        z_prepare = ZRP_Prepare(file_path=self.file_path, **self.params_dict)
+        z_prepare.fit(data)
+        prepared_data = z_prepare.transform(data)
+
+        curpath = dirname(__file__)
+        if self.pipe_path is None:
+            self.pipe_path = join(curpath, "modeling/models")
+
+        z_predict = ZRP_Predict(file_path=self.file_path, pipe_path=self.pipe_path, **self.params_dict)
+        z_predict.fit(prepared_data)
+        predict_out = z_predict.transform(prepared_data)
+
+        if self.bisg:
+            bisg_data = prepared_data.copy()
+            bisg_data = bisg_data[~bisg_data.index.duplicated(keep='first')]
+
+            bisgw = BISGWrapper(**self.params_dict)
+            full_bisg_proxies = bisgw.transform(prepared_data[~prepared_data.index.duplicated(keep='first')])
+            save_feather(full_bisg_proxies, self.out_path, f"bisg_proxy_output.feather")
+
+        try:
+            predict_out = input_data.merge(predict_out.reset_index(drop=False), on=self.key)
+        except KeyError:
+            pass
+
+        return (predict_out)
