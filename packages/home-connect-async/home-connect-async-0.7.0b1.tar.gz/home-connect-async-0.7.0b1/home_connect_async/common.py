f07eb59c693bbfdfab303fd36e9f7ddbfb6d3fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:29 +0000

    Add package home-connect-async/0.7.0b1/home-connect-async-0.7.0b1.tar.gz

---
+++ packages/home-connect-async/home-connect-async-0.7.0b1.tar.gz/home-connect-async-0.7.0b1/home_connect_async/common.py
@@ -0,0 +1,27 @@
+""" Common classes shared across the code """
+
+import asyncio
+
+
+class HomeConnectError(Exception):
+    """ Common exception class for the SDK """
+    def __init__(self, msg:str = None, code:int = None, response = None, inner_exception = None):
+        self.msg:str = msg
+        self.code:int = code
+        self.response = response
+        self.inner_exception = inner_exception
+        if response:
+            self.error_key:str = response.error_key
+            self.error_description:str = response.error_description
+            if not code: self.code = response.status
+        else:
+            self.error_key = None
+            self.error_description = None
+
+        super().__init__(msg, code, self.error_key, self.error_description, inner_exception)
+
+
+class Synchronization():
+    """ Class to hold global syncronization objects """
+    selected_program_lock = asyncio.Lock()
+
