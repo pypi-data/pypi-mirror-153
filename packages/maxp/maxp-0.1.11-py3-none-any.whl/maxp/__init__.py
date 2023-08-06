9da01e55fd763fce0fc23106470ac9e9b2157979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:44 +0000

    Add package maxp/0.1.11/maxp-0.1.11-py3-none-any.whl

---
+++ packages/maxp/maxp-0.1.11-py3-none-any.whl/maxp/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from .runtime import (
+    MAX_HWND,
+    MXSWrapperBase,
+    MXSWrapperObjectSet,
+    MXSWrapperObjectSetIter,
+    pymxs,
+    rt,
+)
---
+++ packages/maxp/maxp-0.1.11-py3-none-any.whl/maxp/values.py
@@ -0,0 +1,8 @@
+from maxp import rt
+
+
+def toArray(iterable: list) -> rt.Array:
+    mxsArray = rt.Array()
+    for item in iterable:
+        rt.Append(mxsArray, item)
+    return mxsArray
