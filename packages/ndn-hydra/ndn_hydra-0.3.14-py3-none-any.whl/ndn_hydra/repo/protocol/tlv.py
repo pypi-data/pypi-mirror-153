27e2c6a435fc443454ee666196c17a2f45f4b4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:04 +0000

    Add package ndn-hydra/0.3.14/ndn_hydra-0.3.14-py3-none-any.whl

---
+++ packages/ndn-hydra/ndn_hydra-0.3.14-py3-none-any.whl/ndn_hydra/repo/protocol/tlv.py
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------
+# NDN Hydra Tlv Types
+# -------------------------------------------------------------
+#  @Project: NDN Hydra
+#  @Date:    2021-01-25
+#  @Authors: Please check AUTHORS.rst
+#  @Source-Code:   https://github.com/justincpresley/ndn-hydra
+#  @Documentation: https://ndn-hydra.readthedocs.io
+#  @Pip-Library:   https://pypi.org/project/ndn-hydra
+# -------------------------------------------------------------
+
+class HydraTlvTypes:
+    MESSAGE = 200
+    MESSAGE_TYPE = 201
+    FILE = 202
+    PACKETS = 203
+    PACKET_SIZE = 204
+    SIZE = 205
+    STATUS_CODE = 206
+    CMD_URI= 207
