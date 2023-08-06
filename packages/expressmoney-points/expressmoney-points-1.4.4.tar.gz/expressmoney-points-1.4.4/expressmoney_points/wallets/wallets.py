165c757fc7cecf41c89ff90390a5332819280330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:17 +0000

    Add package expressmoney-points/1.4.4/expressmoney-points-1.4.4.tar.gz

---
+++ packages/expressmoney-points/expressmoney-points-1.4.4.tar.gz/expressmoney-points-1.4.4/expressmoney_points/wallets/wallets.py
@@ -0,0 +1,62 @@
+__all__ = ('WalletPoint', 'WalletObjectPoint', 'WalletInfoRequestPoint')
+
+from expressmoney.api import *
+
+SERVICE = 'wallets'
+APP = 'wallets'
+
+
+class WalletCreateContract(Contract):
+    RUB = 'RUB'
+    USD = 'USD'
+    CURRENCY_CODE_CHOICES = (
+        (RUB, RUB),
+        (USD, USD),
+    )
+    currency_code = serializers.ChoiceField(choices=CURRENCY_CODE_CHOICES)
+
+
+class WalletReadContract(WalletCreateContract):
+    id = serializers.IntegerField(min_value=1)
+    is_active = serializers.BooleanField()
+    user_id = serializers.IntegerField(min_value=1)
+    balance = serializers.DecimalField(max_digits=16, decimal_places=0)
+
+
+class WalletInfoRequestCreateContract(Contract):
+    wallet_id = serializers.IntegerField(min_value=1)
+
+
+class WalletInfoResponseRetrieveContract(WalletInfoRequestCreateContract):
+    wallet_is_active = serializers.BooleanField()
+    wallet_user_id = serializers.IntegerField(min_value=1)
+    wallet_currency_code = serializers.ChoiceField(choices=WalletCreateContract.CURRENCY_CODE_CHOICES)
+
+
+class WalletID(ID):
+    _service = SERVICE
+    _app = APP
+    _view_set = 'wallet'
+
+
+class WalletInfoRequestID(ID):
+    _service = SERVICE
+    _app = APP
+    _view_set = 'wallet_info_request'
+
+
+class WalletPoint(ListPointMixin, CreatePointMixin, ContractPoint):
+    _point_id = WalletID()
+    _create_contract = WalletCreateContract
+    _read_contract = WalletReadContract
+
+
+class WalletObjectPoint(RetrievePointMixin, ContractObjectPoint):
+    _point_id = WalletID()
+    _read_contract = WalletReadContract
+
+
+class WalletInfoRequestPoint(ResponseMixin, CreatePointMixin, ContractPoint):
+    _point_id = WalletInfoRequestID()
+    _create_contract = WalletInfoRequestCreateContract
+    _response_contract = WalletInfoResponseRetrieveContract
