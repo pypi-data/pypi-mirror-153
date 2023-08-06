f72ac3db64f6b42edb69c2c8d31f869ed134c067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package flask-does-redis/0.2.3/flask-does-redis-0.2.3.tar.gz

---
+++ packages/flask-does-redis/flask-does-redis-0.2.3.tar.gz/flask-does-redis-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flask_does_redis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['redis>=4.3.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'flask-does-redis',
+    'version': '0.2.3',
+    'description': 'Flask extension to easily reuse redis connection pools',
+    'long_description': None,
+    'author': 'jthop',
+    'author_email': 'jh@mode14.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
