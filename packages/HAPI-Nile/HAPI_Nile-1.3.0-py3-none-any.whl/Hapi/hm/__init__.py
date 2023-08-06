179c99ca3d7de2fefc07ad014dce9a7c54d105fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:39 +0000

    Add package HAPI-Nile/1.3.0/HAPI_Nile-1.3.0-py3-none-any.whl

---
+++ packages/HAPI-Nile/HAPI_Nile-1.3.0-py3-none-any.whl/Hapi/hm/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on Sat Apr 24 20:57:45 2021
+
+@author: mofarrag
+"""
+
+# from .hm import *
+import Hapi.hm.calibration as calibration
+import Hapi.hm.crosssection as crosssection
+import Hapi.hm.event as event
+import Hapi.hm.inputs as inputs
+import Hapi.hm.interface as interface
+import Hapi.hm.river as river
+import Hapi.hm.saintvenant as saintvenant
