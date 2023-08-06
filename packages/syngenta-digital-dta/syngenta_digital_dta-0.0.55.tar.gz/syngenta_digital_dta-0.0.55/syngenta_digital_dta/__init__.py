3bfe8698424a7d90e181de78657b8a882c1535e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:47 +0000

    Add package syngenta-digital-dta/0.0.55/syngenta_digital_dta-0.0.55.tar.gz

---
+++ packages/syngenta-digital-dta/syngenta_digital_dta-0.0.55.tar.gz/syngenta_digital_dta-0.0.55/syngenta_digital_dta/__init__.py
@@ -0,0 +1,22 @@
+from syngenta_digital_dta.dynamodb.adapter import DynamodbAdapter
+from syngenta_digital_dta.file_system.adapter import FileSystemAdapter
+from syngenta_digital_dta.postgres.adapter import PostgresAdapter
+from syngenta_digital_dta.elasticsearch.adapter import ElasticsearchAdapter
+from syngenta_digital_dta.s3.adapter import S3Adapter
+
+
+def adapter(**kwargs):
+    if kwargs.get('engine') == 'dynamodb':
+        return DynamodbAdapter(**kwargs)
+    if kwargs.get('engine') == 'redshift':
+        return PostgresAdapter(**kwargs)
+    if kwargs.get('engine') == 'postgres':
+        return PostgresAdapter(**kwargs)
+    if kwargs.get('engine') == 'elasticsearch':
+        return ElasticsearchAdapter(**kwargs)
+    if kwargs.get('engine') == 's3':
+        return S3Adapter(**kwargs)
+    if kwargs.get('engine') == 'file_system':
+        return FileSystemAdapter(**kwargs)
+    raise Exception(
+        f'engine {kwargs.get("engine", "was not supplied, an empty engine kwarg is")} not supported; contribute to get it supported :)')
