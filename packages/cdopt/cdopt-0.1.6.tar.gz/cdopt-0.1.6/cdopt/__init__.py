8e4f04317dccc9b9a285a165485fa7e76c517500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:25 +0000

    Add package cdopt/0.1.6/cdopt-0.1.6.tar.gz

---
+++ packages/cdopt/cdopt-0.1.6.tar.gz/cdopt-0.1.6/cdopt/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["core", "manifold", "manifold_np", "manifold_torch"]
+__author__ = 'Nachuan Xiao, Xiaoyin Hu, Xin Liu, and Kim-Chuan Toh'
+
+from cdopt import core, manifold, manifold_np, manifold_torch
