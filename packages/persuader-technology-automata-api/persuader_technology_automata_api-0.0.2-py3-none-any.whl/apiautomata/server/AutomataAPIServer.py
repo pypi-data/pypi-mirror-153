97db9b4ae3f672eae0700126f1ea119838dacf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:48 +0000

    Add package persuader-technology-automata-api/0.0.2/persuader_technology_automata_api-0.0.2-py3-none-any.whl

---
+++ packages/persuader-technology-automata-api/persuader_technology_automata_api-0.0.2-py3-none-any.whl/apiautomata/server/AutomataAPIServer.py
@@ -0,0 +1,25 @@
+import logging
+
+import uvicorn
+from missingrepo.repository.MissingRepository import MissingRepository
+
+from apiautomata.holder.ItemHolder import ItemHolder
+
+
+class AutomataAPIServer:
+
+    def __init__(self, options):
+        self.log = logging.getLogger(__name__)
+        self.options = options
+        self.port = options['API_SERVER_PORT']
+        self.init_dependencies()
+
+    def init_dependencies(self):
+        self.log.info('Initializing dependencies')
+        item_holder = ItemHolder()
+        item_holder.add(self.options['VERSION'], 'version')
+        item_holder.add_entity(MissingRepository(self.options))
+
+    def run(self):
+        self.log.info('Running')
+        uvicorn.run('apiautomata.API:app', port=self.port, access_log=False)
