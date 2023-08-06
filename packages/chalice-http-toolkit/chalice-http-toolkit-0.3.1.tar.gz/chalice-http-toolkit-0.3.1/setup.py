55d4ed95b8269dba6cdeb5ac51e3db9f75a17e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:07 +0000

    Add package chalice-http-toolkit/0.3.1/chalice-http-toolkit-0.3.1.tar.gz

---
+++ packages/chalice-http-toolkit/chalice-http-toolkit-0.3.1.tar.gz/chalice-http-toolkit-0.3.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chalice-http-toolkit",
+    author="Chris Lapa",
+    author_email="",
+    description="Several helpers for building serverless websites using Chalice",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    url="https://gitlab.com/chalice-http-toolkit/chalice-http-toolkit",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/chalice-http-toolkit/chalice-http-toolkit/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=['chalice>=1.22.3',
+                      'python-magic==0.4.22',
+                      'requests-toolbelt==0.9.1',
+                      'pytz==2021.1'],
+    extras_require={
+        'layered': ['Jinja2==2.10',
+                    'Pillow==8.2.0']
+        },
+    entry_points={
+        'console_scripts': ['chalice-http-toolkit=tools.chalice_http_toolkit:main'],
+        }
+    )
