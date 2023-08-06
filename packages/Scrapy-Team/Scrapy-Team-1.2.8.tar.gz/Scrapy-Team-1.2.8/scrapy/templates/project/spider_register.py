a56dba9cb5ecc34bfe8e09a6bae3dd5c987cbcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package Scrapy-Team/1.2.8/Scrapy-Team-1.2.8.tar.gz

---
+++ packages/Scrapy-Team/Scrapy-Team-1.2.8.tar.gz/Scrapy-Team-1.2.8/scrapy/templates/project/spider_register.py
@@ -0,0 +1,38 @@
+# 爬虫名注册以及配置 配置优先度高于custom_settings 仅配置中间件和管道 该文件未配置的取custom_settings
+zhizhong={
+    # "pyppeteer_demo.crawl":["zz_rohmcrawl",("spider_middlewares"),("downloader_middlewares"),("pipelines"),("extensions")],
+    # "pyppeteer_demo.crawl":["zz_rohmcrawl",("stickyitem"),("ua","proxy","pyppeteer"),("file","file_bak","img","img_bak","bloom","mysql","mongo","rabbit","fctexcel","dpfexcel","kafka"),("closespider"，"mailer")],
+    }
+
+yongjie={
+
+    }
+
+xuqi={
+
+    }
+
+juncheng={
+
+    }
+
+yonghao={
+
+    }
+
+siming={
+
+    }
+
+fengchao={
+
+    }
+
+runtao={
+
+    }
+
+yijia={
+
+    }
+
