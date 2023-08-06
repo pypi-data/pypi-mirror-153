5a52b2a32fffab45bbd00222293892539e62f252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package fantasy-hockey-notifier-cdk/1.0.81/fantasy-hockey-notifier-cdk-1.0.81.tar.gz

---
+++ packages/fantasy-hockey-notifier-cdk/fantasy-hockey-notifier-cdk-1.0.81.tar.gz/fantasy-hockey-notifier-cdk-1.0.81/src/fantasy_hockey_notifier_cdk/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import cdk_iam_floyd._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "fantasy-hockey-notifier-cdk",
+    "1.0.81",
+    __name__[0:-6],
+    "fantasy-hockey-notifier-cdk@1.0.81.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
