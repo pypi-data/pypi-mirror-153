98a6dd278fd7bbd99397c8177b8ca6452a463c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:00 +0000

    Add package matthew-pack/0.0.2/matthew_pack-0.0.2.tar.gz

---
+++ packages/matthew-pack/matthew_pack-0.0.2.tar.gz/matthew_pack-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+name="matthew_pack",
+version="0.0.2",
+author ="Uri Itai & Natan Katz",
+include_package_data=True,
+description="Calcalating Matthew Correlation Coefficient for multi class problems",
+  packages=["matthew_pack"]
+)
