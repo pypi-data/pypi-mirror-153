6e39684e5147ffd017a1a2715e7498d0ac2776cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:16 +0000

    Add package PyGObject-stubs/0.0.9/PyGObject-stubs-0.0.9.tar.gz

---
+++ packages/PyGObject-stubs/PyGObject-stubs-0.0.9.tar.gz/PyGObject-stubs-0.0.9/examples/gtk.py
@@ -0,0 +1,30 @@
+import gi
+from gi.repository import Gtk, Gdk, GdkPixbuf, Gio, Atk, Gst
+
+gi.__version__
+
+gi.require_version
+
+gi.require_versions
+
+Gtk.Button
+
+Gtk.DialogFlags
+
+Gtk.MINOR_VERSION
+
+Gtk.DirectionType
+
+Gtk.Actionable
+
+Gtk.AboutDialog
+
+GdkPixbuf.Pixbuf
+
+Gdk.Atom
+
+Gio.Action
+
+Gst.Bin
+
+Atk.Component
