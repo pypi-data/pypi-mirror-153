626616cee548a7c57e9a4e390ccbddb22a8282df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:04:48 +0000

    Add package collibra-catalog/1.0.0/collibra-catalog-1.0.0.tar.gz

---
+++ packages/collibra-catalog/collibra-catalog-1.0.0.tar.gz/collibra-catalog-1.0.0/collibra_catalog/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_catalog.api.jdbc_schema_ingestion_api import JDBCSchemaIngestionApi
+from collibra_catalog.api.job_server_instance_configuration_api import JobServerInstanceConfigurationApi
+from collibra_catalog.api.marketplace_accounting_api import MarketplaceAccountingApi
+from collibra_catalog.api.profiling_api import ProfilingApi
+from collibra_catalog.api.s3_api import S3Api
