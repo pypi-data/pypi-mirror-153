2a7355809af933559db791adf16267b36e4d8c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:56 +0000

    Add package largesteps/0.2.0/largesteps-0.2.0.tar.gz

---
+++ packages/largesteps/largesteps-0.2.0.tar.gz/largesteps-0.2.0/largesteps/__init__.py
@@ -0,0 +1,9 @@
+"""
+largesteps.
+
+Laplacian parameterization package for shape optimization with differentiable
+rendering.
+"""
+
+__version__ = "0.2.0"
+__author__ = 'Baptiste Nicolet'
---
+++ packages/largesteps/largesteps-0.2.0.tar.gz/largesteps-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='largesteps',
+    version='0.2.0',
+    description='Laplacian parameterization package for shape optimization with differentiable rendering',
+    url='https://github.com/rgl-epfl/large-steps-pytorch',
+    author='Baptiste Nicolet',
+    author_email='baptiste.nicolet@epfl.ch',
+    license='BSD',
+    packages=['largesteps'],
+    install_requires=['numpy',
+                      'scipy',
+                      'cholespy'
+                      ],
+)
