b909d06118c38ec897c87f13be1689cc71bc6e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package mypy-boto3-kms/1.24.0/mypy_boto3_kms-1.24.0-py3-none-any.whl

---
+++ packages/mypy-boto3-kms/mypy_boto3_kms-1.24.0-py3-none-any.whl/mypy_boto3_kms/__init__.py
@@ -0,0 +1,44 @@
+"""
+Main interface for kms service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_kms import (
+        Client,
+        KMSClient,
+        ListAliasesPaginator,
+        ListGrantsPaginator,
+        ListKeyPoliciesPaginator,
+        ListKeysPaginator,
+    )
+
+    session = Session()
+    client: KMSClient = session.client("kms")
+
+    list_aliases_paginator: ListAliasesPaginator = client.get_paginator("list_aliases")
+    list_grants_paginator: ListGrantsPaginator = client.get_paginator("list_grants")
+    list_key_policies_paginator: ListKeyPoliciesPaginator = client.get_paginator("list_key_policies")
+    list_keys_paginator: ListKeysPaginator = client.get_paginator("list_keys")
+    ```
+"""
+from .client import KMSClient
+from .paginator import (
+    ListAliasesPaginator,
+    ListGrantsPaginator,
+    ListKeyPoliciesPaginator,
+    ListKeysPaginator,
+)
+
+Client = KMSClient
+
+
+__all__ = (
+    "Client",
+    "KMSClient",
+    "ListAliasesPaginator",
+    "ListGrantsPaginator",
+    "ListKeyPoliciesPaginator",
+    "ListKeysPaginator",
+)
---
+++ packages/mypy-boto3-kms/mypy_boto3_kms-1.24.0-py3-none-any.whl/mypy_boto3_kms/paginator.py
@@ -0,0 +1,121 @@
+"""
+Type annotations for kms service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_kms.client import KMSClient
+    from mypy_boto3_kms.paginator import (
+        ListAliasesPaginator,
+        ListGrantsPaginator,
+        ListKeyPoliciesPaginator,
+        ListKeysPaginator,
+    )
+
+    session = Session()
+    client: KMSClient = session.client("kms")
+
+    list_aliases_paginator: ListAliasesPaginator = client.get_paginator("list_aliases")
+    list_grants_paginator: ListGrantsPaginator = client.get_paginator("list_grants")
+    list_key_policies_paginator: ListKeyPoliciesPaginator = client.get_paginator("list_key_policies")
+    list_keys_paginator: ListKeysPaginator = client.get_paginator("list_keys")
+    ```
+"""
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator
+from botocore.paginate import Paginator as Boto3Paginator
+
+from .type_defs import (
+    ListAliasesResponseTypeDef,
+    ListGrantsResponseTypeDef,
+    ListKeyPoliciesResponseTypeDef,
+    ListKeysResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "ListAliasesPaginator",
+    "ListGrantsPaginator",
+    "ListKeyPoliciesPaginator",
+    "ListKeysPaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListAliasesPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListAliases)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listaliasespaginator)
+    """
+
+    def paginate(
+        self, *, KeyId: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListAliasesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListAliases.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listaliasespaginator)
+        """
+
+
+class ListGrantsPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListGrants)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listgrantspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        KeyId: str,
+        GrantId: str = ...,
+        GranteePrincipal: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListGrantsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListGrants.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listgrantspaginator)
+        """
+
+
+class ListKeyPoliciesPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListKeyPolicies)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listkeypoliciespaginator)
+    """
+
+    def paginate(
+        self, *, KeyId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListKeyPoliciesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListKeyPolicies.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listkeypoliciespaginator)
+        """
+
+
+class ListKeysPaginator(Boto3Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListKeys)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listkeyspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListKeysResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Paginator.ListKeys.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_kms/paginators/#listkeyspaginator)
+        """
