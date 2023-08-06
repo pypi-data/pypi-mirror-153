7b50698a1e528abfe7a283570d56bae861a8ea51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:23 +0000

    Add package eyes-universal/2.5.21/eyes_universal-2.5.21-py2.py3-none-macosx_11_0_arm64.whl

---
+++ packages/eyes-universal/eyes_universal-2.5.21-py2.py3-none-macosx_11_0_arm64.whl/applitools/eyes_universal/server.py
@@ -0,0 +1,48 @@
+import sys
+import typing
+from logging import getLogger
+from subprocess import check_output
+
+from pkg_resources import resource_filename
+
+if typing.TYPE_CHECKING:
+    from typing import Optional
+
+logger = getLogger(__name__)
+
+_platform_suffixes = {
+    "darwin": "macos",
+    "linux": "linux",
+    "linux2": "linux",  # python2 included kernel major version
+    "win32": "win.exe",
+}
+executable_path = resource_filename(
+    "applitools.eyes_universal",
+    "bin/eyes-universal-" + _platform_suffixes[sys.platform],
+)
+
+
+class SDKServer(object):
+    def __init__(
+        self,
+        port=None,  # type: Optional[int]
+        lazy=False,  # type: bool
+        idle_timeout=None,  # type: Optional[int]
+        executable=executable_path,  # type: str
+    ):
+        self.executable = executable
+        self.log_file_name = None  # backward compatibility with eyes-selenium<=5.6
+        command = [executable]
+        if port is not None:
+            command.extend(["--port", port])
+        if lazy:
+            command.append("--lazy")
+        if idle_timeout is not None:
+            command.extend(["--idle-timeout", idle_timeout])
+        command.append("--fork")
+        output = check_output(command, universal_newlines=True)  # nosec
+        self.port = int(output)
+        logger.info("Started Universal SDK server at %s", self.port)
+
+    def __repr__(self):
+        return "SDKServer(port={})".format(self.port)
