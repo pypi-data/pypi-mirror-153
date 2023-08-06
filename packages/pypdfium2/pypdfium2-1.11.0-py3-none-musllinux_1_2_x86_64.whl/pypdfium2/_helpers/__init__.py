8a0995caebb2d7f258698a0d96d5e3ec1ed905d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:47 +0000

    Add package pypdfium2/1.11.0/pypdfium2-1.11.0-py3-none-musllinux_1_2_x86_64.whl

---
+++ packages/pypdfium2/pypdfium2-1.11.0-py3-none-musllinux_1_2_x86_64.whl/pypdfium2/_helpers/__init__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+# full support model namespace
+
+from pypdfium2._helpers.constants import *
+from pypdfium2._helpers.error_handler import *
+from pypdfium2._helpers.utilities import *
+from pypdfium2._helpers.opener import *
+from pypdfium2._helpers.nativeopener import *
+from pypdfium2._helpers.page_renderer import *
+from pypdfium2._helpers.pdf_renderer import *
+from pypdfium2._helpers.toc import *
+from pypdfium2._helpers.saver import *
+from pypdfium2._helpers.boxes import *
+from pypdfium2._helpers.classes import *
+from pypdfium2._helpers.text_inserter import *
+from pypdfium2._helpers.text_extractor import *
+from pypdfium2._helpers.pageobjects import *
