283a359830be98b935d0c6fb1414ee34586a06c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package mann/0.2.4/mann-0.2.4.tar.gz

---
+++ packages/mann/mann-0.2.4.tar.gz/mann-0.2.4/mann/__init__.py
@@ -0,0 +1,12 @@
+"""
+MANN - Multitask Artificial Neural Network package
+
+This package contains custom utilities and layers to use to build Multitask Artificial Neural Network (MANN) models
+in conjunction with TensorFlow and Keras.
+"""
+
+__version__ = '0.2.4'
+__dev__ = True
+
+import mann.layers
+import mann.utils
