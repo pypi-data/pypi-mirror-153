6c3cd3fb8807b1148dbe5fca23eee95aef93edcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package compose-x-common/1.0.0/compose_x_common-1.0.0.tar.gz

---
+++ packages/compose-x-common/compose_x_common-1.0.0.tar.gz/compose_x_common-1.0.0/src/compose_x_common/aws/acm.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2022 John Mille <john@compose-x.io>
+
+import re
+
+ACM_ARN_RE = re.compile(
+    r"^arn:aws(?:-[a-z]+)?:acm:(?P<region>[\S]+):(?P<accountid>[\d]{12}):certificate/"
+    r"(?P<id>[a-z0-9]{8}(?:-[a-z0-9]{4}){3}-[a-z0-9]{12})$"
+)
