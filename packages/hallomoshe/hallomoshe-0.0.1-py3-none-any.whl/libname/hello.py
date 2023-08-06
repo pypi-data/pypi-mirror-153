281c1c28e862a732cc0261408c3dd78acfa42b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:49 +0000

    Add package hallomoshe/0.0.1/hallomoshe-0.0.1-py3-none-any.whl

---
+++ packages/hallomoshe/hallomoshe-0.0.1-py3-none-any.whl/libname/hello.py
@@ -0,0 +1,9 @@
+def sayhello():
+    print("hello moshe rosenblum")
+
+
+def saybay():
+    print("bay-bay moshe")
+
+def helloeliran():
+    print("ELIRAN you are amazing, really you are unbelievable !!!")
