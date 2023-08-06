dd2d4e20f0195e0e4ac39308e27bd963e27e35e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:46 +0000

    Add package jamegresc/0.1.9/jamegresc-0.1.9.tar.gz

---
+++ packages/jamegresc/jamegresc-0.1.9.tar.gz/jamegresc-0.1.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jamegresc", # 名前
+    version="0.1.9", # バージョン設定
+    author="takahito asakawa", # 名前
+    author_email="s2022051@gmail.com", # メ-ルアドレス
+    description='A package for visualization of Resource recovery amount and gain on sale data in Meguro', # 説明文書書き換え
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TakahitoAsakawa/jamegresc", # GitHubURL
+    project_urls={
+        "Bug Tracker": "https://github.com/TakahitoAsakawa/jamegresc", #GitHubURL
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['jamegresc'], # 設定するモジュール名
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7.7",
+    entry_points = {
+        'console_scripts': [
+            'jamegresc = jamegresc:main' # src
+        ]
+    },
+)
