83814825e82b2358797323c2b09c808a1131e063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:39 +0000

    Add package newtonlib/0.0.2/newtonlib-0.0.2.tar.gz

---
+++ packages/newtonlib/newtonlib-0.0.2.tar.gz/newtonlib-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newtonlib',
+    version='0.0.2',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Python package for a university course',
+    long_description=open('README.md').read(),
+    package_data={
+        "": ["newtonlib.pyd"],
+    },
+    # install_requires=['requests'],
+    url='https://git.e-science.pl/kwazny_252716_dpp/kwazny252716_pythonnative',
+    author='Karol Ważny',
+    author_email='kwazny_252716@e-science.pl'
+)
