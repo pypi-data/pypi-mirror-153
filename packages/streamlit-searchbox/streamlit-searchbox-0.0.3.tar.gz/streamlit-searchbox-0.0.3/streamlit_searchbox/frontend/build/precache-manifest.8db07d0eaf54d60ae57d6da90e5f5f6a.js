f89b97dea2290fba2774a6ef4e5d443adc5f0ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:52 +0000

    Add package streamlit-searchbox/0.0.3/streamlit-searchbox-0.0.3.tar.gz

---
+++ packages/streamlit-searchbox/streamlit-searchbox-0.0.3.tar.gz/streamlit-searchbox-0.0.3/streamlit_searchbox/frontend/build/precache-manifest.8db07d0eaf54d60ae57d6da90e5f5f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edfeb80716167f40ea8b0eb64e8bf0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac5acfc51dd686e56dbe",
+    "url": "./static/js/2.4537580d.chunk.js"
+  },
+  {
+    "revision": "41b8f9f21f3f055166c2ef8fa2c896e2",
+    "url": "./static/js/2.4537580d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76712cb743a06dc22c5a",
+    "url": "./static/js/main.57c30ce5.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  },
+  {
+    "revision": "b713a401735902a95b5940c32ee7b2cf",
+    "url": "./static/media/magnifying-glass-solid.b713a401.svg"
+  }
+]);
