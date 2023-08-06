bc4064d0f960424ef1665651fe3271b282d1a8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:11 +0000

    Add package sqlalchemy-oso/0.26.1/sqlalchemy_oso-0.26.1-py3-none-any.whl

---
+++ packages/sqlalchemy-oso/sqlalchemy_oso-0.26.1-py3-none-any.whl/sqlalchemy_oso/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.26.1"
+
+
+from .auth import register_models
+from .oso import SQLAlchemyOso
+from .session import authorized_sessionmaker
+
+__all__ = [
+    "register_models",
+    "authorized_sessionmaker",
+    "SQLAlchemyOso",
+]
