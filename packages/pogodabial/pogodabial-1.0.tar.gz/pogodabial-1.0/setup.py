a5e8b0c567d7ba77a3862cfcc466db73fe1d75f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package pogodabial/1.0/pogodabial-1.0.tar.gz

---
+++ packages/pogodabial/pogodabial-1.0.tar.gz/pogodabial-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pogodabial',
+    version='1.0',
+    description='Pakiet testowy',
+    url='http://sda.pl',
+    install_requries = [],
+    packages=['pakiet_testowy'],
+)
