f51ce40784844d1f5822063d03eb866c86a7ad7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:53 +0000

    Add package pcdswidgets/0.7.0/pcdswidgets-0.7.0.tar.gz

---
+++ packages/pcdswidgets/pcdswidgets-0.7.0.tar.gz/pcdswidgets-0.7.0/pcdswidgets/icons/__init__.py
@@ -0,0 +1,21 @@
+from .gauges import (RoughGaugeSymbolIcon, CathodeGaugeSymbolIcon,
+                     HotCathodeGaugeSymbolIcon, ColdCathodeGaugeSymbolIcon)
+from .pumps import (IonPumpSymbolIcon, TurboPumpSymbolIcon,
+                    ScrollPumpSymbolIcon, GetterPumpSymbolIcon)
+from .valves import (PneumaticValveSymbolIcon, FastShutterSymbolIcon,
+                     ApertureValveSymbolIcon, RightAngleManualValveSymbolIcon,
+                     NeedleValveSymbolIcon, ProportionalValveSymbolIcon,
+                     ControlValveSymbolIcon, ControlOnlyValveSymbolIcon,
+                     PneumaticValveNOSymbolIcon, PneumaticValveDASymbolIcon)
+from .others import RGASymbolIcon
+
+__all__ = ['RoughGaugeSymbolIcon', 'CathodeGaugeSymbolIcon',
+           'HotCathodeGaugeSymbolIcon', 'ColdCathodeGaugeSymbolIcon',
+           'IonPumpSymbolIcon', 'TurboPumpSymbolIcon', 'ScrollPumpSymbolIcon',
+           'GetterPumpSymbolIcon',
+           'PneumaticValveSymbolIcon', 'FastShutterSymbolIcon',
+           'ApertureValveSymbolIcon', 'RightAngleManualValveSymbolIcon',
+           'NeedleValveSymbolIcon', 'ProportionalValveSymbolIcon',
+           'RGASymbolIcon', 'ControlValveSymbolIcon',
+           'ControlOnlyValveSymbolIcon', 'PneumaticValveNOSymbolIcon',
+           'PneumaticValveDASymbolIcon']
