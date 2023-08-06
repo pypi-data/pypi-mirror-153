bca688cb3697d5291ae2240b48689117cd0617a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package ndpyramid/0.0.6/ndpyramid-0.0.6.tar.gz

---
+++ packages/ndpyramid/ndpyramid-0.0.6.tar.gz/ndpyramid-0.0.6/ndpyramid/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+from ._version import __version__
+from .core import pyramid_coarsen, pyramid_reproject
+from .regrid import pyramid_regrid
---
+++ packages/ndpyramid/ndpyramid-0.0.6.tar.gz/ndpyramid-0.0.6/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""The setup script"""
+
+
+import pathlib
+
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = f.read().strip().split('\n')
+
+LONG_DESCRIPTION = pathlib.Path('README.md').read_text()
+PYTHON_REQUIRES = '>=3.8'
+
+description = 'A small utility for generating ND array pyramids using Xarray and Zarr.'
+
+setup(
+    name='ndpyramid',
+    description=description,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    maintainer='Joe Hamman',
+    maintainer_email='joe@carbonplan.org',
+    url='https://github.com/carbonplan/ndpyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=['pytest'],
+    license='MIT',
+    keywords='zarr, xarray, pyramid',
+    use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
+)
