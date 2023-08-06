c441e32335a09f2f1dcb1e4729d98857905292cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:21 +0000

    Add package keylime/6.4.1/keylime-6.4.1-py3-none-any.whl

---
+++ packages/keylime/keylime-6.4.1-py3-none-any.whl/keylime/cmd/agent.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright 2017 Massachusetts Institute of Technology.
+"""
+
+from keylime import keylime_agent, keylime_logging
+
+logger = keylime_logging.init_logging("cloudagent")
+
+
+def main():
+    keylime_agent.main()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.exception(e)
---
+++ packages/keylime/keylime-6.4.1-py3-none-any.whl/keylime/cmd/tenant.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright 2017 Massachusetts Institute of Technology.
+"""
+
+import sys
+
+from keylime import keylime_logging, tenant
+
+logger = keylime_logging.init_logging("tenant")
+
+
+def main():
+    try:
+        tenant.main()
+    except tenant.UserError as ue:
+        logger.error(str(ue))
+        sys.exit(1)
+    except Exception as e:
+        logger.exception(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
