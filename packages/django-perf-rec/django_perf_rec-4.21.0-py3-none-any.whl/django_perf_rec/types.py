058653f11ed354ff2e65617c860a0265aa2d7423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package django-perf-rec/4.21.0/django_perf_rec-4.21.0-py3-none-any.whl

---
+++ packages/django-perf-rec/django_perf_rec-4.21.0-py3-none-any.whl/django_perf_rec/types.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from typing import Dict, List, Union
+
+PerformanceRecordItem = Dict[str, Union[str, List[str]]]
+PerformanceRecord = List[PerformanceRecordItem]
