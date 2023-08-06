06392e80fa7c1be862f4e79f1ff11cf61b730370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:48 +0000

    Add package make-up-tools/0.0.10/make-up-tools-0.0.10.tar.gz

---
+++ packages/make-up-tools/make-up-tools-0.0.10.tar.gz/make-up-tools-0.0.10/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='make-up-tools',
+      version='0.0.10',
+      description='just for job',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type="text/markdown",
+      author='NA',
+      author_email='martinlord@foxmail.com',
+      url='https://gitee.com/mahaoyang/make_up',
+      packages=find_packages(
+          include=[
+              'make_up_tools',
+              'make_up_tools.ml',
+              'make_up_tools.ml.model',
+              'make_up_tools.ml.nlp*',
+          ]
+      ),
+      package_data={
+          '': ['*.txt'],
+          'make_up_tools/ml/nlp/sentiments/by_dict/dict':['*.txt']
+                    },
+      license='LICENSE.txt',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
