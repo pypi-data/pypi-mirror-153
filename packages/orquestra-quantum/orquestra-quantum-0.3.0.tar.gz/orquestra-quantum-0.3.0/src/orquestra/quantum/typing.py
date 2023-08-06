ab8755afaa0f9965e2c9a6fa7ac4e93972f51c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:56 +0000

    Add package orquestra-quantum/0.3.0/orquestra-quantum-0.3.0.tar.gz

---
+++ packages/orquestra-quantum/orquestra-quantum-0.3.0.tar.gz/orquestra-quantum-0.3.0/src/orquestra/quantum/typing.py
@@ -0,0 +1,47 @@
+################################################################################
+# © Copyright 2021-2022 Zapata Computing Inc.
+################################################################################
+"""Types commonly encountered in orquestra repositories."""
+from numbers import Number
+from os import PathLike
+from typing import Any, Callable, Dict, List, Sequence, Union
+
+import numpy as np
+import sympy
+from typing_extensions import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class Readable(Protocol):
+    def read(self, size: int = 0) -> str:
+        pass
+
+    def writable(self) -> bool:
+        pass
+
+
+@runtime_checkable
+class Writeable(Protocol):
+    def write(self, content: str):
+        pass
+
+    def writable(self) -> bool:
+        pass
+
+
+AnyPath = Union[str, bytes, PathLike]
+
+LoadSource = Union[Readable, AnyPath]
+
+DumpTarget = Union[Writeable, AnyPath]
+
+Specs = Union[str, Dict]
+
+
+Parameter = Union[sympy.Symbol, Number]
+ParameterizedVector = Union[Sequence[Parameter], np.ndarray]
+
+
+class SupportsLessThan(Protocol):
+    def __lt__(self, other: Any) -> bool:
+        """Return result of comparison self < other."""
