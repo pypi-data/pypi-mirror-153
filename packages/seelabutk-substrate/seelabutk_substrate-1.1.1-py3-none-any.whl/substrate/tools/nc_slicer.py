45c62229f008e3b1868407fd4ef39c160e697242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:01 +0000

    Add package seelabutk-substrate/1.1.1/seelabutk_substrate-1.1.1-py3-none-any.whl

---
+++ packages/seelabutk-substrate/seelabutk_substrate-1.1.1-py3-none-any.whl/substrate/tools/nc_slicer.py
@@ -0,0 +1,45 @@
+from docker import from_env
+from docker.types.services import EndpointSpec, ServiceMode
+
+from . import Tool
+
+
+class NetCDFSlicer(Tool):
+	def __init__(self, config, data_sources):
+		super().__init__(config, data_sources)
+
+		self.name = 'nc-slicer'
+		self.port = 8000
+
+		self.config = config
+		self.data_sources = data_sources
+
+		self.service_command = (
+			'docker service create '
+			'--name dchm '
+			'--publish 80:5000/tcp '
+			f'--replicas {self.config.get("aws", {}).get("replicas", 1)} '
+			'--mount type=bind,src=/mnt/efs/data,dst=/data '
+			'npatel79/water-and-land:latest '
+			'flask run --host=0.0.0.0'
+		)
+
+	def start(self):
+		mounts = super().start()
+
+		docker = from_env()
+
+		self.port = self.config['docker'].get('port', self.port)
+		docker.services.create(
+			'npatel79/water-and-land:latest',
+			'flask',
+			args=['run', '--host=0.0.0.0'],
+			endpoint_spec=EndpointSpec(ports={self.port: (5000, 'tcp')}),
+			mode=ServiceMode(
+				mode='replicated',
+				replicas=self.config['docker'].get('replicas', 1)
+			),
+			mounts=mounts,
+			name='nc_slicer',
+			networks=['substrate-nc-slicer-net']
+		)
