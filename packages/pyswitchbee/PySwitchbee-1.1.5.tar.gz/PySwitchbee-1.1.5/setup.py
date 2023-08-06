e79a4b56612a6aa5f5610da6eda609e5c886bd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:24 +0000

    Add package pyswitchbee/1.1.5/PySwitchbee-1.1.5.tar.gz

---
+++ packages/pyswitchbee/PySwitchbee-1.1.5.tar.gz/PySwitchbee-1.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'PySwitchbee',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=['asyncio', 'aiohttp'],
+    version = '1.1.5',
+    description = 'A library to communicate with SwitchBee',
+    author='Jafar Atili',
+    url='https://github.com/jafar-atili/pySwitchbee/',
+    license='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+    python_requires=">=3.6",
+)
