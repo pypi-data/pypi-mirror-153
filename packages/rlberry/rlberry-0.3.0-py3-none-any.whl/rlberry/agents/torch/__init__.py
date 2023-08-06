0368f81590a5d2f193dbdf70e48a3221bd0ca940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:35 +0000

    Add package rlberry/0.3.0/rlberry-0.3.0-py3-none-any.whl

---
+++ packages/rlberry/rlberry-0.3.0-py3-none-any.whl/rlberry/agents/torch/__init__.py
@@ -0,0 +1,4 @@
+# Torch agents (in alphabetical order)
+from .a2c import A2CAgent
+from .dqn import DQNAgent
+from .reinforce import REINFORCEAgent
---
+++ packages/rlberry/rlberry-0.3.0-py3-none-any.whl/rlberry/envs/__init__.py
@@ -0,0 +1,5 @@
+from .gym_make import gym_make, atari_make
+from .basewrapper import Wrapper
+from .classic_control import Acrobot, MountainCar, Pendulum
+from .finite import Chain, FiniteMDP, GridWorld
+from .interface import Model
