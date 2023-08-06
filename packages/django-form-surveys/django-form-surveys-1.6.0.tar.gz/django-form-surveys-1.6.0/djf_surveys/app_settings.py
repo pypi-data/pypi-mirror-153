2d43482d403996f4ba4a23f38849b7bb93a2854c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:21 +0000

    Add package django-form-surveys/1.6.0/django-form-surveys-1.6.0.tar.gz

---
+++ packages/django-form-surveys/django-form-surveys-1.6.0.tar.gz/django-form-surveys-1.6.0/djf_surveys/app_settings.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+
+SURVEY_MASTER_TEMPLATE = settings.SURVEY_MASTER_TEMPLATE if hasattr(settings,
+                                                                    'SURVEY_MASTER_TEMPLATE') else 'djf_surveys/master.html'
+SURVEY_USER_PHOTO_PROFILE = settings.SURVEY_USER_PHOTO_PROFILE if hasattr(settings, 'SURVEY_USER_PHOTO_PROFILE') else ""
+
+DATE_INPUT_FORMAT = settings.DATE_INPUT_FORMAT if hasattr(settings, 'DATE_INPUT_FORMAT') else \
+    ['%Y-%m-%d', '%m/%d/%Y', '%m/%d/%y', '%d/%m/%y', '%d/%m/%Y']
+
+# validators
+field_validators = {
+    'max_length': {
+        'email': 150,
+        'text': 250,
+        'url': 250
+    }
+}
+if hasattr(settings, 'SURVEY_FIELD_VALIDATORS'):
+    max_length = settings.SURVEY_FIELD_VALIDATORS.get('max_length')
+    if max_length:
+        field_validators['max_length'].update(max_length)
+SURVEY_FIELD_VALIDATORS = field_validators
+
+CHART_JS_SRC = settings.CHART_JS_SRC if hasattr(settings, 'CHART_JS_SRC') else '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>'
