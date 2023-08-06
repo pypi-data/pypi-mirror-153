7e4fde9f53e4fe697ef80eaad8b60977e5799586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:05 +0000

    Add package vetiver/0.1.4/vetiver-0.1.4-py3-none-any.whl

---
+++ packages/vetiver/vetiver-0.1.4-py3-none-any.whl/vetiver/handlers/sklearn_vt.py
@@ -0,0 +1,93 @@
+from ..ptype import vetiver_create_ptype
+import pandas as pd
+from ..meta import vetiver_meta
+import sklearn
+
+class SKLearnHandler:
+    """Handler class for creating VetiverModels with sklearn.
+
+    Parameters
+    ----------
+    model : sklearn.base.BaseEstimator
+        a trained sklearn model
+    """
+
+    def __init__(self, model, ptype_data):
+        self.model = model
+        self.ptype_data = ptype_data
+
+    def create_description(self):
+        """Create description for sklearn model
+        """
+        desc = f"Scikit-learn {self.model.__class__} model"
+        return desc
+
+    def vetiver_create_meta(
+        user: list = None,
+        version: str = None,
+        url: str = None,
+        required_pkgs: list = [],
+    ):
+        """Create metadata for sklearn model"""
+        required_pkgs = required_pkgs + ["scikit-learn"]
+        meta = vetiver_meta(user, version, url, required_pkgs)
+
+        return meta
+
+    def ptype(self):
+        """Create data prototype for torch model
+
+        Parameters
+        ----------
+        ptype_data : pd.DataFrame, np.ndarray, or None
+            Training data to create ptype
+
+        Returns
+        -------
+        ptype : pd.DataFrame or None
+            Zero-row DataFrame for storing data types
+        """
+        ptype = vetiver_create_ptype(self.ptype_data)
+        return ptype
+
+    def handler_startup():
+        """Include required packages for prediction
+
+        The `handler_startup` function executes when the API starts. Use this
+        function for tasks like loading packages.
+        """
+        ...
+
+
+    def handler_predict(self, input_data, check_ptype):
+        """Generates method for /predict endpoint in VetiverAPI
+
+        The `handler_predict` function executes at each API call. Use this
+        function for calling `predict()` and any other tasks that must be executed
+        at each API call.
+
+        Parameters
+        ----------
+        input_data:
+            Test data
+
+        Returns
+        -------
+        prediction
+            Prediction from model
+        """
+
+        if check_ptype == True:
+            if isinstance(input_data, pd.DataFrame):
+                prediction = self.model.predict(input_data)
+            else:
+               prediction = self.model.predict([input_data])
+
+        # do not check ptype
+        else:
+            if not isinstance(input_data, list):
+                input_data = [input_data.split(",")]  # user delimiter ?
+
+            prediction = self.model.predict(input_data)
+
+        return prediction
---
+++ packages/vetiver/vetiver-0.1.4-py3-none-any.whl/vetiver/pin_read_write.py
@@ -0,0 +1,85 @@
+import pydantic
+import pins
+import warnings
+import json
+
+from .vetiver_model import VetiverModel
+from .meta import vetiver_meta
+from .write_fastapi import _choose_version
+
+def vetiver_pin_write(board, model: VetiverModel, versioned: bool=True):
+    """
+    Pin a trained VetiverModel along with other model metadata.
+    
+    Parameters
+    ----------
+    board:
+        A pin board, created by `pins.board_folder()` or another `board_` function.
+    model: vetiver.VetiverModel
+        VetiverModel to be written to board
+    versioned: bool
+        Whether or not the pin should be versioned
+    """
+    if not board.allow_pickle_read:
+        raise NotImplementedError # must be pickle-able
+
+    board.pin_write(
+        model.model,
+        name = model.model_name,
+        type = "joblib",
+        description = model.description,
+        metadata = {"required_pkgs": model.metadata.get("required_pkgs"),
+                    "ptype": None if model.ptype == None else model.ptype().json()},
+        versioned=versioned
+    )
+
+    # to do: Model card
+
+    # message = """
+    # Create a Model Card for your published model.
+    # Model Cards provide a framework for transparent, responsible reporting.
+    # Use the vetiver `.Rmd` template as a place to start."""
+
+    # warnings.warn(message=message)
+
+
+def vetiver_pin_read(board, name: str, version: str = None) -> VetiverModel:
+    """
+    Read pin and populate VetiverModel
+    
+    Parameters
+    ----------
+    board:
+        A pin board, created by `pins.board_folder()` or another `board_` function.
+    name: string
+        Pin name
+    version: str
+        Retrieve a specific version of a pin. 
+
+    Returns
+    --------
+    vetiver.VetiverModel
+
+    Notes
+    -----
+    If reading a board from RSConnect, the `board` argument must be in "username/modelname" format.
+    
+    """
+    version = version if version is not None else _choose_version(board.pin_versions(name))
+
+    model = board.pin_read(name, version)
+    meta = board.pin_meta(name)
+
+    v = VetiverModel(model = model,
+        model_name = name,
+        description = meta.description,
+        metadata = vetiver_meta(user = meta.user,
+             version = version,
+             url = meta.user.get("url"), # None all the time, besides Connect
+             required_pkgs = meta.user.get("required_pkgs")
+        ),
+        ptype_data = json.loads(meta.user.get("ptype")) if meta.user.get("ptype") else None,
+        versioned = True
+        )
+    
+    return v
