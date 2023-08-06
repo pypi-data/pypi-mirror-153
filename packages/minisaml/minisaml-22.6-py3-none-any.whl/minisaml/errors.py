704aa6cabcd4ec16d9846274b43c7d0e5df871ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:12 +0000

    Add package minisaml/22.6/minisaml-22.6-py3-none-any.whl

---
+++ packages/minisaml/minisaml-22.6-py3-none-any.whl/minisaml/errors.py
@@ -0,0 +1,28 @@
+import datetime
+from dataclasses import dataclass
+
+
+class MiniSAMLError(Exception):
+    pass
+
+
+class MalformedSAMLResponse(MiniSAMLError):
+    pass
+
+
+@dataclass
+class ResponseExpired(MiniSAMLError):
+    observed_time: datetime.datetime
+    not_on_or_after: datetime.datetime
+
+
+@dataclass
+class ResponseTooEarly(MiniSAMLError):
+    observed_time: datetime.datetime
+    not_before: datetime.datetime
+
+
+@dataclass
+class AudienceMismatch(MiniSAMLError):
+    received_audience: str
+    expected_audience: str
