a11713cfe10e096e14f2ac2c0359c6b346964646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:58 +0000

    Add package nbmeta/0.1a1/nbmeta-0.1a1-py2.py3-none-any.whl

---
+++ packages/nbmeta/nbmeta-0.1a1-py2.py3-none-any.whl/nbmeta/__init__.py
@@ -0,0 +1,18 @@
+"""Manage Jupyter notebooks.
+
+Import the package::
+
+   import nbmeta
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.1a1"  # pre-release for initial release 0.1.0
+
+from ._core import ExampleClass, example_function  # noqa
