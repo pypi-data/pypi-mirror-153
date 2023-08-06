b31952552ffa7b65f555a8930c76a734b6cb910c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:55 +0000

    Add package cpacspy/0.1.2/cpacspy-0.1.2.tar.gz

---
+++ packages/cpacspy/cpacspy-0.1.2.tar.gz/cpacspy-0.1.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+NAME = "cpacspy"
+VERSION = "0.1.2"
+AUTHOR = "Aidan Jungo"
+EMAIL = "aidan.jungo@cfse.ch"
+DESCRIPTION = "PyPI package to work with CPACS file and AeroMaps"
+LONG_DESCRIPTION = "Python package to read, write and analyse CPACS AeroPerformanceMaps. You need to have TIXI and TIGL install on your computer to use this package."
+URL = ""
+REQUIRES_PYTHON = ">=3.7.0"
+REQUIRED = []
+README = "README.md"
+PACKAGE_DIR = "src"
+LICENSE = "Apache License 2.0"
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=URL,
+    include_package_data=True,
+    package_dir={"": PACKAGE_DIR},
+    license=LICENSE,
+    packages=[NAME],
+    python_requires=REQUIRES_PYTHON,
+    keywords=["CPACS", "aircraft", "design", "xml", "aerodynamics", "coefficients", "databases"],
+    install_requires=REQUIRED,
+    # See: https://pypi.org/classifiers/
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+)
