8bc5dad2dc25843e722dc0cb503a29b6c99a86e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:46 +0000

    Add package jsw-bio/1.0.1/jsw-bio-1.0.1.tar.gz

---
+++ packages/jsw-bio/jsw-bio-1.0.1.tar.gz/jsw-bio-1.0.1/README.md
@@ -0,0 +1,17 @@
+# jsw-bio
+> Jsw for biography.
+
+## installation
+```shell
+pip install jsw-bio -U
+```
+
+## usage
+```python
+import jsw_bio as bio
+
+## common methods
+# get fasta/genbank url
+bio.url('7EU9_A', 'fasta')
+bio.url('7EU9_A', 'gb')
+```
---
+++ packages/jsw-bio/jsw-bio-1.0.1.tar.gz/jsw-bio-1.0.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jsw_bio', 'jsw_bio.base']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bs4>=0.0.1,<0.0.2',
+ 'jsw-nx>=1.0.92,<2.0.0',
+ 'psutil>=5.9.0,<6.0.0',
+ 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'jsw-bio',
+    'version': '1.0.1',
+    'description': 'Jsw for biography.',
+    'long_description': "# jsw-bio\n> Jsw for biography.\n\n## installation\n```shell\npip install jsw-bio -U\n```\n\n## usage\n```python\nimport jsw_bio as bio\n\n## common methods\n# get fasta/genbank url\nbio.url('7EU9_A', 'fasta')\nbio.url('7EU9_A', 'gb')\n```\n",
+    'author': 'feizheng',
+    'author_email': '1290657123@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://js.work',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
