0b194c45ee8452a38b6591b7234c7a8b9381c35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package fakedata/0.2/fakedata-0.2.tar.gz

---
+++ packages/fakedata/fakedata-0.2.tar.gz/fakedata-0.2/fakedata/__main__.py
@@ -0,0 +1,33 @@
+import logging
+from fakedata.fakedata import FakeData
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    data = FakeData
+    x = 0
+    while x != 1:
+        nombreRandom = data.RandomName()
+        sex = data.CheckSex(nombreRandom)
+        nombreOne = data.RandomOneName()
+        sexOne = data.CheckSex(nombreOne)
+        nombreTwo = data.RandomTwoName()
+        sexTwo = data.CheckSex(nombreTwo)
+        email = data.RandomEmail(nombreRandom)
+        emailPersonalizado = data.PersonalizedEmail(nombreRandom, 'outlook.com')
+        sexRandom = data.RandomSex()
+        tel = data.RandomTel()
+        logging.debug("Ejecutando pruebas FakeData".center(50, '-'))
+        print(f'Nombre ramdom: {nombreRandom}')
+        print(f'Email random: {email}')
+        print(f'Sexo : {sex}')
+        print(f'Email personalizado: {emailPersonalizado}')
+        print(f'Un solo nombre: {nombreOne}')
+        print(f'Sexo One: {sexOne}')
+        print(f'Dos nombre: {nombreTwo}')
+        print(f'Sexo Two: {sexTwo}')
+        print(f'Sexo Random: {sexRandom}')
+        print(f'Telefono: {tel}')
+        logging.debug("Finalizando pruebas FakeData".center(50, '-'))
+        x += 1
+        
