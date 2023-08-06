3e52ac5233eaba897cd0d7acaa1c9d6948b09725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:58 +0000

    Add package gethash/5.3/gethash-5.3-py3-none-any.whl

---
+++ packages/gethash/gethash-5.3-py3-none-any.whl/gethash/__main__.py
@@ -0,0 +1,45 @@
+from contextlib import suppress
+from typing import List, Optional
+
+import click
+from click import Command, Context
+from natsort import natsort_keygen
+
+from . import __version__
+from ._compat import entry_points
+from .backends import Backend
+from .utils.click import MultiCommandX
+
+PROGRAM_NAME = "gethash"
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"], max_content_width=120)
+EXTRA_SETTINGS = dict(max_suggestions=5, cutoff=0.2)
+
+
+class Cli(MultiCommandX):
+    _entry_points = entry_points(group="gethash.commands")
+
+    def list_commands(self, ctx: Context) -> List[str]:
+        commands = set(self._entry_points.names)
+        for backend in Backend.list_backends():
+            commands.update(backend.algorithms_available)
+        return sorted(commands, key=natsort_keygen())
+
+    def get_command(self, ctx: Context, name: str) -> Optional[Command]:
+        with suppress(KeyError, ImportError):
+            cmd = self._entry_points[name].load()
+            assert isinstance(cmd, Command)
+            return cmd
+        for backend in Backend.list_backends():
+            with suppress(Exception):
+                return backend.load_cmd(name)
+        return None
+
+
+@click.command(PROGRAM_NAME, cls=Cli, context_settings=CONTEXT_SETTINGS, **EXTRA_SETTINGS)
+@click.version_option(__version__, "-V", "--version", prog_name=PROGRAM_NAME)
+def main() -> None:
+    """Generate or check various hash values."""
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gethash/gethash-5.3-py3-none-any.whl/gethash/backends/__init__.py
@@ -0,0 +1,63 @@
+import abc
+from contextlib import suppress
+from typing import Any, List, Set, Tuple
+
+from click import Command
+
+from gethash.script import gethashcli, script_main
+
+__all__ = ["Backend"]
+
+
+class Backend(metaclass=abc.ABCMeta):
+    _registry: List["Backend"] = []
+
+    @classmethod
+    def register_backend(cls) -> None:
+        cls._registry.append(cls())
+
+    @classmethod
+    def list_backends(cls) -> List["Backend"]:
+        return cls._registry
+
+    @property
+    @abc.abstractmethod
+    def algorithms_available(self) -> Set[str]:
+        """Return a set containing the names of the hash algorithms that are
+        available in the backend."""
+
+    @abc.abstractmethod
+    def load_ctx(self, name: str) -> Any:
+        """Load context."""
+
+    def load_cmd(self, name: str) -> Command:
+        """Load command."""
+
+        if name not in self.algorithms_available:
+            raise ValueError(f"unkown algorithm {name!r}")
+
+        display_name = name.upper()
+        doc = f"""Generate or check {display_name}."""
+
+        @gethashcli(command_name=name, display_name=display_name, doc=doc)
+        def main(files: Tuple[str, ...], **kwargs: Any) -> None:
+
+            ctx = self.load_ctx(name)
+            script_main(ctx, files, **kwargs)
+
+        return main
+
+
+def _load_backend() -> None:
+    from .hashlib import HashlibBackend
+
+    HashlibBackend.register_backend()
+
+    # For optional cryptographic backend.
+    with suppress(ImportError):
+        from .pycryptodome import PyCryptodomeBackend
+
+        PyCryptodomeBackend.register_backend()
+
+
+_load_backend()
