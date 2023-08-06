35f03a337c2e0a96f29590ee73e2b25efbb3a8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:56 +0000

    Add package fx-rr/0.1.0/fx_rr-0.1.0.tar.gz

---
+++ packages/fx-rr/fx_rr-0.1.0.tar.gz/fx_rr-0.1.0/README.md
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  3 01:51:30 2022
+
+@author: xieting
+"""
+
+from FX.fx import AUDUSD_return
+from FX.fx import GBPEUR_return
+from FX.fx import USDCAD_return
+from FX.fx import USDJPY_return
+from FX.fx import USDMXN_return
+from FX.fx import EURUSD_return
+from FX.fx import USDCNY_return
+from FX.fx import USDCZK_return
+from FX.fx import USDPLN_return
+from FX.fx import USDINR_return
+from FX.fx import USDRUB_return
+
+
---
+++ packages/fx-rr/fx_rr-0.1.0.tar.gz/fx_rr-0.1.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  3 01:40:21 2022
+
+@author: xieting
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="fx_rr",
+    version="0.1.0",
+    description="Demo library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="TingXie",
+    author_email="tx658@nyu.edu",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+
+    include_package_data=True,
+    install_requires=["math"]
+)
