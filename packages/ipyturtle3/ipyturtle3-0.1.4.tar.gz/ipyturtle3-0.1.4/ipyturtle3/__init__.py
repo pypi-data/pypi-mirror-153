1d846346370cdb89a379c71f07b9b40f34fe63db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:27 +0000

    Add package ipyturtle3/0.1.4/ipyturtle3-0.1.4.tar.gz

---
+++ packages/ipyturtle3/ipyturtle3-0.1.4.tar.gz/ipyturtle3-0.1.4/ipyturtle3/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) William Navaraj.
+# Distributed under Apache 2.0 license
+"""
+ipyturtle3.
+Turtle graphics based on ipycanvas which can run on jupyter lab/vscode. 
+ipyturtle and ipyturtle2 was found to run only on classic jupyter notebook. 
+This was developed to solve this.
+"""
+from .ipyturtle3 import Turtle, Canvas, TurtleScreen,hold_canvas, Shape
+
+__version__ = "0.1.4"
+__author__ = 'William Navaraj'
+__homepage__ = 'https://github.com/williamnavaraj'
+__all__ =['Turtle','Canvas','TurtleScreen','hold_canvas','Shape']
---
+++ packages/ipyturtle3/ipyturtle3-0.1.4.tar.gz/ipyturtle3-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ipyturtle3',
+    version='0.1.4',    
+    description='Turtle graphics based on ipycanvas which can run on jupyter lab/notebook/vscode. ',
+    url='https://github.com/williamnavaraj/ipyturtle3.git',
+    author='William Navaraj',
+    author_email='williamnavaraj@gmail.com',
+    license='Apache 2.0',
+    packages=['ipyturtle3'],
+    install_requires=['ipycanvas',
+                      'webcolors'
+                      ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Apache Software License',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
