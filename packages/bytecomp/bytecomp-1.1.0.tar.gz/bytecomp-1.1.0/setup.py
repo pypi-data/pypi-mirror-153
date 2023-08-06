1d7c766f3acb256b3747cc491b5356b0624312d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:13 +0000

    Add package bytecomp/1.1.0/bytecomp-1.1.0.tar.gz

---
+++ packages/bytecomp/bytecomp-1.1.0.tar.gz/bytecomp-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytecomp",
+    version="1.1.0",
+    author="DeKrypt",
+    author_email="gregcoolkidd@gmail.com",
+    description="Utilities to work with bytecode.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dekrypted/bytecomp",
+    project_urls={
+        "Bug Tracker": "https://github.com/dekrypted/bytecomp/issues",
+    },
+    keywords=['bytecode','python'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.5",
+)
