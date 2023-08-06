9a2d9913d1f986a7e1f01e6490d8cf24114cb30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:10 +0000

    Add package HiPart/0.1.15/HiPart-0.1.15-py3-none-any.whl

---
+++ packages/HiPart/HiPart-0.1.15-py3-none-any.whl/HiPart/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from KDEpy.NaiveKDE import NaiveKDE
+from KDEpy.TreeKDE import TreeKDE
+from KDEpy.FFTKDE import FFTKDE
+
+__version__ = "0.1.15"
+__author__ = "Panagiotis Anagnostou"
+
+TreeKDE = TreeKDE
+NaiveKDE = NaiveKDE
+FFTKDE = FFTKDE
