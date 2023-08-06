992280eca7c19c653ec116ec5b0a762eedfabd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:59 +0000

    Add package minos-microservice-cqrs/0.8.0.dev2/minos_microservice_cqrs-0.8.0.dev2-py3-none-any.whl

---
+++ packages/minos-microservice-cqrs/minos_microservice_cqrs-0.8.0.dev2-py3-none-any.whl/minos/cqrs/handlers.py
@@ -0,0 +1,40 @@
+from __future__ import (
+    annotations,
+)
+
+import logging
+from typing import (
+    TypeVar,
+)
+
+from minos.aggregate import (
+    Delta,
+    RefResolver,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class PreEventHandler:
+    """Pre Event Handler class."""
+
+    @classmethod
+    async def handle(cls, diff: T, resolve_references: bool = False, **kwargs) -> T:
+        """Handle Ref resolution for Events.
+
+        :param diff: The instance containing ``Ref`` instances.
+        :param resolve_references: If ``True`` the resolution is performed, otherwise it is skipped.
+        :param kwargs: Additional named arguments.
+        :return: The original instance with the ``Ref`` references already resolved.
+        """
+        if not isinstance(diff, Delta) or not resolve_references:
+            return diff
+
+        try:
+            return await RefResolver(**kwargs).resolve(diff)
+        except Exception as exc:
+            logger.exception(f"An exception was raised while trying to resolve model references: {exc!r}")
+            return diff
+
+
+T = TypeVar("T")
---
+++ packages/minos-microservice-cqrs/minos_microservice_cqrs-0.8.0.dev2-py3-none-any.whl/minos/cqrs/services.py
@@ -0,0 +1,61 @@
+from abc import (
+    ABC,
+)
+from functools import (
+    partial,
+)
+
+from minos.aggregate import (
+    Aggregate,
+)
+from minos.common import (
+    Inject,
+)
+from minos.networks import (
+    Request,
+    WrappedRequest,
+)
+
+from .exceptions import (
+    MinosIllegalHandlingException,
+)
+from .handlers import (
+    PreEventHandler,
+)
+
+
+class Service(ABC):
+    """Base Service class"""
+
+    @Inject()
+    def __init__(self, aggregate: Aggregate, **kwargs):
+        self.aggregate = aggregate
+
+    @staticmethod
+    def _pre_event_handle(request: Request) -> Request:
+        fn = partial(PreEventHandler.handle, user=request.user)
+        return WrappedRequest(request, fn)
+
+
+class CommandService(Service, ABC):
+    """Command Service class"""
+
+    @staticmethod
+    def _pre_command_handle(request: Request) -> Request:
+        return request
+
+    @staticmethod
+    def _pre_query_handle(request: Request) -> Request:
+        raise MinosIllegalHandlingException("Queries cannot be handled by `CommandService` inherited classes.")
+
+
+class QueryService(Service, ABC):
+    """Query Service class"""
+
+    @staticmethod
+    def _pre_command_handle(request: Request) -> Request:
+        raise MinosIllegalHandlingException("Commands cannot be handled by `QueryService` inherited classes.")
+
+    @staticmethod
+    def _pre_query_handle(request: Request) -> Request:
+        return request
