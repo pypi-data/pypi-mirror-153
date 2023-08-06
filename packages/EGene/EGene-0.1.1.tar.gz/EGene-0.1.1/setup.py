6a1f76080c27459ec9a79e52a0e427be157e78ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package EGene/0.1.1/EGene-0.1.1.tar.gz

---
+++ packages/EGene/EGene-0.1.1.tar.gz/EGene-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="EGene",
+    version="0.1.1",
+    author="Ethan Anderson",
+    author_email="telan4892@gmail.com",
+    url="https://github.com/Elan456/EGene",
+    install_requires=['pygame'],
+    packages=["egene"]
+)
