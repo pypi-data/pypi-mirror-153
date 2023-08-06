c281a44693ed96849d5a5e5f7048518a3ad7a159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:14 +0000

    Add package colemen-utils/0.0.0/colemen_utils-0.0.0-py3-none-any.whl

---
+++ packages/colemen-utils/colemen_utils-0.0.0-py3-none-any.whl/facades/rand_utils_facade.py
@@ -0,0 +1,33 @@
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+'''
+    A module of utility methods used for generating random data or performing various
+    actions randomly.
+
+    ----------
+
+    Meta
+    ----------
+    `author`: Colemen Atwood
+    `created`: 06-03-2022 10:22:15
+    `memberOf`: rand
+    `version`: 1.0
+    `method_name`: rand
+'''
+
+
+
+
+
+
+
+from utils.object_utils import rand_option as option
+from utils.string_generation import text,phone,email,url,abstract_name,rand
+
+
+
+
+
+
+
+
