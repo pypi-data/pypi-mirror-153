1c08f0d15da0cd25bed41df0bb8544a94018654c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:27 +0000

    Add package TAssist/0.0.3/TAssist-0.0.3-py3-none-any.whl

---
+++ packages/TAssist/TAssist-0.0.3-py3-none-any.whl/TAssist/__init__.py
@@ -0,0 +1,10 @@
+import TAssist.TA_Globals
+import TAssist.TA_Graphs
+import TAssist.TAssist
+
+import TAssist.TA_Graphs
+from TAssist.TA_Globals import CATEGORIES, COLORS, course_emojis
+from TAssist.TAssist import Student
+from TAssist.TAssist import Course
+from TAssist.TAssist import Assignment
+from TAssist.TAssist import Mark
