d24ffff3a2a28db684476973c45797cc07aa97de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:16 +0000

    Add package TAssist/0.0.2/TAssist-0.0.2.tar.gz

---
+++ packages/TAssist/TAssist-0.0.2.tar.gz/TAssist-0.0.2/TAssist/__init__.py
@@ -0,0 +1,10 @@
+import TA_Globals
+import TA_Graphs
+import TAssist
+
+import TAssist.TA_Graphs
+from TAssist.TA_Globals import CATEGORIES, COLORS, course_emojis
+from TAssist.TAssist import Student
+from TAssist.TAssist import Course
+from TAssist.TAssist import Assignment
+from TAssist.TAssist import Mark
