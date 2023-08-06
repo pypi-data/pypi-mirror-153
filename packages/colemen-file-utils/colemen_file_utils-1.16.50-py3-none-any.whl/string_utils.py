9597ede6581b348d71624fe51ec4f0c4c192b4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:00 +0000

    Add package colemen-file-utils/1.16.50/colemen_file_utils-1.16.50-py3-none-any.whl

---
+++ packages/colemen-file-utils/colemen_file_utils-1.16.50-py3-none-any.whl/string_utils.py
@@ -0,0 +1,94 @@
+"""
+    String manipulation/generation utilities used by other modules.
+"""
+
+import json
+import shutil
+import re
+from pathlib import Path
+import hashlib
+import objectUtils as objUtils
+import file_read as read
+
+
+# todo - DOCUMENTATION FOR METHODS
+
+# def parse_size(string):
+#     known_sizes = ['b']
+
+
+def sanitize_windows_file_name(file_name):
+    '''
+        Strips (window's) invalid characters from the file_name.
+
+        ----------
+        `file_name` {str}
+            The string to be parsed.
+
+        ----------
+        `return` {str}
+            The parsed string
+    '''
+    return re.sub(r'[<>:"/\|?*]*', "", file_name)
+
+
+def generate_hash(value):
+    """
+        Generates a sha256 hash from the string provided.
+
+        ----------
+        `value` {str}
+            The string to calculate the hash on.
+
+        ----------
+        `return` {str}
+            The sha256 hash
+    """
+    jsonStr = json.dumps(value).encode('utf-8')
+    hex_dig = hashlib.sha256(jsonStr).hexdigest()
+    return hex_dig
+
+
+def format_extension(string):
+    """
+        Formats a file extension to have no leading period.
+
+        ----------
+        `value` {str|list}
+            The file extension or list of extensions to format
+
+        ----------
+        `return` {str}
+            The formatted file extension
+    """
+    new_ext_array = []
+    if isinstance(string, list) is False:
+        string = [string]
+
+    for ext in string:
+        # print(f"ext: {ext}")
+        ext = ext.lower()
+        ext = re.sub(r"^\.*", '', ext)
+        new_ext_array.append(ext)
+
+    if len(new_ext_array) == 1:
+        return new_ext_array[0]
+    return new_ext_array
+
+
+def dynamic_regex_search(string, regex):
+    """
+        Formats a file extension to have no leading period.
+
+        ----------
+        `value` {str|list}
+            The file extension or list of extensions to format
+
+        ----------
+        `return` {str}
+            The formatted file extension
+    """
+    match = re.search(regex, string)
+    if match is not None:
+        return match
+    return False
---
+++ packages/colemen-file-utils/colemen_file_utils-1.16.50-py3-none-any.whl/utils/file_search.py
@@ -0,0 +1,102 @@
+import json
+# import shutil
+import os
+import re
+# from pathlib import Path
+import utils.objectUtils as objUtils
+import utils.file_read as read
+import utils.string_utils as strUtils
+
+# todo - DOCUMENTATION FOR METHODS
+
+
+def by_name(file_name, search_path=None, **kwargs):
+    '''
+        Searches the path provided for files that match the file_name
+
+        ----------
+        Arguments
+        -----------------
+        `file_name` {str|list}
+            The string or list of strings to search for.
+        `search_path`=cwd {str}
+            The directory to search within.
+
+        Keyword Arguments
+        -----------------
+            `recursive`=True {boolean}
+                If True the path is searched recursively
+
+            `case_sensitive`=True {bool}
+                If False case is ignored.
+            `exact_match`=True {bool}
+                If False it will match with any file that contains the file_name argument
+            `regex`=False {bool}
+                If True the file_name arg is treated as a regex string for comparisons.
+
+        Return
+        ----------
+        `return` {None|list}
+            A list of matching files or None if no matching files are found.
+    '''
+    if isinstance(file_name, list) is False:
+        file_name = [file_name]
+    if search_path is None:
+        search_path = os.getcwd()
+
+    extensions = strUtils.format_extension(objUtils.get_kwarg(['extensions', 'ext'], [], (list, str), **kwargs))
+    if isinstance(extensions, str):
+        extensions = [extensions]
+
+    recursive = objUtils.get_kwarg(['recursive', 'recurse'], True, bool, **kwargs)
+    case_sensitive = objUtils.get_kwarg(['case_sensitive'], True, (bool), **kwargs)
+    exact_match = objUtils.get_kwarg(['exact_match'], True, (bool), **kwargs)
+    regex = objUtils.get_kwarg(['regex', 'use regex'], False, (bool), **kwargs)
+
+    if case_sensitive is False and regex is False:
+        new_name_array = []
+        for name in file_name:
+            if isinstance(name, str):
+                new_name_array.append(name.lower())
+        file_name = new_name_array
+
+    result_array = []
+    # pylint: disable=unused-variable
+    # pylint: disable=too-many-nested-blocks
+    for root, folders, files in os.walk(search_path):
+        for file in files:
+            skip = False
+            current_file_path = os.path.join(root, file)
+            test_file_name = file
+
+            test_file_ext = strUtils.format_extension(os.path.splitext(os.path.basename(current_file_path))[1])
+            if case_sensitive is False:
+                test_file_name = test_file_name.lower()
+
+            if len(extensions) > 0:
+                if test_file_ext not in extensions:
+                    skip = True
+
+            if skip is False:
+                for name in file_name:
+                    if len(name) == 0 or "*" == name:
+                        result_array.append(current_file_path)
+                        break
+
+                    if regex is not False:
+                        match = re.search(name, test_file_name)
+                        if match is not None:
+                            result_array.append(current_file_path)
+
+                    if exact_match is True:
+                        if test_file_name == name:
+                            result_array.append(current_file_path)
+                    else:
+                        if name in test_file_name:
+                            result_array.append(current_file_path)
+        if recursive is False:
+            break
+
+    if len(result_array) > 0:
+        return result_array
+    return None
