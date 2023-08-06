a15170ef3c7259e9a913a0509d0f4c23e716a04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:50 +0000

    Add package shenarder/1.0/shenarder-1.0.tar.gz

---
+++ packages/shenarder/shenarder-1.0.tar.gz/shenarder-1.0/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+import setuptools
+packages = ['zpl-test']
+setup(name='shenarder', version='1.0', author='zhou huan', packages=packages, package_dir={'requests': 'requests'})
