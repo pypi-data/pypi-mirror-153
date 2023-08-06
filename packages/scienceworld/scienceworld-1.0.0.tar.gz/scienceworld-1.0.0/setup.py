68b0bd68664d0dd5b9b4b65c25d534f9e111ebe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:43 +0000

    Add package scienceworld/1.0.0/scienceworld-1.0.0.tar.gz

---
+++ packages/scienceworld/scienceworld-1.0.0.tar.gz/scienceworld-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import os.path, sys
+
+from setuptools import setup
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+JAR_FILE = 'scienceworld-1.0.0.jar'
+JAR_PATH = os.path.join(BASEPATH, 'scienceworld', JAR_FILE)
+OBJECTS_LUT_FILE = "object_type_ids.tsv"
+
+# TODO: build jar using package.sh?
+
+if not os.path.isfile(JAR_PATH):
+    print('ERROR: Unable to find required library:', JAR_PATH)
+    sys.exit(1)
+
+setup(name='scienceworld',
+    version='1.0.0',
+    description='ScienceWorld: An interactive text environment to study AI agents on accomplishing tasks from the standardized elementary science curriculum.',
+    author='Peter Jansen',
+    packages=['scienceworld'],
+    include_package_data=True,
+    package_dir={'scienceworld': 'scienceworld'},
+    package_data={'scienceworld': [JAR_FILE, OBJECTS_LUT_FILE]},
+    url="https://scienceworld.github.io",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=open('requirements.txt').readlines(),
+    extras_require={
+        'webserver': open('requirements.txt').readlines() + ['pywebio'],
+    },
+)
