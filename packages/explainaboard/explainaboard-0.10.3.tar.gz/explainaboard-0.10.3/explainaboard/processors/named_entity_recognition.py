9f1175a9fa879dc693eda83ce6404dcb9d4968cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:51 +0000

    Add package explainaboard/0.10.3/explainaboard-0.10.3.tar.gz

---
+++ packages/explainaboard/explainaboard-0.10.3.tar.gz/explainaboard-0.10.3/explainaboard/processors/named_entity_recognition.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from explainaboard import TaskType
+from explainaboard.metric import MetricConfig, SeqF1ScoreConfig
+from explainaboard.processors.processor_registry import register_processor
+from explainaboard.processors.sequence_labeling import SeqLabProcessor
+from explainaboard.utils.span_utils import BIOSpanOps
+
+
+@register_processor(TaskType.named_entity_recognition)
+class NERProcessor(SeqLabProcessor):
+    @classmethod
+    def task_type(cls) -> TaskType:
+        return TaskType.named_entity_recognition
+
+    @classmethod
+    def default_metrics(
+        cls, source_language=None, target_language=None
+    ) -> list[MetricConfig]:
+        return [
+            SeqF1ScoreConfig(
+                name='F1',
+                source_language=source_language,
+                target_language=target_language,
+                tag_schema='bio',
+            )
+        ]
+
+    @classmethod
+    def default_span_ops(cls) -> BIOSpanOps:
+        return BIOSpanOps()
