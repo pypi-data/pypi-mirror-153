767b0d523f8f7d3216139ed4e4eff9c6be349c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:26 +0000

    Add package asyncprocess/0.0.4/asyncprocess-0.0.4.tar.gz

---
+++ packages/asyncprocess/asyncprocess-0.0.4.tar.gz/asyncprocess-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Process managing async task'
+LONG_DESCRIPTION = 'Simple package that help you isolate your code in async task and run them on a single process with asyncio.'
+
+setup(
+    name="asyncprocess",
+    version=VERSION,
+    author="Marc-Antoine St-Pierre",
+    author_email="<yakinquiries@gmail.com>",
+    url='https://github.com/Yakkuru/asyncprocess',
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    python_requires="~=3.7",
+    keywords=['python', 'asyncio', 'async', 'task'],
+)
