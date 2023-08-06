151c8cef521b4d81e02c24220bd444b725bb2518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:43 +0000

    Add package maida/0.1.15/maida-0.1.15.tar.gz

---
+++ packages/maida/maida-0.1.15.tar.gz/maida-0.1.15/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maida",
+    version="0.1.15",
+    author="maida",
+    author_email="624486877@qq.com",
+    description="No description.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LZC6244/maida",
+    packages=setuptools.find_packages(),
+    install_requires=['cnocr'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
