305e9de515da2f58a932566b76674f1b1ac84a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:01 +0000

    Add package datacraft/0.4.0/datacraft-0.4.0.tar.gz

---
+++ packages/datacraft/datacraft-0.4.0.tar.gz/datacraft-0.4.0/datacraft/logging_handler.py
@@ -0,0 +1,32 @@
+"""
+Default module for configuring logging
+"""
+import logging
+
+from . import registries
+
+_MAPPING = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warn": logging.WARN,
+    "error": logging.ERROR
+}
+
+
+@registries.Registry.logging('default')
+def _configure_logging(loglevel: str):
+    """
+    Default Handler for Logging Configuration
+
+    Args:
+        loglevel: loglevel as specified from the command line
+    """
+    if str(loglevel).lower() in ['off', 'stop', 'disable']:
+        logging.disable(logging.CRITICAL)
+    else:
+        level = _MAPPING.get(loglevel, registries.get_default('log_level'))
+        logging.basicConfig(
+            format='%(levelname)s [%(asctime)s] %(message)s',
+            level=level,
+            datefmt='%d-%b-%Y %I:%M:%S %p'
+        )
---
+++ packages/datacraft/datacraft-0.4.0.tar.gz/datacraft-0.4.0/datacraft/supplier/nested.py
@@ -0,0 +1,10 @@
+"""
+Module for nested type implementations
+"""
+from typing import Dict, Any
+
+from .model import KeyProviderInterface, ValueSupplierInterface
+from .exceptions import SupplierException
+
+
+
