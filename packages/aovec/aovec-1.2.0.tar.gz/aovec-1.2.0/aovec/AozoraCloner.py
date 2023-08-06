d645962ff79fa26d92bd4d6d2d6869e0d395257a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:16 +0000

    Add package aovec/1.2.0/aovec-1.2.0.tar.gz

---
+++ packages/aovec/aovec-1.2.0.tar.gz/aovec-1.2.0/aovec/AozoraCloner.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import os
+import subprocess
+import sys
+from shutil import which
+
+
+class GitIsNotInstalled(Exception):
+    pass
+
+
+class AozoraCloner:
+    url = "https://github.com/aozorabunko/aozorabunko"
+
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def get_url(cls) -> str:
+        return cls.url
+
+    @classmethod
+    def set_url(cls, url: str) -> None:
+        cls.url = url
+
+    @classmethod
+    def clone(cls) -> int | None:
+        if not which("git"):
+            raise GitIsNotInstalled
+        else:
+            p = subprocess.Popen("git clone --depth 1 " + cls.url, shell=True)
+            p.wait()
+            return p.poll()
+
+
+if __name__ == "__main__":
+    REPO = os.path.join(os.getcwd(), "aozorabunko")
+
+    if os.path.isdir(REPO) and os.path.isdir(os.path.join(REPO, ".git")):
+        print("aozorabunko already exists!", file=sys.stderr)
+        exit(0)
+    else:
+        AozoraCloner().clone()
