81d5fe2117f1fc3b0065bb8c35662b32e519e2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package malyzm/1.0/malyzm-1.0.tar.gz

---
+++ packages/malyzm/malyzm-1.0.tar.gz/malyzm-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+    name='malyzm',
+    version='1.0',
+    description="Pakiet testowy",
+    url='http://sda.com',
+    install_requires=[],
+    packages=['pakiet_testowy'],
+
+
+)
