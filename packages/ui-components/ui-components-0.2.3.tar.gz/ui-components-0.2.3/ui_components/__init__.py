8e326eca9f4f5365d1ee4c22f4715351049c5505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:23 +0000

    Add package ui-components/0.2.3/ui-components-0.2.3.tar.gz

---
+++ packages/ui-components/ui-components-0.2.3.tar.gz/ui-components-0.2.3/ui_components/__init__.py
@@ -0,0 +1,10 @@
+from ui_components.compiler import DocCompiler
+from ui_components.components import (
+    annotation,
+    component,
+    date_constraint,
+    multiselect,
+    option,
+    select_options,
+    text_input,
+)
