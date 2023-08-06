bfca3a45f7a5ddc549e9f68d6d03959b72c06afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:20 +0000

    Add package maxtest/0.0.2/maxtest-0.0.2.tar.gz

---
+++ packages/maxtest/maxtest-0.0.2.tar.gz/maxtest-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'maxtest'
+DESCRIPTION = 'maxtest'
+URL = 'https://github.com/nevergiveupzsj/maxtest'
+EMAIL = '1105522860@qq.com'
+AUTHOR = 'Sun bz'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.0.2'
+
+# What packages are required for this module to be executed?
+REQUIRED = ["numpy", "matplotlib", "pandas", "polygon", "sqlalchemy"]
+
+# Setting.
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    license="MIT"
+)
