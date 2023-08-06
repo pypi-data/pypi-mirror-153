9605f012994deecaa2d7391ec743c86ac5a27510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:12 +0000

    Add package span-cli/1.2.7/span_cli-1.2.7-py3-none-any.whl

---
+++ packages/span-cli/span_cli-1.2.7-py3-none-any.whl/span/properties.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+home = Path.home()
+files = {
+    'history': f'{home}/span/history',
+    'instances': f'{home}/span/instances',
+    'key': f'{home}/span/key',
+    'account': f'{home}/span/account'
+}
