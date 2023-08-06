6e616652387595c5c0af557cb3891acf62b77576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:07 +0000

    Add package statick/0.8.1/statick-0.8.1.tar.gz

---
+++ packages/statick/statick-0.8.1.tar.gz/statick-0.8.1/statick_tool/plugins/tool/mypy_tool_plugin.py
@@ -0,0 +1,94 @@
+"""Apply mypy tool and gather results."""
+import logging
+import re
+import subprocess
+import sys
+from typing import List, Match, Optional, Pattern
+
+from statick_tool.issue import Issue
+from statick_tool.package import Package
+from statick_tool.tool_plugin import ToolPlugin
+
+
+class MypyToolPlugin(ToolPlugin):
+    """Apply mypy tool and gather results."""
+
+    def get_name(self) -> str:
+        """Get name of tool."""
+        return "mypy"
+
+    # pylint: disable=too-many-locals, too-many-branches, too-many-return-statements
+    def scan(self, package: Package, level: str) -> Optional[List[Issue]]:
+        """Run tool and gather output."""
+        if "python_src" not in package:
+            return []
+
+        flags: List[str] = [
+            "--show-absolute-path",
+            "--show-error-codes",
+            "--no-error-summary",
+        ]
+        user_flags = self.get_user_flags(level)
+        flags += user_flags
+        tool_bin = "mypy"
+        total_output: List[str] = []
+
+        for src in package["python_src"]:
+            try:
+                subproc_args = [tool_bin, src] + flags
+                output = subprocess.check_output(
+                    subproc_args, stderr=subprocess.STDOUT, universal_newlines=True
+                )
+
+            except (IOError, OSError) as ex:
+                logging.warning("mypy binary failed: %s", tool_bin)
+                logging.warning("Error = %s", ex.strerror)
+                return []
+
+            except subprocess.CalledProcessError as ex:
+                logging.warning("mypy binary failed: %s.", tool_bin)
+                logging.warning("Returncode: %d", ex.returncode)
+                logging.warning("%s exception: %s", self.get_name(), ex.output)
+                total_output.append(ex.output)
+                continue
+
+        for output in total_output:
+            logging.debug("%s", output)
+
+        if self.plugin_context and self.plugin_context.args.output_directory:
+            with open(self.get_name() + ".log", "w", encoding="utf8") as fid:
+                for output in total_output:
+                    fid.write(output)
+
+        issues: List[Issue] = self.parse_output(total_output)
+        return issues
+
+    # pylint: disable=too-many-locals, too-many-branches, too-many-return-statements
+
+    def parse_output(self, total_output: List[str]) -> List[Issue]:
+        """Parse tool output and report issues."""
+        # file:line: severity: msg type
+        tool_re = r"(.+):(\d+):\s(.+):\s(.+)\s(.+)"
+        parse: Pattern[str] = re.compile(tool_re)
+        issues: List[Issue] = []
+
+        for output in total_output:
+            lines = output.splitlines()
+            for line in lines:
+                if sys.platform != "win32" and not line.startswith("/"):
+                    continue
+                match: Optional[Match[str]] = parse.match(line)
+                if match:
+                    issue_type = match.group(5).strip("[]")
+                    issues.append(
+                        Issue(
+                            match.group(1),
+                            match.group(2),
+                            self.get_name(),
+                            issue_type,
+                            "5",
+                            match.group(4),
+                            None,
+                        )
+                    )
+        return issues
---
+++ packages/statick/statick-0.8.1.tar.gz/statick-0.8.1/statick_tool/plugins/tool/pydocstyle_tool_plugin.py
@@ -0,0 +1,100 @@
+"""Apply pydocstyle tool and gather results."""
+import logging
+import re
+import subprocess
+from typing import List, Match, Optional, Pattern
+
+from statick_tool.issue import Issue
+from statick_tool.package import Package
+from statick_tool.tool_plugin import ToolPlugin
+
+
+class PydocstyleToolPlugin(ToolPlugin):
+    """Apply pydocstyle tool and gather results."""
+
+    def get_name(self) -> str:
+        """Get name of tool."""
+        return "pydocstyle"
+
+    def scan(self, package: Package, level: str) -> Optional[List[Issue]]:
+        """Run tool and gather output."""
+        flags: List[str] = []
+        user_flags = self.get_user_flags(level)
+        flags += user_flags
+        total_output = []
+
+        tool_bin = "pydocstyle"
+        for src in package["python_src"]:
+            try:
+                subproc_args = [tool_bin, src] + flags
+                output = subprocess.check_output(
+                    subproc_args, stderr=subprocess.STDOUT, universal_newlines=True
+                )
+
+            except subprocess.CalledProcessError as ex:
+                # Return code 1 just means "found problems"
+                if ex.returncode != 1:
+                    logging.warning("Problem %d", ex.returncode)
+                    logging.warning("%s exception: %s", self.get_name(), ex.output)
+                    return None
+
+                output = ex.output
+
+            except OSError as ex:
+                logging.warning("Couldn't find %s! (%s)", tool_bin, ex)
+                return None
+
+            logging.debug("%s: %s", src, output)
+
+            total_output.append(output)
+
+        if self.plugin_context and self.plugin_context.args.output_directory:
+            with open(self.get_name() + ".log", "w", encoding="utf8") as fid:
+                for output in total_output:
+                    fid.write(output)
+
+        issues: List[Issue] = self.parse_output(total_output)
+        return issues
+
+    def parse_output(  # pylint: disable=too-many-locals
+        self, total_output: List[str]
+    ) -> List[Issue]:
+        """Parse tool output and report issues."""
+        tool_re = r"(.+):(\d+)"
+        parse_first: Pattern[str] = re.compile(tool_re)
+        tool_re_second = r"\s(.+):\s(.+)"
+        parse_second: Pattern[str] = re.compile(tool_re_second)
+        issues: List[Issue] = []
+        filename = ""
+        line_number = "0"
+        issue_type = ""
+        message = ""
+
+        for output in total_output:
+            first_line = True
+            for line in output.splitlines():
+                if first_line:
+                    match: Optional[Match[str]] = parse_first.match(line)
+                    first_line = False
+                    if match:
+                        filename = match.group(1)
+                        line_number = match.group(2)
+                else:
+                    match_second: Optional[Match[str]] = parse_second.match(line)
+                    first_line = True
+                    if match_second:
+                        issue_type = match_second.group(1)
+                        message = match_second.group(2)
+                        issues.append(
+                            Issue(
+                                filename,
+                                line_number,
+                                self.get_name(),
+                                issue_type,
+                                "5",
+                                message,
+                                None,
+                            )
+                        )
+
+        return issues
