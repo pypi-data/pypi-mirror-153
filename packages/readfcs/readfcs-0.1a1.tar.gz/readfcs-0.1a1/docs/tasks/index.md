89bfa5968b00ad3167163fade3373a212670316d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:29 +0000

    Add package readfcs/0.1a1/readfcs-0.1a1.tar.gz

---
+++ packages/readfcs/readfcs-0.1a1.tar.gz/readfcs-0.1a1/docs/tasks/index.md
@@ -0,0 +1,9 @@
+# Tasks
+
+```{toctree}
+:maxdepth: 1
+:glob:
+:reversed:
+
+2022-05-24-readfcs-tools
+```
---
+++ packages/readfcs/readfcs-0.1a1.tar.gz/readfcs-0.1a1/readfcs/__init__.py
@@ -0,0 +1,17 @@
+"""Parse fcs files into AnnData.
+
+Import the package::
+
+   import fcsreader
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   FCSFile
+"""
+
+__version__ = "0.1a1"  # pre-release for initial release 0.1.0
+
+from ._core import FCSFile  # noqa
