5a3958c3cba7e04d53ef983321f603432e5503a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:39 +0000

    Add package vedro-allure-reporter/1.3.0/vedro-allure-reporter-1.3.0.tar.gz

---
+++ packages/vedro-allure-reporter/vedro-allure-reporter-1.3.0.tar.gz/vedro-allure-reporter-1.3.0/vedro_allure_reporter/__init__.py
@@ -0,0 +1,6 @@
+from allure_commons.model2 import Label as AllureLabel
+
+from ._allure_reporter import AllureReporter, AllureReporterPlugin
+
+__version__ = "1.3.0"
+__all__ = ("AllureReporter", "AllureReporterPlugin", "AllureLabel",)
