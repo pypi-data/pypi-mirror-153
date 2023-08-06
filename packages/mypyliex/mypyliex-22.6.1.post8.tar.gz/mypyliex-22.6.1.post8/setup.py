1aa20b704d3a04f4eb77fc427868eb55b18f7782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:46 +0000

    Add package mypyliex/22.6.1.post8/mypyliex-22.6.1.post8.tar.gz

---
+++ packages/mypyliex/mypyliex-22.6.1.post8.tar.gz/mypyliex-22.6.1.post8/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['mypyliex', 'mypyliex.classes']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'apischema>=0.17.5,<0.18.0', 'jsonschema>=4.4.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['my_python_library_example = mypyliex.main:main']}
+
+setup_kwargs = {
+    'name': 'mypyliex',
+    'version': '22.6.1.post8',
+    'description': 'Project description',
+    'long_description': '# MLE README file',
+    'author': 'Tadeusz Miszczyk',
+    'author_email': 'tadeusz.miszczyk@bshg.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'http://github.com/8tm/my_python_library_example',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
