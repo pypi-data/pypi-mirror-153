d18397c788f548d12f41cfeecab9eb6521552180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:50 +0000

    Add package dist17/0.1/dist17-0.1.tar.gz

---
+++ packages/dist17/dist17-0.1.tar.gz/dist17-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dist17',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author='Donald Kelly',
+      author_email='myemail017@gmail.com',
+      url='',
+      packages=['distributions'],
+      zip_safe=False)
