c5d67f14970fbd1a9e0c4e9175032f31900b99e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:52 +0000

    Add package martenczuk/0.1.9/martenczuk-0.1.9.tar.gz

---
+++ packages/martenczuk/martenczuk-0.1.9.tar.gz/martenczuk-0.1.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.1.9'
+DESCRIPTION = 'SQL Server,CSV & JSON Connection Objects and Tools'
+
+# Setting up
+setup(
+    name="martenczuk",
+    version=VERSION,
+    author="Kamil Martenczuk",
+    author_email="<kamil.martenczuk@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pypyodbc', 'tqdm', 'colorama'],
+    keywords=['python', 'sql', 'csv', 'json'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
