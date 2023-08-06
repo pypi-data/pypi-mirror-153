35a67c88e522ae7728caeda36fcb254333f57924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:51 +0000

    Add package lightbulb-ext-filament/0.1.2.post0/lightbulb-ext-filament-0.1.2.post0.tar.gz

---
+++ packages/lightbulb-ext-filament/lightbulb-ext-filament-0.1.2.post0.tar.gz/lightbulb-ext-filament-0.1.2.post0/setup.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# Copyright © tandemdude 2020-present
+#
+# This file is part of Filament.
+#
+# Filament is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Filament is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Filament. If not, see <https://www.gnu.org/licenses/>.
+from setuptools import setup, find_namespace_packages
+import os
+import types
+import re
+
+name = "lightbulb"
+
+
+def parse_meta():
+    with open(os.path.join("lightbulb", "ext", "filament", "__init__.py")) as fp:
+        code = fp.read()
+
+    token_pattern = re.compile(r"^__(?P<key>\w+)?__\s*=\s*(?P<quote>(?:'{3}|\"{3}|'|\"))(?P<value>.*?)(?P=quote)", re.M)
+
+    groups = {}
+
+    for match in token_pattern.finditer(code):
+        group = match.groupdict()
+        groups[group["key"]] = group["value"]
+
+    return types.SimpleNamespace(**groups)
+
+
+def long_description():
+    with open("README.md") as fp:
+        return fp.read()
+
+
+def parse_requirements_file(path):
+    with open(path) as fp:
+        dependencies = (d.strip() for d in fp.read().split("\n") if d.strip())
+        return [d for d in dependencies if not d.startswith("#")]
+
+
+meta = parse_meta()
+
+setup(
+    name="lightbulb-ext-filament",
+    version=meta.version,
+    description="Various add-ons, extensions and utilities for hikari-lightbulb",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    author="tandemdude",
+    author_email="tandemdude1@gmail.com",
+    url="https://github.com/tandemdude/filament",
+    packages=find_namespace_packages(include=[name + "*"]),
+    license="LGPL-3.0-ONLY",
+    install_requires=parse_requirements_file("requirements.txt"),
+    include_package_data=True,
+    python_requires=">=3.8.0,<3.11",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
