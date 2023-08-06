e260658acf2fc02c37e49c8d03c95d8f6a529b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:48 +0000

    Add package Draugr/0.9.9/Draugr-0.9.9-py36-none-any.whl

---
+++ packages/Draugr/Draugr-0.9.9-py36-none-any.whl/draugr/os_utilities/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 14/01/2020
+           """
+
+from .platform_selection import *
+from .cuda_device import *
+from .screen_resolution import *
+from .resource_utilities import *
