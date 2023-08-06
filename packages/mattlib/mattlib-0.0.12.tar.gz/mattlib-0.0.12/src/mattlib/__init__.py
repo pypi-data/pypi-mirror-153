bfce6e97bb6dc0266b968aeef6aa1a3f6c3460ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:28:47 +0000

    Add package mattlib/0.0.12/mattlib-0.0.12.tar.gz

---
+++ packages/mattlib/mattlib-0.0.12.tar.gz/mattlib-0.0.12/src/mattlib/__init__.py
@@ -0,0 +1,13 @@
+from .apis.AzureAPI import AzureAPI
+from .apis.GraphAPI import GraphAPI
+from .apis.SalesForceAPI import SalesForceAPI
+from .fourmatters import virtual_network, virtual_machine
+
+def format(data, table_type):
+    formatter_types = {
+        'virtual_network_table_std': virtual_network.virtual_network_table_std,
+        'virtual_machine_table_comprehensive': virtual_machine.virtual_machine_table_comprehensive 
+    }
+    return formatter_types.get(table_type).format(data)
+    
+    
