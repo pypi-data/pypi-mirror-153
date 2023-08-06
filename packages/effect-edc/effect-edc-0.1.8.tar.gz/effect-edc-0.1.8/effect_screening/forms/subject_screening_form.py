18d8e2aa698bcac676329789ad323f8b6a51ccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:05 +0000

    Add package effect-edc/0.1.8/effect-edc-0.1.8.tar.gz

---
+++ packages/effect-edc/effect-edc-0.1.8.tar.gz/effect-edc-0.1.8/effect_screening/forms/subject_screening_form.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from django import forms
+from edc_form_validators import FormValidatorMixin
+from edc_screening.modelform_mixins import AlreadyConsentedFormMixin
+from effect_form_validators.effect_screening import (
+    SubjectScreeningFormValidator as Base,
+)
+
+from ..models import SubjectScreening
+
+
+class SubjectScreeningFormValidator(Base):
+    pass
+
+
+class SubjectScreeningForm(AlreadyConsentedFormMixin, FormValidatorMixin, forms.ModelForm):
+
+    form_validator_cls = SubjectScreeningFormValidator
+
+    screening_identifier = forms.CharField(
+        label="Screening Identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
+
+    subject_identifier = forms.CharField(
+        label="Subject identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
+
+    def clean(self: Any) -> dict:
+        cleaned_data = super().clean()
+        return cleaned_data
+
+    class Meta:
+        model = SubjectScreening
+        fields = "__all__"
