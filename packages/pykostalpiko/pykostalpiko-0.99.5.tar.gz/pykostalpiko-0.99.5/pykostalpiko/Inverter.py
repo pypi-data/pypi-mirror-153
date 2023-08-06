a5346f0ee6adbd06bac0229e79476d45919641d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:40 +0000

    Add package pykostalpiko/0.99.5/pykostalpiko-0.99.5.tar.gz

---
+++ packages/pykostalpiko/pykostalpiko-0.99.5.tar.gz/pykostalpiko-0.99.5/pykostalpiko/Inverter.py
@@ -0,0 +1,75 @@
+import aiohttp
+import asyncio
+
+from pykostalpiko.dxs import Entries
+
+
+class Piko:
+    def __init__(self, client_session: aiohttp.ClientSession, host: str,  username: str = "pvserver", password: str = "pvwr"):
+        """
+        Piko Inverter Instance
+        """
+        self._host = host
+        self._username = username
+        self._password = password
+        self._client_session = client_session
+        self._data = {}
+
+    async def _async_fetch(self, session: aiohttp.ClientSession, *entries: Entries) -> dict:
+        """
+        Fetch the data from the inverter
+
+        Limited to 0 to 25 DXS entries
+        """
+
+        # Check amount of requested entries
+        if (len(entries) == 0):
+            raise Exception('No entries specified')
+        elif (len(entries) > 25):
+            raise Exception('Too many entries specified')
+
+        def _build_param(dxs: Entries) -> str:
+            return f'dxsEntries={dxs.value}'
+
+        params = map(_build_param, entries)
+        url = f'http://{self._host}/api/dxs.json?' + '&'.join(params)
+
+        async with session.get(url) as response:
+            json = await response.json(content_type='text/plain')
+            return self.__format_response(json)
+
+    async def async_fetch(self, *entries: Entries) -> dict:
+        """
+        Fetch the data from the inverter
+        """
+
+        # Spread the entries into groups of 25 to avoid too many dxsEntries
+        entries_paged = [entries[i:i + 25]
+                         for i in range(0, len(entries), 25)]
+        # TODO: Do something with the response
+        for req in asyncio.as_completed([self._async_fetch(self._client_session, *entries_page) for entries_page in entries_paged]):
+            # Wait for the request to complete
+            res = await req
+
+            if res != None:
+                # Combine existing data with newly fetched data
+                self._data = {**self._data, **res}
+        return self._data
+
+    async def async_fetch_all(self) -> dict:
+        """
+        Fetch all available data from the inverter
+        """
+        return await self.async_fetch(*Entries)
+
+    def __format_response(self, json) -> dict:
+        if json == None:
+            return None
+
+        new = {}
+        entries = json['dxsEntries']
+
+        for entry in entries:
+            new[Entries(entry['dxsId']).name] = entry['value']
+
+        return new
---
+++ packages/pykostalpiko/pykostalpiko-0.99.5.tar.gz/pykostalpiko-0.99.5/pykostalpiko/dxs.py
@@ -0,0 +1,78 @@
+from aenum import MultiValueEnum
+
+
+class Entries(MultiValueEnum):
+    AnalaogInput1 = 167772417, "Analog Input 1"
+    AnalaogInput2 = 167772673, "Analog Input 2"
+    AnalaogInput3 = 167772929, "Analog Input 3"
+    AnalaogInput4 = 167773185, "Analog Input 4"
+
+    BatteryVoltage = 33556226, "Battery Voltage"
+    BatteryCharge = 33556229, "Battery Charge"
+    BatteryCurrent = 33556238, "Battery Current"
+    BatteryCurrentDirection = 33556230, "Battery Current Direction"
+    BatteryChargeCycles = 33556228, "Battery Charge Cycles"
+    BatteryTemperature = 33556227, "Battery Temperature"
+
+    GridOutputPower = 67109120, "Grid Output Power"
+    GridFrequency = 67110400, "Grid Frequency"
+    GridCosPhi = 67110656, "Grid Cos Phi"
+    GridLimitation = 67110144, "Grid Limitation"
+    GridVoltageL1 = 67109378, "Grid Voltage L1"
+    GridCurrentL1 = 67109377, "Grid Current L1"
+    GridPowerL1 = 67109379, "Grid Power L1"
+    GridVoltageL2 = 67109634, "Grid Voltage L2"
+    GridCurrentL2 = 67109633, "Grid Current L2"
+    GridPowerL2 = 67109635, "Grid Power L2"
+    GridVoltageL3 = 67109890, "Grid Voltage L3"
+    GridCurrentL3 = 67109889, "Grid Current L3"
+    GridPowerL3 = 67109891, "Grid Power L3"
+
+    HomeConsumptionSolar = 83886336, "Home Consumption Solar"
+    HomeConsumptionBattery = 83886592, "Home Consumption Battery"
+    HomeConsumptionGrid = 83886848, "Home Consumption Grid"
+    HomeConsumptionL1 = 83887106, "Home Consumption L1"
+    HomeConsumptionL2 = 83887362, "Home Consumption L2"
+    HomeConsumptionL3 = 83887618, "Home Consumption L3"
+
+    GeneratorDc1Voltage = 33555202, "Generator DC1 Voltage"
+    GeneratorDc1Current = 33555201, "Generator DC1 Current"
+    GeneratorDc1Power = 33555203, "Generator DC1 Power"
+    GeneratorDc2Voltage = 33555458, "Generator DC2 Voltage"
+    GeneratorDc2Current = 33555457, "Generator DC2 Current"
+    GeneratorDc2Power = 33555459, "Generator DC2 Power"
+    GeneratorDc3Voltage = 33555714, "Generator DC3 Voltage"
+    GeneratorDc3Current = 33555713, "Generator DC3 Current"
+    GeneratorDc3Power = 33555715, "Generator DC3 Power"
+
+    S0InPulseCnt = 184549632, "S0 In Pulse Count"
+    S0InLoginterval = 150995968, "S0 In Loginterval"
+
+    HomeDcPowerPV = 33556736, "Home DC Power PV"
+    HomeOwnConsumption = 83888128, "Home Own Consumption"
+    HomeOperatingStatus = 16780032, "Home Operating Status"
+
+    InverterName = 16777984, "Inverter Name"
+    InverterType = 16780544, "Inverter Type"
+
+    VersionUI = 16779267, "Version UI"
+    VersionFW = 16779265, "Version FW"
+    VersionHW = 16779266, "Version HW"
+    VersionPAR = 16779268, "Version PAR"
+    SerialNumber = 16777728, "Serial Number"
+    ArticleNumber = 16777472, "Article Number"
+    CountrySettingsName = 16779522, "Country Settings Name"
+    CountrySettingsVersion = 16779521, "Country Settings Version"
+
+    StatisticsDayYield = 251658754, "Statistics Day Yield"
+    StatisticsDayHomeConsumption = 251659010, "Statistics Day Home Consumption"
+    StatisticsDayOwnConsumption = 251659266, "Statistics Day Own Consumption"
+    StatisticsDayOwnConsumptionRate = 251659278, "Statistics Day Own Consumption Rate"
+    StatisticsDayAutonomyDegree = 251659279, "Statistics Day Autonomy Degree"
+
+    StatisticsTotalYield = 251658753, "Statistics Total Yield"
+    StatisticsTotalOperatingTime = 251658496, "Statistics Total Operating Time"
+    StatisticsTotalHomeConsumption = 251659009, "Statistics Total Home Consumption"
+    StatisticsTotalOwnConsumption = 251659265, "Statistics Total Own Consumption"
+    StatisticsTotalOwnConsRate = 251659280, "Statistics Total Own Consumption Rate"
+    StatisticsTotalAutonomyDegree = 251659281, "Statistics Total Autonomy Degree"
