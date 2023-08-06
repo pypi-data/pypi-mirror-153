66e0f9333eb57a460d196a82cc507cef8ec996f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:34 +0000

    Add package graia-amnesia/0.5.1/graia-amnesia-0.5.1.tar.gz

---
+++ packages/graia-amnesia/graia-amnesia-0.5.1.tar.gz/graia-amnesia-0.5.1/src/graia/amnesia/builtins/common.py
@@ -0,0 +1,19 @@
+from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING
+
+from launart import ExportInterface
+
+if TYPE_CHECKING:
+    from typing import Any, Awaitable, Callable, MutableMapping, Type
+
+
+class ASGIHandlerProvider(ExportInterface, metaclass=ABCMeta):
+    @abstractmethod
+    def get_asgi_handler(
+        self,
+    ) -> """Callable[[
+        Type[MutableMapping[str, Any]],
+        Awaitable[MutableMapping[str, Any]],
+        Callable[[MutableMapping[str, Any]], Awaitable[None]]
+    ], None]""":
+        ...
