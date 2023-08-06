d84311bf475ef4623c02b0a56b866ad8bbdb00cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:33 +0000

    Add package lineapy/0.1.3/lineapy-0.1.3.tar.gz

---
+++ packages/lineapy/lineapy-0.1.3.tar.gz/lineapy-0.1.3/lineapy/db/utils.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import pickle
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.pool import StaticPool
+
+from lineapy.utils.constants import DB_SQLITE_PREFIX, SQLALCHEMY_ECHO
+
+logger = logging.getLogger(__name__)
+
+
+def create_lineadb_engine(url: str) -> Engine:
+    """Create a SQLAlchemy engine for LineaDB.
+    Take care of the SQLite database file name and configuration.
+    """
+    echo = os.getenv(SQLALCHEMY_ECHO, default="false").lower() == "true"
+    logger.debug(f"Connecting to Linea DB at {url}")
+    additional_args = {}
+    if url.startswith(DB_SQLITE_PREFIX):
+        additional_args = {"check_same_thread": False}
+    return create_engine(
+        url,
+        connect_args=additional_args,
+        poolclass=StaticPool,
+        echo=echo,
+    )
+
+
+class FilePickler:
+    """
+    Tries to pickle an object, and if it fails returns None.
+    """
+
+    @staticmethod
+    def dump(value, fileobj, protocol=pickle.HIGHEST_PROTOCOL):
+        if fileobj is None:
+            return None
+        try:
+            return pickle.dump(value, fileobj, protocol)
+        except pickle.PicklingError:
+            return None
+
+    @staticmethod
+    def load(fileobj):
+        return pickle.load(fileobj)
