a16ad0a094f09075e2fc43db5bfda77d76aea449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:54 +0000

    Add package basix/0.0.4/basix-0.0.4.tar.gz

---
+++ packages/basix/basix-0.0.4.tar.gz/basix-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from basix import __version__
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="basix",
+    version=__version__,
+    # url="",
+    license="MIT License",
+    author="Eduardo M. de Morais",
+    long_description="Implementation of some basic operations",
+    long_description_content_type="text/markdown",
+    author_email="emdemor415@gmail.com",
+    keywords="",
+    description=u"Utils for python",
+    packages=["basix"],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "setuptools",
+        "tqdm",
+        "scikit-learn",
+        "pyarrow",
+    ],
+)
