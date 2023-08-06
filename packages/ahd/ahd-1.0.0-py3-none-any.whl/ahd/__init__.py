c36cc78eb2b085351605caa5ff4ef7fb47b9887c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:55 +0000

    Add package ahd/1.0.0/ahd-1.0.0-py3-none-any.whl

---
+++ packages/ahd/ahd-1.0.0-py3-none-any.whl/ahd/__init__.py
@@ -0,0 +1,8 @@
+"""Top level module for AHD.
+
+This file is simply used to indicate to python this is a package
+
+If you are here to help or contribute to the project, check out the development guide: https://ahd.readthedocs.io/en/latest/contributing/
+"""
+
+__version__ = "1.0.0"
---
+++ packages/ahd/ahd-1.0.0-py3-none-any.whl/ahd/__main__.py
@@ -0,0 +1,5 @@
+"""Allows for invocation using `python -m ahd`"""
+from ahd.cli import main
+
+if __name__ == "__main__":
+    main()
