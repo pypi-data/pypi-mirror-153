305b32a92e6cb45d0a631d950effbfadd0d36a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:21 +0000

    Add package arya-api-framework/0.0.1/arya-api-framework-0.0.1.tar.gz

---
+++ packages/arya-api-framework/arya-api-framework-0.0.1.tar.gz/arya-api-framework-0.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+requirements = [
+    "pydantic>=1.9.1",
+    "yarl>=1.7.2"
+]
+
+extras_require = {
+    "sync": ["requests>=2.27.1"],
+    "async": ["requests>=3.8.1"],
+}
+
+packages = [
+    "arya_api_framework"
+]
+
+setup(
+    name="arya-api-framework",
+    version="0.0.1",
+    description="A simple API framework used in many other API clients I create.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aryathel/ApiFramework",
+    author="Aryathel",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.10.0",
+    packages=packages,
+    install_requires=requirements,
+    extras_require=extras_require,
+    include_package_data=True
+)
