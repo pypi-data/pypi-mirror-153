4d2e2749d46936631b9f7137a035f43f9dd9909b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:52 +0000

    Add package wagtail-headless-preview/0.3.0/wagtail_headless_preview-0.3.0-py2.py3-none-any.whl

---
+++ packages/wagtail-headless-preview/wagtail_headless_preview-0.3.0-py2.py3-none-any.whl/wagtail_headless_preview/deprecation.py
@@ -0,0 +1,9 @@
+class RemovedInWagtailHeadlessPreview030Warning(DeprecationWarning):
+    pass
+
+
+class RemovedInWagtailHeadlessPreview040Warning(PendingDeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtailHeadlessPreview030Warning
