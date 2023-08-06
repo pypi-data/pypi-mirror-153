a36a06897941b8cab15e5eab9636fc3245a70053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:35 +0000

    Add package cca-zoo/1.12.4/cca_zoo-1.12.4-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.12.4-py3-none-any.whl/cca_zoo/models/__init__.py
@@ -0,0 +1,21 @@
+from ._gcca import GCCA, KGCCA
+from ._iterative import (
+    PLS_ALS,
+    SCCA_PMD,
+    ElasticCCA,
+    SCCA_Parkhomenko,
+    SCCA_IPLS,
+    SCCA_ADMM,
+    SCCA_Span,
+    SWCCA,
+)
+from ._mcca import MCCA, KCCA
+from ._ncca import NCCA
+from ._partialcca import PartialCCA
+from ._rcca import rCCA, CCA, PLS
+from ._tcca import TCCA, KTCCA
+
+try:
+    from ._stochastic import StochasticPowerPLS, IncrementalPLS
+except:
+    pass
