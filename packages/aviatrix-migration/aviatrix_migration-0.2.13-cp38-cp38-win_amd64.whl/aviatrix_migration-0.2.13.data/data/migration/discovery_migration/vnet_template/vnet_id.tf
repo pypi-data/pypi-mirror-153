69fda4a0e448bc2518c0a112d88c9d1ae9be1fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:06 +0000

    Add package aviatrix-migration/0.2.13/aviatrix_migration-0.2.13-cp38-cp38-win_amd64.whl

---
+++ packages/aviatrix-migration/aviatrix_migration-0.2.13-cp38-cp38-win_amd64.whl/aviatrix_migration-0.2.13.data/data/migration/discovery_migration/vnet_template/vnet_id.tf
@@ -0,0 +1,27 @@
+{{ data.var_route_tables }}
+
+module "{{ data.vnet_name }}" {
+  source                  = "{{ data.module_source }}"
+{% if data.configure_gw_name %}
+  spoke_gw_name           = "{{ data.spoke_gw_name }}"
+  transit_gw              = "{{ data.transit_gw_name }}"
+{% endif %}
+  vnet_name               = "{{ data.vnet_name }}"
+  vnet_cidr               = {{ data.vnet_cidr }}
+  avtx_cidr               = "{{ data.avtx_cidr }}"
+  hpe                     = {{ data.hpe }}
+  avtx_gw_size            = "{{ data.avtx_gw_size }}"
+  region                  = "{{ data.region }}"
+  use_azs                 = {{ data.use_azs }} # Set to false if region above doesn't support AZs
+  resource_group_name     = "{{ data.resource_group }}"
+  account_name            = var.account_name
+  route_tables            = {{ data.route_tables }}
+  switch_traffic          = false
+  disable_bgp_propagation = {{ data.disable_bgp_propagation }} # Used to configure aviatrix_managed_main RTs
+{% if data.spoke_gw_tags is defined %}
+  tags                    = {{data.spoke_gw_tags}}
+{% endif %}
+  providers = {
+    azurerm = azurerm.{{ data.provider }}
+  }
+}
