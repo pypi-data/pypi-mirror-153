13b23e65ba358e414ef459b508dff753406ac35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:39 +0000

    Add package spider-info-checker/0.0.8/spider_info_checker-0.0.8-py3-none-any.whl

---
+++ packages/spider-info-checker/spider_info_checker-0.0.8-py3-none-any.whl/spider_info_checker/visitors.py
@@ -0,0 +1,20 @@
+from contextlib import suppress
+
+import libcst as cst
+from libcst import matchers as m
+
+
+class CollectSpiderNames(cst.CSTVisitor):
+    def __init__(self, spider_names=None):
+        super().__init__()
+        self.spider_names = spider_names
+
+    def visit_SimpleStatementLine(self, node: "SimpleStatementLine") -> None:
+        if m.matches(
+            node,
+            m.SimpleStatementLine(
+                body=[m.Assign(targets=[m.AssignTarget(target=m.Name("name"))])]
+            ),
+        ):
+            with suppress(AttributeError):
+                self.spider_names.add(node.body[0].value.value.replace('"', ''))
