3b2f5deaffc256960e9f38022f564f6c4095d3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:49 +0000

    Add package OGRE-embed/0.0.1/OGRE-embed-0.0.1.tar.gz

---
+++ packages/OGRE-embed/OGRE-embed-0.0.1.tar.gz/OGRE-embed-0.0.1/src/OGRE/__init__.py
@@ -0,0 +1,4 @@
+# from .evaluation_tasks.eval_utils import *
+# from .evaluation_tasks.link_prediction import *
+# from .evaluation_tasks.plots_utils import *
+from .evaluation_tasks.calculate_static_embeddings import CalculateStaticEmbeddings
