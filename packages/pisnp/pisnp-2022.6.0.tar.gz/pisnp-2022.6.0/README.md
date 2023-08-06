f401e0fc01c2c92c48b2982afbef1c24ae3bf124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:34 +0000

    Add package pisnp/2022.6.0/pisnp-2022.6.0.tar.gz

---
+++ packages/pisnp/pisnp-2022.6.0.tar.gz/pisnp-2022.6.0/README.md
@@ -0,0 +1,11 @@
+# PisNP
+
+Version `2022.6.0`
+
+P is NP? (Polynomial is Non-polynomial?)
+
+## Installation
+
+```shell
+pip install pisnp
+```
---
+++ packages/pisnp/pisnp-2022.6.0.tar.gz/pisnp-2022.6.0/src/pisnp/__main__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+#
+# __main__.py in pisnp
+#
+# created by 谢方圆 (XIE Fangyuan) on 2022-06-04
+# Copyright © 2022 谢方圆 (XIE Fangyuan). All rights reserved.
+#
+# Entry point when running this package as a module from the terminal.
+
+
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    print('P is NP?')
+
+
+if __name__ == '__main__':
+    main()
