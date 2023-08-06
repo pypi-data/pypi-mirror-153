c15d5171cb1321736176f132add1c016af95cb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:57 +0000

    Add package poetryup/0.7.2/poetryup-0.7.2.tar.gz

---
+++ packages/poetryup/poetryup-0.7.2.tar.gz/poetryup-0.7.2/src/poetryup/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import subprocess
+from pathlib import Path
+
+import typer
+
+from poetryup.core.pyproject import Pyproject
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO").upper())
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def poetryup(
+    latest: bool = typer.Option(
+        default=False,
+        help="Whether to update dependencies to their latest version.",
+    ),
+    skip_exact: bool = typer.Option(
+        default=False,
+        help="Whether to skip dependencies with an exact version.",
+    ),
+):
+    """Update dependencies and bump their version in pyproject.toml file"""
+
+    try:
+        pyproject_str = Path("pyproject.toml").read_text()
+    except FileNotFoundError:
+        raise Exception(
+            "poetryup couldn't find a pyproject.toml file in current directory"
+        )
+
+    pyproject = Pyproject(pyproject_str)
+    pyproject.update_dependencies(latest, skip_exact)
+    Path("pyproject.toml").write_text(pyproject.dumps())
+    # refresh the lock file after changes in pyproject.toml
+    subprocess.run(["poetry", "lock", "--no-update"])
+
+
+if __name__ == "__main__":
+    app()
