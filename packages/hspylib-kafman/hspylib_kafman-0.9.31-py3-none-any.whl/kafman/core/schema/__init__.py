3c388805e23b258ff6305df052018ec720d1bd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:22 +0000

    Add package hspylib-kafman/0.9.31/hspylib_kafman-0.9.31-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.31-py3-none-any.whl/kafman/core/schema/__init__.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.30
+#
+# Package: main.kafman.core.schema
+"""Package initialization."""
+
+__all__ = [
+  'avro_schema', 
+  'field', 
+  'json_schema', 
+  'kafka_schema', 
+  'plain_schema', 
+  'registry_subject', 
+  'schema_factory', 
+  'schema_registry', 
+  'schema_type', 
+  'schema_utils', 
+  'widget_utils'
+]
+__version__ = '0.9.30'
