09afd90f18339d70d6dff648cefbe1f7bdb4c622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:10 +0000

    Add package streamlit-cookies/0.0.1/streamlit-cookies-0.0.1.tar.gz

---
+++ packages/streamlit-cookies/streamlit-cookies-0.0.1.tar.gz/streamlit-cookies-0.0.1/streamlit_cookies/frontend/build/precache-manifest.ed4d27b4b4154d1aabe3efb719c2fd14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994382683427b6d5ccfaa6a1f292e340",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47da6b2bb1d8e7b64674",
+    "url": "./static/js/2.10a3df08.chunk.js"
+  },
+  {
+    "revision": "60f6a4447bfde37dff88595909161454",
+    "url": "./static/js/2.10a3df08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9857ed648689c290c7",
+    "url": "./static/js/main.8f735f33.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
