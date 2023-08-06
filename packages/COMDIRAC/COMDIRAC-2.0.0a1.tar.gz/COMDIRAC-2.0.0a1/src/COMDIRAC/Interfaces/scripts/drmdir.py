7387da1e0faaec2d4ae5112324de741e186b4c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:20 +0000

    Add package COMDIRAC/2.0.0a1/COMDIRAC-2.0.0a1.tar.gz

---
+++ packages/COMDIRAC/COMDIRAC-2.0.0a1.tar.gz/COMDIRAC-2.0.0a1/src/COMDIRAC/Interfaces/scripts/drmdir.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+"""
+remove FileCatalog directories. Attention ! This command does not remove
+directories and files on the physical storage.
+"""
+import DIRAC
+from DIRAC.Core.Utilities.DIRACScript import DIRACScript as Script
+
+
+@Script()
+def main():
+    from COMDIRAC.Interfaces import DSession
+    from COMDIRAC.Interfaces import createCatalog
+    from COMDIRAC.Interfaces import pathFromArguments
+
+    from COMDIRAC.Interfaces import ConfigCache
+
+    Script.setUsageMessage(
+        "\n".join(
+            [
+                __doc__.split("\n")[1],
+                "Usage:",
+                "  %s [Path]..." % Script.scriptName,
+                "Arguments:",
+                "  Path:     directory path",
+                "",
+                "Examples:",
+                "  $ drmdir ./some_lfn_directory",
+            ]
+        )
+    )
+
+    configCache = ConfigCache()
+    Script.parseCommandLine(ignoreErrors=True)
+    configCache.cacheConfig()
+
+    args = Script.getPositionalArgs()
+
+    session = DSession()
+
+    if len(args) < 1:
+        print("Error: No argument provided\n%s:" % Script.scriptName)
+        Script.showHelp()
+        DIRAC.exit(-1)
+
+    catalog = createCatalog()
+
+    result = catalog.removeDirectory(pathFromArguments(session, args))
+    if result["OK"]:
+        if result["Value"]["Failed"]:
+            for p in result["Value"]["Failed"]:
+                print('ERROR - "%s": %s' % (p, result["Value"]["Failed"][p]))
+    else:
+        print("ERROR: %s" % result["Message"])
+
+
+if __name__ == "__main__":
+    main()
