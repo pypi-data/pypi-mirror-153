3c34755f6832287cbd49040e957d508dd10ec766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package brikeda-client/0.0.2/brikeda_client-0.0.2-py3-none-any.whl

---
+++ packages/brikeda-client/brikeda_client-0.0.2-py3-none-any.whl/brikeda_client/test.py
@@ -0,0 +1,8 @@
+from brikeda import Brikeda
+print("heelo")
+
+brik=Brikeda("https://brikedaserver20220309142907.azurewebsites.net","fbe515595dc2417a84a914a203022bcc")
+#brik.SyncMessages("#eb8934")
+brik.TestIt("chalupa")
+
+#brik.WeatherForecast()
