1c338abbfda51fc64faa971fc54e60c17b5f1c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:22 +0000

    Add package scrapy-demo/6.0/scrapy_demo-6.0.tar.gz

---
+++ packages/scrapy-demo/scrapy_demo-6.0.tar.gz/scrapy_demo-6.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author : zhy
+# @Time   : 2022-05-30
+# @File   : setup.py
+from setuptools import setup,find_packages
+
+setup(
+    name='scrapy_demo',
+    version='6.0',
+    description='scrapy通用爬虫',
+    include_package_data=True,  # 是否允许上传资源文件
+    packages=find_packages(),#包的目录
+    author='zhy',  # 作者
+    author_email='191517137@qq.com',  # 作者邮件
+    python_requires='==3.7.5',#设置python版本要求
+    install_requires=['scrapy']#安装所需要的库
+)
