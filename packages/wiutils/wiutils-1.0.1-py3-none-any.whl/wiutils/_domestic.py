9ee5facd6a205b476f6cd8323cfe5c29ad74e81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:59 +0000

    Add package wiutils/1.0.1/wiutils-1.0.1-py3-none-any.whl

---
+++ packages/wiutils/wiutils-1.0.1-py3-none-any.whl/wiutils/_domestic.py
@@ -0,0 +1,16 @@
+"""
+Domestic species genera.
+"""
+genera = [
+    "Anas",
+    "Bos",
+    "Bubalus",
+    "Canis",
+    "Equus",
+    "Felis",
+    "Gallus",
+    "Homo",
+    "Meleagris",
+    "Numida",
+    "Sus",
+]
