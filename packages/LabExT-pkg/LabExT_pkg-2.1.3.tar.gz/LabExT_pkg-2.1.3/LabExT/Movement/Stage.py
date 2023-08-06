c87bfba1dcc12d8cf1d0613efa679f2c7ac85965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:24 +0000

    Add package LabExT-pkg/2.1.3/LabExT_pkg-2.1.3.tar.gz

---
+++ packages/LabExT-pkg/LabExT_pkg-2.1.3.tar.gz/LabExT_pkg-2.1.3/LabExT/Movement/Stage.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LabExT  Copyright (C) 2021  ETH Zurich and Polariton Technologies AG
+This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
+"""
+
+import logging
+from functools import wraps
+from abc import ABC, abstractmethod
+from os.path import dirname, join
+
+from LabExT.PluginLoader import PluginLoader
+
+
+class StageError(RuntimeError):
+    pass
+
+
+def assert_stage_connected(func):
+    """
+    Use this decorator to assert that any method of a stage is only executed
+    when the stage is connected.
+    """
+
+    @wraps(func)
+    def wrapper(stage, *args, **kwargs):
+        if not stage.connected:
+            raise StageError(
+                "Stage not connected: Function {} requires an active stage connection. Make sure that you have called connect() before.".format(
+                    func.__name__))
+
+        return func(stage, *args, **kwargs)
+    return wrapper
+
+
+def assert_driver_loaded(func):
+    """
+    Use this decorator to assert that any method of a stage is only executed
+    when the drivers are loaded.
+    """
+
+    @wraps(func)
+    def wrapper(stage, *args, **kwargs):
+        if not stage.driver_loaded:
+            raise StageError(
+                "Stage driver not loaded: Function {} requires previously loaded drivers.".format(
+                    func.__name__))
+
+        return func(stage, *args, **kwargs)
+    return wrapper
+
+
+class Stage(ABC):
+    _logger = logging.getLogger()
+    driver_loaded = False
+
+    @classmethod
+    def find_stage_classes(cls, subdir="Stages") -> list:
+        """
+        Returns a list of all classes which inherit from this class.
+
+        Needs to import Stages module first.
+        """
+        search_path = join(dirname(__file__), subdir)
+        plugin_loader = PluginLoader()
+
+        return [*plugin_loader.load_plugins(search_path, cls).values()]
+
+    @classmethod
+    def find_available_stages(cls):
+        """
+        Returns a list of stage objects. Each object represents a found stage.
+
+        Note: The stage is not yet connected.
+        """
+        stages = []
+        for stage_class in cls.find_stage_classes():
+            try:
+                addresses = stage_class.find_stage_addresses()
+            except StageError:
+                continue
+            for address in addresses:
+                stages.append(stage_class(address))
+
+        return stages
+
+    @classmethod
+    def find_stage_addresses(cls) -> list:
+        """
+        Returns a list of stage locators.
+        """
+        return []
+
+    @classmethod
+    def load_driver(cls, parent=None) -> bool:
+        """
+        Stage specific method to load any missing drivers.
+
+        Returns True, if successfully loaded and False otherwise.
+
+        Needs to be overwritten.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def __init__(self, address):
+        self.address = address
+        self.connected = False
+
+    def __del__(self):
+        if self.connected:
+            self.disconnect()
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    @property
+    def address_string(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def connect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def set_speed_xy(self, umps: float):
+        pass
+
+    @abstractmethod
+    def set_speed_z(self, umps: float):
+        pass
+
+    @abstractmethod
+    def get_speed_xy(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_speed_z(self) -> float:
+        pass
+
+    @abstractmethod
+    def set_acceleration_xy(self):
+        pass
+
+    @abstractmethod
+    def get_acceleration_xy(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_status(self) -> tuple:
+        pass
+
+    @abstractmethod
+    def wiggle_z_axis_positioner(self):
+        pass
+
+    @abstractmethod
+    def lift_stage(self):
+        pass
+
+    @abstractmethod
+    def lower_stage(self):
+        pass
+
+    @abstractmethod
+    def get_current_position(self):
+        pass
+
+    @abstractmethod
+    def move_relative(self, x, y):
+        pass
+
+    @abstractmethod
+    def move_absolute(self, pos):
+        pass
---
+++ packages/LabExT-pkg/LabExT_pkg-2.1.3.tar.gz/LabExT_pkg-2.1.3/LabExT/Tests/Utils.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LabExT  Copyright (C) 2021  ETH Zurich and Polariton Technologies AG
+This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
+"""
+
+
+import _tkinter
+import tkinter
+import pytest
+from unittest import TestCase
+import random
+import string
+
+
+class TKinterTestCase(TestCase):
+    def setUp(self):
+        self.root = tkinter.Tk()
+        self.pump_events()
+
+    def tearDown(self):
+        if self.root:
+            self.root.destroy()
+            self.pump_events()
+
+    def pump_events(self):
+        while self.root.dooneevent(_tkinter.ALL_EVENTS | _tkinter.DONT_WAIT):
+            pass
+
+
+def mark_as_laboratory_test(cls):
+    """
+    Decorator to mark test as laboratory tests. These will be excluded, when run on CI.
+    """
+    skip_if = pytest.skip_laboratory_tests if hasattr(pytest, 'skip_laboratory_tests') else False
+    return pytest.mark.skipif(skip_if, reason="skip tests that require laboratory equipment.")(cls)
+
+
+def ask_user_yes_no(ask_string="Is one kg of feathers lighter than one kg of iron?", default_answer=True):
+    """ Ask the user a yes/no question:
+     * Returns True on yes
+     * False on no.
+     * Raises RuntimeError on abort.
+     * Repeats question if unclear answer.
+
+      You can set a default answer: set to True for default 'yes', set to False for default 'no', set to None
+      for no default and the user has to provide an answer."""
+    yes_answers = ['y', 'yes']
+    no_answers = ['n', 'no']
+    abort_answers = ['a', 'abort']
+
+    if default_answer is None:
+        expl_answer_string = " [y]es/[n]o/[a]bort: "
+    elif default_answer:
+        expl_answer_string = " [Y]es/[n]o/[a]bort: "
+        yes_answers.append('')
+    elif not default_answer:
+        expl_answer_string = " [y]es/[N]o/[a]bort: "
+        no_answers.append('')
+    else:
+        raise ValueError("Argument default_answer must be True, False or None.")
+
+    while True:
+        ans = input(ask_string + expl_answer_string)
+        ans = ans.strip().lower()
+        if ans in yes_answers:
+            return True
+        elif ans in no_answers:
+            return False
+        elif ans in abort_answers:
+            raise RuntimeError("User aborted yes-no-question.")
+        else:
+            continue
+
+
+def randomword(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
