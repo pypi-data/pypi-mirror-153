06b932678e37374abdfd6a2084b0fc215ba79541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:57 +0000

    Add package AutoLayout/0.1.0/AutoLayout-0.1.0.tar.gz

---
+++ packages/AutoLayout/AutoLayout-0.1.0.tar.gz/AutoLayout-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='AutoLayout',
+    version='0.1.0',
+    license='MIT',
+    author="Tin Tran",
+    author_email='trantin0815@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/trezitorul/GDSPYUtils/tree/AutoLayout-package/AutoLayout',
+    keywords='AutoLayout',
+    install_requires=[
+          'rectpack', 'gdspy'
+      ],
+
+)
