457263c453cdaa29504c44d25f8571112169491c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:45 +0000

    Add package chiakilisp/0.12.0/chiakilisp-0.12.0-py3-none-any.whl

---
+++ packages/chiakilisp/chiakilisp-0.12.0-py3-none-any.whl/chiakilisp/cxcompiler/dictionary.py
@@ -0,0 +1,15 @@
+# pylint: disable=line-too-long
+# pylint: disable=missing-module-docstring
+
+DICTIONARY = {
+    '+': 'chiakilisp::add', '-': 'chiakilisp::sub',
+    '/': 'chiakilisp::div', '*': 'chiakilisp::mul',
+    'mod': 'chiakilisp::mod',
+    'identity': 'chiakilisp::identity',
+    'listy': 'chiakilisp::vec', 'dicty': 'chiakilisp::map',
+    'get': 'chiakilisp::get',
+    'int': 'chiakilisp::_int',
+    'str': 'chiakilisp::_str',
+    'prn': 'chiakilisp::println',
+    'print': 'chiakilisp::println', 'println': 'chiakilisp::println',
+}
