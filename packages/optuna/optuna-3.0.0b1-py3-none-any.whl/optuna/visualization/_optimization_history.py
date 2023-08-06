53d6cf586c2973473fa16b93224680792e5e0264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:29 +0000

    Add package optuna/3.0.0b1/optuna-3.0.0b1-py3-none-any.whl

---
+++ packages/optuna/optuna-3.0.0b1-py3-none-any.whl/optuna/visualization/_optimization_history.py
@@ -0,0 +1,265 @@
+import itertools
+from typing import Callable
+from typing import cast
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Union
+
+import numpy as np
+
+from optuna.logging import get_logger
+from optuna.study import Study
+from optuna.study._study_direction import StudyDirection
+from optuna.trial import FrozenTrial
+from optuna.trial import TrialState
+from optuna.visualization._plotly_imports import _imports
+from optuna.visualization._utils import _check_plot_args
+
+
+if _imports.is_successful():
+    from optuna.visualization._plotly_imports import go
+
+_logger = get_logger(__name__)
+
+
+def plot_optimization_history(
+    study: Union[Study, Sequence[Study]],
+    *,
+    target: Optional[Callable[[FrozenTrial], float]] = None,
+    target_name: str = "Objective Value",
+    error_bar: bool = False,
+) -> "go.Figure":
+    """Plot optimization history of all trials in a study.
+
+    Example:
+
+        The following code snippet shows how to plot optimization history.
+
+        .. plotly::
+
+            import optuna
+
+
+            def objective(trial):
+                x = trial.suggest_float("x", -100, 100)
+                y = trial.suggest_categorical("y", [-1, 0, 1])
+                return x ** 2 + y
+
+
+            sampler = optuna.samplers.TPESampler(seed=10)
+            study = optuna.create_study(sampler=sampler)
+            study.optimize(objective, n_trials=10)
+
+            fig = optuna.visualization.plot_optimization_history(study)
+            fig.show()
+
+    Args:
+        study:
+            A :class:`~optuna.study.Study` object whose trials are plotted for their target values.
+            You can pass multiple studies if you want to compare those optimization histories.
+        target:
+            A function to specify the value to display. If it is :obj:`None` and ``study`` is being
+            used for single-objective optimization, the objective values are plotted.
+
+            .. note::
+                Specify this argument if ``study`` is being used for multi-objective optimization.
+        target_name:
+            Target's name to display on the axis label and the legend.
+        error_bar:
+            A flag to show the error bar.
+
+    Returns:
+        A :class:`plotly.graph_objs.Figure` object.
+    """
+
+    _imports.check()
+
+    if isinstance(study, Study):
+        studies = [study]
+    else:
+        studies = list(study)
+
+    _check_plot_args(studies, target, target_name)
+    return _get_optimization_history_plot(studies, target, target_name, error_bar)
+
+
+def _get_optimization_history_plot(
+    studies: List[Study],
+    target: Optional[Callable[[FrozenTrial], float]],
+    target_name: str,
+    error_bar: bool,
+) -> "go.Figure":
+
+    layout = go.Layout(
+        title="Optimization History Plot",
+        xaxis={"title": "Trial"},
+        yaxis={"title": target_name},
+    )
+
+    if len(studies) == 0:
+        _logger.warning("There are no studies.")
+        return go.Figure(data=[], layout=layout)
+
+    all_trials = list(
+        itertools.chain.from_iterable(
+            (
+                trial
+                for trial in study.get_trials(deepcopy=False)
+                if trial.state == TrialState.COMPLETE
+            )
+            for study in studies
+        )
+    )
+
+    if len(all_trials) == 0:
+        _logger.warning("There are no complete trials.")
+        return go.Figure(data=[], layout=layout)
+
+    if error_bar:
+        return _get_optimization_histories_with_error_bar(studies, target, target_name, layout)
+    else:
+        return _get_optimization_histories(studies, target, target_name, layout)
+
+
+def _get_optimization_histories_with_error_bar(
+    studies: List[Study],
+    target: Optional[Callable[[FrozenTrial], float]],
+    target_name: str,
+    layout: "go.Layout",
+) -> "go.Figure":
+    max_trial_number = np.max(
+        [
+            trial.number
+            for study in studies
+            for trial in study.get_trials(states=(TrialState.COMPLETE,))
+        ]
+    )
+
+    _target: Callable[[FrozenTrial], float]
+    if target is None:
+
+        def _target(t: FrozenTrial) -> float:
+            return cast(float, t.value)
+
+    else:
+        _target = target
+
+    target_values: List[List[float]] = [[] for _ in range(max_trial_number + 2)]
+    for study in studies:
+        trials = study.get_trials(states=(TrialState.COMPLETE,))
+        for t in trials:
+            target_values[t.number].append(_target(t))
+
+    mean_of_target_values = [np.mean(v) if len(v) > 0 else None for v in target_values]
+    std_of_target_values = [np.std(v) if len(v) > 0 else None for v in target_values]
+    trial_numbers = np.arange(max_trial_number + 2)[[v is not None for v in mean_of_target_values]]
+    means = np.asarray(mean_of_target_values)[trial_numbers]
+    stds = np.asarray(std_of_target_values)[trial_numbers]
+    traces = [
+        go.Scatter(
+            x=trial_numbers,
+            y=means,
+            error_y={
+                "type": "data",
+                "array": stds,
+                "visible": True,
+            },
+            mode="markers",
+            name=target_name,
+        )
+    ]
+
+    if target is None:
+        best_values: List[List[float]] = [[] for _ in range(max_trial_number + 2)]
+        for study in studies:
+            trials = study.get_trials(states=(TrialState.COMPLETE,))
+
+            if study.direction == StudyDirection.MINIMIZE:
+                best_vs = np.minimum.accumulate([cast(float, t.value) for t in trials])
+            else:
+                best_vs = np.maximum.accumulate([cast(float, t.value) for t in trials])
+
+            for i, t in enumerate(trials):
+                best_values[t.number].append(best_vs[i])
+
+        mean_of_best_values = [np.mean(v) if len(v) > 0 else None for v in best_values]
+        std_of_best_values = [np.std(v) if len(v) > 0 else None for v in best_values]
+        means = np.asarray(mean_of_best_values)[trial_numbers]
+        stds = np.asarray(std_of_best_values)[trial_numbers]
+        traces.append(go.Scatter(x=trial_numbers, y=means, name="Best Value"))
+        traces.append(
+            go.Scatter(
+                x=trial_numbers,
+                y=means + stds,
+                mode="lines",
+                line=dict(width=0.01),
+                showlegend=False,
+            )
+        )
+        traces.append(
+            go.Scatter(
+                x=trial_numbers,
+                y=means - stds,
+                mode="none",
+                showlegend=False,
+                fill="tonexty",
+                fillcolor="rgba(255,0,0,0.2)",
+            )
+        )
+
+    figure = go.Figure(data=traces, layout=layout)
+
+    return figure
+
+
+def _get_optimization_histories(
+    studies: List[Study],
+    target: Optional[Callable[[FrozenTrial], float]],
+    target_name: str,
+    layout: "go.Layout",
+) -> "go.Figure":
+
+    traces = []
+    for study in studies:
+        trials = study.get_trials(states=(TrialState.COMPLETE,))
+        if target is None:
+            if study.direction == StudyDirection.MINIMIZE:
+                best_values = np.minimum.accumulate([cast(float, t.value) for t in trials])
+            else:
+                best_values = np.maximum.accumulate([cast(float, t.value) for t in trials])
+            traces.append(
+                go.Scatter(
+                    x=[t.number for t in trials],
+                    y=[t.value for t in trials],
+                    mode="markers",
+                    name=target_name
+                    if len(studies) == 1
+                    else f"{target_name} of {study.study_name}",
+                )
+            )
+            traces.append(
+                go.Scatter(
+                    x=[t.number for t in trials],
+                    y=best_values,
+                    name="Best Value"
+                    if len(studies) == 1
+                    else f"Best Value of {study.study_name}",
+                )
+            )
+        else:
+            traces.append(
+                go.Scatter(
+                    x=[t.number for t in trials],
+                    y=[target(t) for t in trials],
+                    mode="markers",
+                    name=target_name
+                    if len(studies) == 1
+                    else f"{target_name} of {study.study_name}",
+                )
+            )
+
+    figure = go.Figure(data=traces, layout=layout)
+    figure.update_layout(width=1000, height=400)
+
+    return figure
---
+++ packages/optuna/optuna-3.0.0b1-py3-none-any.whl/optuna/visualization/matplotlib/_param_importances.py
@@ -0,0 +1,158 @@
+from collections import OrderedDict
+from typing import Callable
+from typing import List
+from typing import Optional
+
+import numpy as np
+
+import optuna
+from optuna._experimental import experimental
+from optuna.importance._base import BaseImportanceEvaluator
+from optuna.logging import get_logger
+from optuna.study import Study
+from optuna.trial import FrozenTrial
+from optuna.trial import TrialState
+from optuna.visualization._utils import _check_plot_args
+from optuna.visualization._utils import _filter_nonfinite
+from optuna.visualization.matplotlib._matplotlib_imports import _imports
+
+
+if _imports.is_successful():
+    from optuna.visualization.matplotlib._matplotlib_imports import Axes
+    from optuna.visualization.matplotlib._matplotlib_imports import cm
+    from optuna.visualization.matplotlib._matplotlib_imports import plt
+
+
+_logger = get_logger(__name__)
+
+
+AXES_PADDING_RATIO = 1.05
+
+
+@experimental("2.2.0")
+def plot_param_importances(
+    study: Study,
+    evaluator: Optional[BaseImportanceEvaluator] = None,
+    params: Optional[List[str]] = None,
+    *,
+    target: Optional[Callable[[FrozenTrial], float]] = None,
+    target_name: str = "Objective Value",
+) -> "Axes":
+    """Plot hyperparameter importances with Matplotlib.
+
+    .. seealso::
+        Please refer to :func:`optuna.visualization.plot_param_importances` for an example.
+
+    Example:
+
+        The following code snippet shows how to plot hyperparameter importances.
+
+        .. plot::
+
+            import optuna
+
+
+            def objective(trial):
+                x = trial.suggest_int("x", 0, 2)
+                y = trial.suggest_float("y", -1.0, 1.0)
+                z = trial.suggest_float("z", 0.0, 1.5)
+                return x ** 2 + y ** 3 - z ** 4
+
+
+            sampler = optuna.samplers.RandomSampler(seed=10)
+            study = optuna.create_study(sampler=sampler)
+            study.optimize(objective, n_trials=100)
+
+            optuna.visualization.matplotlib.plot_param_importances(study)
+
+    Args:
+        study:
+            An optimized study.
+        evaluator:
+            An importance evaluator object that specifies which algorithm to base the importance
+            assessment on.
+            Defaults to
+            :class:`~optuna.importance.FanovaImportanceEvaluator`.
+        params:
+            A list of names of parameters to assess.
+            If :obj:`None`, all parameters that are present in all of the completed trials are
+            assessed.
+        target:
+            A function to specify the value to display. If it is :obj:`None` and ``study`` is being
+            used for single-objective optimization, the objective values are plotted.
+
+            .. note::
+                Specify this argument if ``study`` is being used for multi-objective
+                optimization. For example, to get the hyperparameter importance of the first
+                objective, use ``target=lambda t: t.values[0]`` for the target parameter.
+        target_name:
+            Target's name to display on the axis label.
+
+    Returns:
+        A :class:`matplotlib.axes.Axes` object.
+    """
+
+    _imports.check()
+    _check_plot_args(study, target, target_name)
+    return _get_param_importance_plot(study, evaluator, params, target, target_name)
+
+
+def _get_param_importance_plot(
+    study: Study,
+    evaluator: Optional[BaseImportanceEvaluator] = None,
+    params: Optional[List[str]] = None,
+    target: Optional[Callable[[FrozenTrial], float]] = None,
+    target_name: str = "Objective Value",
+) -> "Axes":
+
+    # Set up the graph style.
+    plt.style.use("ggplot")  # Use ggplot style sheet for similar outputs to plotly.
+    fig, ax = plt.subplots()
+    ax.set_title("Hyperparameter Importances")
+    ax.set_xlabel(f"Importance for {target_name}")
+    ax.set_ylabel("Hyperparameter")
+
+    # Prepare data for plotting.
+    # Importances cannot be evaluated without completed trials.
+    # Return an empty figure for consistency with other visualization functions.
+    trials = _filter_nonfinite(
+        study.get_trials(deepcopy=False, states=(TrialState.COMPLETE,)), target=target
+    )
+    if len(trials) == 0:
+        _logger.warning("Study instance does not contain completed trials.")
+        return ax
+
+    importances = optuna.importance.get_param_importances(
+        study, evaluator=evaluator, params=params, target=target
+    )
+
+    importances = OrderedDict(reversed(list(importances.items())))
+    importance_values = list(importances.values())
+    param_names = list(importances.keys())
+    pos = np.arange(len(param_names))
+
+    # Draw horizontal bars.
+    ax.barh(
+        pos,
+        importance_values,
+        align="center",
+        color=cm.get_cmap("tab20c")(0),
+        tick_label=param_names,
+    )
+
+    renderer = fig.canvas.get_renderer()
+    for idx, val in enumerate(importance_values):
+        label = f" {val:.2f}" if val >= 0.01 else " <0.01"
+        text = ax.text(val, idx, label, va="center")
+
+        # Sometimes horizontal axis needs to be re-scaled
+        # to avoid text going over plot area.
+        bbox = text.get_window_extent(renderer)
+        bbox = bbox.transformed(ax.transData.inverted())
+        _, plot_xmax = ax.get_xlim()
+        bbox_xmax = bbox.xmax
+
+        if bbox_xmax > plot_xmax:
+            ax.set_xlim(xmax=AXES_PADDING_RATIO * bbox_xmax)
+
+    return ax
