529d249775f831388b18e97f55303bf932c9ba8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package compose-x-common/1.0.0/compose_x_common-1.0.0-py3-none-any.whl

---
+++ packages/compose-x-common/compose_x_common-1.0.0-py3-none-any.whl/compose_x_common/aws/cognito_userpool.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2022 John Mille <john@compose-x.io>
+
+
+import re
+
+USER_POOL_RE = re.compile(
+    r"^arn:aws(?:-[a-z]+)?:cognito-idp:[\S]+:(?P<accountid>[0-9]{12}):userpool/(?P<id>[\S]+)$"
+)
---
+++ packages/compose-x-common/compose_x_common-1.0.0-py3-none-any.whl/compose_x_common/aws/elasticache.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2022 John Mille <john@compose-x.io>
+
+import re
+
+CACHE_CLUSTER_ARN_RE = re.compile(
+    r"^arn:aws(?:-[a-z]+)?:elasticache:[\w-]+:[0-9]{12}:cluster:(?P<id>[\S]+)$"
+)
