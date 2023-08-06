860c169b77ddf383695c977ad0dedf7f0e684810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:53 +0000

    Add package bioimageio.core/0.5.5/bioimageio.core-0.5.5.tar.gz

---
+++ packages/bioimageio.core/bioimageio.core-0.5.5.tar.gz/bioimageio.core-0.5.5/bioimageio/core/prediction_pipeline/_prediction_pipeline.py
@@ -0,0 +1,228 @@
+import abc
+from dataclasses import dataclass
+from typing import Iterable, List, Optional, Sequence, Tuple
+
+import xarray as xr
+from marshmallow import missing
+
+from bioimageio.core.resource_io import nodes
+from bioimageio.core.resource_io.nodes import InputTensor, Model, OutputTensor
+from ._combined_processing import CombinedProcessing
+from ._model_adapters import ModelAdapter, create_model_adapter
+from ._stat_state import StatsState
+from ._utils import ComputedMeasures, Sample, TensorName
+
+
+@dataclass
+class NamedImplicitOutputShape:
+    reference_input: TensorName = missing
+    scale: List[Tuple[str, float]] = missing
+    offset: List[Tuple[str, int]] = missing
+
+    def __len__(self):
+        return len(self.scale)
+
+
+class PredictionPipeline(abc.ABC):
+    """
+    Represents model computation including preprocessing and postprocessing
+    Note: Ideally use the PredictionPipeline as a context manager
+    """
+
+    @abc.abstractmethod
+    def __enter__(self):
+        ...
+
+    @abc.abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        ...
+
+    @abc.abstractmethod
+    def forward(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        """
+        Compute predictions
+        """
+        ...
+
+    @property
+    @abc.abstractmethod
+    def name(self) -> str:
+        """
+        Name of the pipeline
+        """
+        ...
+
+    @property
+    @abc.abstractmethod
+    def input_specs(self) -> List[InputTensor]:
+        """
+        specs of inputs
+        """
+        ...
+
+    @property
+    @abc.abstractmethod
+    def output_specs(self) -> List[OutputTensor]:
+        """
+        specs of outputs
+        """
+        ...
+
+    @abc.abstractmethod
+    def load(self) -> None:
+        """
+        optional step: load model onto devices before calling forward if not using it as context manager
+        """
+        ...
+
+    @abc.abstractmethod
+    def unload(self) -> None:
+        """
+        free any device memory in use
+        """
+        ...
+
+
+class _PredictionPipelineImpl(PredictionPipeline):
+    def __init__(
+        self,
+        *,
+        name: str,
+        bioimageio_model: Model,
+        preprocessing: CombinedProcessing,
+        postprocessing: CombinedProcessing,
+        ipt_stats: StatsState,
+        out_stats: StatsState,
+        model: ModelAdapter,
+    ) -> None:
+        if bioimageio_model.run_mode:
+            raise NotImplementedError(f"Not yet implemented inference for run mode '{bioimageio_model.run_mode.name}'")
+
+        self._name = name
+        self._input_specs = bioimageio_model.inputs
+        self._output_specs = bioimageio_model.outputs
+        self._preprocessing = preprocessing
+        self._postprocessing = postprocessing
+        self._ipt_stats = ipt_stats
+        self._out_stats = out_stats
+        self._model: ModelAdapter = model
+
+    def __call__(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        return self.forward(*input_tensors)
+
+    def __enter__(self):
+        self.load()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.unload()
+        return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def input_specs(self):
+        return self._input_specs
+
+    @property
+    def output_specs(self):
+        return self._output_specs
+
+    def predict(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        """Predict input_tensor with the model without applying pre/postprocessing."""
+        return self._model.forward(*input_tensors)
+
+    def apply_preprocessing(self, sample: Sample, computed_measures: ComputedMeasures) -> None:
+        """apply preprocessing in-place, also updates given computed_measures"""
+        self._ipt_stats.update_with_sample(sample)
+        for mode, stats in self._ipt_stats.compute_measures().items():
+            if mode not in computed_measures:
+                computed_measures[mode] = {}
+            computed_measures[mode].update(stats)
+
+        self._preprocessing.apply(sample, computed_measures)
+
+    def apply_postprocessing(self, sample: Sample, computed_measures: ComputedMeasures) -> None:
+        """apply postprocessing in-place, also updates given computed_measures"""
+        self._out_stats.update_with_sample(sample)
+        for mode, stats in self._out_stats.compute_measures().items():
+            if mode not in computed_measures:
+                computed_measures[mode] = {}
+            computed_measures[mode].update(stats)
+
+        self._postprocessing.apply(sample, computed_measures)
+
+    def forward(self, *input_tensors: xr.DataArray) -> List[xr.DataArray]:
+        """Apply preprocessing, run prediction and apply postprocessing.
+        Note: The preprocessing might change input_tensors in-pace.
+        """
+        input_sample = dict(zip([ipt.name for ipt in self.input_specs], input_tensors))
+        computed_measures = {}
+        self.apply_preprocessing(input_sample, computed_measures)
+
+        prediction_tensors = self.predict(*list(input_sample.values()))
+        prediction = dict(zip([out.name for out in self.output_specs], prediction_tensors))
+        self.apply_postprocessing(prediction, computed_measures)
+
+        return [prediction[tn] for tn in [out.name for out in self.output_specs]]
+
+    def load(self):
+        self._model.load()
+
+    def unload(self):
+        self._model.unload()
+
+
+def create_prediction_pipeline(
+    bioimageio_model: nodes.Model,
+    *,
+    devices: Optional[Sequence[str]] = None,
+    weight_format: Optional[str] = None,
+    dataset_for_initial_statistics: Iterable[Sequence[xr.DataArray]] = tuple(),
+    update_dataset_stats_after_n_samples: Optional[int] = None,
+    update_dataset_stats_for_n_samples: int = float("inf"),
+    model_adapter: Optional[ModelAdapter] = None,
+) -> PredictionPipeline:
+    """
+    Creates prediction pipeline which includes:
+    * computation of input statistics
+    * preprocessing
+    * model prediction
+    * computation of output statistics
+    * postprocessing
+    """
+    model_adapter: ModelAdapter = model_adapter or create_model_adapter(
+        bioimageio_model=bioimageio_model, devices=devices, weight_format=weight_format
+    )
+
+    preprocessing = CombinedProcessing(bioimageio_model.inputs)
+
+    def sample_dataset():
+        for tensors in dataset_for_initial_statistics:
+            yield dict(zip([ipt.name for ipt in bioimageio_model.inputs], tensors))
+
+    ipt_stats = StatsState(
+        preprocessing.required_measures,
+        dataset=sample_dataset(),
+        update_dataset_stats_after_n_samples=update_dataset_stats_after_n_samples,
+        update_dataset_stats_for_n_samples=update_dataset_stats_for_n_samples,
+    )
+    postprocessing = CombinedProcessing(bioimageio_model.outputs)
+    out_stats = StatsState(
+        postprocessing.required_measures,
+        dataset=tuple(),
+        update_dataset_stats_after_n_samples=0,
+        update_dataset_stats_for_n_samples=ipt_stats.sample_count + update_dataset_stats_for_n_samples,
+    )
+
+    return _PredictionPipelineImpl(
+        name=bioimageio_model.name,
+        bioimageio_model=bioimageio_model,
+        model=model_adapter,
+        preprocessing=preprocessing,
+        postprocessing=postprocessing,
+        ipt_stats=ipt_stats,
+        out_stats=out_stats,
+    )
---
+++ packages/bioimageio.core/bioimageio.core-0.5.5.tar.gz/bioimageio.core-0.5.5/bioimageio/core/resource_tests.py
@@ -0,0 +1,234 @@
+import traceback
+from pathlib import Path
+from typing import List, Optional, Tuple, Union
+
+import numpy
+import numpy as np
+import xarray as xr
+from marshmallow import ValidationError
+
+from bioimageio.core import __version__ as bioimageio_core_version, load_resource_description
+from bioimageio.core.prediction import predict
+from bioimageio.core.prediction_pipeline import create_prediction_pipeline
+from bioimageio.core.resource_io.nodes import (
+    ImplicitOutputShape,
+    Model,
+    ParametrizedInputShape,
+    ResourceDescription,
+    URI,
+)
+from bioimageio.spec import __version__ as bioimageio_spec_version
+from bioimageio.spec.model.raw_nodes import WeightsFormat
+from bioimageio.spec.shared.raw_nodes import ResourceDescription as RawResourceDescription
+
+
+def test_model(
+    model_rdf: Union[URI, Path, str],
+    weight_format: Optional[WeightsFormat] = None,
+    devices: Optional[List[str]] = None,
+    decimal: int = 4,
+) -> dict:
+    """Test whether the test output(s) of a model can be reproduced.
+
+    Returns: summary dict with keys: name, status, error, traceback, bioimageio_spec_version, bioimageio_core_version
+    """
+    # todo: reuse more of 'test_resource'
+    tb = None
+    try:
+        model = load_resource_description(
+            model_rdf, weights_priority_order=None if weight_format is None else [weight_format]
+        )
+    except Exception as e:
+        model = None
+        error = str(e)
+        tb = traceback.format_tb(e.__traceback__)
+    else:
+        error = None
+
+    if isinstance(model, Model):
+        return test_resource(model, weight_format=weight_format, devices=devices, decimal=decimal)
+    else:
+        error = error or f"Expected RDF type Model, got {type(model)} instead."
+
+    return dict(
+        name="reproduced test outputs from test inputs",
+        status="failed",
+        error=error,
+        traceback=tb,
+        bioimageio_spec_version=bioimageio_spec_version,
+        bioimageio_core_version=bioimageio_core_version,
+    )
+
+
+def check_input_shape(shape: Tuple[int, ...], shape_spec) -> bool:
+    if isinstance(shape_spec, list):
+        if shape != tuple(shape_spec):
+            return False
+    elif isinstance(shape_spec, ParametrizedInputShape):
+        assert len(shape_spec.min) == len(shape_spec.step)
+        if len(shape) != len(shape_spec.min):
+            return False
+        min_shape = shape_spec.min
+        step = shape_spec.step
+        # check if the shape is valid for all dimension by seeing if it can be reached with an integer number of steps
+        # NOTE we allow that the valid shape is reached using a different number of steps for each axis here
+        # this is usually valid because dimensions are independent in neural networks
+        is_valid = [(sh - minsh) % st == 0 if st > 0 else sh == minsh for sh, st, minsh in zip(shape, step, min_shape)]
+        return all(is_valid)
+    else:
+        raise TypeError(f"Encountered unexpected shape description of type {type(shape_spec)}")
+
+    return True
+
+
+def check_output_shape(shape: Tuple[int, ...], shape_spec, input_shapes) -> bool:
+    if isinstance(shape_spec, list):
+        return shape == tuple(shape_spec)
+    elif isinstance(shape_spec, ImplicitOutputShape):
+        ref_tensor = shape_spec.reference_tensor
+        if ref_tensor not in input_shapes:
+            raise ValidationError(f"The reference tensor name {ref_tensor} is not in {input_shapes}")
+        ipt_shape = numpy.array(input_shapes[ref_tensor])
+        scale = numpy.array([0.0 if sc is None else sc for sc in shape_spec.scale])
+        offset = numpy.array(shape_spec.offset)
+        exp_shape = numpy.round_(ipt_shape * scale) + 2 * offset
+
+        return shape == tuple(exp_shape)
+    else:
+        raise TypeError(f"Encountered unexpected shape description of type {type(shape_spec)}")
+
+
+def test_resource(
+    rdf: Union[RawResourceDescription, ResourceDescription, URI, Path, str],
+    *,
+    weight_format: Optional[WeightsFormat] = None,
+    devices: Optional[List[str]] = None,
+    decimal: int = 4,
+):
+    """Test RDF dynamically
+
+    Returns: summary dict with keys: name, status, error, traceback, bioimageio_spec_version, bioimageio_core_version
+    """
+    error: Optional[str] = None
+    tb: Optional = None
+    test_name: str = "load resource description"
+
+    try:
+        rd = load_resource_description(rdf, weights_priority_order=None if weight_format is None else [weight_format])
+    except Exception as e:
+        error = str(e)
+        tb = traceback.format_tb(e.__traceback__)
+    else:
+        if isinstance(rd, Model):
+            test_name = "reproduced test outputs from test inputs"
+            model = rd
+            try:
+                inputs = [np.load(str(in_path)) for in_path in model.test_inputs]
+                expected = [np.load(str(out_path)) for out_path in model.test_outputs]
+
+                assert len(inputs) == len(model.inputs)  # should be checked by validation
+                input_shapes = {}
+                for idx, (ipt, ipt_spec) in enumerate(zip(inputs, model.inputs)):
+                    if not check_input_shape(tuple(ipt.shape), ipt_spec.shape):
+                        raise ValidationError(
+                            f"Shape {tuple(ipt.shape)} of test input {idx} '{ipt_spec.name}' does not match "
+                            f"input shape description: {ipt_spec.shape}."
+                        )
+                    input_shapes[ipt_spec.name] = ipt.shape
+
+                assert len(expected) == len(model.outputs)  # should be checked by validation
+                for idx, (out, out_spec) in enumerate(zip(expected, model.outputs)):
+                    if not check_output_shape(tuple(out.shape), out_spec.shape, input_shapes):
+                        error = (error or "") + (
+                            f"Shape {tuple(out.shape)} of test output {idx} '{out_spec.name}' does not match "
+                            f"output shape description: {out_spec.shape}."
+                        )
+
+                with create_prediction_pipeline(
+                    bioimageio_model=model, devices=devices, weight_format=weight_format
+                ) as prediction_pipeline:
+                    results = predict(prediction_pipeline, inputs)
+
+                if len(results) != len(expected):
+                    error = (error or "") + (
+                        f"Number of outputs and number of expected outputs disagree: {len(results)} != {len(expected)}"
+                    )
+                else:
+                    for res, exp in zip(results, expected):
+                        try:
+                            np.testing.assert_array_almost_equal(res, exp, decimal=decimal)
+                        except AssertionError as e:
+                            error = (error or "") + f"Output and expected output disagree:\n {e}"
+            except Exception as e:
+                error = str(e)
+                tb = traceback.format_tb(e.__traceback__)
+
+    # todo: add tests for non-model resources
+
+    return dict(
+        name=test_name,
+        status="passed" if error is None else "failed",
+        error=error,
+        traceback=tb,
+        bioimageio_spec_version=bioimageio_spec_version,
+        bioimageio_core_version=bioimageio_core_version,
+    )
+
+
+def debug_model(
+    model_rdf: Union[RawResourceDescription, ResourceDescription, URI, Path, str],
+    *,
+    weight_format: Optional[WeightsFormat] = None,
+    devices: Optional[List[str]] = None,
+):
+    """Run the model test and return dict with inputs, results, expected results and intermediates.
+
+    Returns dict with tensors "inputs", "inputs_processed", "outputs_raw", "outputs", "expected" and "diff".
+    """
+    inputs: Optional = None
+    inputs_processed: Optional = None
+    outputs_raw: Optional = None
+    outputs: Optional = None
+    expected: Optional = None
+    diff: Optional = None
+
+    model = load_resource_description(
+        model_rdf, weights_priority_order=None if weight_format is None else [weight_format]
+    )
+    if not isinstance(model, Model):
+        raise ValueError(f"Not a bioimageio.model: {model_rdf}")
+
+    prediction_pipeline = create_prediction_pipeline(
+        bioimageio_model=model, devices=devices, weight_format=weight_format
+    )
+    inputs = [
+        xr.DataArray(np.load(str(in_path)), dims=input_spec.axes)
+        for in_path, input_spec in zip(model.test_inputs, model.inputs)
+    ]
+
+    inputs_processed, stats = prediction_pipeline.preprocess(*inputs)
+    outputs_raw = prediction_pipeline.predict(*inputs_processed)
+    outputs, _ = prediction_pipeline.postprocess(*outputs_raw, input_sample_statistics=stats)
+    if isinstance(outputs, (np.ndarray, xr.DataArray)):
+        outputs = [outputs]
+
+    expected = [
+        xr.DataArray(np.load(str(out_path)), dims=output_spec.axes)
+        for out_path, output_spec in zip(model.test_outputs, model.outputs)
+    ]
+    if len(outputs) != len(expected):
+        error = f"Number of outputs and number of expected outputs disagree: {len(outputs)} != {len(expected)}"
+        print(error)
+    else:
+        diff = []
+        for res, exp in zip(outputs, expected):
+            diff.append(res - exp)
+
+    return {
+        "inputs": inputs,
+        "inputs_processed": inputs_processed,
+        "outputs_raw": outputs_raw,
+        "outputs": outputs,
+        "expected": expected,
+        "diff": diff,
+    }
