1e689313a099e4b31d6e3e582a7f802049d72a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package colemen-utils/1.1.1/colemen_utils-1.1.1-py3-none-any.whl

---
+++ packages/colemen-utils/colemen_utils-1.1.1-py3-none-any.whl/facades/rand_string_facade.py
@@ -0,0 +1,34 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+'''
+    A module of utility methods used for generating random information
+    or just doing shit randomly with strings.
+
+    ----------
+
+    Meta
+    ----------
+    `author`: Colemen Atwood
+    `created`: 06-03-2022 10:18:27
+    `memberOf`: random
+    `version`: 1.0
+    `method_name`: random
+'''
+
+
+from utils.string_generation import text,phone,email,url,abstract_name,rand
+
+
+
+
+
+
+
+
+
+
+
+
+
+
