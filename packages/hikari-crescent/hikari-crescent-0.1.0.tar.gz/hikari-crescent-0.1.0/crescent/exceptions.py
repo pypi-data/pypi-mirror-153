b351add66757ef7b3957f702873fe77387d233c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:20 +0000

    Add package hikari-crescent/0.1.0/hikari-crescent-0.1.0.tar.gz

---
+++ packages/hikari-crescent/hikari-crescent-0.1.0.tar.gz/hikari-crescent-0.1.0/crescent/exceptions.py
@@ -0,0 +1,11 @@
+from typing import Sequence
+
+__all__: Sequence[str] = ("CrescentException", "AlreadyRegisteredError")
+
+
+class CrescentException(Exception):
+    """Base Exception for all exceptions Crescent throws"""
+
+
+class AlreadyRegisteredError(CrescentException):
+    """Command or exception catch function was already registered"""
