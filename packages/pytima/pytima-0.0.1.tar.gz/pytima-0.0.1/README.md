bed618023e532fae5039b361e5ad39d391ee6eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:48 +0000

    Add package pytima/0.0.1/pytima-0.0.1.tar.gz

---
+++ packages/pytima/pytima-0.0.1.tar.gz/pytima-0.0.1/README.md
@@ -0,0 +1,5 @@
+﻿<div align="left">
+
+**pytima: Time-series forecast In Machine learning Algorithms** </br>
+**Version 0.0.1 out now!** 
+  
---
+++ packages/pytima/pytima-0.0.1.tar.gz/pytima-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @author: Wenting Tu
+# @email: wtugmail@163.com
+# @date: 2022/06
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pytima",
+    version="0.0.1",
+    description="Time-series forecast In Machine-learning Algorithms",
+    author=["Wenting Tu"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)
