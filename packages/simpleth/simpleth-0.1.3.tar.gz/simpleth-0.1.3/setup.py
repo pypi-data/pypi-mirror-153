76facf9db7b33656023460da8a5dc35d460e83f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:20 +0000

    Add package simpleth/0.1.3/simpleth-0.1.3.tar.gz

---
+++ packages/simpleth/simpleth-0.1.3.tar.gz/simpleth-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpleth',
+    version='0.1.3',
+    author='Stephen Newell',
+    author_email='<snewell4@gmail.com>',
+    description='Simplified Ethereum for Python',
+    long_description=open('README.rst').read(),
+    url='https://github.com/snewell4/simpleth',
+    packages=find_packages(),
+    python_requires='>=3.8',    # adds Type.Final
+    install_requires=[
+        'web3.py>=5.23.0'       # adds maxPriorityFeePerGas
+        ],
+    keywords='blockchain, ganache, Solidity, ethereum, smart-contract',
+    include_package_data=True,  # see manifest.ini
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Other/Nonlisted Topic'
+        ]
+    )
