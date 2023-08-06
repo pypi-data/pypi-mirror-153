c157a27aa2b996edc9114c3bac4c2b31bd2e120c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:12 +0000

    Add package tinytext/3.1.0/tinytext-3.1.0.tar.gz

---
+++ packages/tinytext/tinytext-3.1.0.tar.gz/tinytext-3.1.0/tests/test_tinytext.py
@@ -0,0 +1,16 @@
+from hypothesis_auto import auto_pytest_magic  # type: ignore[import]
+
+import tinytext
+
+
+def test_something() -> None:
+    # Arrange
+
+    # Act
+    tiny: str = tinytext.tinytext("into tinier text")
+
+    # Assert
+    assert tiny == "ᶦᶰᵗᵒ ᵗᶦᶰᶦᵉʳ ᵗᵉˣᵗ"
+
+
+auto_pytest_magic(tinytext.tinytext)
