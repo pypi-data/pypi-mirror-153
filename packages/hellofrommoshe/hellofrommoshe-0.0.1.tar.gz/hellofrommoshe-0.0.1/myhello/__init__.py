7b5b0df34f943ee3af890600773fa0a2b0a275f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:39 +0000

    Add package hellofrommoshe/0.0.1/hellofrommoshe-0.0.1.tar.gz

---
+++ packages/hellofrommoshe/hellofrommoshe-0.0.1.tar.gz/hellofrommoshe-0.0.1/myhello/__init__.py
@@ -0,0 +1,10 @@
+def sayhello():
+    print("hello Moshe Rosenblum have a great day !!!  ( :cd .."
+          "")
+
+
+def saybay():
+    print("bay-bay moshe")
+
+def helloeliran():
+    print("ELIRAN you are amazing, really you are unbelievable !!!")
