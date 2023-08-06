8374020e57d4687afc4ca6d87ba543fc76a69c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:02 +0000

    Add package libtable/0.3.1/libtable-0.3.1-py3-none-any.whl

---
+++ packages/libtable/libtable-0.3.1-py3-none-any.whl/libtable/tabledit.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2022 Sebastien Guerri
+#
+# This file is part of libtable.
+#
+# libtable is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# libtable is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from ._baseclass import BaseTableData
+from .tableeditcontrol import TableEditControl
+
+from prompt_toolkit import Application
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.layout.containers import HSplit
+from prompt_toolkit.layout.containers import Window
+
+
+class TableEdit:
+    def __init__(self,
+                 table: BaseTableData,
+                 full_screen: bool = False,
+                 erase_when_done: bool = False,
+                 show_header: bool = None,
+                 show_auto: bool = None,
+                 show_search: bool = True,
+                 show_sort: bool = False
+                 ) -> None:
+        self.table = table
+        self.full_screen = full_screen
+        self.erase_when_done = erase_when_done
+        self.table_control = TableEditControl(self.table,
+                                              show_header=show_header,
+                                              show_auto=show_auto,
+                                              show_search=show_search,
+                                              show_sort=show_sort
+                                              )
+
+    def addEvent(self, *name: str, fn) -> None:
+        self.table_control.addEvent(*name, fn=fn)
+
+    def show(self) -> tuple:
+        body = HSplit([
+            Window(content=self.table_control)
+        ])
+        self.app = Application(layout=Layout(body),
+                               full_screen=self.full_screen,
+                               key_bindings=self.table_control.get_key_bindings(),
+                               erase_when_done=self.erase_when_done
+                               )
+        self.app.run()
+        return self.table_control.get_response()
