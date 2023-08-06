8739227ee25b2184d20f2c5bde347bb9769f9a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:33 +0000

    Add package HalSMPy/0.0.0.4/HalSMPy-0.0.0.4.tar.gz

---
+++ packages/HalSMPy/HalSMPy-0.0.0.4.tar.gz/HalSMPy-0.0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from distutils.core import setup,Extension
+
+setup(
+    name='HalSMPy',
+    version='0.0.0.4',
+    author="Halwarsing",
+    author_email='halolxz@gmail.com',
+    description="HalSMCompiler for python",
+    ext_modules=[
+        Extension(
+            'HalSM',
+            ['halsm.c',
+             'HalSM/HalSM.c'],
+            extra_compile_args=['-std=c11','-IHalSM']
+        )
+    ],
+    long_description="HalSMC extension for python",
+    packages=['HalSM'],
+    package_dir={'HalSM':'HalSM'}
+)
