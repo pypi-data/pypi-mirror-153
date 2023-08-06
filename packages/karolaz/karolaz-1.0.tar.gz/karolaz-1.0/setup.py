f1480508cc757f9c960b689d405735b0ff64c8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package karolaz/1.0/karolaz-1.0.tar.gz

---
+++ packages/karolaz/karolaz-1.0.tar.gz/karolaz-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup (
+
+    name='karolaz',
+    version='1.0',
+    description='modul testowy',
+    ulr='http://sda.com',
+    install_requires=[],
+    packages=['pakiet_testowy'],
+
+)
