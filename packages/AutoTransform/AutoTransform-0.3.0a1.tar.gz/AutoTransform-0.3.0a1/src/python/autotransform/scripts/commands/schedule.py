f59b382f44ddbe7cd7c96119b1039b6ce91bb28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:11 +0000

    Add package AutoTransform/0.3.0a1/AutoTransform-0.3.0a1.tar.gz

---
+++ packages/AutoTransform/AutoTransform-0.3.0a1.tar.gz/AutoTransform-0.3.0a1/src/python/autotransform/scripts/commands/schedule.py
@@ -0,0 +1,77 @@
+# AutoTransform
+# Large scale, component based code modification library
+#
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
+
+# @black_format
+
+"""The schedule command is used to schedule runs of AutoTransform."""
+
+import time
+from argparse import ArgumentParser, Namespace
+
+from autotransform.event.debug import DebugEvent
+from autotransform.event.handler import EventHandler
+from autotransform.event.logginglevel import LoggingLevel
+from autotransform.event.run import ScriptRunEvent
+from autotransform.util.scheduler import Scheduler
+
+
+def add_args(parser: ArgumentParser) -> None:
+    """Adds the args to a subparser that are required to schedule runs.
+
+    Args:
+        parser (ArgumentParser): The parser for the schedule command.
+    """
+
+    parser.add_argument(
+        "schedule",
+        metavar="schedule",
+        type=str,
+        help="A file path to the JSON encoded schedule of schema runs to execute.",
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        metavar="time",
+        type=int,
+        required=False,
+        help="The timestamp to use in place of the current time, used in cases where delays in "
+        + "scheduling are likely.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        required=False,
+        help="Tells the script to output verbose logs.",
+    )
+    parser.set_defaults(func=schedule_command_main)
+
+
+def schedule_command_main(args: Namespace) -> None:
+    """The main method for the schedule command, handles the actual execution of scheduling runs.
+
+    Args:
+        args (Namespace): The arguments supplied to the schedule command, such as the JSON file.
+    """
+
+    # pylint: disable=unspecified-encoding
+
+    start_time = int(args.time) if args.time is not None else int(time.time())
+    event_handler = EventHandler.get()
+    if args.verbose:
+        event_handler.set_logging_level(LoggingLevel.DEBUG)
+
+    # Get Scheduler
+    schedule_file = args.schedule
+    event_args = {"scheduler_file": schedule_file}
+    scheduler = Scheduler.read(schedule_file, start_time)
+    event_args["scheduler"] = scheduler
+    event_handler.handle(ScriptRunEvent({"script": "schedule", "args": event_args}))
+
+    event_handler.get().handle(DebugEvent({"message": f"Running scheduler: {scheduler}"}))
+    scheduler.run(start_time)
