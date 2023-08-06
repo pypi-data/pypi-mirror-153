3360de5ea9da2b58f187cacab0469aca78f58349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:42 +0000

    Add package quanestimation/0.1.0/quanestimation-0.1.0-py2.py3-none-any.whl

---
+++ packages/quanestimation/quanestimation-0.1.0-py2.py3-none-any.whl/quanestimation/JuliaSrc/ObjectiveFunc/ObjectiveFunc.jl
@@ -0,0 +1,11 @@
+abstract type AbstractObj end
+
+abstract type quantum end
+abstract type classical end
+
+abstract type AbstractParaType end
+abstract type single_para <: AbstractParaType end
+abstract type multi_para <: AbstractParaType end
+
+include("AsymptoticBound/AsymptoticBound.jl")
+include("BayesianBound/BayesianBound.jl")
