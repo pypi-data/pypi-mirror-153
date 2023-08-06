5ff47f8d69dff7ff3e28f77c37688c63486b3d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:18 +0000

    Add package jwin/0.0.3/jwin-0.0.3.tar.gz

---
+++ packages/jwin/jwin-0.0.3.tar.gz/jwin-0.0.3/README.md
@@ -0,0 +1,12 @@
+# J-league
+Jリーグの2000~2019年の勝利数をグラフで表示
+
+チーム名の指定が必要(デフォルト札幌)
+--team チーム名
+
+
+```
+$python J-win.py --team 札幌
+```
+
+<img width="566" alt="スクリーンショット 2022-06-03 15 53 00" src="https://user-images.githubusercontent.com/72128278/171802824-3590eac5-5189-4f97-9c33-ffc4a080a9a0.png">
---
+++ packages/jwin/jwin-0.0.3.tar.gz/jwin-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jwin",
+    version="0.0.3",
+    author="ryosuke yamamoto",
+    author_email="ryosukey1724@gmail.com",
+    description='A package for visualization of aggregate data of "J League"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Doraemon-desu/J-league",
+    project_urls={
+        "Bug Tracker": "https://github.com/Doraemon-desu/J-league",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['jwin'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'jwin = jwin:main'
+        ]
+    },
+)
