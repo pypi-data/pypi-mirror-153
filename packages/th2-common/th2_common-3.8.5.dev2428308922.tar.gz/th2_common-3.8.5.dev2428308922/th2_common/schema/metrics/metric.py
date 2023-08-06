aa06bf2db8816204923abed72e04bbf2dcaa6256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:01 +0000

    Add package th2-common/3.8.5.dev2428308922/th2_common-3.8.5.dev2428308922.tar.gz

---
+++ packages/th2-common/th2_common-3.8.5.dev2428308922.tar.gz/th2_common-3.8.5.dev2428308922/th2_common/schema/metrics/metric.py
@@ -0,0 +1,59 @@
+#   Copyright 2021-2022 Exactpro (Exactpro Systems Limited)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from abc import ABC, abstractmethod
+
+import th2_common.schema.metrics.metric_monitor as metric_monitor
+
+
+class Metric(ABC):
+
+    @property
+    @abstractmethod
+    def enabled(self) -> bool:
+        pass
+
+    def create_monitor(self, name: str) -> metric_monitor.MetricMonitor:
+        """
+        Creates new monitor for the metric
+
+        :param name: monitor name
+        :return: MetricMonitor which can change metrics value
+        """
+        pass
+
+    def is_enabled(self, monitor: metric_monitor.MetricMonitor) -> bool:
+        """
+        Checks if status of a monitor is `enabled`
+
+        :param monitor: MetricMonitor
+        :return: Status (bool) of MetricMonitor
+        """
+        pass
+
+    def enable(self, monitor: metric_monitor.MetricMonitor) -> None:
+        """
+        Changes status of a monitor to `enabled`
+
+        :param monitor: MetricMonitor
+        """
+        pass
+
+    def disable(self, monitor: metric_monitor.MetricMonitor) -> None:
+        """
+        Changes status of a monitor to `disabled`
+
+        :param monitor: MetricMonitor
+        """
+        pass
