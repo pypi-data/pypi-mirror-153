e8234abef7b1be05b536f2f14cfb88f24a1c224f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package compose-x-render/0.5.5/compose_x_render-0.5.5-py3-none-any.whl

---
+++ packages/compose-x-render/compose_x_render-0.5.5-py3-none-any.whl/compose_x_render/consts.py
@@ -0,0 +1,12 @@
+#  -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2021 John Mille <john@compose-x.io>
+
+"""
+Module to define generic constants of names etc. found in docker-compose files.
+"""
+
+SERVICES = "services"
+VOLUMES = "volumes"
+SECRETS = "secrets"
+PORTS = "ports"
