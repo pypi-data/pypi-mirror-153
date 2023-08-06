22ffb4c8f8fbbdd505908ad6f2ec58e05ffb0ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package tima/0.0.1/tima-0.0.1.tar.gz

---
+++ packages/tima/tima-0.0.1.tar.gz/tima-0.0.1/README.md
@@ -0,0 +1,5 @@
+﻿<div align="left">
+
+**TIMA: Time-series forecast In Machine learning Algorithm** </br>
+**Version 0.0.1 out now!** 
+  
---
+++ packages/tima/tima-0.0.1.tar.gz/tima-0.0.1/tima/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @author: Wenting Tu
+# @email: wtugmail@163.com
+# @date: 2022/06
+
+import warnings
+warnings.filterwarnings("ignore")
+
+__version__ = "0.0.1"
+
+
+
