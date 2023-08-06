d63a450b71b98f5dfcd0017c18234a9801ad3489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:07 +0000

    Add package pysensibo/1.0.17/pysensibo-1.0.17.tar.gz

---
+++ packages/pysensibo/pysensibo-1.0.17.tar.gz/pysensibo-1.0.17/setup.py
@@ -0,0 +1,32 @@
+"""A setuptools based setup module."""
+
+from setuptools import setup, find_packages
+
+print(find_packages())
+setup(
+    name="pysensibo",
+    version="1.0.17",
+    description="asyncio-friendly python API for Sensibo",
+    long_description="asyncio-friendly python API for Sensibo"
+    "(https://sensibo.com). Requires Python 3.4+",
+    url="https://github.com/andrey-git/pysensibo",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="Sensibo",
+    install_requires=["aiohttp"],
+    zip_safe=False,
+    author="andrey-git",
+    author_email="andrey-git@users.noreply.github.com",
+    packages=find_packages(),
+    package_data={"pysensibo": ["py.typed"]},
+)
