7414cf352a37c01324c86b875d39a98d11e922a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:50 +0000

    Add package zephony-helpers/0.3/zephony_helpers-0.3.tar.gz

---
+++ packages/zephony-helpers/zephony_helpers-0.3.tar.gz/zephony_helpers-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='zephony_helpers',
+    packages=['zephony_helpers'],
+    description='Helper functions for Python web development',
+    version='0.3',
+    url='https://github.com/Zephony/helpers',
+    author='Kevin Isaac',
+    author_email='kevin@zephony.com',
+    keywords=['zephony', 'helpers', 'web'],
+    install_requires=[
+        'voluptuous',
+        'twilio',
+    ],
+)
+
