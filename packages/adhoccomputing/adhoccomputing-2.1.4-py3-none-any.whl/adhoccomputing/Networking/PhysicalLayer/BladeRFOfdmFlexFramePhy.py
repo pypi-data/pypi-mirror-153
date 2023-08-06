e48bc401afbdaf78172b9ff81ce384b1feda7774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:33 +0000

    Add package adhoccomputing/2.1.4/adhoccomputing-2.1.4-py3-none-any.whl

---
+++ packages/adhoccomputing/adhoccomputing-2.1.4-py3-none-any.whl/adhoccomputing/Networking/PhysicalLayer/BladeRFOfdmFlexFramePhy.py
@@ -0,0 +1,90 @@
+from ctypes import *
+import pickle
+from threading import Lock
+from .FrameHandlerBase import *
+from ...Generics import *
+from .LiquidDspUtils import *
+import numpy as np
+import zlib 
+
+mutex = Lock()
+
+framers: FramerObjects = FramerObjects()
+
+#def ofdm_callback(header:POINTER(c_ubyte), header_valid:c_uint32, payload:POINTER(c_ubyte), payload_len:c_uint32, payload_valid:c_int32, stats:struct_c__SA_framesyncstats_s, userdata:POINTER(None)):
+def ofdm_callback(header:POINTER(c_ubyte), header_valid:c_int, payload:POINTER(c_ubyte), payload_len:c_uint, payload_valid:c_int, stats:struct_c__SA_framesyncstats_s, userdata:c_void_p ):
+    #mutex.acquire(1)
+    try:
+        framer = framers.get_framer_by_id(userdata)
+        #logger.applog(f"Node {framer.componentinstancenumber} RSSI {stats.rssi} {framer.sdrdev.rssi} {payload_valid}")
+        if payload_valid != 0:
+            #ofdmflexframesync_print(framer.fs) 
+            pload = string_at(payload, payload_len)
+            phymsg = pickle.loads(zlib.decompress(pload))
+            msg = GenericMessage(phymsg.header, phymsg.payload)
+            framer.send_self(Event(framer, PhyEventTypes.RECV, msg))
+            #logger.applog(f"Message= {str(msg)}   RSSI= {stats.rssi}")   
+    except Exception as ex:
+        logger.critical(f"Exception_ofdm_callback: {ex}")
+    #mutex.release()
+    ofdmflexframesync_reset(framer.fs)
+    return 0
+
+
+  
+class BladeRFOfdmFlexFramePhy(FrameHandlerBase):
+    
+
+    def rx_callback(self, num_rx_samps, recv_buffer):
+        try:
+            ofdmflexframesync_execute_sc16q11(self.fs, recv_buffer , num_rx_samps)
+        except Exception as ex:
+            logger.critical(f"Exception rx_callback: {ex}")
+
+    
+    def transmit(self, _header, _payload, _payload_len, _mod, _fec0, _fec1):   
+        logger.debug(f"{self.componentname}-{self.componentinstancenumber} will send {_payload_len} bytes")
+        ofdmflexframegen_assemble(self.fg, _header, _payload, c_uint32(_payload_len))
+        last_symbol = 0
+        self.fgbuffer[:] = 0
+        while (last_symbol == 0):
+            last_symbol = ofdmflexframegen_write_sc16q11(self.fg, self.fgbuffer, c_uint32(self.fgbuffer_len))
+            #self.rx_callback( c_uint32(self.fgbuffer_len), self.fgbuffer.flatten (order="C"))  #loopback for trial
+            self.sdrdev.transmit_samples(self.fgbuffer)
+        self.sdrdev.finalize_transmit_samples()
+        
+    def configure(self):
+        self.fgprops = ofdmflexframegenprops_s(LIQUID_CRC_32, LIQUID_FEC_NONE, LIQUID_FEC_HAMMING74, LIQUID_MODEM_QPSK)
+        res = ofdmflexframegenprops_init_default(byref(self.fgprops))
+        self.fgprops.check = LIQUID_CRC_32
+        self.fgprops.fec0 = LIQUID_FEC_NONE
+        self.fgprops.fec1 = LIQUID_FEC_HAMMING74
+        self.fgprops.mod_scheme = LIQUID_MODEM_QPSK
+        self.M = 1024
+        self.cp_len = 8
+        self.taper_len = 8
+        self.fg = ofdmflexframegen_create(self.M, self.cp_len, self.taper_len, None, byref(self.fgprops))
+       
+        self.fgbuffer_len = 8192 #1024 #(self.M + self.cp_len + self.taper_len)*8
+        self.fgbuffer = np.zeros(self.fgbuffer_len*self.sdrdev.bytes_per_sample//sizeof(c_int16), dtype=np.int16) # sc16q1 samples
+        
+        #res = ofdmflexframegen_print(self.fg)
+        
+        self.ofdm_callback_function = framesync_callback(ofdm_callback)
+        
+        try: 
+            # WILL PASS ID of THIS OBJECT in userdata then will find the object in FramerObjects
+            self.fs :ofdmflexframesync = ofdmflexframesync_create(self.M, self.cp_len, self.taper_len, None, self.ofdm_callback_function, id(self))
+        except Exception as ex:
+            logger.critical(f"Exception configure: {ex}")
+
+        ofdmflexframegen_reset(self.fg)
+        ofdmflexframesync_reset(self.fs)
+        
+    # Callbacks have to be outside since the c library does not like "self"
+    # Because of this reason will use userdata to get access back to the framer object 
+
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, usrpconfig=None, num_worker_threads=1, topology=None):
+        self.framers = framers
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, usrpconfig, num_worker_threads, topology, self.framers, SDRType="x115")
+        
---
+++ packages/adhoccomputing/adhoccomputing-2.1.4-py3-none-any.whl/adhoccomputing/Networking/PhysicalLayer/FrameHandlerBase.py
@@ -0,0 +1,115 @@
+from ...GenericModel import GenericModel
+import queue
+from threading import Thread
+from .SDRUtils import SDRUtils
+import zlib
+from .LiquidDspUtils import *
+from enum import Enum
+from ...Generics import *
+from ctypes import *
+import pickle
+import numpy as np
+import copy
+# define your own message types
+class PhyMessageTypes(Enum):
+  PHYFRAMEDATA = "PHYFRAMEDATA"
+
+
+class PhyEventTypes(Enum):
+  RECV = "recv"
+
+
+# define your own message header structure
+class PhyMessageHeader(GenericMessageHeader):
+    pass
+
+class PhyFrame():
+  def __init__(self, num_rx_samps, recv_buffer):
+    self.num_rx_samps = num_rx_samps
+    #self.recv_buffer= copy.deepcopy(recv_buffer)
+    self.recv_buffer= recv_buffer
+    
+# define your own message payload structure
+class PhyMessagePayload(GenericMessagePayload):
+
+  def __init__(self, header, payload):
+    self.phyheader = header
+    self.phypayload = payload
+
+
+class FrameHandlerBase(GenericModel):
+  #SDR type can be b200 or x115 for ettus and bladerf respectively.
+  def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, usrpconfig=None, num_worker_threads=1, topology=None, framers=None, SDRType="b200"):
+    super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
+    self.frame_in_queue = queue.Queue()  # For speed up, a sparate queue for incoming frames will be used.
+    self.number_of_frame_handler_threads = 1
+    self.t = [None]*self.number_of_frame_handler_threads
+    for i in range(self.number_of_frame_handler_threads):
+        self.t[i] = Thread(target=self.frame_in_queue_handler, args=[self.frame_in_queue])
+        self.t[i].daemon = True
+        self.t[i].start()
+
+    self.usrpconfig = usrpconfig # should be UsrpConfiguration
+    framers.add_framer(id(self), self)
+    if SDRType=="b200":
+      from .UhdUtils import AhcUhdUtils
+      self.sdrdev = AhcUhdUtils(self.componentinstancenumber)
+    else:
+      if SDRType=="x115":
+        from .BladeRFUtils import BladeRFUtils
+        self.sdrdev = BladeRFUtils(self.componentinstancenumber)
+      else:
+        self.sdrdev = SDRUtils(self.componentinstancenumber)
+    self.sdrdev.configureSdr(type=SDRType, sdrconfig=self.usrpconfig)
+    self.configure()
+    self.eventhandlers[PhyEventTypes.RECV] = self.on_recv
+
+  def on_exit(self, eventobj: Event):
+    self.sdrdev.receiveenabled = False
+    self.sdrdev.shutdown(0)
+    return super().on_exit(eventobj)
+
+  def on_init(self, eventobj: Event):
+    logger.debug(f"====> I WILL START RECEIVING SAMPLES :  {self.componentname} - {self.componentinstancenumber}")
+    self.sdrdev.receiveenabled = True
+    self.sdrdev.start_rx(self.rx_callback, self)
+    return super().on_init(eventobj)
+        
+
+        
+  def frame_in_queue_handler(self, myqueue):
+    while not self.terminated:
+      #logger.applog(f"{self.componentname} {self.componentinstancenumber} received frame from sdr")
+      eventobj = myqueue.get()
+      recv_buffer= eventobj.eventcontent.recv_buffer
+      num_rx_samps= eventobj.eventcontent.num_rx_samps
+      self.rx_callback(num_rx_samps, recv_buffer)
+      myqueue.task_done()
+
+  def on_recv(self, eventobj: Event):
+    logger.debug(f"{self.componentname}.{self.componentinstancenumber} RECEIVED {str(eventobj)}")
+
+    #if eventobj.eventcontent.payload.phyheader.messagefrom != self.componentinstancenumber:
+    msg = GenericMessage(eventobj.eventcontent.payload.phyheader, eventobj.eventcontent.payload.phypayload)
+    self.send_up(Event(self, EventTypes.MFRB, msg))
+
+  def on_message_from_top(self, eventobj: Event):
+# channel receives the input message and will process the message by the process event in the next pipeline stage
+# Preserve the event id through the pipeline
+    try:
+      header  = np.zeros(8, dtype=np.ubyte)
+      #for i in range(8):
+      #  header[i] = i
+      hdr = PhyMessageHeader(PhyMessageTypes.PHYFRAMEDATA, self.componentinstancenumber,MessageDestinationIdentifiers.LINKLAYERBROADCAST)
+      pld = PhyMessagePayload(eventobj.eventcontent.header, eventobj.eventcontent.payload )
+      msg = GenericMessage(hdr, pld)
+      
+      ##### COMPRESS
+      byte_arr_msg = bytearray(zlib.compress(pickle.dumps(msg)))
+      payload_len = len(byte_arr_msg)
+      payload = np.frombuffer(byte_arr_msg, dtype=np.ubyte)
+      self.transmit(header, payload, payload_len, LIQUID_MODEM_QPSK, LIQUID_FEC_NONE, LIQUID_FEC_HAMMING74 )  # TODO: Check params
+      #logger.applog(f"Trasmitting {payload_len} bytes")
+      
+    except Exception as ex:
+      logger.critical(f"exection in pickle {ex}")
