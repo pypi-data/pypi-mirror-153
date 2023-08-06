6d6aed449983d18745571c778e06159602aec7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:41 +0000

    Add package malym/1.0/malym-1.0.tar.gz

---
+++ packages/malym/malym-1.0.tar.gz/malym-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+
+    name='malym',
+    version='1.0',
+    description="Pakiet testowy",
+    url='http://sda.com',
+    install_requires=[],
+    packages=['pakiet_testowy'],
+
+)
