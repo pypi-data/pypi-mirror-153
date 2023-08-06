3e284b127847ac8d3f99f1288dda6faa2c941731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:02 +0000

    Add package dotwiz/0.1.0/dotwiz-0.1.0-py2.py3-none-any.whl

---
+++ packages/dotwiz/dotwiz-0.1.0-py2.py3-none-any.whl/dotwiz/__init__.py
@@ -0,0 +1,29 @@
+"""
+``dotwiz``
+~~~~~~~~~~
+
+DotWiz is a ``dict`` subclass that enables accessing (nested) keys
+in dot notation.
+
+Sample Usage:
+
+    >>> from dotwiz import DotWiz
+    >>> dw = DotWiz.from_dict({'this': {'works': {'for': [{'nested': 'values'}]}}})
+    >>> dw
+    DotWiz(this=DotWiz(works=DotWiz(for=[DotWiz(nested='values')])))
+    >>> dw.this.works['for'][0].nested
+    'values'
+
+For full documentation and more advanced usage, please see
+<https://dotwiz.readthedocs.io>.
+
+:copyright: (c) 2022 by Ritvik Nag.
+:license:MIT, see LICENSE for more details.
+"""
+
+__all__ = [
+    'DotWiz',
+    'make_dot_wiz'
+]
+
+from .main import DotWiz, make_dot_wiz
