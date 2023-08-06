1d042fee0eac346de982a8d55a498658ce8e17a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:43 +0000

    Add package colemen-utils/0.1.1/colemen_utils-0.1.1-py3-none-any.whl

---
+++ packages/colemen-utils/colemen_utils-0.1.1-py3-none-any.whl/facades/sql_generate_facade.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+'''
+    A module of utility methods used for Generating SQL code
+
+    ----------
+
+    Meta
+    ----------
+    `author`: Colemen Atwood
+    `created`: 06-03-2022 10:22:15
+    `memberOf`: sql
+    `version`: 1.0
+'''
+
+
+
+from utils.parse_sql import drop_table,insert_sql
