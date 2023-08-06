ca91a484429280f5e77ffb080726d4b35f7fd3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:38 +0000

    Add package sky-api-client/1.0.6/sky_api_client-1.0.6-py3-none-any.whl

---
+++ packages/sky-api-client/sky_api_client-1.0.6-py3-none-any.whl/sky_api_client/entity/custom_field.py
@@ -0,0 +1,11 @@
+from sky_api_client.entity.base import Entity
+from sky_api_client.entity.registry import EntityRegistry
+
+
+@EntityRegistry.register('custom_field')
+class CustomField(Entity):
+    LIST_URL = '/constituent/v1/constituents/customfields/'
+    CREATE_URL = '/constituent/v1/constituents/customfields/'
+    GET_URL = '/constituent/v1/constituents/{id}/customfields/'
+    UPDATE_URL = '/constituent/v1/constituents/customfields/{id}/'
+    DELETE_URL = '/constituent/v1/constituents/customfields/{id}/'
