61f857391637f2b99eabeb3c1664fdd46572be31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:12 +0000

    Add package metamock/0.2.5/metamock-0.2.5.tar.gz

---
+++ packages/metamock/metamock-0.2.5.tar.gz/metamock-0.2.5/metamock/cli.py
@@ -0,0 +1,113 @@
+"""Console script for metamock."""
+
+import os
+import sys
+from pathlib import Path
+
+import click
+
+from .config import write_config
+from .server import USER_CONFIG_FILENAME, Server
+from .version import __timestamp__, __version__
+
+header = f"{__name__.split('.')[0]} v{__version__} {__timestamp__}"
+
+
+@click.group("metamock")
+@click.version_option(message=header)
+@click.option(
+    "-c",
+    "--config-file",
+    type=click.Path(dir_okay=False, path_type=Path),
+    help=f"config file [~./{USER_CONFIG_FILENAME}]",
+)
+@click.option(
+    "-h", "--host", type=str, help="override config file host address"
+)
+@click.option("-p", "--port", type=int, help="override config file port")
+@click.option("-d", "--debug", is_flag=True, help="debug mode")
+@click.pass_context
+def cli(ctx, **kwargs):
+    class Context:
+        def __init__(self, args):
+            for k, v in args.items():
+                setattr(self, k, v)
+
+    ctx.obj = Context(kwargs)
+
+    def exception_handler(
+        exception_type, exception, traceback, debug_hook=sys.excepthook
+    ):
+
+        if ctx.obj.debug:
+            debug_hook(exception_type, exception, traceback)
+        else:
+            click.echo(f"{exception_type.__name__}: {exception}", err=True)
+
+    sys.excepthook = exception_handler
+
+
+@cli.command()
+@click.option("-n", "--profile_name", type=str, help="configuration profile")
+@click.option("-s", "--server", type=str, help="optional WSGI server name")
+@click.pass_context
+def run(ctx, profile_name, server):
+    """serve mocked aws instance metadata"""
+
+    click.echo(f"metamock {__version__} - serve mocked AWS instance metadata")
+
+    args = ctx.obj
+    config_file = args.config_file
+    debug = args.debug
+    host = args.host
+    port = args.port
+    return Server(config_file, profile_name, debug).run(host, port, server)
+
+
+@cli.command()
+@click.option("-i", "--id", "_id", type=str, help="AWS_ACCESS_KEY_ID")
+@click.option("-k", "--key", type=str, help="AWS_SECRET_ACCESS_KEY")
+@click.option("-r", "--region", type=str, help="AWS_REGION")
+@click.option("-e", "--mfa_enable", is_flag=True, help="MFA enable")
+@click.option("-s", "--mfa_secret", type=str, help="MFA secret")
+@click.option(
+    "-d", "--mfa_token_duration", type=int, help="MFA token duration"
+)
+@click.option("-a", "--mfa_role_arn", type=str, help="MFA role ARN")
+@click.argument("output", type=click.File("w"), default="-")
+@click.pass_context
+def configure(
+    ctx,
+    _id,
+    key,
+    region,
+    mfa_enable,
+    mfa_secret,
+    mfa_token_duration,
+    mfa_role_arn,
+    output,
+):
+    """write a config file using provided arguments"""
+
+    args = ctx.obj
+    id_name = "AWS_ACCESS_KEY_ID"
+    key_name = "AWS_SECRET_ACCESS_KEY"
+    region_name = "AWS_REGION"
+
+    return write_config(
+        output,
+        args.host or "0.0.0.0",
+        args.port or 80,
+        _id or os.environ.get(id_name, "SET_" + id_name),
+        key or os.environ.get(key_name, "SET_" + key_name),
+        region or os.environ.get(region_name, "SET_" + region_name),
+        mfa_enable,
+        mfa_secret,
+        mfa_token_duration,
+        mfa_role_arn,
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(cli())  # pragma: no cover
---
+++ packages/metamock/metamock-0.2.5.tar.gz/metamock-0.2.5/tests/test_cli.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import os
+import shlex
+
+import pytest
+from click.testing import CliRunner
+
+from metamock import __version__, cli
+
+
+@pytest.fixture
+def runner():
+    def _runner(cmd=[], **kwargs):
+        if isinstance(cmd, str):
+            cmd = shlex.split(cmd)
+        cli_runner = CliRunner()
+        assert isinstance(cmd, list)
+        ret = cli_runner.invoke(cli, cmd, **kwargs)
+        assert ret.exit_code == 0, ret.output
+        assert not ret.exception, str(ret.exception)
+        return ret
+
+    return _runner
+
+
+def test_cli_version():
+    assert cli.name == "metamock"
+    assert __version__
+    assert isinstance(__version__, str)
+
+
+def test_cli_no_args(runner):
+    result = runner()
+    assert result.exit_code == 0, result
+    assert "metamock" in result.output
+
+
+def test_cli_help(runner):
+    result = runner("--help")
+    assert result.exit_code == 0, result
+    assert "Show this message and exit." in result.output
+
+
+def test_cli_configure(runner, shared_datadir):
+    test_config = shared_datadir / "default-config"
+    assert test_config.is_file()
+    test_out = test_config.read_text()
+
+    for k in os.environ.keys():
+        if k.startswith("AWS_"):
+            os.environ.pop(k)
+
+    ret = runner("configure")
+    configure_out = ret.stdout
+
+    assert configure_out == test_out
+
+
+@pytest.mark.skip(reason="unit test only")
+def test_cli_run_config(runner, shared_datadir):
+    test_config = shared_datadir / "default_config"
+    result = runner(["--debug", "--config-file", test_config, "run"])
+    assert result.ok
