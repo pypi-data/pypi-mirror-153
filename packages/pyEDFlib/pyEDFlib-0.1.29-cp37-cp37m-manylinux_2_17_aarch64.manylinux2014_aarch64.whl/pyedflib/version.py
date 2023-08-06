e9ba63afdfd144bbf66de0ed55399393aafc8299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package pyEDFlib/0.1.29/pyEDFlib-0.1.29-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.29-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.29'
+version = '0.1.29'
+full_version = '0.1.29'
+git_revision = '902e956d1adaf20e1c5c149eb1531715f173d874'
+release = True
+
+if not release:
+    version = full_version
