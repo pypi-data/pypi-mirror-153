2877b7f0b2d9ec0792c4457148b22faedebb1191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:15 +0000

    Add package rrshare/4.2.0/rrshare-4.2.0.tar.gz

---
+++ packages/rrshare/rrshare-4.2.0.tar.gz/rrshare-4.2.0/rrshare/rqUtil/rqSql.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+import asyncio
+import asyncpg
+import psycopg2
+from rrshare.rqUtil import setting
+
+
+host = setting['IP_DATABASE_ALIYUN']
+db_name = setting["DATABASE_RRSHARE"]
+passwd = setting["PGSQL_PASSWORD"]
+uri = f'postgresql://postgres:{passwd}@{host}:5432/{db_name}'
+
+# connect client
+def rq_util_sql_postgres_setting(uri=uri):
+    # 使用uri代替ip,port的连接方式
+    # 这样可以对postgresql进行加密:
+    # uri=postgresql://user:passwor@ip:port/daatbase
+    client = psycopg2.connect(uri)
+    #print(client)
+    return client
+
+
+if __name__ == '__main__':
+    # test client
+    print(rq_util_sql_postgres_setting())
+
