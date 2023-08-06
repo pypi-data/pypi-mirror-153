b63fe21febf9247c3efee3fff5ddef03632ab630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:17 +0000

    Add package hdf5plugin/3.3.1/hdf5plugin-3.3.1-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/hdf5plugin/hdf5plugin-3.3.1-py3-none-macosx_10_9_x86_64.whl/hdf5plugin/_config.py
@@ -0,0 +1,1 @@
+config = {'openmp': False, 'native': False, 'sse2': True, 'avx2': False, 'cpp11': True, 'filter_file_extension': '.dylib', 'embedded_filters': ('blosc', 'bshuf', 'fcidecomp', 'lz4', 'zfp', 'zstd')}
