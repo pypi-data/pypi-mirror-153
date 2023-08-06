9f47b1cadd926d59f82aa6aa7bd69e5d626315ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:00 +0000

    Add package metamock/0.2.3/metamock-0.2.3-py2.py3-none-any.whl

---
+++ packages/metamock/metamock-0.2.3-py2.py3-none-any.whl/metamock/config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+def optional(key, value):
+    if value:
+        return f"{key}={value}\n"
+    else:
+        return f"#{key}=...\n"
+
+
+def write_config(
+    output,
+    host,
+    port,
+    access_key,
+    secret_key,
+    region,
+    mfa_enabled,
+    mfa_secret,
+    mfa_token_duration,
+    mfa_role_arn,
+):
+    output.write("[aws]\n")
+    output.write(f"access_key={access_key}\n")
+    output.write(f"secret_key={secret_key}\n")
+    output.write(f"region={region}\n")
+    output.write(f"mfa_enabled={mfa_enabled}\n")
+    output.write(optional("mfa_secret", mfa_secret))
+    output.write(optional("mfa_token_duration", mfa_token_duration))
+    output.write(optional("mfa_role_arn", mfa_role_arn))
+    output.write("[metadata]\n")
+    output.write(f"host={host}\n")
+    output.write(f"port={port}\n")
+    output.write("profile=default\n")
