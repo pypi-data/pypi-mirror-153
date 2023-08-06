4f317bee7c9e631f1df6a7cbcdc9f95cac6bad72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:05 +0000

    Add package pydaniel/0.0.2/pydaniel-0.0.2.tar.gz

---
+++ packages/pydaniel/pydaniel-0.0.2.tar.gz/pydaniel-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pydaniel',
+    description='this package does Daniel things',
+    version='0.0.2',
+    license='Apache2',
+    author="Eren Güven",
+    author_email='erenguven0@gmail.com',
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
+    # url='https://github.com/gmyrianthous/example-publish-pypi',
+    # keywords='example project',
+    # install_requires=[
+    #       'scikit-learn',
+    #   ],
+
+)
