11b22684a3724335ee3b41c92a72f5ac58b951ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:47 +0000

    Add package text-highlighter/0.0.5/text-highlighter-0.0.5.tar.gz

---
+++ packages/text-highlighter/text-highlighter-0.0.5.tar.gz/text-highlighter-0.0.5/text_highlighter/frontend/build/precache-manifest.46f02c662db446b0052a4defa86f4600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b47fe9c03973ee30ce6d93826e3951",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4bf4d339a944c43027b",
+    "url": "./static/js/2.c75557a6.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.c75557a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ecd4d4306518924e39",
+    "url": "./static/js/main.76180ef6.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
