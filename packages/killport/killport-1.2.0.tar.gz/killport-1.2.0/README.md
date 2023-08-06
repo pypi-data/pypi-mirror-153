a9fb8a96e3db7de8600ab6a6b4786b2b3ac5ed27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:15 +0000

    Add package killport/1.2.0/killport-1.2.0.tar.gz

---
+++ packages/killport/killport-1.2.0.tar.gz/killport-1.2.0/README.md
@@ -0,0 +1,23 @@
+# `killport`
+
+Kill processes using a given port on your laptop.
+
+## Installation
+
+`pip install killport`
+
+## Usage
+
+`killport 1234`
+- kills any process using the 1234 port on your laptop
+
+`killport 1234 2345`
+- kills any process using either the 1234 or 2345 ports on your laptop
+
+`killport 1234 --view-only`
+- Displays the processes that would be deleted, but doesn't actually delete them.
+
+## How does this differ from...
+
+- [freeport](https://github.com/yashbathia/freeport/) -- Windows support, since this uses `psutil` rather than `lsof`
+- [kill-port](https://github.com/tiaanduplessis/kill-port) -- Very similar, but this is available in PyPI vs NPM
---
+++ packages/killport/killport-1.2.0.tar.gz/killport-1.2.0/killport.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import argparse
+from typing import NamedTuple
+
+import psutil
+
+
+class ProcessInfo(NamedTuple):
+    port: int
+    process: psutil.Process
+
+
+def get_processes(ports: list[int]) -> list[ProcessInfo]:
+    processes = set()
+    for process in psutil.process_iter():
+        try:
+            conns = process.connections(kind='inet')
+        except (psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+
+        for conn in conns:
+            port = conn.laddr.port
+            if port in ports:
+                processes.add(ProcessInfo(port=port, process=process))
+
+    return sorted(processes, key=lambda p: p.port)
+
+
+def kill_ports(*, ports: list[int], view_only: bool = False) -> int:
+    if not ports:
+        print('Please supply a list of ports to kill')
+        return 1
+
+    processes = get_processes(ports)
+    if processes:
+        print('Killing:' if not view_only else 'Would kill:')
+
+    for pinfo in processes:
+        emoji = '⚠️ ' if view_only else '❌'
+        process = pinfo.process
+        print(
+            f'{emoji} {process.name()} (pid {process.pid}) '
+            f'on port {pinfo.port}',
+        )
+        if not view_only:
+            process.kill()
+
+    return 1 if processes else 0
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ports', type=int, nargs='*')
+    parser.add_argument('--view-only', action='store_true')
+    args = parser.parse_args()
+
+    return kill_ports(ports=args.ports, view_only=args.view_only)
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
