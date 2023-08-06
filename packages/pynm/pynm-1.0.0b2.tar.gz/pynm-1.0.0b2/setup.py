3e059ae6a3d80de622d81b93f5ff83a59ec66cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:51 +0000

    Add package pynm/1.0.0b2/pynm-1.0.0b2.tar.gz

---
+++ packages/pynm/pynm-1.0.0b2.tar.gz/pynm-1.0.0b2/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+setup(
+    name="pynm",
+    version="1.0.0b2",
+    author="Annabelle HARVEY, Guillaume DUMAS",
+    author_email="annabelle.harvey@umontreal.ca, guillaume.dumas@ppsp.team",
+    description=("Python implementation of Normative Modelling", 
+                 "with GAMLSS, Gaussian Processes, LOESS & Centiles approaches."),
+    long_description_content_type="text/x-rst",
+    license="BSD",
+    keywords="gaussian processes statistics modeling",
+    url="https://github.com/ppsp-team/PyNM",
+    packages=['pynm', 'test'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: BSD License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pynm = pynm.cli:main',
+        ],
+    },
+   install_requires=[
+        'matplotlib >= 3.3.4',
+        'numpy >= 1.18.5',
+        'pandas >= 1.1.5',
+        'scikit_learn >= 0.24.1',
+        'scipy >= 1.5.3',
+        'seaborn >= 0.11.1',
+        'statsmodels >= 0.12.2',
+        'tqdm == 4.59.0'
+   ],
+)
