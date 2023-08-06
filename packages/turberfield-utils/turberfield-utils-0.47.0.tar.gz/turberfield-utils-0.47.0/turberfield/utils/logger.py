ee85fdf7719fcb217b7e471bc7b2d785702d8ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:43 +0000

    Add package turberfield-utils/0.47.0/turberfield-utils-0.47.0.tar.gz

---
+++ packages/turberfield-utils/turberfield-utils-0.47.0.tar.gz/turberfield-utils-0.47.0/turberfield/utils/logger.py
@@ -0,0 +1,433 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+.. code-block:: python
+
+    from turberfield.utils.logger import LogManager
+
+    logger = LogManager().get_logger("root")
+    logger.info("Hello, World!")
+
+.. code-block:: shell
+
+    2022-05-25 18:40:00.766978|    INFO|root| Hello, World!
+
+The main feature of these Loggers is that you can set them to format any
+arbitrary data you might want to send them.
+
+A logger has a frame which is a list of the formats of all the fields
+it can present to a LogAdapter.
+
+.. code-block:: python
+
+    print(logger.frame)
+
+.. code-block:: shell
+
+    ['{now}', '{level.name:>8}', '{logger.name}', ' {0}']
+
+This frame can be modified at run time to add extra fields to the log.
+Unlike the standard logging module, it doesn't matter if that data is missing:
+
+.. code-block:: python
+
+    import http
+
+    logger.frame += ["{status.name}"]
+    logger.info("Situation report", status=http.HTTPStatus.OK)
+
+.. code-block:: shell
+
+    2022-05-25 18:53:51.767937|    INFO|root| Situation report|OK
+
+You can supply a customised LogAdapter to perform filtering or reformatting
+on an endpoint-specific basis.
+
+.. code-block:: python
+
+    import re
+    import platform
+
+    from turberfield.utils.logger import LogAdapter
+
+    class Alarmist(LogAdapter):
+
+        patterns = [
+            (re.compile("NOTE"), (234, 0, 255)),
+            (re.compile("INFO"), (0, 255, 255)),
+            (re.compile("ERROR"), (234, 255, 0)),
+            (re.compile("WARNING"), (255, 106, 0)),
+            (re.compile("CRITICAL"), (255, 0, 106)),
+        ]
+
+        def colour_field(self, field, word):
+            if "level" in field:
+                r, g, b = next(
+                    (c for r, c in self.patterns if r.search(word)),
+                    (200, 200, 200)
+                )
+                return f"\033[38;2;{r};{g};{b}m{word}\033[0m"
+            else:
+                return word
+
+        def render(self, entry):
+            if platform.system().lower() == "windows":
+                return super().render(entry)
+
+            return "|".join(
+                self.colour_field(f, w)
+                for f, w in zip(entry.origin.frame, entry.tokens)
+            )
+
+Once you have configured a logger, you can clone it as the basis of another.
+
+.. code-block:: python
+
+    root_logger = log_manager.get_logger("root")
+    logger = root_logger.clone("main")
+    logger.set_route(logger.Level.INFO, Alarmist(), sys.stderr)
+
+    logger.info("Hello, World!")
+    logger.warning("Stay safe out there!")
+
+This example is included in the module. Run ``python -m turberfield.utils.logger`` to see
+that these log entries are now in colour on the terminal.
+
+By default, logging goes to sys.stderr.
+You can log to a file by passing the necessary path.
+
+.. code-block:: python
+
+    logger.set_route(logger.Level.DEBUG, LogAdapter(), pathlib.Path("logger.log"))
+    logger.error("I didn't mean that.")
+    logger.debug("It doesn't hurt to check.")
+    logger.note("Whistle a happy tune!")
+    logger.critical(
+        "We've run out of disk space!",
+        status=http.HTTPStatus.INSUFFICIENT_STORAGE
+    )
+
+::
+
+    $ cat logger.log
+    2022-05-25 19:11:55.260938|   ERROR|main| I didn't mean that.|
+    2022-05-25 19:11:55.261088|   DEBUG|main| It doesn't hurt to check.|
+    2022-05-25 19:11:55.261178|    NOTE|main| Whistle a happy tune!|
+    2022-05-25 19:11:55.261241|CRITICAL|main| We've run out of disk space!|INSUFFICIENT_STORAGE
+
+"""
+
+
+import asyncio
+from collections import defaultdict
+from collections import namedtuple
+import datetime
+import enum
+import functools
+import io
+import logging
+import pathlib
+import sys
+
+
+class Logger:
+
+    Entry = namedtuple("Entry", ("origin", "level", "tokens", "metadata"))
+    Level = enum.Enum(
+        "Level",
+        [(label, getattr(logging, label, 15)) for label in [
+            "NOTSET", "DEBUG", "NOTE", "INFO", "WARNING", "ERROR", "CRITICAL"
+        ]]
+    )
+
+    def __init__(self, name, manager, frame=None):
+        self.name = name
+        self.manager = manager
+        self.frame = frame or [
+            "{now}", "{level.name:>8}", "{logger.name}", " {0}"
+        ]
+
+    def set_route(self, level, adapter, endpoint, replace=True, registry=None):
+        return self.manager.set_route(self, level, adapter, endpoint, replace, registry)
+
+    def clone(self, name, **kwargs):
+        return self.manager.clone(self, name, **kwargs)
+
+    @property
+    def metadata(self):
+        return {
+            "logger": self,
+            "now": datetime.datetime.utcnow()
+        }
+
+    def format(self, *args, **kwargs):
+        for field in self.frame:
+            try:
+                yield field.format(*args, **kwargs)
+            except Exception:
+                yield ""
+
+    def entry(self, level, *args, **kwargs):
+        metadata = dict(self.metadata, level=level, **kwargs)
+        tokens = tuple(self.format(*args, **metadata))
+        return self.Entry(self, level, tokens, metadata)
+
+    def log(self, level, *args, **kwargs):
+        entry = self.entry(level, *args, **kwargs)
+        try:
+            self.manager.queue.put_nowait(entry)
+        except asyncio.QueueFull:
+            pass
+        finally:
+            self.manager.notify(self)
+
+    debug = functools.partialmethod(log, Level.DEBUG)
+    note = functools.partialmethod(log, Level.NOTE)
+    info = functools.partialmethod(log, Level.INFO)
+    warning = functools.partialmethod(log, Level.WARNING)
+    error = functools.partialmethod(log, Level.ERROR)
+    critical = functools.partialmethod(log, Level.CRITICAL)
+
+
+class LogManager:
+
+    endings = {}
+    loggers = {}
+    routing = defaultdict(set)
+
+    Pair = namedtuple("Pair", ("logger_name", "endpoint_name"))
+    Route = namedtuple("Route", ("logger", "level", "adapter", "endpoint"))
+
+    def __init__(
+        self, *args,
+        defaults: list=None, queue=None, loop=None, executor=None, timeout=None, **kwargs
+    ):
+        self.defaults = defaults or [self.Route(None, Logger.Level.INFO, LogAdapter(), sys.stderr)]
+        self.queue = queue or asyncio.Queue()
+        self.loop = loop
+        self.timeout = timeout
+        self.executor = executor
+
+        self.endpoints = [self.register_endpoint(i) for i in args]
+
+        self.register_endpoint(sys.stderr)
+        for route in self.defaults:
+            self.register_endpoint(route.endpoint)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        to_close = filter(None, (self.endings.get(i, None) for i in self.endpoints))
+        for i in to_close:
+            if i is not sys.stderr:
+                try:
+                    self.wait_for(i.close)
+                except Exception as e:
+                    sys.stderr.write("Error on exit: ")
+                    sys.stderr.write(repr(e))
+                    sys.stderr.write("\n")
+        return False
+
+    @property
+    def pairings(self):
+        return [(k, v) for k, v in self.routing.items() if isinstance(k, self.Pair)]
+
+    def register_endpoint(self, obj, registry=None):
+        registry = registry if registry is not None else self.endings
+        if isinstance(obj, io.TextIOBase):
+            try:
+                registry[obj.name] = obj
+            except AttributeError:
+                obj.name = id(obj)
+                registry[obj.name] = obj
+            finally:
+                return obj
+
+        elif isinstance(obj, str):
+            obj = pathlib.Path(obj)
+
+        if isinstance(obj, pathlib.Path):
+            f = obj.open(mode="at", buffering=1)
+            registry[obj] = f
+            return obj
+
+    def wait_for(self, func, *args, **kwargs):
+        if self.loop:
+            return self.loop.run_until_complete(
+                asyncio.wait_for(
+                    self.loop.run_in_executor(self.executor, func, *args),
+                    self.timeout
+                )
+            )
+        else:
+            return func(*args, **kwargs)
+
+    def get_logger(self, name, factory=Logger, frame=None, routing=None, registry=None, **kwargs):
+        routing = routing or self.defaults
+        registry = registry if registry is not None else self.loggers
+
+        if name in registry:
+            return registry[name]
+
+        logger = factory(name, self, frame=frame, **kwargs)
+        registry[name] = logger
+
+        if not any(pair.logger_name == name for pair, _ in self.pairings):
+            for route in routing:
+                try:
+                    self.set_route(logger, route.level, route.adapter, route.endpoint)
+                except Exception:
+                    sys.stderr.write(f"Failed to set {route} to {logger}\n")
+
+        return logger
+
+    def clone(self, logger, name, **kwargs):
+        routes = [
+            r for k, v in self.pairings
+            for r in v
+            if k.logger_name == logger.name
+        ]
+        return self.get_logger(name, factory=type(logger), frame=logger.frame, routing=routes)
+
+    def set_route(self, logger, level, adapter, endpoint, replace=True, registry=None):
+        registry = registry if registry is not None else self.routing
+        endpoint = self.register_endpoint(endpoint)
+        try:
+            pair = self.Pair(logger.name, endpoint.resolve())
+        except AttributeError:
+            pair = self.Pair(logger.name, endpoint.name)
+        route = self.Route(logger, level, adapter, endpoint)
+
+        if replace:
+            registry[pair].clear()
+
+        registry[pair].add(route)
+        return route
+
+    def notify(self, client):
+        rv = self.queue.qsize()
+        while self.queue.qsize():
+            entry = self.queue.get_nowait()
+            try:
+                routes = (
+                    r for k, v in self.pairings
+                    for r in v
+                    if k.logger_name == entry.origin.name
+                )
+                for route in routes:
+                    try:
+                        route.adapter.emit(entry, route)
+                    except Exception as e:
+                        sys.stderr.write("Error in emit: ")
+                        sys.stderr.write(repr(e))
+                        sys.stderr.write("\n")
+            finally:
+                self.queue.task_done()
+
+        return rv
+
+
+class LogAdapter:
+
+    def render(self, entry):
+        return "|".join(entry.tokens)
+
+    def emit(self, entry, route):
+        try:
+            allow = entry.level.value >= route.level.value
+        except AttributeError:
+            allow = False
+
+        if allow:
+            endpoint = entry.origin.manager.endings.get(route.endpoint, route.endpoint)
+            text = self.render(entry)
+            endpoint.write(text)
+            endpoint.write("\n")
+
+
+class LogLocation(LogManager):
+
+    def __init__(self, path, **kwargs):
+        super().__init__(**kwargs)
+        self.path = pathlib.Path(path)
+
+    @property
+    def stat(self):
+        return self.wait_for(self.path.stat)
+
+
+if __name__ == "__main__":
+    import http
+    import platform
+    import re
+
+    logger = LogManager().get_logger("root")
+    logger.info("Hello, World!")
+
+    logger.frame += ["{status.name}"]
+    logger.info("Situation report", status=http.HTTPStatus.OK)
+
+    class Alarmist(LogAdapter):
+
+        patterns = [
+            (re.compile("NOTE"), (234, 0, 255)),
+            (re.compile("INFO"), (0, 255, 255)),
+            (re.compile("ERROR"), (234, 255, 0)),
+            (re.compile("WARNING"), (255, 106, 0)),
+            (re.compile("CRITICAL"), (255, 0, 106)),
+        ]
+
+        def colour_field(self, field, word):
+            if "level" in field:
+                r, g, b = next(
+                    (c for r, c in self.patterns if r.search(word)),
+                    (200, 200, 200)
+                )
+                return f"\033[38;2;{r};{g};{b}m{word}\033[0m"
+            else:
+                return word
+
+        def render(self, entry):
+            if platform.system().lower() == "windows":
+                return super().render(entry)
+
+            return "|".join(
+                self.colour_field(f, w)
+                for f, w in zip(entry.origin.frame, entry.tokens)
+            )
+
+
+    with LogManager() as log_manager:
+
+        # Once you have configured a logger, you can clone it as the basis of another
+        logger = log_manager.clone(log_manager.get_logger("root"), "main")
+        log_manager.set_route(logger, logger.Level.INFO, Alarmist(), sys.stderr)
+
+        logger.info("Hello, World!")
+        logger.warning("Stay safe out there!")
+
+        log_manager.set_route(logger, logger.Level.DEBUG, LogAdapter(), pathlib.Path("logger.log"))
+        logger.error("I didn't mean that.")
+        logger.debug("It doesn't hurt to check.")
+        logger.note("Whistle a happy tune!")
+        logger.critical(
+            "We've run out of disk space!",
+            status=http.HTTPStatus.INSUFFICIENT_STORAGE
+        )
---
+++ packages/turberfield-utils/turberfield-utils-0.47.0.tar.gz/turberfield-utils-0.47.0/turberfield/utils/test/test_logger.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+import asyncio
+import logging
+import io
+import os
+import pathlib
+import sys
+from tempfile import TemporaryDirectory
+import unittest
+import uuid
+
+from turberfield.utils.logger import Logger
+from turberfield.utils.logger import LogAdapter
+from turberfield.utils.logger import LogLocation
+from turberfield.utils.logger import LogManager
+
+
+class LoggerTests(unittest.TestCase):
+
+    def test_format_attribute_error(self):
+        manager = LogManager()
+        logger = manager.get_logger("test_attribute_error")
+        logger.frame += ["{id.hex}"]
+        self.assertTrue(list(logger.format("test message", id=uuid.uuid4())))
+        self.assertTrue(list(logger.format("test message", id="no_hex_attribute")))
+
+    def test_format_index_error(self):
+        manager = LogManager()
+        logger = manager.get_logger("test_index_error")
+        logger.frame += ["{data[1]}"]
+        self.assertTrue(list(logger.format("test message", data="0123")))
+        self.assertTrue(list(logger.format("test message", data="")))
+
+    def test_format_key_error(self):
+        manager = LogManager()
+        logger = manager.get_logger("test_key_error")
+        logger.frame += ["{obj[foo]}"]
+        self.assertTrue(list(logger.format("test message", obj={"foo": "bar"})))
+        self.assertTrue(list(logger.format("test message", obj={})))
+
+    def test_format_type_error(self):
+        manager = LogManager()
+        logger = manager.get_logger("test_key_error")
+        logger.frame += ["{1[0]", "{obj[foo]}"]
+        self.assertTrue(list(logger.format("test message", None, obj=None)))
+
+
+class EndpointRegistrationTests(unittest.TestCase):
+
+    def setUp(self):
+        self.manager = LogManager()
+        self.assertIn(sys.stderr, self.manager.endings.values())
+
+    def tearDown(self):
+        self.manager.loggers.clear()
+        self.manager.routing.clear()
+        self.manager.endings.clear()
+
+    def test_register_stderr(self):
+        d = {}
+        rv = self.manager.register_endpoint(sys.stderr, registry=d)
+        self.assertEqual(d, {sys.stderr.name: sys.stderr})
+
+    def test_register_stream(self):
+        stream = io.StringIO()
+        d = {}
+        rv = self.manager.register_endpoint(stream, registry=d)
+        self.assertEqual(d, {id(stream): stream})
+
+
+class LogStreamTests(unittest.TestCase):
+
+    def setUp(self):
+        self.stream = io.StringIO()
+        self.stream.name = "test stream"
+        self.manager = LogManager(
+            defaults=[LogManager.Route(None, Logger.Level.INFO, LogAdapter(), self.stream)]
+        )
+
+    def tearDown(self):
+        self.manager.loggers.clear()
+        self.manager.routing.clear()
+        self.manager.endings.clear()
+
+    def test_log_blocked(self):
+        logger = self.manager.get_logger("unit.test.log")
+        logger.log(logger.Level.DEBUG, "Debug message")
+        self.assertFalse(self.stream.getvalue())
+
+    def test_log_printed(self):
+        logger = self.manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Info message")
+        self.assertIn("Info message", self.stream.getvalue())
+
+    def test_log_newlines(self):
+        logger = self.manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Message")
+        logger.log(logger.Level.INFO, "Message")
+        lines = self.stream.getvalue().splitlines()
+        self.assertEqual(2, len(lines))
+
+    def test_bad_default_level(self):
+        manager = LogManager(
+            defaults=[LogManager.Route(None, None, LogAdapter(), self.stream)]
+        )
+        logger = manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Info message")
+        self.assertFalse(self.stream.getvalue())
+
+    def test_bad_adapter(self):
+        manager = LogManager(
+            defaults=[LogManager.Route(None, Logger.Level.INFO, None, self.stream)]
+        )
+        logger = manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Info message")
+        self.assertFalse(self.stream.getvalue())
+
+    def test_bad_endpoint(self):
+        manager = LogManager(
+            defaults=[LogManager.Route(None, Logger.Level.INFO, LogAdapter(), None)]
+        )
+        logger = manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Info message")
+        self.assertFalse(self.stream.getvalue())
+
+
+class CloneTests(unittest.TestCase):
+
+    def setUp(self):
+        self.manager = LogManager()
+
+    def tearDown(self):
+        self.manager.loggers.clear()
+        self.manager.routing.clear()
+        self.manager.endings.clear()
+
+    def test_clone_from_log(self):
+        a = self.manager.get_logger("a")
+
+        b = a.manager.get_logger("a")
+        self.assertIs(a, b)
+
+        c = a.clone("c")
+        self.assertIsNot(c, a)
+
+    def test_frame(self):
+        a = self.manager.get_logger("a")
+        a.frame += ["extra"]
+        self.assertIn("extra", a.frame)
+
+        b = self.manager.get_logger("a")
+        self.assertIs(a, b)
+
+        c = self.manager.clone(self.manager.get_logger("a"), "c")
+        self.assertIsNot(c, a)
+        self.assertEqual(c.frame, a.frame)
+
+    def test_routes(self):
+        self.manager = LogManager()
+        n_routes = len(self.manager.routing)
+        a = self.manager.get_logger("a")
+        self.assertEqual(n_routes + 1, len(self.manager.routing))
+
+        b = self.manager.get_logger("a")
+        self.assertIs(a, b)
+        self.assertEqual(n_routes + 1, len(self.manager.routing))
+
+        self.manager.set_route(b, Logger.Level.INFO, LogAdapter(), sys.stderr)
+        self.assertEqual(n_routes + 1, len(self.manager.routing))
+
+        c = self.manager.clone(self.manager.get_logger("a"), "c")
+        self.assertIsNot(c, a)
+        self.assertEqual(n_routes + 2, len(self.manager.routing))
+
+        c.set_route(Logger.Level.INFO, LogAdapter(), sys.stderr)
+        self.assertEqual(n_routes + 2, len(self.manager.routing))
+
+
+
+class LocationTests:
+
+    def setUp(self):
+        self.locn = TemporaryDirectory()
+        self.locn.__enter__()
+
+    def tearDown(self):
+        self.locn.__exit__(None, None, None)
+
+
+class LogPathTests(LocationTests, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        uid = uuid.uuid4()
+        self.path = pathlib.Path(self.locn.name, f"{uid.hex}.log")
+        self.manager = LogManager(
+            self.path,
+            defaults=[LogManager.Route(None, Logger.Level.INFO, LogAdapter(), self.path)]
+        )
+        self.manager.__enter__()
+
+    def tearDown(self):
+        self.manager.__exit__(None, None, None)
+        super().tearDown()
+
+    def test_register_path(self):
+        d = {}
+        rv = self.manager.register_endpoint(self.path, registry=d)
+        self.assertIs(rv, self.path)
+        self.assertIn(self.path, d)
+        self.assertIsInstance(d[self.path], io.TextIOBase)
+
+    def test_log_written(self):
+        logger = self.manager.get_logger("unit.test.log")
+        logger.log(logger.Level.INFO, "Info message")
+        self.assertIn(self.path, self.manager.endings)
+        self.assertTrue(self.path.exists())
+        self.assertIn("Info message", self.path.read_text())
+        self.assertTrue(any(
+            self.path.resolve() == k.endpoint_name for k, v in self.manager.pairings
+        ), self.manager.pairings)
+
+
+class LocationSyncTests(LocationTests, unittest.TestCase):
+
+    def test_empty_location_string(self):
+        l = LogLocation(self.locn.name)
+        self.assertIsInstance(l.path, pathlib.Path)
+
+    def test_empty_location_path(self):
+        l = LogLocation(pathlib.Path(self.locn.name))
+        self.assertIsInstance(l.path, pathlib.Path)
+
+    def test_stat_empty_location(self):
+        l = LogLocation(self.locn.name)
+        self.assertIsInstance(l.path, pathlib.Path)
+        self.assertIsInstance(l.stat, os.stat_result)
+
+
+@unittest.skip("noisy resource errors on __exit__")
+class LocationAsyncTests(LocationTests, unittest.TestCase):
+
+    def test_stat_asyncio(self):
+        loop = asyncio.new_event_loop()
+        l = LogLocation(self.locn.name, loop=loop)
+        with l:
+            self.assertIs(l.loop, loop)
+            self.assertIsInstance(l.stat, os.stat_result)
