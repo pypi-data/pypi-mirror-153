bb2cf0e8c2a17eedf1d18692bd9ea44302682039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:30 +0000

    Add package poetry-template/0.1.4/poetry_template-0.1.4-py3-none-any.whl

---
+++ packages/poetry-template/poetry_template-0.1.4-py3-none-any.whl/poetry_template/package_two/module_two.py
@@ -0,0 +1,23 @@
+"""
+    Library module.
+"""
+
+
+def add_two_numbers(x: int, y: int) -> int:
+    """
+    Add two numbers.
+
+    :param x: first whole number
+    :param y: second whole number
+    """
+    return x + y
+
+
+def multiply_two_numbers(x: int, y: int) -> int:
+    """
+    Multiply two numbers.
+
+    :param x: first whole number
+    :param y: second whole number
+    """
+    return x * y
