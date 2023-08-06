3ced260d04570724e67bcabf3a0b7ad545b288b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:49 +0000

    Add package mdut/22.6.0/mdut-22.6.0-py3-none-any.whl

---
+++ packages/mdut/mdut-22.6.0-py3-none-any.whl/CHANGELOG.md
@@ -0,0 +1,83 @@
+# mdut Changelog
+
+
+<!--
+headers:
+Added      - new features
+Changed    - changes in existing functionality
+Deprecated - soon-to-be removed features
+Removed    - now removed features
+Fixed      - any bug fixes
+Security   - in case of vulnerabilities
+-->
+
+
+## [Unreleased]
+
+### Fixed
+
+- Whitespace surrounding title text
+
+
+## [22.3.1]
+
+### Changed
+
+- CalVer from `YYYY.minor.micro` to `YY.minor.micro`
+
+
+## [2022.3.1] - ~~[yanked]~~ deleted
+
+### Fixed
+
+- Quotes in titles are now escaped
+
+
+## [2022.3.0] - ~~[yanked]~~ deleted
+
+### Added
+
+- Missing reference style tests
+- Slack style
+
+### Changed
+
+- Reordered styles to put default first
+
+
+## [2022.2.0] - ~~[yanked]~~ deleted
+
+### Added
+
+- Fallback from no protocol and http to https
+
+
+## [2022.1.0] - ~~[yanked]~~ deleted
+
+### Added
+
+- CI workflow for automated code quality checks
+- Support for Python 3.7, 3.8, and 3.9
+
+### Fixed
+
+- Minor linter errors
+
+
+## [2022.0.1] - ~~[yanked]~~ deleted
+
+### Added
+
+- Initial release! 🎉
+
+
+[yanked]: https://nkantar.com/blog/2022/01/reversioning-mdut/ "Reversioning mdut | Blog | Nik Kantar"
+
+
+[Unreleased]: https://github.com/nkantar/mdut/compare/22.3.1...HEAD
+[22.3.1]: https://github.com/nkantar/mdut/compare/2022.3.1...22.3.1
+[2022.3.1]: https://github.com/nkantar/mdut/compare/2022.3.0...2022.3.1
+[2022.3.0]: https://github.com/nkantar/mdut/compare/2022.2.0...2022.3.0
+[2022.2.0]: https://github.com/nkantar/mdut/compare/2022.1.0...2022.2.0
+[2022.1.0]: https://github.com/nkantar/mdut/compare/2022.0.1...2022.1.0
+[2022.0.1]: https://github.com/nkantar/mdut/releases/tag/2022.0.1
---
+++ packages/mdut/mdut-22.6.0-py3-none-any.whl/mdut/mdut.py
@@ -0,0 +1,100 @@
+from string import Template
+from typing import Optional
+
+from bs4 import BeautifulSoup
+import click
+import httpx
+import pyperclip  # type: ignore
+
+
+TODO = "TODO"
+
+TAG_STYLES = {
+    "reference": Template('[TODO]: $url "$title"'),
+    "inline": Template('[TODO]($url "$title")'),
+    "slack": Template("[TODO]($url)"),
+}
+
+DEFAULT_STYLE = "reference"
+
+
+def fetch_html(url: str) -> str:
+    try:
+        text = httpx.get(url).text
+
+        # more and more sites are https-only
+        if not text and url.startswith("http://"):
+            text = fetch_html(url.replace("http", "https"))
+
+        return text
+
+    # allow being lazy, e.g., just "example.com"
+    except httpx.UnsupportedProtocol:
+        return fetch_html(f"https://{url}")
+
+
+def sanitize_title(title: str) -> str:
+    # strip whitespace
+    clean_title = title.strip()
+
+    # escape quotation marks
+    clean_title = clean_title.replace('"', '\\"')
+
+    return clean_title
+
+
+def extract_title(html: str) -> str:
+    try:
+        soup = BeautifulSoup(html, "html.parser")
+        title = getattr(getattr(getattr(soup, "head"), "title"), "text") or TODO
+    except (AttributeError, TypeError):
+        title = TODO
+    return sanitize_title(title)
+
+
+def generate_tag(url: str, title: Optional[str], style: str = DEFAULT_STYLE) -> str:
+    if title is None:
+        title = TODO
+    return TAG_STYLES[style].substitute(url=url, title=title)
+
+
+def markdown_url_tag(url: str, style: str) -> str:
+    html = fetch_html(url)
+    title = extract_title(html)
+    return generate_tag(url, title, style.lower())
+
+
+@click.command()
+@click.option(
+    "-s",
+    "--style",
+    type=click.Choice(list(TAG_STYLES.keys()), case_sensitive=False),
+    default=DEFAULT_STYLE,
+    help=f'Tag style, default "{DEFAULT_STYLE}".',
+)
+@click.argument("url")
+def mdut(url: str, style: str = DEFAULT_STYLE) -> str:
+    """Markdown URL tag generator."""
+    tag = markdown_url_tag(url, style)
+    pyperclip.copy(tag)
+    click.echo(f"Copied to clipboard:\n{tag}")
+    return tag
+
+
+if __name__ == "__main__":
+    mdut()
+
+
+def reference(url: str) -> str:
+    return markdown_url_tag(url, "reference")
+
+
+def inline(url: str) -> str:
+    return markdown_url_tag(url, "inline")
+
+
+def slack(url: str) -> str:
+    return markdown_url_tag(url, "slack")
+
+
+__all__ = ["reference", "inline", "slack"]
