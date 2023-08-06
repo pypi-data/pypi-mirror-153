44e00474ce8d173d0c9c2762bbc33e84a6c7f02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:42 +0000

    Add package spirvsmith-server-client/0.3.0/spirvsmith-server-client-0.3.0.tar.gz

---
+++ packages/spirvsmith-server-client/spirvsmith-server-client-0.3.0.tar.gz/spirvsmith-server-client-0.3.0/spirvsmith_server_client/models/__init__.py
@@ -0,0 +1,17 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .buffer_submission import BufferSubmission
+from .execution_platform import ExecutionPlatform
+from .execution_platform_available_hardware import ExecutionPlatformAvailableHardware
+from .execution_queue import ExecutionQueue
+from .execution_queues import ExecutionQueues
+from .execution_queues_queues import ExecutionQueuesQueues
+from .hardware_information import HardwareInformation
+from .hardware_type import HardwareType
+from .hardware_vendor import HardwareVendor
+from .http_validation_error import HTTPValidationError
+from .operating_system import OperatingSystem
+from .shader_data import ShaderData
+from .shader_submission import ShaderSubmission
+from .validation_error import ValidationError
+from .vulkan_backend import VulkanBackend
