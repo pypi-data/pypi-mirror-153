e455173dc5a319e5f322185d7313dfeec23383ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:04 +0000

    Add package dict-and-html/1.0.11/dict_and_html-1.0.11.tar.gz

---
+++ packages/dict-and-html/dict_and_html-1.0.11.tar.gz/dict_and_html-1.0.11/src/dict_and_html/modules/strategies/strategy_values.py
@@ -0,0 +1,26 @@
+###########################
+# strategy_values.py
+###########################
+def attribute_strategy_for_value(value,
+                                 value_strategy: dict = None) -> dict:
+    attributes = {}
+
+    if value_strategy is None:
+        # print("default value strategy", len([value]))
+        len([value])  # just to avoid triggering warnings
+    else:
+        ##
+        # Example of strategy
+        ##
+        # if type(value) == float:
+        #     attributes.update({
+        #         'style': 'font-style: italic;'
+        #     })
+        # else:
+        #     print("Not float: %s" % value)
+        pass
+
+    return attributes
+###########################
+# strategy_value.py
+###########################
