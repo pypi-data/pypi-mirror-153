3e79824bfeca7b79251df4739c76945a37881e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package translators/5.2.2/translators-5.2.2-py3-none-any.whl

---
+++ packages/translators/translators-5.2.2-py3-none-any.whl/translators/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "5.2.2"
+__author__ = "UlionTse"
+
+from translators.apis import (alibaba, argos, baidu, bing, caiyun, deepl, google, iciba, itranslate,
+                              papago, reverso, sogou, tencent, translateCom, utibet, yandex, youdao)
+from translators.apis import (_alibaba, _argos, _baidu, _bing, _caiyun, _deepl, _google, _iciba, _itranslate,
+                              _papago, _reverso, _sogou, _tencent, _translateCom, _utibet, _yandex, _youdao)
+from translators.apis import translate_html
