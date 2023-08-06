e8becadaed36a656e75f1a927de81db8cf9714ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:37 +0000

    Add package JoyCat/0.0.5/JoyCat-0.0.5.tar.gz

---
+++ packages/JoyCat/JoyCat-0.0.5.tar.gz/JoyCat-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JoyCat",
+    version="0.0.5",
+    author="yuta ishii",
+    author_email="s2022052@stu.musashino-u.ac.jp",
+    description = 'This package visualizes the happiness of cats based on Japanese traditional lore.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuta08/JoyCat",
+    project_urls={
+        "Bug Tracker": "https://github.com/yuta08/JoyCat",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['JoyCat'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'JoyCat = JoyCat:main'
+        ]
+    },
+)
