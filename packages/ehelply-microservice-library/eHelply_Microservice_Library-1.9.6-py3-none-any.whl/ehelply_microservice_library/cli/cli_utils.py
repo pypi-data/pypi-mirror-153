ffef8d8b6068eba304dca75f5d4dd4ad7719a3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:47 +0000

    Add package ehelply-microservice-library/1.9.6/eHelply_Microservice_Library-1.9.6-py3-none-any.whl

---
+++ packages/ehelply-microservice-library/eHelply_Microservice_Library-1.9.6-py3-none-any.whl/ehelply_microservice_library/cli/cli_utils.py
@@ -0,0 +1,17 @@
+import typer
+import base64
+from typing import List
+
+cli = typer.Typer()
+
+
+RESERVED_WORDS: List[str] = ["playground", "test", "testing", "privileged"]
+
+@cli.command()
+def encode_json_header(
+        json: str = typer.Option(..., prompt=True, help="JSON string to encrypt"),
+):
+    json_bytes = json.encode('utf-8')
+    base64_bytes = base64.b64encode(json_bytes)
+    base64_str = base64_bytes.decode('utf-8')
+    typer.echo(base64_str)
