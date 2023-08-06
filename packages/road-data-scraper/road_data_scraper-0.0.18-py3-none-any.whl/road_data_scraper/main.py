87404e9c318c92097274eaffdf3d6b3608bd8f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:39 +0000

    Add package road-data-scraper/0.0.18/road_data_scraper-0.0.18-py3-none-any.whl

---
+++ packages/road-data-scraper/road_data_scraper-0.0.18-py3-none-any.whl/road_data_scraper/main.py
@@ -0,0 +1,135 @@
+import ast
+import calendar
+import configparser
+import logging
+import shutil
+import time
+from datetime import datetime
+from functools import partial
+from pathlib import Path
+
+from dateutil.relativedelta import relativedelta
+
+from road_data_scraper import TMP_LOG_PATH
+from road_data_scraper.report.report import run_reports
+from road_data_scraper.steps.download import THREAD_POOL, download
+from road_data_scraper.steps.file_handler import (
+    dump_config,
+    file_handler,
+    gcp_upload_from_directory,
+)
+from road_data_scraper.steps.metadata import get_sensor_urls, get_sites_by_sensor
+
+LOGGER = logging.getLogger(__name__)
+
+
+def run(config: dict, api_run: bool):
+    """
+    Runs the Pipeline to Scrape Road Traffic Sensor Data
+    from Highways England WebTRIS API.
+
+    Args:
+        config (dict): Configuration file for this run.
+        api_run (bool): True if using FastAPI for this run.
+    """
+    start_time = time.time()
+
+    if api_run:
+
+        def my_ast(*args):
+            return args[0]
+
+    else:
+
+        def my_ast(*args):
+            return ast.literal_eval(*args)
+
+    start_date = my_ast(config["user_settings"]["start_date"])
+    end_date = my_ast(config["user_settings"]["end_date"])
+
+    if not start_date and not end_date:
+
+        # 2-month API data lag (date today minus two months)
+        date_object_today = datetime.strptime(time.strftime("%Y-%m"), "%Y-%m")
+        minus_two_months = date_object_today - relativedelta(months=2)
+
+        year, month = map(int, minus_two_months.strftime("%Y %m").split())
+        last_day_of_month = calendar.monthrange(year, month)[1]
+
+        start_date = f"{year}-{month}-01"
+        end_date = f"{year}-{month}-{last_day_of_month}"
+
+    data_path, metadata_path, report_path, run_id_path = file_handler(
+        config, api_run, start_date, end_date
+    )
+
+    LOGGER.info(f"Using {THREAD_POOL} threads")
+
+    test_run = my_ast(config["user_settings"]["test_run"])
+    generate_report = my_ast(config["user_settings"]["generate_report"])
+
+    LOGGER.info("Getting Road Sensor Lookup Table")
+
+    sensor_tables, lookup_df = get_sites_by_sensor()
+    lookup_df.to_csv(f"{str(metadata_path)}/road_data_sensor_lookup.csv", index=False)
+
+    midas_metadata, tmu_metadata, tame_metadata = get_sensor_urls(
+        sensor_tables, start_date, end_date
+    )
+
+    LOGGER.info("Processed Road Sensor Lookup Table")
+
+    if test_run:
+        LOGGER.info("Test Run")
+        midas_metadata = midas_metadata[1:2]
+        tmu_metadata = tmu_metadata[1:2]
+        tame_metadata = tame_metadata[1:2]
+
+    download_partial = partial(
+        download,
+        start_date=start_date,
+        end_date=end_date,
+        test_run=test_run,
+        run_id_path=data_path,
+    )
+
+    download_partial(site_name="midas", metadata=midas_metadata)
+    download_partial(site_name="tmu", metadata=tmu_metadata)
+    download_partial(site_name="tame", metadata=tame_metadata)
+
+    if generate_report:
+        run_reports(lookup_df, report_path, start_date, end_date)
+
+    if api_run:
+        dump_config(config, metadata_path, api_run=True)
+    else:
+        dump_config(config, metadata_path, api_run=False)
+
+    LOGGER.info(f"Script Run Time: {round((time.time() - start_time)/60, 2)} minutes")
+
+    log_file_path = f"{metadata_path}/road_data_pipeline.log"
+    shutil.copyfile(TMP_LOG_PATH, log_file_path)
+
+    gcp_storage = my_ast(config["user_settings"]["gcp_storage"])
+    if gcp_storage:
+        gcp_upload_from_directory(
+            run_id_path,
+            destination_bucket_name=my_ast(config["user_settings"]["gcp_bucket_name"]),
+            destination_blob_name=my_ast(config["user_settings"]["gcp_blob_name"]),
+            gcp_credentials=my_ast(config["user_settings"]["gcp_credentials"]),
+        )
+
+    rm_dir = my_ast(config["user_settings"]["rm_dir"])
+    if rm_dir:
+        LOGGER.info(
+            f"Removing {run_id_path[run_id_path.find('output_data/'):].split('/')[1]} folder"
+        )
+        shutil.rmtree(Path(run_id_path))
+
+
+if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read("./config.ini")
+
+    run(config, api_run=False)
---
+++ packages/road-data-scraper/road_data_scraper-0.0.18-py3-none-any.whl/road_data_scraper/steps/download.py
@@ -0,0 +1,250 @@
+import csv
+import logging
+import multiprocessing
+import time
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
+from pathlib import Path
+
+import pandas as pd
+import requests
+
+THREAD_POOL = multiprocessing.cpu_count()
+
+session = requests.Session()
+session.mount(
+    "https://",
+    requests.adapters.HTTPAdapter(
+        pool_maxsize=THREAD_POOL, max_retries=3, pool_block=True
+    ),
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _response_to_df(
+    response,
+    site_id,
+    longitude,
+    latitude,
+    status,
+    site_type,
+    direction,
+    easting,
+    northing,
+):
+    """
+    Converts JSON stored `requests` response object to a Pandas DataFrame.
+    Then adds additional metadata/columns to the DataFrame:
+        - site_id
+        - longitude
+        - latitude
+        - site_type
+        - direction
+        - easting
+        - northing
+
+    Args:
+        response (_type_): `requests` response object.
+        site_id (str): Unique ID of Road Traffic Sensor.
+        longitude (str): Longitude of Road Traffic Sensor for a given Site ID.
+        latitude (str): Latitude of Road Traffic Sensor for a given Site ID.
+        status (str): Status of Road Traffic Sensor for a given Site ID.
+        site_type (str): Road Traffic Sensor Type: midas, tmu, tame.
+        direction (str): Direction of Road Traffic Sensor for a given Site ID.
+        easting (str): Easting of Road Traffic Sensor for a given Site ID.
+        northing (str): Northing of Road Traffic Sensor for a given Site ID.
+
+    Returns:
+        df (pd.DataFrame): Pandas DataFrame.
+    """
+
+    df = pd.DataFrame.from_dict(response.json()["Rows"])
+
+    df.insert(0, "site_id", site_id)
+
+    df = df.assign(
+        longitude=longitude,
+        latitude=latitude,
+        sites_status=status,
+        type=site_type,
+        direction=direction,
+        easting=easting,
+        northing=northing,
+    )
+
+    return df
+
+
+def get_url(
+    site_name: str,
+    start_date: str,
+    end_date: str,
+    test_run: bool,
+    full_csv_name: str,
+    url: str,
+    site_id: str,
+    site_type: str,
+    direction: str,
+    longitude: str,
+    latitude: str,
+    status: str,
+    easting: str,
+    northing: str,
+):
+    """
+    Scrapes a URL from WebTRIS Highways /reports/daily/ endpoint as a Pandas DataFrame,
+    and appends the DataFrame to a CSV.
+
+    Args:
+        site_name (str): Road Traffic Sensor Name.
+        start_date (str): Start Date; format: %Y-%m-%d.
+        end_date (str): End Date; format: %Y-%m-%d.
+        test_run (bool): If True, will only download a small subset data from WebTRIS Highways England API.
+        full_csv_name (str): Output CSV File Path.
+        url (str): URL to Scrape -- Road Traffic Sensor for a given Site ID.
+        site_id (str): Unique ID of Road Traffic Sensor.
+        site_type (str): Road Traffic Sensor Type: midas, tmu, tame.
+        direction (str): Direction of Road Traffic Sensor for a given Site ID.
+        longitude (str): Longitude of Road Traffic Sensor for a given Site ID.
+        latitude (str): Latitude of Road Traffic Sensor for a given Site ID.
+        status (str): Status of Road Traffic Sensor for a given Site ID.
+        easting (str): Easting of Road Traffic Sensor for a given Site ID.
+        northing (str): Northing of Road Traffic Sensor for a given Site ID.
+    """
+
+    message = "Parallel request of data for use in ONS. Emerging Platforms Team. @GitHub: dombean/road_data_scraper"
+    headers = {"Message": f"{message}"}
+
+    response = session.get(url, headers=headers)
+
+    LOGGER.info(
+        f"Request was completed in {response.elapsed.total_seconds()} seconds [{site_name}] [{response.url}]"
+    )
+
+    df = _response_to_df(
+        response=response,
+        site_id=site_id,
+        longitude=longitude,
+        latitude=latitude,
+        status=status,
+        site_type=site_type,
+        direction=direction,
+        easting=easting,
+        northing=northing,
+    )
+    df.to_csv(f"{full_csv_name}", mode="a", header=False, index=False)
+
+    if response.status_code != 200:
+        logging.error(
+            "request failed, error code %s [%s]", response.status_code, response.url
+        )
+
+    if 500 <= response.status_code < 600:
+        # server is overloaded? give it a break
+        time.sleep(5)
+
+    return response
+
+
+def download(
+    site_name: str,
+    start_date: str,
+    end_date: str,
+    metadata: pd.DataFrame,
+    test_run: bool,
+    run_id_path: Path,
+):
+    """
+    Scrapes data from Highways England WebTRIS API
+    in Parallel.
+
+    Args:
+        site_name (str): Name of Road Traffic Sensor.
+        start_date (str): Start Date; format: %Y-%m-%d.
+        end_date (str): End Date; format: %Y-%m-%d.
+        metadata (List[tup]): List of Tuples. Each Tuple contains a URL and associated
+                              Metadata for a Road Traffic Sensor.
+        test_run (bool): If True, will only download a small subset data from WebTRIS Highways England API.
+        run_id_path (Path): Path object to run_id directory.
+    """
+
+    if site_name not in ("midas", "tame", "tmu"):
+        raise ValueError("Available sites are: midas, tame, tmu.")
+
+    headers = [
+        "site_id",
+        "site_name",
+        "report_date",
+        "time_period_end",
+        "interval",
+        "len_0_520_cm",
+        "len_521_660_cm",
+        "len_661_1160_cm",
+        "len_1160_plus_cm",
+        "speed_0_10_mph",
+        "speed_11_15_mph",
+        "speed_16_20_mph",
+        "speed_21_25_mph",
+        "speed_26_30_mph",
+        "speed_31_35_mph",
+        "speed_36_40_mph",
+        "speed_41_45_mph",
+        "speed_46_50_mph",
+        "speed_51_55_mph",
+        "speed_56_60_mph",
+        "speed_61_70_mph",
+        "speed_71_80_mph",
+        "speed_80_plus_mph",
+        "speed_avg_mph",
+        "total_vol",
+        "longitude",
+        "latitude",
+        "sites_status",
+        "type",
+        "direction",
+        "easting",
+        "northing",
+    ]
+
+    if test_run:
+        full_csv_name = (
+            f"{str(run_id_path)}/{site_name}_{start_date}-{end_date}_TEST_RUN.csv"
+        )
+    else:
+        full_csv_name = f"{str(run_id_path)}/{site_name}_{start_date}-{end_date}.csv"
+
+    with open(full_csv_name, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+
+    (
+        url,
+        site_id,
+        site_type,
+        direction,
+        longitude,
+        latitude,
+        status,
+        easting,
+        northing,
+    ) = zip(*metadata)
+
+    with ThreadPoolExecutor(max_workers=THREAD_POOL) as executor:
+        executor.map(
+            get_url,
+            repeat(site_name),
+            repeat(start_date),
+            repeat(end_date),
+            repeat(test_run),
+            repeat(full_csv_name),
+            url,
+            site_id,
+            site_type,
+            direction,
+            longitude,
+            latitude,
+            status,
+            easting,
+            northing,
+        )
