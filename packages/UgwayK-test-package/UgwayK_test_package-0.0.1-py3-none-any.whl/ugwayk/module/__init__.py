fde558f2386b0294ccf3cf091acf56b97da63e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:58 +0000

    Add package UgwayK-test-package/0.0.1/UgwayK_test_package-0.0.1-py3-none-any.whl

---
+++ packages/UgwayK-test-package/UgwayK_test_package-0.0.1-py3-none-any.whl/ugwayk/module/__init__.py
@@ -0,0 +1,5 @@
+# 숫자 디폴트 값을 지정한다
+
+class UgkSetDefault:
+    def __init__(self, start_num = 0):
+        self.start_num = start_num
---
+++ packages/UgwayK-test-package/UgwayK_test_package-0.0.1-py3-none-any.whl/ugwayk/module/calculator/__init__.py
@@ -0,0 +1,4 @@
+# 혹시 기존에 해당 이름을 변수로 쓰고 있었다면 초기화 해주는 작업
+plus = None
+
+from .ugk_calc import UgkCalc
