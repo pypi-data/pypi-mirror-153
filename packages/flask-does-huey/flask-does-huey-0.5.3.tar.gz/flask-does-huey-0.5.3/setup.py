cb199decca57c28c548deec2137735a122c1f3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:18 +0000

    Add package flask-does-huey/0.5.3/flask-does-huey-0.5.3.tar.gz

---
+++ packages/flask-does-huey/flask-does-huey-0.5.3.tar.gz/flask-does-huey-0.5.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flask_does_huey']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['huey>=2.4.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'flask-does-huey',
+    'version': '0.5.3',
+    'description': 'Flask extension to use huey task queues',
+    'long_description': None,
+    'author': 'jthop',
+    'author_email': 'jh@mode14.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
