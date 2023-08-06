42a426a66ed4418c1f4a5c06ec52f44eb0ffbc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:07 +0000

    Add package mondeja-bump/0.1.0/mondeja-bump-0.1.0.tar.gz

---
+++ packages/mondeja-bump/mondeja-bump-0.1.0.tar.gz/mondeja-bump-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['mondeja_bump']
+install_requires = \
+['tomlkit>=0.11.0,<0.12.0']
+
+entry_points = \
+{'console_scripts': ['bump = mondeja_bump:main']}
+
+setup_kwargs = {
+    'name': 'mondeja-bump',
+    'version': '0.1.0',
+    'description': 'Just bump semantic version.',
+    'long_description': "# mondeja's bump\n",
+    'author': 'Álvaro Mondéjar Rubio',
+    'author_email': 'mondejar1994@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
