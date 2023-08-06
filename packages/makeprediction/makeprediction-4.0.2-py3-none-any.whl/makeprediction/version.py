c045b8ae4d486aea496094126f1593924a756fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:44 +0000

    Add package makeprediction/4.0.2/makeprediction-4.0.2-py3-none-any.whl

---
+++ packages/makeprediction/makeprediction-4.0.2-py3-none-any.whl/makeprediction/version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author = "Hanany Tolba"
+# 01/07/2022
+
+
+
+__author__ = "Hanany Tolba"
+__copyright__ = "Copyright 2022, MAKEPREDICTION."
+__credits__ = "Hanany Tolba"
+__license__ = "GPLv3"
+__version__ = "4.0.2"
+__maintainer__ = "Hanany Tolba"
+__email__ = "hananytolba@yahoo.com"
+__status__ = "4 - Beta"
+
