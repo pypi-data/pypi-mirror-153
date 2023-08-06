0ffcb344acea4031d2051ffe640d3473a4d7d398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:31 +0000

    Add package susc/1.5.5/susc-1.5.5.tar.gz

---
+++ packages/susc/susc-1.5.5.tar.gz/susc-1.5.5/susc/linker.py
@@ -0,0 +1,210 @@
+from .things import *
+from . import log
+from .exceptions import *
+
+MAGIC_IDENTIFIERS = ["Entity"]
+
+def validate_fields(identifiers: List[str], field_sets: List[List[SusField]]) -> None:
+    log.verbose("Validating fields", "linker")
+    diag = []
+
+    for fields in field_sets:
+        for f1 in fields:
+            # optional values mod 256
+            if f1.optional != None and f1.optional >= 256:
+                diag.append(Diagnostic([f1.location], DiagLevel.WARN, 8,
+                    f"Optional value '{f1.optional}' will be taken mod 256 ('{f1.optional % 256}')"))
+                f1.optional %= 256
+
+            # the fields within one set shouldn't have matching names
+            equal = [f2 for f2 in fields if f2.name == f1.name]
+            if len(equal) > 1:
+                diag.append(Diagnostic([f.location for f in equal], DiagLevel.ERROR, 9,
+                    f"Multiple fields with matching names '{f1.name}'"))
+            
+            # or values
+            equal = [f2 for f2 in fields if f2.optional == f1.optional and f1.optional != None]
+            if len(equal) > 1:
+                diag.append(Diagnostic([f.location for f in equal], DiagLevel.ERROR, 10,
+                    f"Multiple fields with matching opt() values '{f1.optional}'"))
+
+            # validate the type
+            type_err = f1.type_.find_errors(identifiers)
+            if type_err != None:
+                diag.append(Diagnostic([f1.type_.location], DiagLevel.ERROR, 11, type_err))
+
+    return diag
+
+def combine(things: List[SusThing]) -> Tuple[list[SusThing], list[Diagnostic]]:
+    log.verbose(f"Combining {len(things)} total definitions", "linker")
+    diag = []
+    ignore, out = [], []
+
+    for thing1 in things:
+        if thing1.name in ignore:
+            continue
+
+        # find things with same name
+        with_matching_name = [thing2 for thing2 in things if thing2.name == thing1.name]
+
+        if len(with_matching_name) > 1:
+            # diagnose redefinitions
+            fine = True
+            for thing2 in with_matching_name:
+                if type(thing1) != type(thing2) or not isinstance(thing1, (SusEnum, SusBitfield)):
+                    diag.append(Diagnostic([t.location for t in with_matching_name], DiagLevel.ERROR, 12,
+                        f"Redefinition of '{thing1.name}' (only enums and bitfields can be combined)"))
+                    fine = False
+
+            if fine:
+                # match sizes
+                if not all([t.size == thing1.size for t in with_matching_name]):
+                    diag.append(Diagnostic([t.location for t in with_matching_name],
+                        DiagLevel.ERROR, 13, "Can't combine things of different sizes"))
+
+                # record members of enums and mitfields
+                opt_members = []
+                for t in with_matching_name:
+                    opt_members += t.members
+
+                # finally, combine all members
+                constructor = SusEnum if isinstance(thing1, SusEnum) else SusBitfield
+                doc = (thing1.docstring or "") + "\n" + (thing2.docstring or "")
+                if doc == "\n": doc = None
+                new_thing = constructor(thing1.location, doc, thing1.name, thing1.size, opt_members)
+                out.append(new_thing)
+                log.verbose(f"{Fore.LIGHTBLACK_EX}Combined {constructor.__name__[3:].lower()} {Fore.WHITE}{thing1.name}{Fore.LIGHTBLACK_EX} members across {len(with_matching_name)} definitions: {Fore.WHITE}{new_thing}", "linker")
+        else:
+            out.append(thing1)
+        ignore.append(thing1.name)
+
+        # check numeric values
+        if isinstance(thing1, (SusEnum, SusBitfield)):
+            maximum = (thing1.size * 8 if isinstance(thing1, SusBitfield) else 256 ** thing1.size) - 1
+            for member in thing1.members:
+                if member.value > maximum:
+                    diag.append(Diagnostic([member.location], DiagLevel.WARN, 8,
+                        f"Member value '{member.value}' overflow (max '{maximum}')"))
+
+        if isinstance(thing1, (SusEntity, SusMethod)) and thing1.value > 127:
+            diag.append(Diagnostic([thing1.location], DiagLevel.WARN, 8, f"Value '{thing1.value}' overflow (max '127')"))
+
+        if isinstance(thing1, SusConfirmation) and thing1.value > 15:
+            diag.append(Diagnostic([thing1.location], DiagLevel.WARN, 8, f"Value '{thing1.value}' overflow (max '15')"))
+
+    log.verbose(f"Combined {len(things)} definitions into {len(out)} things", "linker")
+    return out, diag
+
+def validate_method_meta(things: List[SusThing], method_sets: List[List[SusMethod]]) -> None:
+    log.verbose("Validating method metdata", "linker")
+    diag = []
+
+    errors = [t for t in things if isinstance(t, SusEnum) and t.name == "ErrorCode"]
+    confirmations = [t.name for t in things if isinstance(t, SusConfirmation)]
+
+    for m_set in method_sets:
+        for method in m_set:
+            for conf in method.confirmations:
+                if conf not in confirmations:
+                    diag.append(Diagnostic([method.location], DiagLevel.ERROR, 14, f"Undefined confirmation '{conf}'"))
+
+            if len(method.errors) > 0 and len(errors) == 0:
+                diag.append(Diagnostic([method.location], DiagLevel.WARN, 15,
+                    "No 'ErrorCode' enum defined. Include 'impostor.sus' or use a custom definition"))
+                continue
+            if len(method.errors) == 0:
+                continue
+            error_names = [m.name for m in errors[0].members]
+
+            for err in method.errors:
+                if err not in error_names:
+                    diag.append(Diagnostic([method.location], DiagLevel.ERROR, 16, f"Undefined error code '{err}'"))
+    
+    return diag
+
+def validate_values(entities: List[SusEntity], method_sets: List[List[SusMethod]], confirmations: List[SusConfirmation]) -> None:
+    log.verbose("Validating numeric values", "linker")
+    diag = []
+
+    for thing in entities:
+        matching = [t for t in entities if t.value == thing.value]
+        if len(matching) != 1:
+            diag.append(Diagnostic([t.location for t in matching], DiagLevel.ERROR, 17,
+                f"Multiple entities with matching values '{thing.value}'"))
+
+        # check ID field
+        id_field = [f for f in thing.fields if f.name == "id"]
+        if not id_field:
+            diag.append(Diagnostic([thing.location], DiagLevel.ERROR, 18, f"No 'id' field"))
+        else:
+            id_field = id_field[0]
+            if id_field.optional != None:
+                diag.append(Diagnostic([id_field.location], DiagLevel.ERROR, 18, f"'id' field can't be optional"))
+
+    for m_set in method_sets:
+        for method in m_set:
+            matching = [t for t in m_set if t.value == method.value]
+            if len(matching) != 1:
+                diag.append(Diagnostic([t.location for t in matching], DiagLevel.ERROR, 17,
+                    f"Multiple methods with matching values '{method.value}'"))
+
+    for thing in confirmations:
+        matching = [t for t in confirmations if t.value == thing.value]
+        if len(matching) != 1:
+            diag.append(Diagnostic([t.location for t in matching], DiagLevel.ERROR, 17,
+                f"Multiple confirmations with matching values '{thing.value}'"))
+    
+    return diag
+
+def strip_docstrings(things: List[SusThing]) -> List[SusThing]:
+    for thing in things:
+        doc = thing.docstring
+        thing.docstring = doc.strip() if doc else None
+    return things
+
+def run(things: List[SusThing]) -> List[SusThing]:
+    log.verbose("Running linker", "linker")
+
+    # get all identifiers that can be referenced
+    identifiers = [t.name for t in things if not isinstance(t, SusMethod)] + MAGIC_IDENTIFIERS
+    # get all possible fields: entity fields, method params and method return vals
+    entities = [t for t in things if isinstance(t, SusEntity)]
+    methods = [t for t in things if isinstance(t, SusMethod)]
+    confirmations = [t for t in things if isinstance(t, SusConfirmation)]
+    compounds = [t for t in things if isinstance(t, SusCompound)]
+    for e in entities: methods += e.methods
+    field_sets = [e.fields for e in entities]
+    field_sets += [m.parameters for m in methods]
+    field_sets += [m.returns for m in methods]
+    field_sets += [m.req_parameters for m in confirmations]
+    field_sets += [m.resp_parameters for m in confirmations]
+    field_sets += [c.fields for c in compounds]
+    # get all methods
+    method_sets = [[t for t in things if isinstance(t, SusMethod)]]
+    for e in entities:
+        method_sets.append([m for m in e.methods if m.static])
+        method_sets.append([m for m in e.methods if not m.static])
+
+    # run substeps collecting diagnostics
+    things, diag = combine(things)
+    diag += validate_fields(identifiers, field_sets)
+    diag += validate_method_meta(things, method_sets)
+    diag += validate_values(entities, method_sets, confirmations)
+    things = strip_docstrings(things)
+
+    # sort by severity
+    diag = sorted(diag, key=lambda d: d.level.value)
+
+    # remove diagnostics in the same location
+    deduplicated = []
+    used_locs = set()
+    for d in diag:
+        for loc in d.locations:
+            has_duplicates = True
+            if loc not in used_locs:
+                has_duplicates = False
+                used_locs.add(loc)
+        if not has_duplicates:
+            deduplicated.append(d)
+
+    return things, deduplicated
---
+++ packages/susc/susc-1.5.5.tar.gz/susc-1.5.5/susc/things.py
@@ -0,0 +1,363 @@
+from abc import ABC
+from dataclasses import dataclass
+from os import times
+from typing import *
+from enum import Enum
+from lark.lexer import Token
+from lark.tree import Tree
+import re
+from .exceptions import SourceError, Location
+from .log import verbose
+from textwrap import dedent
+
+@dataclass
+class SusThing(ABC):
+    location: Location
+    docstring: str
+
+@dataclass
+class SusValidator(SusThing):
+    param: str
+    restriction: Any
+
+class SusTypeBase(SusThing):
+    @overload
+    def find_errors(self, identifiers):
+        raise NotImplemented()
+
+@dataclass
+class SusType(SusTypeBase):
+    name: str
+    args: List[Any]
+    validators: List[SusValidator]
+    def find_errors(self, identifiers):
+        if self.name == "Int":
+            if len(self.args) != 1:
+                return "Int requires one argument"
+            if not isinstance(self.args[0], int) or self.args[0] <= 0:
+                return "Argument to Int should be a positive integer"
+            for v in self.validators:
+                if v.param != "val":
+                    return "The following validators are valid for Int: 'val'"
+                if not isinstance(v.restriction, range):
+                    return "Int[val] must be a range"
+        elif self.name == "Str":
+            if len(self.args) != 0:
+                return "Str takes no arguments"
+            for v in self.validators:
+                if v.param not in ["len", "match"]:
+                    return "The following validators are valid for Str: 'len', 'match'"
+                if v.param == "len" and not isinstance(v.restriction, range):
+                    return "Str[len] must be a range"
+                if v.param == "match" and not isinstance(v.restriction, re.Pattern):
+                    return "Str[match] must be a regular expression"
+        elif self.name == "List":
+            if len(self.args) != 2:
+                return "List takes two arguments"
+            if not isinstance(self.args[0], SusTypeBase):
+                return "First argument to List should be a type"
+            err = self.args[0].find_errors(identifiers)
+            if err is not None: return err
+            if not isinstance(self.args[1], int) or self.args[1] <= 0:
+                return "Second argument to List should be a positive integer"
+            for v in self.validators:
+                if v.param != "len":
+                    return "The following validators are valid for List: 'len'"
+                if not isinstance(v.restriction, range):
+                    return "List[len] must be a range"
+        elif self.name == "Bool":
+            if len(self.args) != 0:
+                return "Bool takes no arguments"
+            if len(self.validators) != 0:
+                return "Bool can't be validated"
+        elif self.name == "Bin":
+            if len(self.args) != 0:
+                return "Bin takes no arguments"
+            for v in self.validators:
+                if v.param != "len":
+                    return "The following validators are valid for Bin: 'len'"
+                if not isinstance(v.restriction, range):
+                    return "Bin[len] must be a range"
+        elif self.name not in identifiers:
+            return f"Unknown type '{self.name}'"
+        else:
+            if len(self.args):
+                return f"{self.name} takes no arguments"
+            if len(self.validators):
+                return f"{self.name} can't be validated"
+
+@dataclass
+class SusCompoundMember(SusThing):
+    name: str
+    type_: SusTypeBase
+
+@dataclass
+class SusCompound(SusTypeBase):
+    members: List[SusCompoundMember]
+    def find_errors(self, identifiers):
+        for m in self.members:
+            err = m.type_.find_errors(identifiers)
+            if err != None:
+                return err
+
+@dataclass
+class SusEnumMember(SusThing):
+    name: str
+    value: int
+
+@dataclass
+class SusEnum(SusThing):
+    name: str
+    size: int
+    members: List[SusEnumMember]
+
+@dataclass
+class SusBitfield(SusThing):
+    name: str
+    size: int
+    members: List[SusEnumMember]
+
+@dataclass
+class SusField(SusThing):
+    name: str
+    type_: SusType
+    optional: int
+
+@dataclass
+class SusMethod(SusThing):
+    static: bool
+    name: str
+    value: int
+    parameters: List[SusField]
+    returns: List[SusField]
+    errors: List[str]
+    confirmations: List[str]
+    rate_limit: Tuple[int, int]
+
+@dataclass
+class SusEntity(SusThing):
+    name: str
+    value: int
+    fields: List[SusField]
+    methods: List[SusMethod]
+
+@dataclass
+class SusConfirmation(SusThing):
+    name: str
+    value: int
+    req_parameters: List[SusField]
+    resp_parameters: List[SusField]
+
+@dataclass
+class SusCompound(SusThing):
+    name: str
+    fields: List[SusField]
+
+
+def convert_docstring(doc: str) -> str:
+    if doc is None:
+        return None
+    doc = doc.value
+    doc = doc.strip(" \r\n")
+    doc = doc[2:-2] # remove @> and <@
+    doc = doc.strip("\r\n")
+    return dedent(doc).strip()
+
+def convert_ebf_members(ast, file): # Enum or Bitfield
+    list = []
+    for member in ast:
+        if not member: continue
+        doc = convert_docstring(member.children[0])
+        name = member.children[1]
+        value = int(member.children[2].value)
+        list.append(SusEnumMember(Location(file, name.line, name.column, len(name.value)), doc, name.value, value))
+    return list
+    
+def convert_range(ast, max_val):
+    if len(ast.children) == 1:
+        return range(int(ast.children[0].value), max_val)
+    elif len(ast.children) == 2:
+        return range(int(ast.children[0].value), int(ast.children[1].value) + 1) # python ranges exclude the right end
+
+def convert_type(ast, file):
+    name = ast.children[0]
+    args = []
+    validators = []
+    for directive in ast.children[1:]:
+        if directive is None:
+            continue
+        elif directive.data == "type_argument":
+            arg_val = directive.children[0]
+            if isinstance(arg_val, Token) and arg_val.type == "NUMBER":
+                args.append(int(arg_val.value))
+            elif isinstance(arg_val, Tree) and arg_val.data in ["type", "compound"]:
+                args.append(convert_type(arg_val, file))
+        elif directive.data == "type_validator":
+            val_name = directive.children[0] # validator name
+            val_val = directive.children[1].children[0] # validator value
+            if isinstance(val_val, Token) and val_val.type == "SIGNED_NUMBER":
+                val_val = int(val_val)
+            elif isinstance(val_val, Token) and val_val.type == "REGEX":
+                regex_tokens = val_val.split('/')
+                regex = '/'.join(regex_tokens[1:-1])
+                flags = regex_tokens[-1]
+                flags_num = 0
+                for f in flags:
+                    if f == "i": flags_num |= re.I
+                    if f == "m": flags_num |= re.M
+                    if f == "s": flags_num |= re.S
+                try:
+                    val_val = re.compile(regex, flags_num)
+                except re.error as exc:
+                    raise SourceError([Location(file, val_val.line, val_val.column + exc.pos + 1, 0)],
+                        f"Invalid regular expression: {exc.msg}")
+            elif isinstance(val_val, Tree) and val_val.data == "range":
+                max_val = 2 ** 64
+                # we can infer the maximum value from the int length
+                if name == "Int" and len(args) == 1 and isinstance(args[0], int):
+                    max_val = 2 ** (args[0] * 8)
+                val_val = convert_range(val_val, max_val)
+            validators.append(SusValidator(Location(file, val_name.line, val_name.column, len(val_name.value)), None,
+                val_name.value, val_val))
+
+    location = Location(file, name.line, name.column, len(name.value))
+    return SusType(location, None, name.value, args, validators)
+
+def convert_opt(ast):
+    if ast is None:
+        return ast
+    return int(ast.value)
+
+def convert_value(ast):
+    if isinstance(ast, Tree) and ast.data == "value":
+        ast = ast.children[0]
+    if isinstance(ast, Token):
+        if ast.type == "SIGNED_NUMBER": return int(ast.value)
+        if ast.type == "BOOL": return ast.value == "true"
+        if ast.type == "STRING": return ast.value[1:-1] # strip quotation marks
+    elif isinstance(ast, Tree):
+        if ast.data == "list":
+            return [convert_value(val) for val in ast.children]
+
+def convert_param(ast, file):
+    doc = convert_docstring(ast.children[0])
+    name = ast.children[1]
+    opt = convert_opt(ast.children[2])
+    type_ = convert_type(ast.children[3], file)
+
+    location = Location(file, name.line, name.column, len(name.value))
+
+    return SusField(location, doc, name.value, type_, opt)
+
+def convert_timeout(val):
+    num, mul = re.match("(\d+)(\w+)", val).groups()
+    num = int(num)
+    return num * {
+        "ms":                      1,
+        "s":                    1000,
+        "m":               60 * 1000,
+        "h":             3600 * 1000,
+        "d":        24 * 3600 * 1000,
+        "mo":  30 * 24 * 3600 * 1000,
+        "y":  356 * 24 * 3600 * 1000,
+    }[mul]
+
+def convert_method(ast, file):
+    static = None
+    if ast.data == "static_method": static = True
+    elif ast.data == "normal_method": static = False
+
+    doc = convert_docstring(ast.children[0])
+    name = ast.children[1]
+    value = int(ast.children[2].value)
+
+    params, returns, errors, confirmations, states, rate_limit = [], [], [], [], [], None
+    for directive in ast.children[3:]:
+        if not directive: continue
+        if directive.data == "method_param":
+            params.append(convert_param(directive, file))
+        elif directive.data == "returns":
+            for p in directive.children:
+                returns.append(convert_param(p, file))
+        elif directive.data in ["errors", "confirmations"]:
+            lst = {"errors": errors, "confirmations": confirmations}[directive.data]
+            for e in directive.children:
+                if not e: continue
+                if e.value in lst:
+                    SourceError([Location(file, e.line, e.column, len(e.value))],
+                        f"Duplicate member \"{e.value}\" for this directive").print_warn()
+                lst.append(e.value)
+        elif directive.data == "rate_limit":
+            amount = int(directive.children[0].value)
+            window = convert_timeout(directive.children[1].value)
+            rate_limit = (amount, window)
+
+    return SusMethod(Location(file, name.line, name.column, len(name.value)), doc, static, name.value,
+        value, params, returns, errors, confirmations, rate_limit)
+
+def convert_ast(ast, file):
+    if ast.data in ["enum", "bitfield"]:
+        constructor = SusEnum if ast.data == "enum" else SusBitfield
+        doc = convert_docstring(ast.children[0])
+        size = int(ast.children[1].value)
+        name = ast.children[2]
+        return constructor(Location(file, name.line, name.column, len(name)), doc,
+            name.value, size, convert_ebf_members(ast.children[3:], file))
+
+    elif ast.data == "entity":
+        doc = convert_docstring(ast.children[0])
+        name = ast.children[1]
+        value = ast.children[2]
+
+        directives = ast.children[3:]
+        fields, methods = [], []
+        for directive in directives:
+            if directive.data == "entity_field":
+                f_doc = convert_docstring(directive.children[0])
+                identifier = directive.children[1]
+                opt = convert_opt(directive.children[2])
+                type_ = convert_type(directive.children[3], file)
+                location = Location(file, identifier.line, identifier.column, len(identifier.value))
+
+                fields.append(SusField(location, f_doc, identifier.value, type_, opt))
+
+            if directive.data.endswith("method"):
+                methods.append(convert_method(directive, file))
+
+        return SusEntity(Location(file, name.line, name.column, len(name.value)), doc,
+            name.value, int(value.value), fields, methods)
+
+    elif ast.data == "global_method":
+        return convert_method(ast, file)
+
+    elif ast.data == "confirmation":
+        doc = convert_docstring(ast.children[0])
+        name = ast.children[1]
+        value = int(ast.children[2].value)
+        req = ast.children[3]
+        resp = ast.children[4]
+
+        req = [convert_param(par, file) for par in req.children if par]
+        resp = [convert_param(par, file) for par in resp.children if par]
+
+        return SusConfirmation(Location(file, name.line, name.column, len(name)), doc, name.value, value,
+            req, resp)
+
+    elif ast.data == "compound":
+        doc = convert_docstring(ast.children[0])
+        name = ast.children[1]
+
+        directives = ast.children[2:]
+        fields, methods = [], []
+        for directive in directives:
+            if not directive: continue
+            d_doc = convert_docstring(directive.children[0])
+            identifier = directive.children[1]
+            opt = convert_opt(directive.children[2])
+            type_ = convert_type(directive.children[3], file)
+            location = Location(file, identifier.line, identifier.column, len(identifier.value))
+
+            fields.append(SusField(location, d_doc, identifier.value, type_, opt))
+
+        return SusCompound(Location(file, name.line, name.column, len(name.value)), doc,
+            name.value, fields)
