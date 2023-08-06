79d85e415746600ddb4b68d79ad50050bb32b955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:13 +0000

    Add package indcomp/0.2.1/indcomp-0.2.1.tar.gz

---
+++ packages/indcomp/indcomp-0.2.1.tar.gz/indcomp-0.2.1/indcomp/_utils.py
@@ -0,0 +1,14 @@
+"""The `indcomp._utils` module contains utility functions.
+"""
+
+from typing import List
+
+
+def get_colour_palette() -> List[str]:
+    """Function for retrieving indcomp colour palette as a list of hexcodes
+
+    For comparison of 'target' (aggregate patient data) and 'index' (individual patient
+    data) trials, list index 0 should be used for the target trial and list index 1
+    should be used for the index trial
+    """
+    return ["#458CFF", "#63A7FF"]
