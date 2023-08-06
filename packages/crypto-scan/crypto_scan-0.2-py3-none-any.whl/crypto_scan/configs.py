40d5fac235a7dd7e6498843fd56666640d9065f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:06 +0000

    Add package crypto-scan/0.2/crypto_scan-0.2-py3-none-any.whl

---
+++ packages/crypto-scan/crypto_scan-0.2-py3-none-any.whl/crypto_scan/configs.py
@@ -0,0 +1,7 @@
+ETH_CHAIN = 'ETHERIUM'
+POLYGON_CHAIN = 'POLYGON'
+SOLANA_CHAIN = 'SOLANA'
+
+ETHNET = "https://api.etherscan.io"
+POLYGONNET = "https://api.polygonscan.com"
+BSCNET = "https://api.bscscan.com/"
