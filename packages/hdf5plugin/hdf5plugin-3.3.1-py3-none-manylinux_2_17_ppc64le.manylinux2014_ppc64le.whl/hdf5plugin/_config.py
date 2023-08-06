a5544045cdf5074524671e1bcce9f90bb456157d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:18 +0000

    Add package hdf5plugin/3.3.1/hdf5plugin-3.3.1-py3-none-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/hdf5plugin/hdf5plugin-3.3.1-py3-none-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/hdf5plugin/_config.py
@@ -0,0 +1,1 @@
+config = {'openmp': False, 'native': False, 'sse2': True, 'avx2': False, 'cpp11': True, 'filter_file_extension': '.so', 'embedded_filters': ('blosc', 'bshuf', 'fcidecomp', 'lz4', 'zfp', 'zstd')}
