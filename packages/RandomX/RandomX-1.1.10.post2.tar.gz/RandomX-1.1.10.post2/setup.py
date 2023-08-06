2ad8efb4e76548fd875ae7bdd06d74143a25eec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:17 +0000

    Add package RandomX/1.1.10.post2/RandomX-1.1.10.post2.tar.gz

---
+++ packages/RandomX/RandomX-1.1.10.post2.tar.gz/RandomX-1.1.10.post2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+import glob, os, platform
+
+machine = platform.machine()
+
+# TODO: build submodule with cmake
+compile_flags = ['-march=native','-std=c++11','-fpic', '-O3']
+
+source_root_dir = os.path.join('RandomX', 'src')
+sources = []
+sources.extend(glob.glob('**/*.c', root_dir=source_root_dir, recursive=True))
+sources.extend(glob.glob('**/*.cpp', root_dir=source_root_dir, recursive=True))
+sources.extend(glob.glob('**/*.S', root_dir=source_root_dir, recursive=True))
+sources = [source for source in sources if 'jit_' not in source and  'tests' not in source]
+if machine in ['i386', 'i686', 'x86_64']:
+    sources.extend(['jit_compiler_x86.cpp', 'jit_compiler_x86_static.S'])
+elif machine in ['aarch64_be', 'aarch64', 'armv8b', 'armv8l']:
+    sources.extend(['jit_compiler_a64_static.s', 'jit_compiler_a64.cpp'])
+    compile_flags.append('-DHAVE_HWCAP')
+elif machine.startswith('ppc'):
+    compile_flags.append('-mcpu=native')
+sources = [os.path.join(source_root_dir,source) for source in sources]
+
+setup(
+    name='RandomX',
+    version=open('version').read(),
+    package_dir={'randomx': 'randomx'},
+    packages=['randomx'],
+    include_package_data=True,
+    package_data={'': ['*.pyx', '*.pxd', '*.inc', '*.S', '*.h', '*.c', '*.hpp', '*.cpp']},
+    ext_modules=cythonize(
+        Extension(
+            'randomx', [
+                os.path.join('randomx','randomx.pyx'),
+                *[source for source in sources if source.endswith('.c') or source.endswith('.cpp')]
+            ],
+            include_dirs=[os.path.join('RandomX','src')],
+            extra_objects=[source for source in sources if source.endswith('.S')],
+            extra_compile_args=compile_flags,
+            #libraries=['randomx'],
+            language='c++',
+        ),
+        compiler_directives=dict(
+            language_level='3',
+            embedsignature=True
+        )
+    )
+)
