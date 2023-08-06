afb625db67ef365c98219bd2d23c6ebf0477120a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:27:27 +0000

    Add package empanada-napari/0.2.0/empanada_napari-0.2.0-py3-none-any.whl

---
+++ packages/empanada-napari/empanada_napari-0.2.0-py3-none-any.whl/empanada_napari/_merge_split_widget.py
@@ -0,0 +1,277 @@
+from napari_plugin_engine import napari_hook_implementation
+from magicgui import magicgui
+from empanada.array_utils import crop_and_binarize
+from skimage.measure import regionprops
+from scipy import ndimage as ndi
+from skimage.segmentation import watershed
+from skimage.feature import peak_local_max
+import napari
+import numpy as np
+import dask.array as da
+
+def delete_labels():
+    @magicgui(
+        call_button='Delete labels',
+        layout='vertical',
+    )
+
+    def widget(
+        viewer: napari.viewer.Viewer,
+        labels_layer: napari.layers.Labels,
+        points_layer: napari.layers.Points,
+
+    ):
+        if points_layer is None:
+            points_layer = viewer.add_points([])
+            points_layer.mode = 'ADD'
+            return
+
+        labels = labels_layer.data
+        world_points = points_layer.data
+
+        # get points as indices in local coordinates
+        local_points = []
+        for pt in world_points:
+            local_points.append(tuple([int(c) for c in labels_layer.world_to_data(pt)]))
+
+        if type(labels) == da.core.Array:
+            label_ids = [labels[pt].compute() for pt in local_points]
+        else:
+            label_ids = [labels[pt].item() for pt in local_points]
+
+        # drop any label_ids equal to 0 in case point
+        # was placed on the background
+        label_ids = list(filter(lambda x: x > 0, label_ids))
+
+        # replace labels with minimum of the selected labels
+        for l in label_ids:
+            labels[labels == l] = 0
+
+        labels_layer.data = labels
+        points_layer.data = []
+
+        print(f'Removed labels {label_ids}')
+
+    return widget
+
+def merge_labels():
+    @magicgui(
+        call_button='Merge labels',
+        layout='vertical',
+    )
+
+    def widget(
+        viewer: napari.viewer.Viewer,
+        labels_layer: napari.layers.Labels,
+        points_layer: napari.layers.Points,
+    ):
+        if points_layer is None:
+            points_layer = viewer.add_points([])
+            points_layer.mode = 'ADD'
+            return
+
+        labels = labels_layer.data
+        world_points = points_layer.data
+
+        # get points as indices in local coordinates
+        local_points = []
+        for pt in world_points:
+            local_points.append(tuple([int(c) for c in labels_layer.world_to_data(pt)]))
+
+        if type(labels) == da.core.Array:
+            label_ids = [labels[pt].compute() for pt in local_points]
+        else:
+            label_ids = [labels[pt].item() for pt in local_points]
+
+        # drop any label_ids equal to 0 in case point
+        # was placed on the background
+        label_ids = list(filter(lambda x: x > 0, label_ids))
+
+        # get merged label value
+        # prefer the currently selected label
+        if labels_layer.selected_label in label_ids:
+            new_label_id = labels_layer.selected_label
+        else:
+            new_label_id = min(label_ids)
+
+        # replace labels with minimum of the selected labels
+        for l in label_ids:
+            if l != new_label_id:
+                labels[labels == l] = new_label_id
+
+        labels_layer.data = labels
+        points_layer.data = []
+
+        print(f'Merged labels {label_ids} to {new_label_id}')
+
+    return widget
+
+def split_labels():
+
+    def _box_to_slice(shed_box):
+        n = len(shed_box)
+        n_dim = n//2
+
+        slices = []
+        for i in range(n_dim):
+            slices.append(slice(shed_box[i], shed_box[i+n_dim]))
+
+        return tuple(slices)
+
+    def _translate_point_in_box(point, shed_box):
+        n_dim = len(shed_box) // 2
+        return tuple([int(point[i] - shed_box[i]) for i in range(n_dim)])
+
+    @magicgui(
+        call_button='Split labels',
+        layout='vertical',
+        min_distance=dict(widget_type='Slider', label='Minimum Distance', min=1, max=100, value=10, tooltip='Min Distance between Markers'),
+        points_as_markers=dict(widget_type='CheckBox', text='Use points as markers', value=False, tooltip='Whether to use the placed points as markers for watershed. If checked, Min. Distance is ignored.'),
+    )
+
+    def widget(
+        viewer: napari.viewer.Viewer,
+        labels_layer: napari.layers.Labels,
+        points_layer: napari.layers.Points,
+        min_distance: int,
+        points_as_markers: bool
+    ):
+        if points_layer is None:
+            points_layer = viewer.add_points([])
+            points_layer.mode = 'ADD'
+            return
+
+        labels = labels_layer.data
+        world_points = points_layer.data
+
+        # get points as indices in local coordinates
+        local_points = []
+        for pt in world_points:
+            local_points.append(tuple([int(c) for c in labels_layer.world_to_data(pt)]))
+
+        if type(labels) == da.core.Array:
+            raise Exception(f'Split operation is not supported on Dask Array labels!')
+
+        label_ids = np.array([labels[pt].item() for pt in local_points])
+        local_points = np.stack(local_points, axis=0)
+
+        # drop any label_ids equal to 0; in case point
+        # was placed on the background
+        background_pts = label_ids == 0
+        local_points = local_points[~background_pts]
+        label_ids = label_ids[~background_pts]
+
+        label_id = label_ids[0]
+
+        if len(np.unique(label_ids)) > 1:
+            print('Split operation only supports 1 label at a time.')
+
+            # drop points corresponding to extraneous labels
+            local_points = local_points[label_ids == label_id]
+
+        shed_box = [rp.bbox for rp in regionprops(labels) if rp.label == label_id][0]
+        binary = crop_and_binarize(labels, shed_box, label_id)
+
+        if points_as_markers:
+            energy = binary
+            markers = np.zeros(binary.shape, dtype=bool)
+            for local_pt in local_points:
+                markers[_translate_point_in_box(local_pt, shed_box)] = True
+
+        else:
+            distance = ndi.distance_transform_edt(binary)
+            energy = -distance
+
+            # handle irritating quirk of peak_local_max
+            # when any dimension has length 1
+            if any([s == 1 for s in distance.shape]):
+                coords = peak_local_max(np.squeeze(distance), min_distance=min_distance)
+                markers = np.zeros(np.squeeze(distance).shape, dtype=bool)
+                mask[tuple(coords.T)] = True
+
+                expand_axis = [s == 1 for s in distance.shape].index(True)
+                mask = np.expand_dims(mask, axis=expand_axis)
+            else:
+                coords = peak_local_max(distance, min_distance=min_distance)
+                markers = np.zeros(distance.shape, dtype=bool)
+                markers[tuple(coords.T)] = True
+
+        # label markers and run watershed
+        markers, _ = ndi.label(markers)
+        marker_ids = np.unique(markers)[1:]
+
+        if len(marker_ids) > 1:
+            new_labels = watershed(energy, markers, mask=binary)
+            slices = _box_to_slice(shed_box)
+
+            max_label = labels.max()
+            labels[slices][binary] = new_labels[binary] + max_label
+            print(f'Split label {label_id} to {marker_ids + max_label}')
+        else:
+            print('Nothing to split.')
+
+        labels_layer.data = labels
+        points_layer.data = []
+
+    return widget
+
+def jump_to_label():
+    @magicgui(
+        call_button='Jump to label',
+        layout='vertical',
+        label_id=dict(widget_type='LineEdit', label='Label ID', value='1', tooltip='Label to jump to'),
+    )
+
+    def widget(
+        viewer: napari.viewer.Viewer,
+        labels_layer: napari.layers.Labels,
+        label_id
+    ):
+        assert labels_layer.data.ndim == 3
+        label_id = int(label_id)
+
+        bbox = None
+        for rp in regionprops(labels_layer.data):
+            if rp.label == label_id:
+                bbox = rp.bbox
+                break
+
+        if bbox is None:
+            raise Exception(f'No label {label_id} in {labels_layer.name}')
+
+        # split bounding box by axis
+        ndim = len(bbox) // 2
+        bbox = tuple([
+            *labels_layer._data_to_world(bbox[:ndim]),
+            *labels_layer._data_to_world(bbox[ndim:]),
+        ])
+
+        bbox = [(bbox[i], bbox[i+ndim]) for i in range(ndim)]
+
+        # get the current axis
+        axis = viewer.dims.order[0]
+
+        new_step = list(viewer.dims.current_step)
+        new_step[axis] = bbox[axis][0]
+
+        # set the current step to first slice of bbox
+        viewer.dims.current_step = tuple(new_step)
+        labels_layer.selected_label = label_id
+
+    return widget
+
+@napari_hook_implementation(specname='napari_experimental_provide_dock_widget')
+def delete_labels_widget():
+    return delete_labels, {'name': 'Delete Labels'}
+
+@napari_hook_implementation(specname='napari_experimental_provide_dock_widget')
+def merge_labels_widget():
+    return merge_labels, {'name': 'Merge Labels'}
+
+@napari_hook_implementation(specname='napari_experimental_provide_dock_widget')
+def split_labels_widget():
+    return split_labels, {'name': 'Split Labels'}
+
+@napari_hook_implementation(specname='napari_experimental_provide_dock_widget')
+def jump_to_label_widget():
+    return jump_to_label, {'name': 'Jump to Label'}
---
+++ packages/empanada-napari/empanada_napari-0.2.0-py3-none-any.whl/empanada_napari/inference.py
@@ -0,0 +1,496 @@
+import os, platform
+import zarr
+import yaml
+import numpy as np
+import torch
+
+import torch.multiprocessing as mp
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from empanada.data import VolumeDataset
+from empanada.data.utils import resize_by_factor
+
+from empanada.inference import filters
+from empanada.inference.engines import (
+    PanopticDeepLabRenderEngine, PanopticDeepLabRenderEngine3d
+)
+from empanada.inference.tracker import InstanceTracker
+from empanada.array_utils import put
+from empanada.inference.patterns import *
+
+from napari.qt.threading import thread_worker
+from empanada_napari.utils import Preprocessor, load_model_to_device
+
+MODEL_DIR = os.path.join(os.path.expanduser('~'), '.empanada')
+torch.hub.set_dir(MODEL_DIR)
+
+def instance_relabel(tracker):
+    r"""Relabels instances starting from 1"""
+    instance_id = 1
+    instances = {}
+    for instance_attr in tracker.instances.values():
+        # vote on indices that should belong to an object
+        runs_cat = np.stack([
+            instance_attr['starts'], instance_attr['runs']
+        ], axis=1)
+
+        sort_idx = np.argsort(runs_cat[:, 0], kind='stable')
+        runs_cat = runs_cat[sort_idx]
+
+        # TODO: technically this could break the zarr_fill_instances function
+        # if an object has a pixel in the bottom right corner of the Nth z slice
+        # and a pixel in the top left corner of the N+1th z slice
+        # only applies to yz axis
+        instances[instance_id] = {}
+        instances[instance_id]['box'] = instance_attr['box']
+        instances[instance_id]['starts'] = runs_cat[:, 0]
+        instances[instance_id]['runs'] = runs_cat[:, 1]
+        instance_id += 1
+
+    return instances
+
+@thread_worker
+def stack_postprocessing(
+    trackers,
+    store_url,
+    model_config,
+    label_divisor=1000,
+    min_size=200,
+    min_extent=4,
+    dtype=np.uint32,
+    chunk_size=(96, 96, 96)
+):
+    r"""Relabels and filters each class defined in trackers. Yields a numpy
+    or zarr volume along with the name of the class that is segmented.
+    """
+    labels = model_config['labels']
+    class_names = model_config['class_names']
+    if store_url is not None:
+        zarr_store = zarr.open(store_url)
+    else:
+        zarr_store = None
+
+    # create the final instance segmentations
+    for class_id, class_name in zip(labels, class_names):
+        print(f'Creating stack segmentation for class {class_name}...')
+
+        class_tracker = get_axis_trackers_by_class(trackers, class_id)[0]
+        shape3d = class_tracker.shape3d
+
+        # merge instances from orthoplane inference
+        stack_tracker = InstanceTracker(class_id, label_divisor, shape3d, 'xy')
+        stack_tracker.instances = instance_relabel(class_tracker)
+
+        # inplace apply filters to final merged segmentation
+        filters.remove_small_objects(stack_tracker, min_size=min_size)
+        filters.remove_pancakes(stack_tracker, min_span=min_extent)
+
+        print(f'Total {class_name} objects {len(stack_tracker.instances.keys())}')
+
+        # decode and fill the instances
+        if zarr_store is not None:
+            stack_vol = zarr_store.create_dataset(
+                f'{class_name}_pred', shape=shape3d, dtype=dtype,
+                overwrite=True, chunks=chunk_size
+            )
+        else:
+            stack_vol = np.zeros(shape3d, dtype=dtype)
+
+        fill_volume(stack_vol, stack_tracker.instances)
+
+        yield stack_vol, class_name
+
+@thread_worker
+def tracker_consensus(
+    trackers,
+    store_url,
+    model_config,
+    label_divisor=1000,
+    pixel_vote_thr=2,
+    cluster_iou_thr=0.75,
+    allow_one_view=False,
+    min_size=200,
+    min_extent=4,
+    dtype=np.uint32,
+    chunk_size=(96, 96, 96)
+):
+    r"""Calculate the orthoplane consensus from trackers. Yields a numpy
+    or zarr volume along with the name of the class that is segmented.
+    """
+    labels = model_config['labels']
+    thing_list = model_config['thing_list']
+    class_names = model_config['class_names']
+    if store_url is not None:
+        zarr_store = zarr.open(store_url)
+    else:
+        zarr_store = None
+
+    # create the final instance segmentations
+    for class_id, class_name in zip(labels, class_names):
+        # get the relevant trackers for the class_label
+        print(f'Creating consensus segmentation for class {class_name}...')
+
+        class_trackers = get_axis_trackers_by_class(trackers, class_id)
+        shape3d = class_trackers[0].shape3d
+
+        # consensus from orthoplane
+        if class_id in thing_list:
+            consensus_tracker = create_instance_consensus(
+                class_trackers, pixel_vote_thr, cluster_iou_thr, allow_one_view
+            )
+            filters.remove_small_objects(consensus_tracker, min_size=min_size)
+            filters.remove_pancakes(consensus_tracker, min_span=min_extent)
+        else:
+            consensus_tracker = create_semantic_consensus(class_trackers, pixel_vote_thr)
+
+        print(f'Total {class_name} objects {len(consensus_tracker.instances.keys())}')
+
+        # decode and fill the instances
+        if zarr_store is not None:
+            consensus_vol = zarr_store.create_dataset(
+                f'{class_name}_pred', shape=shape3d, dtype=dtype,
+                overwrite=True, chunks=chunk_size
+            )
+        else:
+            consensus_vol = np.zeros(shape3d, dtype=dtype)
+
+        fill_volume(consensus_vol, consensus_tracker.instances)
+
+        yield consensus_vol, class_name
+
+class Engine2d:
+    r"""Engine for 2D and parameter testing."""
+    def __init__(
+        self,
+        model_config,
+        inference_scale=1,
+        label_divisor=1000,
+        nms_threshold=0.1,
+        nms_kernel=3,
+        confidence_thr=0.3,
+        semantic_only=False,
+        fine_boundaries=False,
+        use_gpu=True,
+        use_quantized=False
+    ):
+        # check whether GPU is available
+        device = torch.device('cuda:0' if torch.cuda.is_available() and use_gpu else 'cpu')
+        if use_quantized and str(device) == 'cpu' and model_config.get('model_quantized') is not None:
+            model_url = model_config['model_quantized']
+        else:
+            model_url = model_config['model']
+
+        model = load_model_to_device(model_url, device)
+        model = model.to(device)
+
+        self.thing_list = model_config['thing_list']
+        self.labels = model_config['labels']
+        self.class_names = model_config['class_names']
+        self.label_divisor = label_divisor
+        self.padding_factor = model_config['padding_factor']
+        self.inference_scale = inference_scale
+        self.fine_boundaries = fine_boundaries
+
+        if semantic_only:
+            thing_list = []
+        else:
+            thing_list = self.thing_list
+
+        # create the inference engine
+        self.engine = PanopticDeepLabRenderEngine(
+            model, thing_list=thing_list,
+            label_divisor=label_divisor,
+            nms_threshold=nms_threshold,
+            nms_kernel=nms_kernel,
+            confidence_thr=confidence_thr,
+            padding_factor=self.padding_factor,
+            coarse_boundaries=not fine_boundaries
+        )
+
+        # set the image transforms
+        norms = model_config['norms']
+        gray_channels = 1
+        self.preprocessor = Preprocessor(**norms)
+
+    def update_params(
+        self,
+        inference_scale,
+        label_divisor,
+        nms_threshold,
+        nms_kernel,
+        confidence_thr,
+        fine_boundaries,
+        semantic_only=False
+    ):
+        # note that input_scale is the variable name in the engine
+        self.inference_scale = inference_scale
+        self.engine.input_scale = inference_scale
+
+        self.label_divisor = label_divisor
+        self.engine.label_divisor = label_divisor
+
+        self.nms_threshold = nms_threshold
+        self.engine.nms_threshold = nms_threshold
+
+        self.nms_kernel = nms_kernel
+        self.engine.nms_kernel = nms_kernel
+
+        self.confidence_thr = confidence_thr
+        self.engine.confidence_thr = confidence_thr
+
+        self.fine_boundaries = fine_boundaries
+        self.engine.coarse_boundaries = not fine_boundaries
+
+        if semantic_only:
+            self.engine.thing_list = []
+        else:
+            self.engine.thing_list = self.thing_list
+
+    def infer(self, image):
+        # resize image to correct scale
+        size = image.shape
+        image = resize_by_factor(image, self.inference_scale)
+        image = self.preprocessor(image)['image'].unsqueeze(0)
+
+        # engine handles upsampling and padding
+        pan_seg = self.engine(image, size, upsampling=self.inference_scale)
+        return pan_seg.squeeze().cpu().numpy().astype(np.uint32)
+
+class Engine3d:
+    r"""Engine for 3D ortho-plane and stack inference"""
+    def __init__(
+        self,
+        model_config,
+        inference_scale=1,
+        label_divisor=1000,
+        median_kernel_size=5,
+        stuff_area=64,
+        void_label=0,
+        nms_threshold=0.1,
+        nms_kernel=3,
+        confidence_thr=0.3,
+        force_connected=True,
+        min_size=500,
+        min_extent=4,
+        fine_boundaries=False,
+        semantic_only=False,
+        use_gpu=True,
+        use_quantized=False,
+        store_url=None,
+        save_panoptic=False
+    ):
+        # check whether GPU is available
+        # check whether GPU is available
+        device = torch.device('cuda:0' if torch.cuda.is_available() and use_gpu else 'cpu')
+        if use_quantized and str(device) == 'cpu' and model_config.get('model_quantized') is not None:
+            model_url = model_config['model_quantized']
+        else:
+            model_url = model_config['model']
+
+        model = load_model_to_device(model_url, device)
+        model = model.to(device)
+
+        self.model_config = model_config
+        self.labels = model_config['labels']
+        self.class_names = model_config['class_names']
+        self.label_divisor = label_divisor
+        self.padding_factor = model_config['padding_factor']
+        self.inference_scale = inference_scale
+
+        # downgrade all thing classes
+        if semantic_only:
+            self.thing_list = []
+        else:
+            self.thing_list = model_config['thing_list']
+
+        # create the inference engine
+        self.engine = PanopticDeepLabRenderEngine3d(
+            model, thing_list=self.thing_list,
+            median_kernel_size=median_kernel_size,
+            label_divisor=label_divisor,
+            nms_threshold=nms_threshold,
+            nms_kernel=nms_kernel,
+            confidence_thr=confidence_thr,
+            padding_factor=self.padding_factor,
+            coarse_boundaries=not fine_boundaries
+        )
+
+        # set the image transforms
+        norms = model_config['norms']
+        gray_channels = 1
+        self.preprocessor = Preprocessor(**norms)
+
+        self.axes = {'xy': 0, 'xz': 1, 'yz': 2}
+        self.merge_iou_thr = 0.25
+        self.merge_ioa_thr = 0.25
+        self.force_connected = force_connected
+        self.min_size = min_size
+        self.min_extent = min_extent
+        self.fine_boundaries = fine_boundaries
+
+        self.save_panoptic = save_panoptic
+        if store_url is not None:
+            self.zarr_store = zarr.open(store_url, mode='w')
+        else:
+            self.zarr_store = None
+
+        self.set_dtype()
+
+    def set_dtype(self):
+        # maximum possible value in panoptic seg
+        max_index = self.label_divisor * (1 + max(self.labels))
+        if max_index < 2 ** 8:
+            self.dtype = np.uint8
+        elif max_index < 2 ** 16:
+            self.dtype = np.uint16
+        elif max_index < 2 ** 32:
+            self.dtype = np.uint32
+        else:
+            self.dtype = np.uint64
+
+    def update_params(
+        self,
+        inference_scale,
+        label_divisor,
+        median_kernel_size,
+        nms_threshold,
+        nms_kernel,
+        confidence_thr,
+        min_size,
+        min_extent,
+        fine_boundaries,
+        semantic_only,
+        store_url,
+        save_panoptic
+    ):
+        self.label_divisor = label_divisor
+        self.inference_scale = inference_scale
+        self.min_size = min_size
+        self.min_extent = min_extent
+        self.fine_boundaries = fine_boundaries
+
+        self.engine.label_divisor = label_divisor
+        self.engine.ks = median_kernel_size
+        self.engine.mid_idx = (median_kernel_size - 1) // 2
+        self.engine.nms_threshold = nms_threshold
+        self.engine.nms_kernel = nms_kernel
+        self.engine.confidence_thr = confidence_thr
+        self.engine.coarse_boundaries = not fine_boundaries
+
+        if semantic_only:
+            self.thing_list = []
+            self.engine.thing_list = []
+        else:
+            self.thing_list = self.model_config['thing_list']
+            self.engine.thing_list = self.thing_list
+
+        # reset median queue for good measure
+        self.engine.reset()
+
+        self.save_panoptic = save_panoptic
+        if store_url is not None:
+            self.zarr_store = zarr.open(store_url, mode='w')
+        else:
+            self.zarr_store = None
+
+        self.set_dtype()
+
+    def create_trackers(self, shape3d, axis_name):
+        trackers = [
+            InstanceTracker(label, self.label_divisor, shape3d, axis_name)
+            for label in self.labels
+        ]
+        return trackers
+
+    def create_panoptic_stack(self, axis_name, shape3d):
+        # faster IO with chunking only along
+        # the given axis, orthogonal viewing is slow though
+        if self.zarr_store is not None and self.save_panoptic:
+            chunks = [None, None, None]
+            chunks[self.axes[axis_name]] = 1
+            stack = self.zarr_store.create_dataset(
+                f'panoptic_{axis_name}', shape=shape3d,
+                dtype=self.dtype, chunks=tuple(chunks), overwrite=True
+            )
+
+        elif self.save_panoptic:
+            # we'll use uint32 for in memory segs
+            stack = np.zeros(shape3d, dtype=self.dtype)
+        else:
+            stack = None
+
+        return stack
+
+    def infer_on_axis(self, volume, axis_name):
+        axis = self.axes[axis_name]
+        # create the dataloader
+        dataset = VolumeDataset(volume, axis, self.preprocessor, scale=self.inference_scale)
+        dataloader = DataLoader(
+            dataset, batch_size=1, shuffle=False, pin_memory=False,
+            drop_last=False, num_workers=0
+        )
+
+        # create necessary matchers and trackers
+        trackers = self.create_trackers(volume.shape, axis_name)
+        matchers = create_matchers(
+            self.thing_list, self.label_divisor,
+            self.merge_iou_thr, self.merge_ioa_thr
+        )
+        stack = self.create_panoptic_stack(axis_name, volume.shape)
+
+        if platform.system() == "Darwin":
+            try:
+                mp.set_start_method('spawn')
+            except RuntimeError:
+                pass
+
+        # setup matcher for multiprocessing
+        queue = mp.Queue()
+        rle_stack = []
+        matcher_out, matcher_in = mp.Pipe()
+        matcher_args = (
+            matchers, queue, rle_stack, matcher_in,
+            self.labels, self.label_divisor, self.thing_list
+        )
+        matcher_proc = mp.Process(target=forward_matching, args=matcher_args)
+        matcher_proc.start()
+
+        print(f'Predicting {axis_name}...')
+        for batch in tqdm(dataloader, total=len(dataloader)):
+            image = batch['image']
+            size = batch['size']
+            pan_seg = self.engine(image, size, self.inference_scale)
+
+            if pan_seg is None:
+                # building the median queue
+                queue.put(None)
+                continue
+            else:
+                pan_seg = pan_seg.squeeze().cpu().numpy()
+                queue.put(pan_seg)
+
+        final_segs = self.engine.end(self.inference_scale)
+        if final_segs:
+            for i, pan_seg in enumerate(final_segs):
+                pan_seg = pan_seg.squeeze().cpu().numpy()
+                queue.put(pan_seg)
+
+        # finish and close forward matching process
+        queue.put('finish')
+        rle_stack = matcher_out.recv()[0]
+        matcher_proc.join()
+
+        print(f'Propagating labels backward through the stack...')
+        axis_len = volume.shape[axis]
+        for index,rle_seg in tqdm(backward_matching(rle_stack, matchers, axis_len), total=axis_len):
+            update_trackers(rle_seg, index, trackers, axis, stack)
+
+        finish_tracking(trackers)
+        for tracker in trackers:
+            filters.remove_small_objects(tracker, min_size=self.min_size)
+            filters.remove_pancakes(tracker, min_span=self.min_extent)
+
+        self.engine.reset()
+
+        return stack, trackers
