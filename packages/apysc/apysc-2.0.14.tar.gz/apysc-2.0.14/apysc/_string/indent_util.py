02154b40e50320f4858410d8ecf1c52ba608bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:28 +0000

    Add package apysc/2.0.14/apysc-2.0.14.tar.gz

---
+++ packages/apysc/apysc-2.0.14.tar.gz/apysc-2.0.14/apysc/_string/indent_util.py
@@ -0,0 +1,58 @@
+"""Common indentation related utility implementations.
+
+Mainly following interfaces are defined:
+
+- make_spaces_for_html: Make spaces that multiply 2 to
+    a specified indentation number.
+- append_spaces_to_expression: Append spaces to a expression string.
+"""
+
+
+from typing import List
+
+
+def make_spaces_for_html(*, indent_num: int) -> str:
+    """
+    Make spaces that multiply 2 to a specified indentation number.
+
+    Parameters
+    ----------
+    indent_num : int
+        Indentation number.
+
+    Returns
+    -------
+    spaces : str
+        Result spaces string.
+    """
+    from apysc._validation import number_validation
+    number_validation.validate_integer(integer=indent_num)
+    number_validation.validate_num_is_gte_zero(num=indent_num)
+    spaces: str = ' ' * (indent_num * 2)
+    return spaces
+
+
+def append_spaces_to_expression(*, expression: str, indent_num: int) -> str:
+    """
+    Append spaces to a js expression string.
+
+    Parameters
+    ----------
+    expression : str
+        JavaScript expression string to add spaces.
+    indent_num : int
+        Indentation number. If 1 is specified, spaces become 2.
+
+    Returns
+    -------
+    expression : str
+        Expression string after adding spaces.
+    """
+    if indent_num == 0:
+        return expression
+    lines: List[str] = expression.splitlines()
+    spaces: str = make_spaces_for_html(indent_num=indent_num)
+    for i, line in enumerate(lines):
+        lines[i] = f'{spaces}{line}'
+    expression = '\n'.join(lines)
+    return expression
