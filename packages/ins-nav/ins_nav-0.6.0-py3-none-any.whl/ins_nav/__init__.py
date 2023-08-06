5b14ffd6444b2eb19e9bfb43704fda1ee8ed7df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:10 +0000

    Add package ins-nav/0.6.0/ins_nav-0.6.0-py3-none-any.whl

---
+++ packages/ins-nav/ins_nav-0.6.0-py3-none-any.whl/ins_nav/__init__.py
@@ -0,0 +1,12 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2016 Kevin Walchko
+# see LICENSE for full details
+##############################################
+
+
+from importlib.metadata import version # type: ignore
+
+__author__ = 'Kevin J. Walchko'
+__license__ = 'MIT'
+__version__ = version("ins_nav")
