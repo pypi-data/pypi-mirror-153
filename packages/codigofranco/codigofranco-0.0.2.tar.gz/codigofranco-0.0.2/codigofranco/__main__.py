38c9572935dd178549c3234988f09ab151e78dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:16 +0000

    Add package codigofranco/0.0.2/codigofranco-0.0.2.tar.gz

---
+++ packages/codigofranco/codigofranco-0.0.2.tar.gz/codigofranco-0.0.2/codigofranco/__main__.py
@@ -0,0 +1,24 @@
+import logging  #Muestra mensajes en consola. Por default warning o superior
+
+from codigofranco.workshops import unreleased   #Ejecutar funciones desde una librería
+
+"""
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30
+ERROR -> 40
+CRITICAL -> 50
+"""
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info(unreleased())
+
+#Ejecuta todo lo del bloque si y solo si este archivo se ejecuta como principal
+if __name__ == '__main__':
+    logging.debug(">>> Estamos comenzando la ejecución del paquete")
+    #Es una buena práctica reemplazar todos los print por logging al crear una librería
+    main()
+    #logging.debug(help(unreleased)) #Muestra el docstring
+    
+    logging.debug(">>> Estamos finalizando la ejecución del paquete")
