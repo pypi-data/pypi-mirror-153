fe6533d3d1fe44ab1fc7480b9bc48a5f6a3cce0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:41 +0000

    Add package pycivet/0.0.8.post1/pycivet-0.0.8.post1-py3-none-any.whl

---
+++ packages/pycivet/pycivet-0.0.8.post1-py3-none-any.whl/civet/obj.py
@@ -0,0 +1,28 @@
+"""
+Defines types related to surface meshes (`.obj` file format).
+"""
+from civet.xfm import TransformableMixin
+from civet.minc import GenericMask
+from typing import TypeVar, Generic
+from dataclasses import dataclass
+
+_S = TypeVar('_S', bound='GenericSurface')
+_M = TypeVar('_M', bound=GenericMask)
+
+
+@dataclass(frozen=True)
+class GenericSurface(TransformableMixin[_S], Generic[_S]):
+    preferred_suffix = '.obj'
+    transform_program = 'transform_objects'
+
+    def surface_mask2(self, in_volume: _M) -> _M:
+        def command(output):
+            return 'surface_mask2', in_volume, self, output
+        return in_volume.create_command(command)
+
+
+class Surface(GenericSurface['Surface']):
+    """
+    Represents a polygonal mesh of a brain surface in `.obj` file format.
+    """
+    pass
