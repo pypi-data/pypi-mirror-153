766a0e03c743f9b81e223d162032042590366191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:03 +0000

    Add package myloguru-deskent/0.0.7/myloguru-deskent-0.0.7.tar.gz

---
+++ packages/myloguru-deskent/myloguru-deskent-0.0.7.tar.gz/myloguru-deskent-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myloguru-deskent',
+    version='0.0.7',
+    author='Deskent',
+    author_email='battenetciz@gmail.com',
+    description='My loguru config',
+    install_requires=[
+        'loguru==0.5.3',
+        'pytest==7.1.2 ',
+    ],
+    scripts=['src/myloguru/my_loguru.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/Deskent/my_loguru",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
+)
