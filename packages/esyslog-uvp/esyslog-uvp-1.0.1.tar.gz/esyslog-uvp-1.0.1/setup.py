edac22674a0895a857472daf2e46f10ff5ff0280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:27 +0000

    Add package esyslog-uvp/1.0.1/esyslog-uvp-1.0.1.tar.gz

---
+++ packages/esyslog-uvp/esyslog-uvp-1.0.1.tar.gz/esyslog-uvp-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+packages = []
+
+setup(
+    name='esyslog-uvp',
+    version='1.0.1',
+    author='t.feng',
+    author_email='fengtao40@huawei.com',
+    packages=find_packages()
+)
