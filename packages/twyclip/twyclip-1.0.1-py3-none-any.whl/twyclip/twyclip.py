73efdd01dee8d87c3016154ecea04ca763030eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:23 +0000

    Add package twyclip/1.0.1/twyclip-1.0.1-py3-none-any.whl

---
+++ packages/twyclip/twyclip-1.0.1-py3-none-any.whl/twyclip/twyclip.py
@@ -0,0 +1,13 @@
+import requests
+
+def clipData(clip_url):
+	clip_response = requests.get(f"https://twiclips.com/twitch-download/clip?clip_url={clip_url}")
+	clip_json = clip_response.json()
+	return {
+		'author': clip_json['data']['info']['author'],
+		'category': clip_json['data']['info']['category'],
+		'clip_author': clip_json['data']['info']['clip_author'],
+		'date': clip_json['data']['info']['date'],
+		'views': clip_json['data']['info']['views'],
+		'download_link': clip_json['data']['info']['play_url']
+	}
