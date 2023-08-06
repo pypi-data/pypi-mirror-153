60cc17997650e4030df41780aa69a56fdd6044bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package PyHoTools/1.0/PyHoTools-1.0.tar.gz

---
+++ packages/PyHoTools/PyHoTools-1.0.tar.gz/PyHoTools-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PyHoTools',
+    version='1.0',
+    license='MIT',
+    author="Corruptus Memus",
+    author_email='funkoallman@gmail.com',
+    packages=find_packages(r'C:\Users\felix\Desktop\Projects\HkLib'),
+    package_dir={'': r'C:\Users\felix\Desktop\Projects\HkLib'},
+    url='https://github.com/HiMemX/PyHoTools',
+    keywords='',
+    install_requires=[],
+
+)
