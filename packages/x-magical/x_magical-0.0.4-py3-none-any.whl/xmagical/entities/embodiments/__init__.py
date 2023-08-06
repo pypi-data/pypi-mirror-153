bb476580e45b26e49834bcfa1d55dda2b1038923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:59 +0000

    Add package x-magical/0.0.4/x_magical-0.0.4-py3-none-any.whl

---
+++ packages/x-magical/x_magical-0.0.4-py3-none-any.whl/xmagical/entities/embodiments/__init__.py
@@ -0,0 +1,5 @@
+from .base import Embodiment, NonHolonomicEmbodiment
+from .gripper import NonHolonomicGripperEmbodiment
+from .poly import PolygonEmbodiment
+from .star import StarEmbodiment
+from .stick import LongstickEmbodiment, MediumstickEmbodiment, ShortstickEmbodiment
