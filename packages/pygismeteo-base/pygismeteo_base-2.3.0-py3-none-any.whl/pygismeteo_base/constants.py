26d130afdcb8706a9c75e0e1a2c58ef26c49ed42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:30:04 +0000

    Add package pygismeteo-base/2.3.0/pygismeteo_base-2.3.0-py3-none-any.whl

---
+++ packages/pygismeteo-base/pygismeteo_base-2.3.0-py3-none-any.whl/pygismeteo_base/constants.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+DEFAULT_TOKEN = "5b18f4510bd152.48552413"
---
+++ packages/pygismeteo-base/pygismeteo_base-2.3.0-py3-none-any.whl/pygismeteo_base/models/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pygismeteo_base.models import (
+    current,
+    search_by_coordinates,
+    search_by_ip,
+    search_by_query,
+    step3or6,
+    step24,
+)
+
+__all__ = (
+    "current",
+    "search_by_coordinates",
+    "search_by_ip",
+    "search_by_query",
+    "step3or6",
+    "step24",
+)
