15b5f1b0d35c2b213e727262b3af25e83c353d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package pyformlang/1.0.0/pyformlang-1.0.0-py3-none-any.whl

---
+++ packages/pyformlang/pyformlang-1.0.0-py3-none-any.whl/pyformlang/cfg/cfg_object.py
@@ -0,0 +1,27 @@
+""" An object in a CFG (Variable and Terminal)"""
+
+from typing import Any
+
+
+class CFGObject:  # pylint: disable=too-few-public-methods
+    """ An object in a CFG
+
+    Parameters
+    -----------
+    value : any
+        The value of the object
+    """
+
+    __slots__ = ["_value", "_hash"]
+
+    def __init__(self, value: Any):
+        self._value = value
+        self._hash = None
+
+    @property
+    def value(self) -> Any:
+        """Gets the value of the object"""
+        return self._value
+
+    def to_text(self) -> str:
+        raise NotImplementedError
