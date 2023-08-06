e6459c18dbb3b161b02c75305cca0c07c6699cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:02 +0000

    Add package pytest-object-getter/0.1.0/pytest_object_getter-0.1.0.tar.gz

---
+++ packages/pytest-object-getter/pytest_object_getter-0.1.0.tar.gz/pytest_object_getter-0.1.0/src/pytest_object_getter/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.0'
+
+__all__ = [
+    'get_object',
+    'object_getter_class',
+    'generic_object_getter_class',
+    'attribute_getter',
+]
+
+
+from .fixtures import (
+    attribute_getter,
+    generic_object_getter_class,
+    get_object,
+    object_getter_class,
+)
