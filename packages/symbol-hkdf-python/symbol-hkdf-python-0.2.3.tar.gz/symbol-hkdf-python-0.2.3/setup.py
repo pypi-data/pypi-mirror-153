cb5cc8c3de3cd762f4852fd5c5eb61d38aff380f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:25 +0000

    Add package symbol-hkdf-python/0.2.3/symbol-hkdf-python-0.2.3.tar.gz

---
+++ packages/symbol-hkdf-python/symbol-hkdf-python-0.2.3.tar.gz/symbol-hkdf-python-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+requires = ["cryptography >= 37.0.2"]
+
+
+setup(
+    name='symbol-hkdf-python',
+    version='0.2.3',
+    description='encrypt and decrypt tool for symbol',
+    url='https://github.com/monakaJP/symbol-hkdf-python',
+    author='monakaJP',
+    author_email='notify.monakaxym@gmail.com',
+    license='apache2.0',
+    keywords='https://github.com/monakaJP/symbol-hkdf-python',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
