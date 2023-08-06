d4299dbbad4506f87a3872b9e892aba27b1fd571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:16 +0000

    Add package TAssist/0.0.1/TAssist-0.0.1.tar.gz

---
+++ packages/TAssist/TAssist-0.0.1.tar.gz/TAssist-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'An unnoficial python wrapper for the TA Public API (2.0) by Pegasis'
+
+# Setting up
+setup(
+    name="TAssist",
+    version=VERSION,
+    author="BearGaming123 (Supreeth Govindaraju)",
+    author_email="<supreethG@gmx.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib', 'requests'],
+    keywords=['python', 'Teach Assist', 'YRDSB'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
