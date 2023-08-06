82d4f6b44862c51c662b4c86b101af44a7821855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:33 +0000

    Add package paketmutfak/0.0.9/paketmutfak-0.0.9-py3-none-any.whl

---
+++ packages/paketmutfak/paketmutfak-0.0.9-py3-none-any.whl/tools/functions.py
@@ -0,0 +1,20 @@
+def test_func(x):
+    return x + 1
+
+
+def sum_two_number(x, y):
+    """
+    :param x: first number
+    :param y: second number
+    :return: sum of two numbers
+    """
+    return x + y
+
+
+def add_string(x, y):
+    """
+    :param x: first number
+    :param y: second number
+    :return: sum of two numbers
+    """
+    return x + y
