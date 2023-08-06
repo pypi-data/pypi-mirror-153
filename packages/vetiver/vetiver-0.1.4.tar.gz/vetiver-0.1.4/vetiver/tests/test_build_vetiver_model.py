a7eedf1f6777489cd3bb1c759a91eee44004be2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:05 +0000

    Add package vetiver/0.1.4/vetiver-0.1.4.tar.gz

---
+++ packages/vetiver/vetiver-0.1.4.tar.gz/vetiver-0.1.4/vetiver/tests/test_build_vetiver_model.py
@@ -0,0 +1,72 @@
+import pytest
+
+from vetiver.vetiver_model import VetiverModel
+from vetiver.mock import get_mock_data, get_mock_model
+
+import pandas as pd
+from numpy import int64
+
+# Load data, model
+X_df, y = get_mock_data()
+X_array = pd.DataFrame(X_df).to_numpy()
+model = get_mock_model().fit(X_df, y)
+
+
+def test_vetiver_model_array_ptype():
+    # build VetiverModel, no ptype
+    vt1 = VetiverModel(
+        model=model,
+        ptype_data=X_array,
+        model_name="iris",
+        versioned=None,
+        description=None,
+        metadata=None,
+    )
+
+    assert vt1.model == model
+    assert list(vt1.ptype.__fields__.values())[0].type_ == int
+
+
+def test_vetiver_model_df_ptype():
+    # build VetiverModel, df ptype_data
+    vt2 = VetiverModel(
+        model=model,
+        ptype_data=X_df,
+        model_name="iris",
+        versioned=None,
+        description=None,
+        metadata=None,
+    )
+
+    assert vt2.model == model
+    assert list(vt2.ptype.__fields__.values())[0].type_ == int
+
+
+def test_vetiver_model_dict_ptype():
+    dict_data = {"B": 0, "C": 0, "D": 0}
+    vt3 = VetiverModel(
+        model=model,
+        ptype_data=dict_data,
+        model_name="iris",
+        versioned=None,
+        description=None,
+        metadata=None,
+    )
+
+    assert vt3.model == model
+    assert list(vt3.ptype.__fields__.values())[0].type_ == int
+
+
+def test_vetiver_model_no_ptype():
+    # build VetiverModel, no ptype
+    vt4 = VetiverModel(
+        model=model,
+        ptype_data=None,
+        model_name="iris",
+        versioned=None,
+        description=None,
+        metadata=None,
+    )
+
+    assert vt4.model == model
+    assert vt4.ptype == None
---
+++ packages/vetiver/vetiver-0.1.4.tar.gz/vetiver-0.1.4/vetiver/vetiver_model.py
@@ -0,0 +1,59 @@
+from vetiver.handlers._interface import create_translator
+
+
+class NoModelAvailableError(Exception):
+    """
+    Throw an error if we don't find a method
+    available to prepare a `model`
+    """
+
+    def __init__(
+        self,
+        message="There is no model available",
+    ):
+        self.message = message
+        super().__init__(self.message)
+
+
+class VetiverModel:
+    """Create VetiverModel class for serving.
+
+    Attributes
+    ----------
+    model :
+        A trained model, such as an sklearn or spacy model
+    name : string
+        Model name or ID
+    ptype_data : pd.DataFrame, np.array
+        Sample of data model should expect when it is being served
+    versioned :
+        Should the model be served when created?
+    description : str
+        A detailed description of the model. if omitted, a brief description will be generated
+    metadata : dict
+        Other details to be saved and accessed for serving
+    """
+
+    def __init__(
+        self,
+        model,
+        model_name: str,
+        ptype_data=None,
+        versioned=None,
+        description: str = None,
+        metadata: dict = None,
+        **kwargs
+    ):
+        translator = create_translator(model, ptype_data)
+
+        self.model = model
+        self.ptype = translator.ptype()
+        self.model_name = model_name
+        self.description = description if description else translator.create_description()
+        self.versioned = versioned
+        self.metadata = translator.vetiver_create_meta(
+            metadata, required_pkgs=["vetiver"]
+        )
+        self.handler_predict = translator.handler_predict
+
+        
