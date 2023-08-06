9a3b7f50103d9ad8f45d6d5bff84664775c9a817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package puft/0.4.0rc3/puft-0.4.0rc3.tar.gz

---
+++ packages/puft/puft-0.4.0rc3.tar.gz/puft-0.4.0rc3/puft/core/sock/sock.py
@@ -0,0 +1,18 @@
+from flask_socketio import SocketIO, Namespace
+
+from .socket import Socket
+
+
+class Sock(Namespace):
+    def __init__(self, namespace=None):
+        super().__init__(namespace)
+        self.socket: Socket = Socket.instance()
+
+    def on_connect(self):
+        self.socket.send('Connected')
+
+    def on_disconnect(self):
+        self.socket.send('Disconnected')
+
+    def on_message(self, message):
+        self.socket.send(f'Hello {message}!')
