e04798fa22fd4714e86b6387278aae40dc81293f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:50 +0000

    Add package polygon/1.0.7/polygon-1.0.7-py2.py3-none-any.whl

---
+++ packages/polygon/polygon-1.0.7-py2.py3-none-any.whl/polygon/__init__.py
@@ -0,0 +1,16 @@
+# ========================================================= #
+from .stocks import StocksClient
+from .streaming import StreamClient, AsyncStreamClient
+from .forex import ForexClient
+from .crypto import CryptoClient
+from .reference_apis import ReferenceClient
+from .options import (OptionsClient, build_option_symbol, parse_option_symbol, OptionSymbol,
+                      build_option_symbol_for_tda, parse_option_symbol_from_tda, convert_from_polygon_to_tda_format,
+                      convert_from_tda_to_polygon_format)
+from .base_client import (BaseClient, BaseAsyncClient)
+# ========================================================= #
+
+
+__version__ = '1.0.7'
+
+# ========================================================= #
