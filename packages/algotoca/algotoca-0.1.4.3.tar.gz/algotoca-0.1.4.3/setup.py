92b7a7ad4766f827672845cb1f273f36b0b292fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:04 +0000

    Add package algotoca/0.1.4.3/algotoca-0.1.4.3.tar.gz

---
+++ packages/algotoca/algotoca-0.1.4.3.tar.gz/algotoca-0.1.4.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['algotoca', 'algotoca.grafos', 'algotoca.grafos.coloracao']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cairocffi>=1.3.0,<2.0.0', 'igraph>=0.9.9,<0.10.0']
+
+setup_kwargs = {
+    'name': 'algotoca',
+    'version': '0.1.4.3',
+    'description': '',
+    'long_description': None,
+    'author': 'WLAraujo',
+    'author_email': 'lima.wesleyaraujo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
