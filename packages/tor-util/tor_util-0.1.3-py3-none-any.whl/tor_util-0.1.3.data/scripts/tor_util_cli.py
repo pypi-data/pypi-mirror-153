fcd5e6b40a3e42b0baa4aa6f93ffc2fd8994ecf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package tor-util/0.1.3/tor_util-0.1.3-py3-none-any.whl

---
+++ packages/tor-util/tor_util-0.1.3-py3-none-any.whl/tor_util-0.1.3.data/scripts/tor_util_cli.py
@@ -0,0 +1,181 @@
+#!python
+# Exit codes 0: success, 1: program failure, 2: user failure, 4: help/info
+tor_util_desc='''
+Utility for controling TOR via the API. Command line version.
+
+Does Two Things:
+1. Send commands to the API over the network
+2. Generates hashed passwords for use in the torrc file
+
+API  Host,  Port,  and Password as specified as a JSON file in 
+~/.config/tor_util/config, or can be overridden with switches
+
+TOR API Commands:
+	new_ip		Sends the NEWNYM command which gets a new IP. This
+generates new tunnels, and with it, a new exit node that has a new IP.
+
+	flush_dns		Flushes DNS cache on TOR daemon.
+
+	dormant_mode \__	Turns Dormant Mode on/off. Newish feature to TOR
+	active_mode  /
+    
+    daemon_status		Checks operational status of the TOR Daemon.
+
+Local Utilities(Commands):
+	gen_passwd_hash		Generates a password hash for use in torrc.
+    
+	touch_config		Quit after generating config. useful for first
+run
+'''
+tor_util_desc = tor_util_desc.strip()
+
+from tor_util import common as lib
+
+import argparse
+import sys
+from getpass import getpass
+
+class colors:
+    '''pretty terminal colors'''
+    reset='\033[0m'
+    bold='\033[01m'
+    red='\033[31m'
+    cyan='\033[36m'
+    yellow='\033[93m'
+
+def message(message):
+    print("tor_util_cli: " + message)
+
+def softerr(message):
+    '''combination of submsg and error'''
+    print(colors.red + colors.bold + "¡ERROR!: " + colors.reset + message, file=sys.stderr)
+    return
+
+def exit_with_error(exit,message):
+    print("tor_util_cli:" + colors.red + colors.bold + " ¡ERROR!: " + colors.reset + message, file=sys.stderr)
+    sys.exit(exit)
+
+def warn(message):
+    print("tor_util_cli:" + colors.yellow + colors.bold + "¡WARN!: " + colors.reset + message, file=sys.stderr)
+    return
+    
+def print_password_hash(in_password):
+    '''returns a hashed password string for torrc. takes one parameter, plaintext password'''
+    in_password = str(in_password)
+    hashed_pass = lib.generate_tor_hash(in_password)
+    output_message = '''
+# Paste the following line in your torrc file
+# Delete any previous HashedControlPassword lines
+'''
+    output_message += "HashedControlPassword " + hashed_pass + "\n"
+    print(output_message)
+    sys.exit()
+
+def main():
+    parser = argparse.ArgumentParser(description=tor_util_desc,epilog="\n\n",add_help=False,formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("command", nargs="?"     , help="See above for description of commands")
+    parser.add_argument("-?", "--help"           , help="Show This Help Message", action="help")
+
+    parser.add_argument("-t","--password-prompt" , help="Prompt for password. overrides settings",action="store_true")
+    parser.add_argument("-w","--password"        , help="Password. overrides settings",type=str)
+    parser.add_argument("-V","--version"         , help="Print Version and Exit", action="store_true")
+    
+    parser_network = parser.add_argument_group("Network","Network Settings for send commands")
+    parser_network.add_argument("-h","--host"    , help="Address/hostname of TOR daemon")
+    parser_network.add_argument("-p","--port"    , help="Port of TOR daemon",type=int)
+    args = parser.parse_args()
+    
+    # Check version. Don't touch anything, just exit when done
+    if args.version == True:
+        message("VERSION: " + lib.prog_meta["name"] + "\t" + lib.prog_meta["version"])
+        sys.exit(4)
+    
+    # Load Config
+    try:
+        config = lib.proc_config_start()
+    except:
+        warn("Could not load config, using defaults...")
+        config = lib.default_config
+    # If we are just generating the config, leave it along
+    if args.command == "touch_config":
+       sys.exit(0)
+
+    ## proc options:
+    #Check password
+    if args.password_prompt == True:
+       config['password'] = getpass()
+    elif args.password != None:
+       config['password'] = args.password
+    #host and port
+    if args.host != None:
+        config['tor_host'] = args.host
+    if args.port != None:
+        config['tor_port'] = args.port
+
+    ## Sanity checking
+    #check to make sure port is valid:
+    if lib.check_port(config['tor_port']) != True:
+        exit_with_error(2,"Invalid port: " + str(config['tor_port']))
+
+    # commands
+    command = ""
+    if args.command == None:
+        exit_with_error(2,"No Command Specified, see --help")   
+    elif args.command == "new_ip":
+        message("Sending New IP Request...")
+        command = "SIGNAL NEWNYM"
+    elif args.command == "flush_dns":
+        message("Clearing DNS Cache...")
+        command = "SIGNAL CLEARDNSCACHE"
+    elif args.command == "dormant_mode":
+        message("Putting TOR Daemon in Dormant Mode...")
+        command = "SIGNAL DORMANT"
+    elif args.command == "active_mode":
+        message("Restoring TOR Daemon to Active Mode...")
+        command = "SIGNAL ACTIVE"
+    elif args.command == "daemon_status":
+        message("Checking Daemon Status...")
+    elif args.command == "gen_passwd_hash":
+        if  args.password == None:
+            hash_pass  = getpass()
+        else:
+            hash_pass  = args.password
+        
+        print_password_hash(hash_pass)
+
+    else:
+        exit_with_error(2,"Command " + args.command + " is not supported.")
+
+    if args.command == "daemon_status":
+        result = lib.tor_daemon_status(config['tor_host'],config['tor_port'],config['password'])
+    else:
+        result = lib.send_tor_command(command,config['tor_host'],config['tor_port'],config['password'])
+
+    errors_count = 0
+    # characters of left column for output, output data
+    col = 35
+    for line in result:
+        error_code = int(line[0])
+        line = line[-1]
+        if error_code != 250:
+            errors_count += 1
+            softerr(line + "\n")
+        else:
+            if "=" in line:
+                split_line = line.split(" ")
+                for line_item in split_line:
+                    line_item = line_item.split("=")
+                    if len(line_item) == 1:
+                        continue
+                    lin_len = len(line_item[0])
+                    print( line_item[0] + ":\t".expandtabs(col - lin_len) + line_item[1])
+            else:
+                print(line)
+
+    if errors_count == 0:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+    
+if __name__ == "__main__":
+    main()
---
+++ packages/tor-util/tor_util-0.1.3-py3-none-any.whl/tor_util/__init__.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+'''
+This is a utility for controling TOR via the API
+
+Common Library
+'''
+
+import os
+import sys
+import hashlib
+import binascii
+import json
+import stem
+import stem.connection
+
+class common:
+    conf_file = os.getenv("HOME") + "/.config/tor_util/config"
+    if "win" in sys.platform:
+        conf_file = os.getenv("APPDATA") + "/tor_util/config"
+    prog_meta = {
+        'name'    : "TOR API Utility",
+        'version' : "0.1.3"
+    }
+    default_config = {
+        'tor_host' : "127.0.0.1",
+        'tor_port' : 9051,
+        'password' : ""
+    }
+
+    def read_config(file_name):
+        '''Loads JSON config returns dict{} with keys'''
+    
+        file_obj = open(file_name,"r")
+        contents = file_obj.read()
+        file_obj.close()
+    
+        contents = json.loads(contents)
+        return contents
+
+    def write_config(file_name,config_obj):
+        '''write config to JSON file'''
+    
+        contents  = json.dumps(config_obj,indent=4)
+        contents += "\n"
+    
+        file_obj = open(file_name, "w")
+        file_obj.write(contents)
+        file_obj.close()
+
+    def proc_config_start():
+        '''checks for config, writes defaults if not present, returns dict with keys''' 
+        conf_dir = os.path.dirname(common.conf_file)
+    
+        # check if config directory exists. If not make it, and then return defaults:
+        if os.path.isdir(conf_dir)    == False and os.path.exists(conf_dir) == True:
+            raise "BadConfDir"
+        elif os.path.exists(conf_dir) == False:
+            os.mkdir(conf_dir)
+            common.write_config(common.conf_file,common.default_config)
+            os.chmod(common.conf_file, 0o600)
+            return common.default_config
+        
+        # check if config file exists, if not make it then return defaults:
+        if os.path.isfile(common.conf_file)   == False and os.path.exists(common.conf_file) == True:
+            raise "BadConfFile"
+        elif os.path.exists(common.conf_file) == False:
+            common.write_config(common.conf_file,common.default_config)
+            os.chmod(common.conf_file, 0o600)
+            return common.default_config
+        
+        # load config
+        loaded_config = common.read_config(common.conf_file)
+        # check to make sure all items are present. If not, load them
+        for item in common.default_config.keys():
+            if item not in loaded_config.keys():
+                loaded_config[item] = common.default_config[item]
+
+        return loaded_config
+
+    def check_port(port):
+        '''Check if port is a valid TCP Port number. Takes one parameter, the port, returns bool(True/False)'''
+        # ports are intergers
+        try:
+            port = int(port)
+        except:
+            return False
+        #between 1 and 65535, or 2^16 - 1
+        if 1 <= port <= 65535:
+            return True
+        else:
+            return False
+    
+    def send_tor_command(command,host,port,password=""):
+        '''Send a command tor TOR daremon. Needs command, host, port, and optionally password. returns tupple with error code and message'''
+        '''error code: 0 is success, everything else is fail. Message is for updating status'''
+        # build the command.
+        auth_string = "AUTHENTICATE " + '\"' + password + '\"' + "\n"
+        output = ""
+
+        # Set up the control object
+        control_obj = stem.socket.ControlPort(address=host, port=port, connect=False)
+        try:
+            control_obj.connect()
+        except stem.SocketError:
+            output = [("1", "Could Not Connect!")]
+            return output
+        try:
+            stem.connection.authenticate(control_obj,password)
+        except stem.connection.IncorrectPassword:
+            output = [("1", "Incorrect Password")]
+            return output
+        
+        try:
+            control_obj.send(command)
+        except:
+            output = [("1","Could not send command?")]
+            return output
+        
+        message_obj = control_obj.recv()
+        output      = message_obj.content()
+    
+        control_obj.close()
+    
+        return output
+        
+    def tor_daemon_status(host,port,password=""):
+        '''Run several commands to determine status of router. returns list object'''
+        # build the command.
+        auth_string = "AUTHENTICATE " + '\"' + password + '\"' + "\n"
+        output = ""
+
+        # Set up the control object
+        control_obj = stem.socket.ControlPort(address=host, port=port, connect=False)
+        try:
+            control_obj.connect()
+        except stem.SocketError:
+            output = [("1", "Could Not Connect!")]
+            return output
+        try:
+            stem.connection.authenticate(control_obj,password)
+        except stem.connection.IncorrectPassword:
+            output = [("1", "Incorrect Password")]
+            return output
+        
+        command_list = { "Version":"GETINFO version", "Circut Status:":"GETINFO status/circuit-established", "Directory Status:":"GETINFO status/enough-dir-info", "Descriptor Status:":"GETINFO status/good-server-descriptor", "Bootstrap:":"GETINFO status/bootstrap-phase" }
+
+        # Commands
+        output = []
+        for item in command_list:
+            command = command_list[item]
+        
+            try:
+                control_obj.send(command)
+            except:
+                output.append( ("1","Could not send " + command + " command") )
+                continue
+            
+            message_obj = control_obj.recv()
+            raw_output = message_obj.content()
+            if raw_output[-1][0] == "250" and raw_output[-1][-1] == "OK":
+                del(raw_output[-1])
+            for line in raw_output:
+                output.append(line)
+    
+        control_obj.close()
+        return output
+        
+    def generate_tor_hash(password):
+        """https://github.com/ray0be/totororequests/blob/master/totororequests/totoro.py
+        Copyright (c) 2020 Victor Paynat-Sautivet <contact@ray0.be>"""
+        secret = bytes(password, 'ascii')
+        indicator = bytes(chr(96), 'ascii')
+        salt = b"".join([os.urandom(8), indicator])
+
+        c = 96
+        EXPBIAS = 6
+        count = (16+(c & 15)) << ((c >> 4) + EXPBIAS)
+
+        d = hashlib.sha1()
+        tmp = salt[:8]+secret
+
+        slen = len(tmp)
+        while count:
+            if count > slen:
+                d.update(tmp)
+                count -= slen
+            else:
+                d.update(tmp[:count])
+                count = 0
+
+        salt = binascii.b2a_hex(salt[:8]).upper().decode('ascii')
+        torhash = d.hexdigest().upper()
+
+        return '16:{}{}{}'.format(salt, '60', torhash)
