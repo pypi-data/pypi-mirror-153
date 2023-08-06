e60979b184b9c07f01f335ecb4e054a4327b3aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:37 +0000

    Add package poetry-add-requirements-txt/0.1.2/poetry-add-requirements.txt-0.1.2.tar.gz

---
+++ packages/poetry-add-requirements-txt/poetry-add-requirements.txt-0.1.2.tar.gz/poetry-add-requirements.txt-0.1.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_add_requirements_txt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['charset-normalizer>=2.0.12,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['poeareq = poetry_add_requirements_txt.cli:main',
+                     'poetry-add-requirements.txt = '
+                     'poetry_add_requirements_txt.cli:main']}
+
+setup_kwargs = {
+    'name': 'poetry-add-requirements.txt',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tddschn/poetry-add-requirements.txt',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
