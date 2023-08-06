c97713179128b0f2f262bc7d3eea1b96308947f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:58 +0000

    Add package globus-sdk/3.9.0/globus_sdk-3.9.0-py3-none-any.whl

---
+++ packages/globus-sdk/globus_sdk-3.9.0-py3-none-any.whl/globus_sdk/services/gcs/data/__init__.py
@@ -0,0 +1,45 @@
+from .collection import (
+    CollectionDocument,
+    GuestCollectionDocument,
+    MappedCollectionDocument,
+)
+from .role import GCSRoleDocument
+from .storage_gateway import (
+    ActiveScaleStoragePolicies,
+    AzureBlobStoragePolicies,
+    BlackPearlStoragePolicies,
+    BoxStoragePolicies,
+    CephStoragePolicies,
+    GoogleCloudStoragePolicies,
+    GoogleDriveStoragePolicies,
+    HPSSStoragePolicies,
+    IrodsStoragePolicies,
+    OneDriveStoragePolicies,
+    POSIXStagingStoragePolicies,
+    POSIXStoragePolicies,
+    S3StoragePolicies,
+    StorageGatewayDocument,
+    StorageGatewayPolicies,
+)
+
+__all__ = (
+    "MappedCollectionDocument",
+    "GuestCollectionDocument",
+    "CollectionDocument",
+    "GCSRoleDocument",
+    "StorageGatewayDocument",
+    "StorageGatewayPolicies",
+    "POSIXStoragePolicies",
+    "POSIXStagingStoragePolicies",
+    "BlackPearlStoragePolicies",
+    "BoxStoragePolicies",
+    "CephStoragePolicies",
+    "GoogleDriveStoragePolicies",
+    "GoogleCloudStoragePolicies",
+    "OneDriveStoragePolicies",
+    "AzureBlobStoragePolicies",
+    "S3StoragePolicies",
+    "ActiveScaleStoragePolicies",
+    "IrodsStoragePolicies",
+    "HPSSStoragePolicies",
+)
