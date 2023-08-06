7e01d8def097268a7574346cba24f3aace676ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:29 +0000

    Add package ramdisk-root-config-mbsc/1.0/ramdisk-root-config-mbsc-1.0.tar.gz

---
+++ packages/ramdisk-root-config-mbsc/ramdisk-root-config-mbsc-1.0.tar.gz/ramdisk-root-config-mbsc-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+packages = []
+
+setup(
+    name='ramdisk-root-config-mbsc',
+    version='1.0',
+    author='t.feng',
+    author_email='fengtao40@huawei.com',
+    packages=find_packages()
+)
