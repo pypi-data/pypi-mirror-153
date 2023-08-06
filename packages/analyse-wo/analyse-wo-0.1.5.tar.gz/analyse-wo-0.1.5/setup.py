8ac30f7e3ff6c015375b7aa48a0a233df593ef45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:48 +0000

    Add package analyse-wo/0.1.5/analyse-wo-0.1.5.tar.gz

---
+++ packages/analyse-wo/analyse-wo-0.1.5.tar.gz/analyse-wo-0.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['analyse_wo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['getting-and-setting>=0.3.17,<0.4.0', 'pandas>=1.4.2,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['wocli = analyse_wo.main:main']}
+
+setup_kwargs = {
+    'name': 'analyse-wo',
+    'version': '0.1.5',
+    'description': "Analyse WO's in M3",
+    'long_description': None,
+    'author': 'Kim Timothy Engh',
+    'author_email': 'kimothy@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
