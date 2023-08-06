6b25b1acd0d65a29b7da33bcbd1948e73cb8836b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:05 +0000

    Add package fulmar-astro/0.1.9/fulmar_astro-0.1.9-py3-none-any.whl

---
+++ packages/fulmar-astro/fulmar_astro-0.1.9-py3-none-any.whl/fulmar/version.py
@@ -0,0 +1,5 @@
+# It is important to store the version number in a separate file
+# so that we can read it from setup.py without importing the package
+
+FULMAR_VERSION = "0.1.9"
+FULMAR_DATE = "26 May 2022"
