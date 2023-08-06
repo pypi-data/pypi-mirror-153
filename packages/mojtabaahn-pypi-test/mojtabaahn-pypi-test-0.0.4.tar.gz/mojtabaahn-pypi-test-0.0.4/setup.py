0e7714bd696367c4543d26ea9888795283970bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:28 +0000

    Add package mojtabaahn-pypi-test/0.0.4/mojtabaahn-pypi-test-0.0.4.tar.gz

---
+++ packages/mojtabaahn-pypi-test/mojtabaahn-pypi-test-0.0.4.tar.gz/mojtabaahn-pypi-test-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(
+    name="mojtabaahn-pypi-test",
+    version="0.0.4",
+    url="https://github.com/mojtabaahn/pypi-test",
+    author="Mojtabaa Habibain",
+    author_email="mojtabaa.hn@gmail.com",
+    description="Testing Pypi",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
