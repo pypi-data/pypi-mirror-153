af4218748017ccfdf91c7880252961f620c26dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:31 +0000

    Add package jef-ud-distributions/0.1/jef-ud-distributions-0.1.tar.gz

---
+++ packages/jef-ud-distributions/jef-ud-distributions-0.1.tar.gz/jef-ud-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jef-ud-distributions',
+      version='0.1',
+      description='Gaussian and Binominal distributions',
+      packages=['jef-ud-distributions'],
+      zip_safe=False)
