b98fe102ab078ee8810f21fff7da7381bd58de40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:20 +0000

    Add package fintualistic/0.1/fintualistic-0.1.tar.gz

---
+++ packages/fintualistic/fintualistic-0.1.tar.gz/fintualistic-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fintualistic',
+    version='0.1',
+    author="Fernando Suarez",
+    author_email='fsuarez1@uc.cl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/fsuarezb/fintualistic',
+    keywords='fintual fintualistic',
+    install_requires=[
+          'pandas', 'plotly'
+      ],
+
+)
