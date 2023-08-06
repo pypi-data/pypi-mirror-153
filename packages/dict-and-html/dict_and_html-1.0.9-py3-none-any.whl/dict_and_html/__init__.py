9fcddffbe59c5c0cc345f1c541b696fd3761aa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package dict-and-html/1.0.9/dict_and_html-1.0.9-py3-none-any.whl

---
+++ packages/dict-and-html/dict_and_html-1.0.9-py3-none-any.whl/dict_and_html/__init__.py
@@ -0,0 +1,10 @@
+from dict_and_html.d_and_h import (
+    DOCTYPE,
+    HTML_STYLE,
+    TABLE_STYLE,
+    dict_and_html)
+
+__all__ = ["DOCTYPE",
+           "HTML_STYLE",
+           "TABLE_STYLE",
+           "dict_and_html"]
