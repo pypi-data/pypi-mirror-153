dcd7b8156a4eab39a4f6e625c2751c734d6c24d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:47 +0000

    Add package systool/0.2.0/systool-0.2.0.tar.gz

---
+++ packages/systool/systool-0.2.0.tar.gz/systool-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='systool',
+    version='0.02.00',
+    license='MIT',
+    author="Bruna Calazans (bruna-calazans) & Pedro Chiachio (MoleDownTheHole)",
+    author_email='pcardoso@systra.com',
+    package_dir={'': 'main'},
+    include_package_data=True,
+    package_data={'utils': ['charts.py', 'maps.py', 'readWrite.py']},
+    url='https://github.com/MoleDownTheHole/SysTool',
+    keywords='systool',
+    install_requires=['pytest', 'pandas', 'numpy', 'matplotlib', 'matplotlib_scalebar', 'geopandas'],
+)
