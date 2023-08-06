93827fd725c8cee9123a04e81f6af190afbf844d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:17 +0000

    Add package parsehecssp/0.0.1/parsehecssp-0.0.1.tar.gz

---
+++ packages/parsehecssp/parsehecssp-0.0.1.tar.gz/parsehecssp-0.0.1/parsehecssp/volumeFrequency.py
@@ -0,0 +1,38 @@
+from ast import In
+from .features.header import Header
+from .features.inputdata import InputData
+from .features.nDayResults import NDayResult
+import os.path
+
+class ParseVolumeFrequency(object):
+    def __init__(self, rpt_filename) -> str:
+        self.analysis_parts = []
+
+        with open(rpt_filename, 'rt') as rpt_file:
+            next(rpt_file)
+            for line in rpt_file:
+
+                if line == '\n':
+                    self.analysis_parts.append(line)
+                elif Header.test(line):
+                    hh = Header()
+                    hh.import_rpt(line, rpt_file)
+                    self.analysis_parts.append(hh)
+                elif InputData.test(line):
+                    id = InputData()
+                    id.import_rpt(line, rpt_file)
+                    self.analysis_parts.append(id)
+                elif NDayResult.test(line):
+                    nd = NDayResult()
+                    nd.import_rpt(line, rpt_file, id.durations.ndays[-1])
+                    self.analysis_parts.append(nd)
+                else:
+                    if line != '============================================================================\n':
+                        #Unknown line
+                        self.analysis_parts.append(line)
+
+    def write(self, out_rpt_filename):
+
+        with open(out_rpt_filename, 'w') as outfile:
+            for line in self.analysis_parts:
+                outfile.write(str(line))
---
+++ packages/parsehecssp/parsehecssp-0.0.1.tar.gz/parsehecssp-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setuptools.setup(
+ name='parsehecssp',
+ version = '0.0.1',
+ author = 'Daniel Hamill',
+ author_email = 'daniel.hamill@hey.com',
+ description = 'Parse HEC-SSP report Files',
+ long_description=readme,
+ packages = ['parsehecssp', 'parsehecssp.features'],
+ license='MIT',
+#  package_dir={'':'parsehecssp'},
+ classifiers=[
+    "Development Status :: 4 - Beta",
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    "License :: OSI Approved :: MIT License",
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    "Topic :: Utilities",
+    'Topic :: Scientific/Engineering :: GIS',
+]
+ 
+)
