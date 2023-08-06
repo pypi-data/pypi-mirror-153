d8c8297416896e1663580a6d379bf615f5ded4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:14 +0000

    Add package colemen-utils/0.0.0/colemen_utils-0.0.0.tar.gz

---
+++ packages/colemen-utils/colemen_utils-0.0.0.tar.gz/colemen_utils-0.0.0/facades/sql_parse_facade.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+'''
+    A module of utility methods used for Parsing SQL code
+
+    ----------
+
+    Meta
+    ----------
+    `author`: Colemen Atwood
+    `created`: 06-03-2022 10:22:15
+    `memberOf`: sql
+    `version`: 1.0
+'''
+
+
+
+
+from utils.parse_sql import strip_sql_quotes,strip_comments,escape_quoted_chars,parse,sql_data_types
---
+++ packages/colemen-utils/colemen_utils-0.0.0.tar.gz/colemen_utils-0.0.0/setup.py
@@ -0,0 +1,87 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Colemen Utils'
+LONG_DESCRIPTION = 'Colemen Utils is a composite library of shit I find useful.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="colemen_utils",
+    version=VERSION,
+    author="Colemen Atwood",
+    author_email="<atwoodcolemen@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    py_modules=[
+        'colemen_utils',
+        'facades.dict_utils_facade',
+        'facades.directory_facade',
+        'facades.files_facade',
+        'facades.list_utils_facade',
+        'facades.rand_string_facade',
+        'facades.rand_utils_facade',
+        'facades.sql_convert_facade',
+        'facades.sql_facade',
+        'facades.sql_generate_facade',
+        'facades.sql_parse_facade',
+        'facades.string_facade',
+        'facades.types_facade',
+        'facades.dict_utils_facade',
+        'facades.directory_facade',
+        'facades.files_facade',
+        'facades.list_utils_facade',
+        'facades.rand_string_facade',
+        'facades.rand_utils_facade',
+        'facades.sql_convert_facade',
+        'facades.sql_facade',
+        'facades.sql_generate_facade',
+        'facades.sql_parse_facade',
+        'facades.string_facade',
+        'facades.types_facade',
+        'utils.object_utils',
+        'utils.parse_sql',
+        'utils.parse_utils',
+        'utils.files.dir',
+        'utils.string_conversion',
+        'utils.string_format',
+        'utils.files.dir_compression',
+        'utils.string_generation',
+        'utils.types',
+        'utils.files.dir_search',
+        'utils.files.exiftool',
+        'utils.files.file',
+        'utils.files.file_compression',
+        'utils.files.file_convert',
+        'utils.files.file_image',
+        'utils.files.file_read',
+        'utils.files.file_search',
+        'utils.files.file_write',
+        'utils.files.resources',
+    ],
+    # add any additional packages that
+    # need to be installed along with your package. Eg: 'caer'
+    install_requires=[
+        'secure_delete',
+        'ftputil',
+        'ffmpeg-python',
+        'pillow',
+        'iptcinfo3',
+        'patool',
+        'pyparsing',
+        'sqlparse',
+        'colorama',
+    ],
+
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
