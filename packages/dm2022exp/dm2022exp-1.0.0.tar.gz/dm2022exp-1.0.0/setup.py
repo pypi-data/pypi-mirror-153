611d1209c283190bc6650929a169b4f69fc90a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:19:31 +0000

    Add package dm2022exp/1.0.0/dm2022exp-1.0.0.tar.gz

---
+++ packages/dm2022exp/dm2022exp-1.0.0.tar.gz/dm2022exp-1.0.0/setup.py
@@ -0,0 +1,12 @@
+
+import setuptools
+
+
+setuptools.setup(
+  name="dm2022exp",
+  version="1.0.0",
+  author="HuChang",
+  author_email="changlehung@gmail.com",
+  description="experimental package of HZAU 2022 data mining course",
+  packages=setuptools.find_packages(),
+)
