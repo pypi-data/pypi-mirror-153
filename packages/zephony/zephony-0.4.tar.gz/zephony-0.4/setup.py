66467cbcbf7d0cd0618849d54b59b706a21ccbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:36 +0000

    Add package zephony/0.4/zephony-0.4.tar.gz

---
+++ packages/zephony/zephony-0.4.tar.gz/zephony-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='zephony',
+    packages=['zephony'],
+    description='Helpers for Python web development',
+    version='0.4',
+    url='https://github.com/Zephony/zephony-pypi',
+    author='Kevin Isaac',
+    author_email='kevin@zephony.com',
+    keywords=['zephony', 'helpers', 'web'],
+    install_requires=[
+        'voluptuous',
+        'twilio',
+    ],
+)
+
