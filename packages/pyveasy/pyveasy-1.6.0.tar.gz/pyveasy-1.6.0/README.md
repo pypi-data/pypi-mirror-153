e21478ccd7ee2907290f87093bcc8ddb8d620663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:54 +0000

    Add package pyveasy/1.6.0/pyveasy-1.6.0.tar.gz

---
+++ packages/pyveasy/pyveasy-1.6.0.tar.gz/pyveasy-1.6.0/README.md
@@ -0,0 +1,27 @@
+
+PyEasy is a project generator for bootstrapping basic Python to VSCode, ready to be shared on PyPI and installable via pip. It is easy to use and encourages the adoption of the best tools and practices of the Python ecosystem, helping you and your team to stay sane, happy and productive.
+
+## Installing
+
+```shell
+$ pip3 install pyveasy
+```
+
+## A Simple Example
+
+```shell
+$ pyveasy
+Nome do Projeto [python-project]: sample
+Criando projeto... [sample]
+Projeto: [sample] - Criado com sucesso...
+```
+
+## Requeriments 
+
+Add content on ~/.zshrc or ~/.bash_profile
+
+```shell
+# PYTHON
+export PYTHON_HOME=~/Library/Python/3.8/
+export PATH=$PATH:$PYTHON_HOME/bin
+```
---
+++ packages/pyveasy/pyveasy-1.6.0.tar.gz/pyveasy-1.6.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyveasy",
+    version="1.6.0",
+    author="Anderson Braz de Sousa",
+    author_email="contato@andersonbraz.com",
+    url="https://pypi.org/project/pyveasy/",
+    description="PyEasy is a project generator for bootstrapping basic Python to VSCode.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["src", "src.*"]),
+    include_package_data=True,
+    install_requires=["click==8.0.3"],
+    entry_points={"console_scripts": ["pyveasy=src.main:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
