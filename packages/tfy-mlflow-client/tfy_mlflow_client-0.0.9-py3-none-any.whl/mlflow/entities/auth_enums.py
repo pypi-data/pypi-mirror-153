77c194cce5672b9d36958ebfcc14b9bd36dd9b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:49 +0000

    Add package tfy-mlflow-client/0.0.9/tfy_mlflow_client-0.0.9-py3-none-any.whl

---
+++ packages/tfy-mlflow-client/tfy_mlflow_client-0.0.9-py3-none-any.whl/mlflow/entities/auth_enums.py
@@ -0,0 +1,29 @@
+import enum
+
+from mlflow.exceptions import MlflowException
+from mlflow.protos.databricks_pb2 import BAD_REQUEST
+
+
+@enum.unique
+class SubjectType(enum.Enum):
+    USER = "USER"
+
+    @classmethod
+    def _missing_(cls, value):
+        raise MlflowException(f"Unknown Subject Type: {value}", error_code=BAD_REQUEST)
+
+
+@enum.unique
+class EntityType(enum.Enum):
+    EXPERIMENT = "EXPERIMENT"
+
+    @classmethod
+    def _missing_(cls, value):
+        raise MlflowException(f"Unknown Entity Type: {value}", error_code=BAD_REQUEST)
+
+
+@enum.unique
+class Role(enum.IntEnum):
+    READ = 1
+    WRITE = 3
+    ADMIN = 7
