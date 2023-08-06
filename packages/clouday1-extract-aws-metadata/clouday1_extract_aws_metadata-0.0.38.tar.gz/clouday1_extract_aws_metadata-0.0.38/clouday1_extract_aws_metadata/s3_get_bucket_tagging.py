e2e60eb5a3ffdc8c68508524cdff0bbf771b44b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:29:28 +0000

    Add package clouday1-extract-aws-metadata/0.0.38/clouday1_extract_aws_metadata-0.0.38.tar.gz

---
+++ packages/clouday1-extract-aws-metadata/clouday1_extract_aws_metadata-0.0.38.tar.gz/clouday1_extract_aws_metadata-0.0.38/clouday1_extract_aws_metadata/s3_get_bucket_tagging.py
@@ -0,0 +1,27 @@
+import os
+import boto3
+import json
+
+def s3_get_bucket_tagging(region,AWS_ACCESS_KEY,AWS_SECRET_KEY):
+    client = boto3.client('s3', region_name=region,
+    aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+    script_dir = os.path.dirname('.')
+    file_path_read = os.path.join(
+        script_dir, 'data/s3-list-bucket-'+region+'.json')
+    with open(file_path_read, 'r') as openfile:
+        s3_object = json.load(openfile)
+        for item in s3_object['Buckets']:
+            try:
+                #get tags for each s3 bucket
+                response = client.get_bucket_tagging(
+                    Bucket=item['Name']
+                )
+                json_list = json.dumps(response, indent=4)
+                file_path2 = os.path.join(
+                    script_dir, 'data/s3-list-tags/get-bucket-tagging-'+item['Name']+'-'+region+'.json')
+                with open(file_path2, 'w')as outfile:
+                    outfile.write(json_list)
+                    outfile.close()
+                
+            except:
+                print("The TagSet does not exist")
