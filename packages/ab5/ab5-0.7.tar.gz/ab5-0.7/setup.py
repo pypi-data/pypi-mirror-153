72ed1a7632e8eea889531065fe85c47ae2a89b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package ab5/0.7/ab5-0.7.tar.gz

---
+++ packages/ab5/ab5-0.7.tar.gz/ab5-0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='ab5',
+    version='0.7',
+    author="Ab.5#3363",
+    description="print ascii art with gratient colors",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", encoding="utf-8").read(),
+    url = "https://github.com/xxa2005/",
+    keywords=['color', 'python', 'colors', 'cool', 'gratient', 'fade', 'shadow'],
+    packages=["ab5"],
+    classifiers=[
+      "Intended Audience :: Developers",
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ]
+)
