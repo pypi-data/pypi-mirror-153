02fd56b59ec16236b460abec932bb072a70934b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:26:19 +0000

    Add package wrapt-timeout-decorator/1.3.12.2/wrapt_timeout_decorator-1.3.12.2-py3.6.egg

---
+++ packages/wrapt-timeout-decorator/wrapt_timeout_decorator-1.3.12.2-py3.6.egg/wrapt_timeout_decorator/__init__conf__.py
@@ -0,0 +1,24 @@
+# CONF
+
+name = "wrapt_timeout_decorator"
+title = "The better timout decorator"
+version = "v1.3.12.2"
+url = "https://github.com/bitranox/wrapt_timeout_decorator"
+author = "Robert Nowotny"
+author_email = "bitranox@gmail.com"
+shell_command = "wrapt_timeout_decorator"
+
+
+def print_info() -> None:
+    print(
+        """\
+
+Info for wrapt_timeout_decorator:
+
+    The better timout decorator
+
+    Version : v1.3.12.2
+    Url     : https://github.com/bitranox/wrapt_timeout_decorator
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com"""
+    )
