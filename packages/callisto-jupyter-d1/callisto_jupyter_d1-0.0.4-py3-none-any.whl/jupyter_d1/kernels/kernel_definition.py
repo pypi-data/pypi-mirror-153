a0490f4166dbbb489eb703271315c045a8ffe867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:08 +0000

    Add package callisto-jupyter-d1/0.0.4/callisto_jupyter_d1-0.0.4-py3-none-any.whl

---
+++ packages/callisto-jupyter-d1/callisto_jupyter_d1-0.0.4-py3-none-any.whl/jupyter_d1/kernels/kernel_definition.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from .vars_manager import VarsManager
+from .workdir_manager import WorkDirManager
+
+
+class KernelDefinition(ABC):
+    @abstractmethod
+    def create_vars_manager(self) -> VarsManager:
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_workdir_manager(self, workdir: str) -> WorkDirManager:
+        raise NotImplementedError
